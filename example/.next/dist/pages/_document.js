57df0378f1a9dd6e5a21978289f1e31e81364f48
Iain Collins <me@iaincollins.com>
2018-01-27 12:41:28 +0000

    Removing build dir
    Commited by accident

--- example/.next/dist/pages/_document.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('next/dist/server/document.js');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvcGFnZXMvX2RvY3VtZW50LmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOztBQUFBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJfZG9jdW1lbnQuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2lhaW4vRGV2ZWxvcG1lbnQvbmV4dC1hdXRoL2V4YW1wbGUifQ==
--- example/.next/dist/pages/_error.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-
-module.exports = require('next/dist/lib/error.js');
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIm5vZGVfbW9kdWxlcy9uZXh0L2Rpc3QvcGFnZXMvX2Vycm9yLmpzIl0sIm5hbWVzIjpbIm1vZHVsZSIsImV4cG9ydHMiLCJyZXF1aXJlIl0sIm1hcHBpbmdzIjoiOztBQUFBLE9BQU8sQUFBUCxVQUFpQixBQUFqQiIsImZpbGUiOiJfZXJyb3IuanM/ZW50cnkiLCJzb3VyY2VSb290IjoiL1VzZXJzL2lhaW4vRGV2ZWxvcG1lbnQvbmV4dC1hdXRoL2V4YW1wbGUifQ==
