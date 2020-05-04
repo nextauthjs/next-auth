ea69d1e904a5fced4fcdb72d8e8f270511998236
Iain Collins <me@iaincollins.com>
2020-05-04 09:26:21 +0100

    Initial commit of signin flow
    * Flow not fully complete or tested, but can complete signin journey.
    * Does not include source code for adapter (test adapater source currently in client respository while under development).
    * Wrapped all calls in promise to avoid early termination of serverless function.
    * Callback and Session cookie names and cookie options can now be changed by passing options for them in (feature request from 1.x).

--- src/server/index.js
+++ src/server/index.js
@@ -5,73 +5,83 @@ import callback from './routes/callback'
 import session from './routes/session'
 
 export default (req, res, _options) => {
-  const { url, query } = req
-  const {
-    slug,
-    action = slug[0],
-    provider = slug[1],
-    site = 'http://localhost:3000',
-    callbackUrl = site,
-    pathPrefix = '/api/auth',
-  } = query
+  return new Promise(async resolve => {
+    const { url, query } = req
+    const {
+      slug,
+      action = slug[0],
+      provider = slug[1],
+      site = 'http://localhost:3000',
+      callbackUrl = site,
+      pathPrefix = '/api/auth',
+    } = query
 
-  const urlPrefix = `${(site || '')}${pathPrefix}`
+    const urlPrefix = `${(site || '')}${pathPrefix}`
 
-  const options = {
-    ..._options,
-    urlPrefix: urlPrefix,
-    providers: parseProviders(_options.providers, urlPrefix),
-    site,
-    action,
-    provider,
-    callbackUrl,
-  }
-  
-  if (req.method === 'GET') {
-    switch (action) {
-      case 'providers':
-        providers(req, res, options)
-        break
-      case 'session':
-        session(req, res, options)
-        break
-      case 'signin':
-        if (provider && options.providers[provider]) {
-          signin(req, res, options)
-        } else {
-          res.setHeader('Content-Type', 'text/html')
-          res.end(`<html><body style="margin: 2em; text-align: center; font-family: sans-serif;"><h1>Sign up / Sign in</h1>${
-            Object.values(options.providers).map(provider =>
-             `<p><a href="${provider.signinUrl}?callbackUrl=${options.callbackUrl}">Sign in with ${provider.name}</a></p>`
-          ).join('')}</ul><p><small><em>This page is a placeholder!</em></small></p><body></html>`)
-        }
-        break
-      case 'callback':
-        if (provider && options.providers[provider]) {
-          callback(req, res, options)
-        } else {
+    const options = {
+      sessionIdCookieName: 'session-id',
+      callbackUrlCookieName: 'callback-url',
+      cookieOptions: {},
+      ..._options,
+      urlPrefix: urlPrefix,
+      providers: parseProviders(_options.providers, urlPrefix),
+      site,
+      action,
+      provider,
+      callbackUrl,
+    }
+    
+    if (req.method === 'GET') {
+      switch (action) {
+        case 'providers':
+          providers(req, res, options, resolve)
+          break
+        case 'session':
+          session(req, res, options, resolve)
+          break
+        case 'signin':
+          if (provider && options.providers[provider]) {
+            signin(req, res, options, resolve)
+          } else {
+            res.setHeader('Content-Type', 'text/html')
+            res.end(`<html><body style="margin: 2em; text-align: center; font-family: sans-serif;"><h1>Sign up / Sign in</h1>${
+              Object.values(options.providers).map(provider =>
+              `<p><a href="${provider.signinUrl}?callbackUrl=${options.callbackUrl}">Sign in with ${provider.name}</a></p>`
+            ).join('')}</ul><p><small><em>This page is a placeholder!</em></small></p><body></html>`)
+          }
+          break
+        case 'callback':
+          if (provider && options.providers[provider]) {
+            callback(req, res, options, resolve)
+          } else {
+            res.statusCode = 400
+            res.end(`Error: HTTP GET is not supported for ${url}`)
+            return resolve()
+          }
+          break
+        case 'unlink':
+          break
+        case 'signout':
+          break
+        case 'done':
+          res.end(`If you can see this, it worked!`)
+          return resolve()
+        default:
           res.statusCode = 400
           res.end(`Error: HTTP GET is not supported for ${url}`)
-        }
-        break
-      case 'unlink':
-        break
-      case 'signout':
-        break
-      case 'done':
-        res.end(`If you can see this, it worked!`)
-      default:
-        res.statusCode = 400
-        res.end(`Error: HTTP GET is not supported for ${url}`)
+          return resolve()
+      }
+    } else if (req.method === 'POST') {
+      switch (route) {
+        default:
+          res.statusCode = 400
+          res.end(`Error: HTTP POST is not supported for ${url}`)
+          return resolve()
+      }
+    } else {
+      res.statusCode = 400
+      res.end(`Error: HTTP ${req.method} is not supported for ${url}`)
+      return resolve()
     }
-  } else if (req.method === 'POST') {
-    switch (route) {
-      default:
-        res.statusCode = 400
-        res.end(`Error: HTTP POST is not supported for ${url}`)
-    }
-  } else {
-    res.statusCode = 400
-    res.end(`Error: HTTP ${req.method} is not supported for ${url}`)
-  }
+  })
 }
