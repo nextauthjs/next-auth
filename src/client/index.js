bbe05972e83a3d605513f529d4e23202e17153bf
Iain Collins <me@iaincollins.com>
2020-05-05 21:36:06 +0100

    Add simple client

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,18 @@
-export default () => {
-  throw new Error('Not yet implemented') // @TODO Import client from next-auth 1.x
+'use strict'
+
+import fetch from 'isomorphic-unfetch'
+
+export default class {
+
+  static async session() {
+    return new Promise(resolve => {
+      fetch('http://localhost:3000/api/auth/session')
+      .then(r => r.json())
+      .then(data =>{
+        console.log('session', data)
+        resolve(data)
+      })
+    })
+  }
+
 }
