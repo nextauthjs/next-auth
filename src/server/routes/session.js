651f3c98870bddd1002df8e1947c743f37dfa82a
Iain Collins <me@iaincollins.com>
2020-05-05 19:37:29 +0100

    Improve session and account linking

--- src/server/routes/session.js
+++ src/server/routes/session.js
@@ -1,30 +1,32 @@
 import { User } from "../../models/user"
 
-// Returns a JSON object with current session object
 export default async (req, res, options, resolve) => {
   const { sessionIdCookieName, adapter } = options
   const _adapter = await adapter.getAdapter()
   const { getUserById, getSessionById } = _adapter
   const sessionId = req.cookies[sessionIdCookieName]
 
+  let response = {}
 
-  // @TODO provide adapter method to make it easy 
-  // to safely seralize user information and avoid
-  // making assumptions about the user / session model
-  // (They should be loosely coupled.)
+  // @TODO provide adapter method to make it easy  to safely seralize user information and avoid
+  // making assumptions about the user / session model (they should be loosely coupled.)
   const session = await getSessionById(sessionId)
   if (session) {
-    const user = await getUserById(session.user)
-    session.user = user
-
-    // Avoid exposing Session ID or User ID
-    // (but do expose the accessToken!)
-    delete session.id
-    delete session.user.id
+    const user = await getUserById(session.userId)
+    // Only expose *name* and *email* to client side.
+    // This is for presentation purposes (e.g. "you are logged in as…")
+    response = {
+      user: {
+        name: user.name,
+        email: user.email,
+      },
+      accessToken: session.accessToken,
+      accessTokenExpires: session.accessTokenExpires
+    }
   }
 
   res.statusCode = 200
   res.setHeader('Content-Type', 'application/json')
-  res.end(JSON.stringify(session))
+  res.end(JSON.stringify(response))
   return resolve()
 }
