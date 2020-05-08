eee927a6cd6b872c1b71b78f894a9c005acc02c7
Iain Collins <me@iaincollins.com>
2020-05-08 02:23:37 +0100

    Add CSRF token and improve cookie security
    Better default security for cookies, without impacting UX or DX.
    
    Further work to do on CSRF protection, but this is a good start.

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,5 @@
+import { createHash, randomBytes } from 'crypto'
+import cookie from './lib/cookie'
 import parseProviders from './lib/providers'
 import providers from './routes/providers'
 import signin from './routes/signin'
@@ -24,29 +26,98 @@ export default (req, res, _options) => {
 
     // If no callback URL is provided, use site name
     const callbackUrl = query.callbackUrl || site
+    
+    // Use secure cookies if the site uses HTTPS
+    // This being conditional allows cookies to work non-HTTPS development URLs
+    // Honour secure cookie option, which sets 'secure' and also adds '__SECURE-'
+    // prefix, but enable them by default if the site URL is HTTPS; but not for
+    // non-HTTPS URLs like http://localhost which are used in development).
+    // For more on prefixes see https://googlechrome.github.io/samples/cookie-prefixes/
+    const secureCookies = _options.secureCookies || urlPrefix.startsWith('https://')
+    const cookiePrefix = secureCookies ? '__SECURE-' : ''
 
+    // @TODO Review cookie settings (names, options)
     const cookies = {
       // default cookie options
       sessionId: {
-        name: 'next-auth.session-id',
+        name: `${cookiePrefix}next-auth.session-id`,
         options: {
-          httpOnly: true
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+          secure: secureCookies
         }
       },
       callbackUrl: {
-        name: 'next-auth.callback-url',
-        options: {}
-      },
-      urlPrefix: {
-        name: 'next-auth.url-prefix',
+        name: `${cookiePrefix}next-auth.callback-url`,
         options: {
-          httpOnly: true
+          sameSite: 'lax',
+          path: '/',
+          secure: secureCookies
         }
       },
-      // Allow user cookie options to override them
+      urlPrefix: {
+        name: `${cookiePrefix}next-auth.url-prefix`,
+        options: {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+          secure: secureCookies
+        }
+      },
+      csrfToken: {
+        // Default to __HOST- for CSRF token for additional protection if using secureCookies
+        // NB: The `__HOST-` prefix is stricted than the `__SECURE-` prefix.
+        name: `${ secureCookies ? '__Host-' : '' }next-auth.csrf-token`,
+        options: {
+          httpOnly: true,
+          sameSite: 'lax',
+          path: '/',
+          secure: secureCookies
+        }
+      },
+      // Allow user cookie options to override any cookie settings above
       ..._options.cookies
     }
     
+    // Secret used salt cookies and tokens (e.g. for CSRF protection).
+    // If no secret option is specified then it creates one on the fly
+    // based on options passed here. A options contains unique data, such as
+    // oAuth provider secrets and database credentials it should be sufficent.
+    const secret = _options.secret || createHash('sha256').update(JSON.stringify(_options)).digest('hex')
+
+    // Ensure CSRF Token cookie is set for any subsequent requests.
+    // Used as part of the strateigy for mitigation for CSRF tokens.
+    //
+    // Creates a cookie like 'next-auth.csrf-token' with the value 'token|hash',
+    // where 'token' is the CSRF token and 'hash' is a hash made of the token and
+    // the secret, and the two values are joined by a pipe '|'. By storing the
+    // value and the hash of the value (with the secret used as a salt) we can
+    // verify the cookie was set by the server and not by a malicous attacker.
+    //
+    // For more details, see the following OWASP links:
+    // https://cheatsheetseries.owasp.org/cheatsheets/Cross-Site_Request_Forgery_Prevention_Cheat_Sheet.html#double-submit-cookie
+    // https://owasp.org/www-chapter-london/assets/slides/David_Johansson-Double_Defeat_of_Double-Submit_Cookie.pdf
+    let csrfToken
+    if (req.cookies[cookies.csrfToken.name]) {
+      const [ csrfTokenValue, csrfTokenHash ] = req.cookies[cookies.csrfToken.name].split('|')
+      if (csrfTokenHash == createHash('sha256').update(`${csrfTokenValue}${secret}`).digest('hex')) {
+        // If hash matches then we trust the CSRF token value
+        csrfToken = csrfTokenValue
+      } else {
+        // @TODO Add handler to reject POST requests for auth routes if the token doesn't match.
+        // Only triggered when there is a cookie but the hash doesn't match. Should be handled gracefully.
+        console.warn('CSRF_TOKEN_CHECK_WARNING')
+      }
+    }
+    if (!csrfToken) {
+      // If no csrfToken - because it's not been set yet, or because the hash doesn't match
+      // (e.g. because it's been modifed or because the secret has changed) create a new token.
+      csrfToken = randomBytes(32).toString('hex')
+      const newCsrfTokenCookie = `${csrfToken}|${createHash('sha256').update(`${csrfToken}${secret}`).digest('hex')}`
+      cookie.set(res, cookies.csrfToken.name, newCsrfTokenCookie, cookies.csrfToken.options)
+    }
+
     // User provided options are overriden by other options,
     // except for the options with special handlign above
     const options = {
@@ -58,6 +129,8 @@ export default (req, res, _options) => {
       action,
       provider,
       cookies,
+      secret,
+      csrfToken,
       providers: parseProviders(_options.providers, urlPrefix),
     }
     
