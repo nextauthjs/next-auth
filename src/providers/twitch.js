2152f435347dd3e736dde6e39945f98322684e33
Lori <lori.karikari@gmail.com>
2020-05-08 23:46:19 +0200

    added Twitch provider

---
+++ src/providers/twitch.js
@@ -0,0 +1,24 @@
+export default (options) => {
+  return {
+    id: 'twitch',
+    name: 'Twitch',
+    type: 'oauth',
+    version: '2.0',
+    scope: 'user:read:email',
+    options: { grant_type: 'authorization_code' },
+    accessTokenUrl: 'https://id.twitch.tv/oauth2/token',
+    authorizationUrl:
+      'https://id.twitch.tv/oauth2/authorize?response_type=code',
+    profileUrl: 'https://api.twitch.tv/helix/users',
+    profile: (profile) => {
+      const data = profile.data[0];
+      return {
+        id: data.id,
+        name: data.display_name,
+        image: data.profile_image_url,
+        email: data.email,
+      };
+    },
+    ...options,
+  };
+};
