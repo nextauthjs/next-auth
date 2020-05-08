97d88f9e4e4a87d13805b069b73ec8dc1ef5f5ac
Iain Collins <me@iaincollins.com>
2020-05-08 05:03:36 +0100

    Add signout page
    This commit does not include handling of actual signout request.

--- src/server/pages/signin.js
+++ src/server/pages/signin.js
@@ -3,7 +3,7 @@ import render from 'preact-render-to-string'
 
 export default ({ site, providers, callbackUrl }) => {
   return render(
-    <div className="signin">
+    <div className='signin'>
       <p><a className='site' href={site}>{site.replace(/^https?:\/\//, '')}</a></p>
       {providers.map(provider => 
         <p><a data-provider={provider.id} href={`${provider.signinUrl}?callbackUrl=${callbackUrl}`}>Sign in with {provider.name}</a></p>
