c5a5d8fc3235f364a4a3ab42076f56052c3079d9
Iain Collins <me@iaincollins.com>
2020-05-08 03:19:04 +0100

    Improve signin page
    * Better contrast
    * Displays site name correctly

--- src/server/pages/signin.js
+++ src/server/pages/signin.js
@@ -1,13 +1,13 @@
 import { h } from 'preact'
 import render from 'preact-render-to-string'
 
-export default ({ providers, callbackUrl }) => {
+export default ({ site, providers, callbackUrl }) => {
   return render(
     <div className="signin">
+      <p><a className='site' href={site}>{site.replace(/^https?:\/\//, '')}</a></p>
       {providers.map(provider => 
         <p><a data-provider={provider.id} href={`${provider.signinUrl}?callbackUrl=${callbackUrl}`}>Sign in with {provider.name}</a></p>
       )}
-      <p><small><a href={process.env.SITE_NAME}>{process.env.SITE_NAME}</a></small></p>
     </div>
   )
 }
