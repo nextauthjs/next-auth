aeddd2bcc147d3ea00e8a889686f2eff8c18d14a
Iain Collins <me@iaincollins.com>
2020-05-10 02:25:03 +0100

    Improve auth page CSS

--- src/server/pages/signout.js
+++ src/server/pages/signout.js
@@ -1,10 +1,9 @@
 import { h } from 'preact'
 import render from 'preact-render-to-string'
 
-export default ({ site, urlPrefix, csrfToken, callbackUrl }) => {
+export default ({ urlPrefix, csrfToken }) => {
   return render(
     <div className='signout'>
-      <p><a className='site' href={site}>{site.replace(/^https?:\/\//, '')}</a></p>
       <form action={`${urlPrefix}/signout`} method='POST'>
         <input type='hidden' name='csrfToken' value={csrfToken}/>
         <button type='submit'>Sign out</button>
