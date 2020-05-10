d0a403e56a6d352d4ef8bd2a7e0ef80cd3bcc9c0
Iain Collins <me@iaincollins.com>
2020-05-10 02:25:03 +0100

    Improve auth page CSS

--- src/server/pages/signin.js
+++ src/server/pages/signin.js
@@ -1,11 +1,10 @@
 import { h } from 'preact'
 import render from 'preact-render-to-string'
 
-export default ({ site, providers, callbackUrl }) => {
+export default ({ providers, callbackUrl }) => {
   const withCallbackUrl = callbackUrl ? `?callbackUrl=${callbackUrl}` : ''
   return render(
     <div className='signin'>
-      <p><a className='site' href={site}>{site.replace(/^https?:\/\//, '')}</a></p>
       {providers.map(provider => 
         <p><a className='button' data-provider={provider.id} href={`${provider.signinUrl}${withCallbackUrl}`}>Sign in with {provider.name}</a></p>
       )}
