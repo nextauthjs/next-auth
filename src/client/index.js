2e6f6f0117b9fa7138ce1ac1ae2c98ba53551157
Iain Collins <me@iaincollins.com>
2020-05-12 13:32:15 +0100

    Improve client
    * Improve options passing
    * Fix bug with cookie parsing
    * Remove isomorphic-unfetch (fetch built from Next.js 9.4)

--- src/client/index.js
+++ src/client/index.js
@@ -1,12 +1,13 @@
+// fetch() is built in to Next.js 9.4 (you can use a polyfill if using an older version)
+/* global fetch:false */
 import { useState, useEffect, useContext, createContext, createElement } from 'react'
-import fetch from 'isomorphic-unfetch'
 
 const URL_PREFIX_COOKIE = 'next-auth.url-prefix'
 const DEFAULT_SITE = ''
 const DEFAULT_PATH_PREFIX = '/api/auth'
 
 // Isomorphic get session method
-const session = async ({ req, site, pathPrefix, urlPrefixCookieName }) => {
+const session = async ({ req, site, pathPrefix, urlPrefixCookieName } = {}) => {
   // If we have a 'req' object are running sever side and should get cookies from headers
   const cookies = req ? _parseCookies(req.headers.cookie) : null
   // We will need the raw header as it will have the HTTP only Session ID cookie in it
@@ -14,15 +15,17 @@ const session = async ({ req, site, pathPrefix, urlPrefixCookieName }) => {
   const fetchOptions = req ? { headers: { cookie: req.headers.cookie } } : {}
 
   // If site and/or pathPrefix are specified, use them (or the defaults)
-  // otherwise get the server URL from the HTTP only cookie.
+  // otherwise get the server URL from the signed HTTP only cookie.
   //
   // If no options specified (which usually will be the case) then grab
   // the configuration dynamically from a cookie. Prefer the _Secure-
   // prefixed version if it is avalible, but fall back to checking the
-  // version without the prefix so it sill works on URLs like http://localhost
-  const urlPrefix = (site || pathPrefix)
+  // version without the prefix so it still works on URLs like http://localhost
+  //
+  // If there is no cookie set, then we use the default prefix (/api/auth).
+  const urlPrefix = (site || pathPrefix || !cookies)
     ? `${site || DEFAULT_SITE}${pathPrefix || DEFAULT_PATH_PREFIX}`
-    : _getUrlPrefix(cookies, urlPrefixCookieName)
+    : _getUrlPrefixFromCookies(cookies, urlPrefixCookieName)
 
   // If we are rendering server side but don't have a URL prefix, then give up
   // because we can't call fetch server side an absolute URL to query.
@@ -42,7 +45,7 @@ const session = async ({ req, site, pathPrefix, urlPrefixCookieName }) => {
 const SessionContext = createContext()
 
 // Internal hook for getting session from the api.
-const useSessionData = (session, pathPrefix) => {
+const useSessionData = (session, { pathPrefix }) => {
   const [data, setData] = useState(session)
   const [loading, setLoading] = useState(true)
   const getSession = async () => {
@@ -61,18 +64,17 @@ const useSessionData = (session, pathPrefix) => {
 
 // Provider to wrap the app in to make session data available globally
 const Provider = ({ children, session, pathPrefix }) => {
-  const value = useSession(session, pathPrefix)
+  const value = useSession(session, { pathPrefix })
 
   return createElement(SessionContext.Provider, { value }, children)
 }
 
 // Hook to access the session data stored in the context
-const useSession = (session, pathPrefix) => {
+const useSession = (session, { pathPrefix }) => {
   const value = useContext(SessionContext)
-  // If we have no Provider in the tree
-  // we call the actual hook for fetching the session
+  // If we have no Provider in the tree we call the actual hook for fetching the session
   if (value === undefined) {
-    return useSessionData(session, pathPrefix)
+    return useSessionData(session, { pathPrefix })
   }
 
   return value
@@ -97,7 +99,7 @@ const _parseCookies = (string) => {
   }
 }
 
-const _getUrlPrefix = (cookies, urlPrefixCookieName) => {
+const _getUrlPrefixFromCookies = (cookies, urlPrefixCookieName) => {
   const cookieValue = cookies[urlPrefixCookieName] || cookies[`__Secure-${URL_PREFIX_COOKIE}`] || cookies[URL_PREFIX_COOKIE]
   const [urlPrefixValue] = cookieValue ? cookieValue.split('|') : [null]
   return urlPrefixValue
