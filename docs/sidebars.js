d7a2cde57ed174dc74b5b1134c8545f38f7079ab
ndo@ndo3 <yo@ndo.dev>
2020-05-17 17:30:17 +0200

    update: sidebar labels

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -1,12 +1,12 @@
 module.exports = {
   sidebar: {
-    'next-auth-v2': [
+    'v2': [
       'getting-started', 
       'client', 
       'auth',
       'upgrade'
     ],
-    'next-auth-v1': [
+    'v1': [
       'v1/getting-started-v1', 
       'v1/client-v1', 
       'v1/auth-v1'
