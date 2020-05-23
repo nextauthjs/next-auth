a944870eb22821d5c4c2f1484d2514d89f0fa8bb
Lori Karikari <lori.karikari@gmail.com>
2020-05-23 16:32:44 +0200

    changed options to params (#154)

--- src/providers/reddit.js
+++ src/providers/reddit.js
@@ -6,7 +6,7 @@ export default (options) => {
     type: 'oauth',
     version: '2.0',
     scope: 'identity',
-    options: { grant_type: 'authorization_code' },
+    params: { grant_type: 'authorization_code' },
     accessTokenUrl: ' https://www.reddit.com/api/v1/access_token',
     authorizationUrl:
       'https://www.reddit.com/api/v1/authorize?response_type=code',
--- src/providers/slack.js
+++ src/providers/slack.js
@@ -5,7 +5,7 @@ export default (options) => {
     type: 'oauth',
     version: '2.0',
     scope: 'identity.basic identity.email identity.avatar',
-    options: { grant_type: 'authorization_code' },
+    params: { grant_type: 'authorization_code' },
     accessTokenUrl: 'https://slack.com/api/oauth.access',
     authorizationUrl: 'https://slack.com/oauth/authorize?response_type=code',
     profileUrl: 'https://slack.com/api/users.identity',
