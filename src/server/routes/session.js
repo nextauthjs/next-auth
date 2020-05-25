63ceb1a2606c25ac0ace6bfa942650c95b54ab52
Iain Collins <me@iaincollins.com>
2020-05-25 21:21:59 +0100

    Don't lookup session if session token empty

--- src/server/routes/session.js
+++ src/server/routes/session.js
@@ -8,37 +8,40 @@ export default async (req, res, options, done) => {
 
   let response = {}
 
-  try {
-    const session = await getSession(sessionToken)
-    if (session) {
-      // Trigger update to session object to update sessionExpires
-      await updateSession(session)
+  if (sessionToken) { // Don't check session if it doesn't exist!
+    try {
+      const session = await getSession(sessionToken)
+      if (session) {
+        // Trigger update to session object to update sessionExpires
+        await updateSession(session)
 
-      const user = await getUser(session.userId)
-      // Only expose a limited subset of information to the client as needed
-      // for presentation purposes (e.g. "you are logged in as…").
-      //
-      // @TODO Should support `async seralizeUser({ user, function })` style
-      // middleware function to allow response to be customized.
-      response = {
-        user: {
-          name: user.name,
-          email: user.email,
-          image: user.image
-        },
-        accessToken: session.accessToken,
-        expires: session.sessionExpires
+        const user = await getUser(session.userId)
+
+        // Only expose a limited subset of information to the client as needed
+        // for presentation purposes (e.g. "you are logged in as…").
+        //
+        // @TODO Should support `async seralizeUser({ user, function })` style
+        // middleware function to allow response to be customized.
+        response = {
+          user: {
+            name: user.name,
+            email: user.email,
+            image: user.image
+          },
+          accessToken: session.accessToken,
+          expires: session.sessionExpires
+        }
+
+        // Update expiry on Session Token cookie
+        cookie.set(res, cookies.sessionToken.name, sessionToken, { expires: session.sessionExpires, ...cookies.sessionToken.options })
+      } else if (sessionToken) {
+        // If sessionToken was found set but it's not valid for a session then
+        // remove the sessionToken cookie from browser.
+        cookie.set(res, cookies.sessionToken.name, '', { ...cookies.sessionToken.options, maxAge: 0 })
       }
-
-      // Update expiry on Session Token cookie
-      cookie.set(res, cookies.sessionToken.name, sessionToken, { expires: session.sessionExpires, ...cookies.sessionToken.options })
-    } else if (sessionToken) {
-      // If sessionToken was found set but it's not valid for a session then
-      // remove the sessionToken cookie from browser.
-      cookie.set(res, cookies.sessionToken.name, '', { ...cookies.sessionToken.options, maxAge: 0 })
+    } catch (error) {
+      console.error('SESSION_ERROR', error)
     }
-  } catch (error) {
-    console.error('SESSION_ERROR', error)
   }
 
   res.setHeader('Content-Type', 'application/json')
