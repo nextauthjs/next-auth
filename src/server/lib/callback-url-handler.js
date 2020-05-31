dfe2a4dcc0deb57e8e230ff59896517190028183
Iain Collins <me@iaincollins.com>
2020-05-31 23:08:40 +0100

    Add signin and signout methods to client
    * Using method preserves current URL on signin / signout
    * Reloads browser window once complete to force session update
    
    The force reload behaviour may change to something more graceful in future, but is good for now as ensure page state is correct.

--- src/server/lib/callback-url-handler.js
+++ src/server/lib/callback-url-handler.js
@@ -2,18 +2,25 @@ import cookie from '../lib/cookie'
 
 export default async (req, res, options) => {
   const { query } = req
+  const { body } = req
   const { cookies, site, defaultCallbackUrl } = options
+
+  // The callbackUrlHandler function is used to validate callback URLs, so you
+  // can easily allow (or deny) specific callback URLs.
   const callbackUrlHandler = options.callbackUrlHandler || callbackUrlHandlerDefaultFunction
 
   // Handle preserving and validating callback URLs
   // If no defaultCallbackUrl option specified, default to the homepage for the site
   let callbackUrl = defaultCallbackUrl || site
-  const callbackUrlParamValue = query.callbackUrl || null
+  
+  // Try reading callbackUrlParamValue from request body (form submission) then from query param (get request)
+  const callbackUrlParamValue = body.callbackUrl || query.callbackUrl || null
   const callbackUrlCookieValue = req.cookies[cookies.callbackUrl.name] || null
   if (callbackUrlParamValue) {
-    // If callbackUrl query parameter is passed, validate against callbackUrlHandler() function
+    // If callbackUrl form field or query parameter is passed try to use it if allowed
     callbackUrl = await callbackUrlHandler(callbackUrlParamValue, options)
   } else if (callbackUrlCookieValue) {
+    // If no callbackUrl specified, try using the value from the cookie if allowed
     callbackUrl = await callbackUrlHandler(callbackUrlCookieValue, options)
   }
 
@@ -23,8 +30,8 @@ export default async (req, res, options) => {
   return Promise.resolve(callbackUrl)
 }
 
-// Default handler for callbackUrlHandler(url) checks the protcol and host
-// matches the site name, if not then returns the canonical site url
+// Default handler for callbackUrlHandler(url) checks the protocol and host
+// matches the site name, if not then returns the canonical site url.
 const callbackUrlHandlerDefaultFunction = (url, options) => {
   if (url.startsWith(options.site)) {
     return Promise.resolve(url)
