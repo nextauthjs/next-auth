15424d2d03663920cd9134a7f60608fd2dc67509
Iain Collins <me@iaincollins.com>
2020-06-01 01:46:51 +0100

    Fix bug with callbackUrls not being passed to custom pages

--- src/server/index.js
+++ src/server/index.js
@@ -220,13 +220,13 @@ export default async (req, res, userSuppliedOptions) => {
           if (provider && options.providers[provider]) {
             signin(req, res, options, done)
           } else {
-            if (options.pages.signin) { return redirect(`${options.pages.signin}${options.pages.signin.includes('?') ? '&' : '?'}callbackUrl=${callbackUrl}`) }
-            
+            if (options.pages.signin) { return redirect(`${options.pages.signin}${options.pages.signin.includes('?') ? '&' : '?'}callbackUrl=${options.callbackUrl}`) }
+
             pages.render(req, res, 'signin', { site, providers: Object.values(options.providers), callbackUrl: options.callbackUrl, csrfToken }, done)
           }
           break
         case 'signout':
-          if (options.pages.signin) { return redirect(`${options.pages.signout}${options.pages.signout.includes('?') ? '&' : '?'}callbackUrl=${callbackUrl}`) }
+          if (options.pages.signin) { return redirect(`${options.pages.signout}${options.pages.signout.includes('?') ? '&' : '?'}callbackUrl=${options.callbackUrl}`) }
 
           pages.render(req, res, 'signout', { site, baseUrl, csrfToken, callbackUrl: options.callbackUrl }, done)
           break
