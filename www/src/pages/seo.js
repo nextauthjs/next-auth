5537514b4fe902c145e557ae4a0421e017a5a529
ndo@ndo3 <yo@ndo.dev>
2020-06-05 22:13:10 +0200

    update: pull meta info from docusaurus config

--- www/src/pages/seo.js
+++ www/src/pages/seo.js
@@ -1,19 +1,28 @@
 import React from 'react'
 import Head from '@docusaurus/Head'
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
 
-const Seo = () => (
-  <>
+const Seo = () => {
+  const context = useDocusaurusContext()
+  const { siteConfig = {} } = context
+  const {
+    title,
+    tagline,
+    url
+  } = siteConfig
+
+  return (
     <Head>
       <meta charSet='utf-8' />
-      <link rel='canonical' href='https://next-auth.js.org' />
+      <link rel='canonical' href={url} />
 
-      <meta property='og:title' content='Next-Auth Docs' />
-      <meta property='og:description' content='Serverless Authentication Library for Next.js' />
-      <meta property='og:image' content='https://next-auth.js.org/img/logo/logo-with-icon-sm.png' />
-      <meta property='og:url' content='https://next-auth.js.org' />
+      <meta property='og:title' content={title} />
+      <meta property='og:description' content={tagline} />
+      <meta property='og:image' content={`${url}/img/logo/logo-with-icon-sm.png`} />
+      <meta property='og:url' content={url} />
       <meta name='twitter:card' content='summary_large_image' />
     </Head>
-  </>
-)
+  )
+}
 
 export default Seo
