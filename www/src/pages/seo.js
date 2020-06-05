42363101f8ec03f0ee8377f5bd447fcbb86af1f8
ndo@ndo3 <yo@ndo.dev>
2020-06-05 22:04:44 +0200

    update: essential meta tags

--- www/src/pages/seo.js
+++ www/src/pages/seo.js
@@ -4,9 +4,14 @@ import Head from '@docusaurus/Head'
 const Seo = () => (
   <>
     <Head>
-      <meta property='og:description' content='Next-auth description' />
       <meta charSet='utf-8' />
       <link rel='canonical' href='https://next-auth.js.org' />
+
+      <meta property='og:title' content='Next-Auth Docs' />
+      <meta property='og:description' content='Serverless Authentication Library for Next.js' />
+      <meta property='og:image' content='https://next-auth.js.org/img/logo/logo-with-icon-sm.png' />
+      <meta property='og:url' content='https://next-auth.js.org' />
+      <meta name='twitter:card' content='summary_large_image' />
     </Head>
   </>
 )
