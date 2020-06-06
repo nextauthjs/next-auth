9db352e2370e533a3d9740a058176408b1879aa4
ndo@ndo3 <yo@ndo.dev>
2020-06-06 23:55:33 +0200

    fix: lowercase errorCode required for hash

--- src/lib/consoleErr.js
+++ src/lib/consoleErr.js
@@ -4,7 +4,7 @@ export default (function () {
     console.error = (errCode, text) => {
       !text
         ? cx(errCode)
-        : cx(`${text}\nDocs: https://next-auth-docs-git-test-error-urls.iaincollins.now.sh/errors#${errCode}`)
+        : cx(`${text}\nDocs: https://next-auth-docs-git-test-error-urls.iaincollins.now.sh/errors#${errCode.toLowerCase()}`)
     }
   }
 })()
