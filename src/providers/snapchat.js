db22feae089dad836163b3c96d87946e978c82d0
Lori <lori.karikari@gmail.com>
2020-06-06 21:58:16 +0200

    updated snapchat scope

--- src/providers/snapchat.js
+++ src/providers/snapchat.js
@@ -1,10 +1,11 @@
+// Snapchat doesn't return an email address, currently not supported
 export default (options) => {
   return {
     id: 'snapchat',
     name: 'Snapchat',
     type: 'oauth',
     version: '2.0',
-    scope: 'user.display_name, user.bitmoji.avatar',
+    scope: 'user.display_name user.bitmoji.avatar user.external_id',
     params: { grant_type: 'authorization_code' },
     accessTokenUrl: 'https://accounts.snapchat.com/accounts/oauth2/token',
     authorizationUrl:
