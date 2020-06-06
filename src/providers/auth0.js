e34fece15964d0a882cad3d7bb0de94b3366cfa1
Lori <lori.karikari@gmail.com>
2020-06-06 18:20:38 +0200

    simplified auth0 conf and made domain stuff more consistent across providers that need one

--- src/providers/auth0.js
+++ src/providers/auth0.js
@@ -1,11 +1,15 @@
 export default (options) => {
+	console.log(options)
   return {
     id: 'auth0',
     name: 'Auth0',
     type: 'oauth',
     version: '2.0',
-    params: { grant_type: 'authorization_code' },
-    scope: 'openid email profile',
+    params: { grant_type: 'authorization_code', respponse_type: 'code' },
+		scope: 'openid email profile',
+		accessTokenUrl: `https://${options.subdomain}.auth0/oauth/token`,
+		authorizationUrl: `https://${options.subdomain}.auth0.com/authorize?`,
+		profileUrl: `http://${options.subdomain}.auth0.com/userinfo`,
     profile: (profile) => {
       return {
         id: profile.sub,
