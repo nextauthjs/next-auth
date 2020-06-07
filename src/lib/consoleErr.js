a43e486be040fe12d6fd0bc17e0f5f0a6a42cb06
ndo@ndo3 <yo@ndo.dev>
2020-06-07 19:06:36 +0200

    add: bold docs msg in console

--- src/lib/consoleErr.js
+++ src/lib/consoleErr.js
@@ -2,7 +2,7 @@ const nextAuthError = (errCode, ...text) => {
   if (console) {
     !text
       ? console.error(errCode)
-      : console.error(`${text} \nDocs: https://next-auth-docs-git-test-error-urls.iaincollins.now.sh/errors#${errCode.toLowerCase()}`)
+      : console.error(`${text} \n[1mDocs: https://next-auth-docs-git-test-error-urls.iaincollins.now.sh/errors#${errCode.toLowerCase()}[22m`)
   }
 }
 
