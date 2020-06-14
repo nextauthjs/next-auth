d2205870182ac6bf661df8f20c963226a178b8be
Iain Collins <me@iaincollins.com>
2020-06-14 22:37:16 +0100

    Add description for each event

--- src/server/lib/events.js
+++ src/server/lib/events.js
@@ -1,20 +1,40 @@
-const signin = async (message) => { }
+const signin = async (message) => {
+  // Event triggered on successful sign in
+ }
 
-const signout = async (message) => { }
+const signout = async (message) => { 
+  // @TODO Event triggered on signout
+}
 
-const createUser = async (message) => { }
+const createUser = async (message) => {
+  // @TODO Event triggered on user creation
+}
 
-const updateUser = async (message) => { }
+const updateUser = async (message) => { 
+  // @TODO Event triggered on update to user
+}
 
-const deleteUser = async (message) => { }
+const deleteUser = async (message) => {
+  // @TODO Event triggered on user deletion
+ }
 
-const linkAccount = async (message) => { }
+const linkAccount = async (message) => {
+  // @TODO Event triggered when an account is linked to a user
+}
 
-const unlinkAccount = async (message) => { }
+const unlinkAccount = async (message) => {
+  // @TODO Event triggered when an account is unlinked from a user
+}
 
-const session = async (message) => { }
+const session = async (message) => {
+  // @TODO Event triggered when an account is session is active
+  // This event is triggered any time a session is active!
+}
 
-const error = async (message) => { }
+const error = async (message) => {
+  // @TODO Event triggered when something goes wrong in an authentication flow
+  // This event may be fired multiple times when an error occurs
+}
 
 export default {
   signin,
