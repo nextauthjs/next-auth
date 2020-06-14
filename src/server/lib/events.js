6b40a9fdb2dd5d37e915cf7c9fd48cfdd64be793
Iain Collins <me@iaincollins.com>
2020-06-14 18:13:18 +0100

    Add callbacks and events

---
+++ src/server/lib/events.js
@@ -0,0 +1,32 @@
+const signin = async (message) => { }
+
+const signout = async (message) => { }
+
+const createUser = async (message) => { }
+
+const updateUser = async (message) => { }
+
+const deleteUser = async (message) => { }
+
+const linkAccount = async (message) => { }
+
+const unlinkAccount = async (message) => { }
+
+const redirect = async (message) => { }
+
+const session = async (message) => { }
+
+const error = async (message) => { }
+
+export default {
+  signin,
+  signout,
+  createUser,
+  updateUser,
+  deleteUser,
+  linkAccount,
+  unlinkAccount,
+  redirect,
+  session,
+  error
+}
