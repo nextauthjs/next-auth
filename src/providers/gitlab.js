8229a3b4209b8d75322c2403f448cde5ed479a52
SabariVig <33371586+SabariVig@users.noreply.github.com>
2020-06-19 14:52:09 +0530

    Gitlab Provider

---
+++ src/providers/gitlab.js
@@ -0,0 +1,23 @@
+export default (options) => {
+    return {
+      id: 'gitlab',
+      name: 'Gitlab',
+      type: 'oauth',
+      version: '2.0',
+      scope: 'read_user',
+      params: { grant_type: 'authorization_code' },
+      accessTokenUrl: 'https://gitlab.com/oauth/token',
+      authorizationUrl: 'https://gitlab.com/oauth/authorize?response_type=code',
+      profileUrl: 'https://gitlab.com/api/v4/user',
+      profile: (profile) => {
+        return {
+          id: profile.id,
+          name: profile.username,
+          email: profile.email,
+          image: profile.avatar_url,
+        }
+      },
+      ...options
+    }
+  }
+  
