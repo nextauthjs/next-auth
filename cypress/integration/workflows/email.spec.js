1272ab9cdc2bf65fc756ba608d470a926db56999
me@jeffersonbledsoe.com <me@jeffersonbledsoe.com>
2020-06-22 00:43:25 +0100

    Use home-page sign in button for email workflow

--- cypress/integration/workflows/email.spec.js
+++ cypress/integration/workflows/email.spec.js
@@ -3,9 +3,15 @@
 /// <reference types="Cypress" />
 
 describe("Email authentication workflow", async () => {
+  beforeEach(() => {
+    cy.visit("/");
+    cy.findByRole("link", { name: "Sign in" }).click();
+    cy.location().should((loc) => {
+      expect(loc.pathname).to.eq("/api/auth/signin");
+    });
+  });
   //   context("A valid email signup", () => {});
   it("An invalid email is entered", () => {
-    cy.visit("/api/auth/signin");
     cy.findByLabelText("Email").type("An invalid email");
     cy.findByRole("button", { name: "Sign in with Email" }).click();
 
