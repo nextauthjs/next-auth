5b3b3b18085da1e3481bf5f600efa0543d845527
Lori <lori.karikari@gmail.com>
2020-06-23 10:31:55 +0200

    use domain instead of subdomain to stay consistent across providers

--- src/providers/cognito.js
+++ src/providers/cognito.js
@@ -1,5 +1,5 @@
 export default (options) => {
-  const { region, subdomain } = options;
+  const { region, domain } = options;
   return {
     id: 'cognito',
     name: 'Cognito',
@@ -7,9 +7,9 @@ export default (options) => {
     version: '2.0',
     scope: 'openid profile email',
     params: { grant_type: 'authorization_code' },
-    accessTokenUrl: `https://${subdomain}.auth.${region}.amazoncognito.com/oauth2/token`,
-    authorizationUrl: `https://${subdomain}.auth.${region}.amazoncognito.com/oauth2/authorize?response_type=code`,
-    profileUrl: `https://${subdomain}.auth.${region}.amazoncognito.com/oauth2/userInfo`,
+    accessTokenUrl: `https://${domain}.auth.${region}.amazoncognito.com/oauth2/token`,
+    authorizationUrl: `https://${domain}.auth.${region}.amazoncognito.com/oauth2/authorize?response_type=code`,
+    profileUrl: `https://${domain}.auth.${region}.amazoncognito.com/oauth2/userInfo`,
     profile: (profile) => {
       return {
         id: profile.sub,
