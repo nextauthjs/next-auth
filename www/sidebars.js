52b69a6d68b8ee0d13f1d63c0ba49beb485b9a45
Nico Domino <yo@ndo.dev>
2020-07-04 21:47:00 +0200

    Add: testing with cypress docs (#357)
    * add: testing with cypress docs
    
    * update: add tutorials group

--- www/sidebars.js
+++ www/sidebars.js
@@ -1,53 +1,54 @@
 module.exports = {
   sidebar: {
-    'Getting Started': [
-      'getting-started/introduction',
-      'getting-started/example',
-      'getting-started/client',
-      'getting-started/rest-api'
+    "Getting Started": [
+      "getting-started/introduction",
+      "getting-started/example",
+      "getting-started/client",
+      "getting-started/rest-api",
     ],
     Configuration: [
-      'configuration/options',
-      'configuration/providers',
-      'configuration/databases',
-      'configuration/pages',
-      'configuration/callbacks',
-      'configuration/events'
+      "configuration/options",
+      "configuration/providers",
+      "configuration/databases",
+      "configuration/pages",
+      "configuration/callbacks",
+      "configuration/events",
     ],
-    'Providers': [
-      'providers/apple',
-      'providers/auth0',
-      'providers/battle.net',
-      'providers/box',
-      'providers/cognito',
-      'providers/discord',
-      'providers/email',
-      'providers/credentials',
-      'providers/facebook',
-      'providers/github',
-      'providers/gitlab',
-      'providers/google',
-      'providers/identity-server4',
-      'providers/mixer',
-      'providers/okta',
-      'providers/slack',
-      'providers/twitch',
-      'providers/twitter',
-      'providers/yandex'
+    Providers: [
+      "providers/apple",
+      "providers/auth0",
+      "providers/battle.net",
+      "providers/box",
+      "providers/cognito",
+      "providers/discord",
+      "providers/email",
+      "providers/credentials",
+      "providers/facebook",
+      "providers/github",
+      "providers/gitlab",
+      "providers/google",
+      "providers/identity-server4",
+      "providers/mixer",
+      "providers/okta",
+      "providers/slack",
+      "providers/twitch",
+      "providers/twitter",
+      "providers/yandex",
     ],
-    'Models & Schemas': [
-      'schemas/models',
-      'schemas/mysql',
-      'schemas/postgres',
-      'schemas/mongodb',
-      'schemas/adapters'
-    ]
-  /*
+    "Models & Schemas": [
+      "schemas/models",
+      "schemas/mysql",
+      "schemas/postgres",
+      "schemas/mongodb",
+      "schemas/adapters",
+    ],
+    Tutorials: ["tutorials/testing"],
+    /*
     'Version 1 (Legacy)': [
       'v1/getting-started-v1',
       'v1/client-v1',
       'v1/auth-v1'
     ]
   */
-  }
-}
+  },
+};
