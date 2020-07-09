5db05e1031d07f3651e6e58f1fa046acb1018135
Jake Harding <j1315b@gmail.com>
2020-07-09 01:42:29 -0700

    Use id_str for reading in Twitter user ID

--- src/providers/twitter.js
+++ src/providers/twitter.js
@@ -8,10 +8,11 @@ export default (options) => {
     accessTokenUrl: 'https://api.twitter.com/oauth/access_token',
     requestTokenUrl: 'https://api.twitter.com/oauth/request_token',
     authorizationUrl: 'https://api.twitter.com/oauth/authenticate',
-    profileUrl: 'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
+    profileUrl:
+      'https://api.twitter.com/1.1/account/verify_credentials.json?include_email=true',
     profile: (profile) => {
       return {
-        id: profile.id,
+        id: profile.id_str,
         name: profile.name,
         email: profile.email,
         image: profile.profile_image_url_https.replace(/_normal\.jpg$/, '.jpg')
