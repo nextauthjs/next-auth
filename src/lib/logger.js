452c0cc4c03e5b346b4184a3fea664eb3dd5abe6
Iain Collins <me@iaincollins.com>
2020-07-13 01:18:22 +0100

    Implement JWE

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,26 +1,25 @@
 const logger = {
   error: (errorCode, ...text) => {
-    if (console) {
-      !text
-        ? console.error(errorCode)
-        : console.error(
-          `[next-auth][error][${errorCode.toLowerCase()}]`,
-          text,
-          `\nhttps://next-auth.js.org/errors#${errorCode.toLowerCase()}`
-        )
-    }
+    if (!console) { return }    
+    if (text && text.length <= 1) { text = text[0] || '' }
+    console.error(
+      `[next-auth][error][${errorCode.toLowerCase()}]`,
+      text,
+      `\nhttps://next-auth.js.org/errors#${errorCode.toLowerCase()}`
+    )
   },
   warn: (warnCode, ...text) => {
-    if (console) {
-      !text
-        ? console.warn(warnCode)
-        : console.warn(
-          `[next-auth][warn][${warnCode.toLowerCase()}]`,
-          text
-        )
-    }
+    if (!console) { return }
+    if (text && text.length <= 1) { text = text[0] || '' }
+    console.warn(
+      `[next-auth][warn][${warnCode.toLowerCase()}]`,
+      text,
+      `\nhttps://next-auth.js.org/warning#${warnCode.toLowerCase()}`
+    )
   },
   debug: (debugCode, ...text) => {
+    if (!console) { return }
+    if (text && text.length <= 1) { text = text[0] || '' }
     if (process && process.env && process.env._NEXTAUTH_DEBUG) {
       console.log(
         `[next-auth][debug][${debugCode.toLowerCase()}]`,
