d1efc1437f9d129a9a9d968db97f96d8dcce1a08
Mr D <d10221@protonmail.com>
2020-08-02 10:06:00 +0200

    Adding 'nextauth' user as sysadmin.

--- test/docker/mssql/setup.sql
+++ test/docker/mssql/setup.sql
@@ -1,15 +1,29 @@
-USE master;  
-if not exists (select name from sys.syslogins where name = 'nextauth')
+USE master;
+/* did you tear down the container ? */
+if not exists (select name
+from sys.syslogins
+where name = 'nextauth')
 CREATE LOGIN nextauth   
     WITH PASSWORD = 'password',
     CHECK_POLICY = OFF;   
 GO
-if not exists (select name from sys.databases where name = 'nextauth' )
+/* did you tear down the container ? */
+if not exists (select name
+from sys.databases
+where name = 'nextauth' )
 CREATE database nextauth
 GO
-USE nextauth;  
-if not exists(select [name] from sys.sysusers where name= 'nextauth')
-CREATE USER nextauth FOR LOGIN nextauth   
+/* did you tear down the container ? */
+if not exists(select [name]
+from sys.sysusers
+where name= 'nextauth')
+CREATE USER nextauth 
     WITH DEFAULT_SCHEMA =[dbo];  
 GO
-EXEC sp_addrolemember 'db_owner', 'nextauth'
+/* 
+ * Adding user as sysadmin, 
+ * So you can easily drop/create/re-create/alter the database
+ * You will need to login to 'master' to do that
+ */
+exec sp_addsrvrolemember @loginame = N'nextauth', @rolename = N'sysadmin'
+
