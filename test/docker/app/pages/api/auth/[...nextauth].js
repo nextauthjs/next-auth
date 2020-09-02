c7b6f3b78115d9fa45a119bc62969c7001d872e9
Iain Collins <me@iaincollins.com>
2020-09-02 20:20:13 +0100

    Fix linter errors

--- test/docker/app/pages/api/auth/[...nextauth].js
+++ test/docker/app/pages/api/auth/[...nextauth].js
@@ -7,16 +7,16 @@ const options = {
   // https://next-auth.js.org/configuration/providers
   providers: [
     Providers.Email({
-      server: process.env.EMAIL_SERVER, 
-      from: process.env.EMAIL_FROM,
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
     }),
     Providers.Apple({
       clientId: process.env.APPLE_ID,
-      clientSecret: { 
+      clientSecret: {
         appleId: process.env.APPLE_ID,
         teamId: process.env.APPLE_TEAM_ID,
         privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
+        keyId: process.env.APPLE_KEY_ID
       }
     }),
     Providers.Auth0({
@@ -39,7 +39,7 @@ const options = {
     Providers.Twitter({
       clientId: process.env.TWITTER_ID,
       clientSecret: process.env.TWITTER_SECRET
-    }),
+    })
   ],
   // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
   // https://next-auth.js.org/configuration/database
@@ -58,14 +58,14 @@ const options = {
     // Use JSON Web Tokens for session instead of database sessions.
     // This option can be used with or without a database for users/accounts.
     // Note: `jwt` is automatically set to `true` if no database is specified.
-    jwt: true, 
-    
+    jwt: true
+
     // Seconds - How long until an idle session expires and is no longer valid.
     // maxAge: 30 * 24 * 60 * 60, // 30 days
 
     // Seconds - Throttle how frequently to write to database to extend a session.
     // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens 
+    // Note: This option is ignored if using JSON Web Tokens
     // updateAge: 24 * 60 * 60, // 24 hours
   },
 
@@ -74,8 +74,8 @@ const options = {
   // https://next-auth.js.org/configuration/options#jwt
   jwt: {
     // A secret to use for key generation (you should set this explicitly)
-    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw', 
-    
+    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw',
+
     // Set to true to use encryption (default: false)
     // encryption: true,
 
@@ -99,8 +99,8 @@ const options = {
 
   // Callbacks are asynchronous functions you can use to control what happens
   // when an action is performed.
-  // https://next-auth.js.org/configuration/callbacks 
-  callbacks: { 
+  // https://next-auth.js.org/configuration/callbacks
+  callbacks: {
     // signIn: async (user, account, profile) => { return Promise.resolve(true) },
     // redirect: async (url, baseUrl) => { return Promise.resolve(baseUrl) },
     // session: async (session, user) => { return Promise.resolve(session) },
@@ -112,7 +112,7 @@ const options = {
   events: { },
 
   // Enable debug messages in the console if you are having problems
-  debug: false,
+  debug: false
 }
 
 export default (req, res) => NextAuth(req, res, options)
