9c0acc382620c71b9b8362f74895e1b69d76f6f6
Iain Collins <me@iaincollins.com>
2020-09-03 12:21:55 +0100

    Fix linting errors & ignore Cypress tests for now

--- cypress/integration/pages/signout.spec.js
+++ cypress/integration/pages/signout.spec.js
@@ -2,13 +2,13 @@
 // https://on.cypress.io/intelligent-code-completion
 /// <reference types="Cypress" />
 
-describe("the sign out page", () => {
+describe('the sign out page', () => {
   beforeEach(() => {
-    cy.visit("/api/auth/signout");
-  });
-  it("displays the sign out button and descriptive text", () => {
-    cy.findByRole("button", { name: "Sign out" }).should("be.visible");
+    cy.visit('/api/auth/signout')
+  })
+  it('displays the sign out button and descriptive text', () => {
+    cy.findByRole('button', { name: 'Sign out' }).should('be.visible')
 
-    cy.findByText("Are you sure you want to sign out?").should("be.visible");
-  });
-});
+    cy.findByText('Are you sure you want to sign out?').should('be.visible')
+  })
+})
