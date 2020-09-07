d79bb828f286aa2e7ac966f95870f130ef20f5be
Iain Collins <me@iaincollins.com>
2020-09-07 15:12:21 +0100

    Refactor Twitter integration test

--- test/integration/twitter.js
+++ test/integration/twitter.js
@@ -12,16 +12,26 @@ const {
   NEXTAUTH_TWITTER_PASSWORD: PASSWORD
 } = process.env
 
-describe('Twitter (OAuth 1.1 flow)', function () {
+describe.only('Twitter (OAuth 1.1 flow)', async function () {
   this.timeout(1000 * 60)
-  it('should be able to sign in', async function () {
-    const browser = await puppeteer.launch({ headless: true })
-    const page = await browser.newPage()
+
+  let browser,page
+
+  before(async () => {
+    browser = await puppeteer.launch({ headless: true })
+    page = await browser.newPage()
+    return Promise.resolve()
+  })
+
+  it('should show button on sign in page', async function () {
     page.setDefaultTimeout(1000 * 60)
     await page.goto(`${BASE_URL}/api/auth/signin?callbackUrl=${encodeURIComponent(CALLBACK_URL)}`)
-
+    await page.waitForSelector(`form[action="${BASE_URL}/api/auth/signin/twitter"] button`)
     await page.click(`form[action="${BASE_URL}/api/auth/signin/twitter"] button`)
+    return Promise.resolve()
+  })
 
+  it('should be redirected to provider', async function () {
     // Enter username
     await page.waitForSelector('input[name="session[username_or_email]"]')
     await page.click('input[name="session[username_or_email]"]')
@@ -31,10 +41,16 @@ describe('Twitter (OAuth 1.1 flow)', function () {
     await page.waitForSelector('input[name="session[password]"]')
     await page.click('input[name="session[password]"]')
     await page.keyboard.type(PASSWORD)
+    return Promise.resolve()
+  })
 
+  it('should be able to sign in with provider', async function () {
     // Click submit
     await page.click('form[action="https://api.twitter.com/oauth/authenticate"] [type="submit"]')
+    return Promise.resolve()
+  })
 
+  it('should be returned to app and signed in', async function () {
     // Wait for page to submit and callback
     await page.waitForSelector('#nextauth-signed-in')
 
