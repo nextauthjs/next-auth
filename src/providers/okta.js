d45cc5c375742acc52db0f7458c85f0c9cede329
ohheyalanray <ohheyalanray@gmail.com>
2020-10-09 18:15:06 +0000

    bugfix: update Okta routes
    the current routing for the Okta provider does not follow the standard
    set by Okta, and as such doesn't allow for custom subdomains. this
    update amends the routes to allow for customer subdomains, and also
    aligns next-auth with Okta's documentation.

--- src/providers/okta.js
+++ src/providers/okta.js
@@ -11,9 +11,9 @@ export default (options) => {
       client_secret: options.clientSecret
     },
     // These will be different depending on the Org.
-    accessTokenUrl: `https://${options.domain}/oauth2/v1/token`,
-    authorizationUrl: `https://${options.domain}/oauth2/v1/authorize/?response_type=code`,
-    profileUrl: `https://${options.domain}/oauth2/v1/userinfo/`,
+    accessTokenUrl: `https://${options.domain}/v1/token`,
+    authorizationUrl: `https://${options.domain}/v1/authorize/?response_type=code`,
+    profileUrl: `https://${options.domain}/v1/userinfo/`,
     profile: (profile) => {
       return { ...profile, id: profile.sub }
     },
