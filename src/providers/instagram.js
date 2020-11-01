b7e0d6a382862af50ec5eb67e3dc2343259c5ff9
PolMrt <pol@marnette.be>
2020-11-01 10:46:48 +0100

    lint code

--- src/providers/instagram.js
+++ src/providers/instagram.js
@@ -8,11 +8,11 @@ export default (options) => {
     version: '2.0',
     scope: 'user_profile',
     params: { grant_type: 'authorization_code' },
-    accessTokenUrl: 'https://api.instagram.com/oauth/access_token', 
-    authorizationUrl: 'https://api.instagram.com/oauth/authorize?response_type=code', 
+    accessTokenUrl: 'https://api.instagram.com/oauth/access_token',
+    authorizationUrl: 'https://api.instagram.com/oauth/authorize?response_type=code',
     profileUrl: 'https://graph.instagram.com/me?fields=id,username,account_type,name',
     profile: async (profile) => {
-      const instagramData = await fetch('https://www.instagram.com/'+profile.username+'/?__a=1').then(res => res.json())
+      const instagramData = await fetch('https://www.instagram.com/' + profile.username + '/?__a=1').then(res => res.json())
       return {
         id: profile.id,
         name: instagramData.graphql.user.full_name,
