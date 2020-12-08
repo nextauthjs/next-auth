34f88a1f27cdce77b24d04c9897ce5e5968e6b47
Sam Bauch <sbauch@gmail.com>
2020-12-07 23:10:53 -0500

    rm hardcoded domain

--- src/providers/osso.js
+++ src/providers/osso.js
@@ -6,14 +6,13 @@ export default (options) => {
     version: '2.0',
     params: { grant_type: 'authorization_code' },
     accessTokenUrl: `https://${options.domain}/oauth/token`,
-    authorizationUrl: `https://${options.domain}/oauth/authorize?response_type=code&domain=vcardme.com`,
+    authorizationUrl: `https://${options.domain}/oauth/authorize?response_type=code`,
     profileUrl: `https://${options.domain}/oauth/me`,
     profile: (profile) => {
       return {
         id: profile.id,
-        name: null,
+        name: profile.name,
         email: profile.email,
-        image: null
       }
     },
     ...options
