37649cc28f9e5bcebc005207c5f1849c3d5e3bee
Aymeric <34040599+afoyer@users.noreply.github.com>
2021-01-12 10:51:40 -0700

    added reddit provider

--- src/providers/index.js
+++ src/providers/index.js
@@ -1,67 +1,53 @@
 import Apple from './apple'
 import Atlassian from './atlassian'
 import Auth0 from './auth0'
-import AzureADB2C from './azure-ad-b2c'
 import Basecamp from './basecamp'
 import BattleNet from './battlenet'
 import Box from './box'
-import Bungie from './bungie'
-import Cognito from './cognito'
 import Credentials from './credentials'
+import Cognito from './cognito'
 import Discord from './discord'
 import Email from './email'
 import Facebook from './facebook'
-import Foursquare from './foursquare'
 import FusionAuth from './fusionauth'
 import GitHub from './github'
 import GitLab from './gitlab'
 import Google from './google'
 import IdentityServer4 from './identity-server4'
-import LINE from './line'
 import LinkedIn from './linkedin'
-import MailRu from './mailru'
 import Mixer from './mixer'
-import Netlify from './netlify'
 import Okta from './okta'
+import Reddit from './reddit'
 import Slack from './slack'
 import Spotify from './spotify'
-import Strava from './strava'
 import Twitch from './twitch'
 import Twitter from './twitter'
-import VK from './vk'
 import Yandex from './yandex'
 
 export default {
-  Apple,
   Atlassian,
   Auth0,
-  AzureADB2C,
+  Apple,
   Basecamp,
   BattleNet,
   Box,
-  Bungie,
-  Cognito,
   Credentials,
+  Cognito,
   Discord,
   Email,
   Facebook,
-  Foursquare,
   FusionAuth,
   GitHub,
   GitLab,
   Google,
   IdentityServer4,
-  LINE,
   LinkedIn,
-  MailRu,
   Mixer,
-  Netlify,
   Okta,
+  Reddit,
   Slack,
   Spotify,
-  Strava,
-  Twitch,
   Twitter,
-  VK,
+  Twitch,
   Yandex
 }
