2865b8cc2d6ebfc3301fc943976b5de8e603e810
Balázs Orbán <info@balazsorban.com>
2021-01-12 17:03:13 +0100

    refactor: show signin page in dev app [skip release]

--- components/header.js
+++ components/header.js
@@ -30,7 +30,7 @@ export default function Header () {
                 className={styles.buttonPrimary}
                 onClick={(e) => {
                   e.preventDefault()
-                  signIn('github')
+                  signIn()
                 }}
               >
                 Sign in
