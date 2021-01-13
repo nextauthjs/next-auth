502e8ddc18181772d44fdbe390ac49d83d71c8c1
Balázs Orbán <info@balazsorban.com>
2021-01-13 23:12:37 +0100

    refactor: use openid-client for signin

--- src/server/lib/signin/oauth.js
+++ src/server/lib/signin/oauth.js
@@ -1,39 +1,69 @@
-import oAuthClient from '../oauth/client'
+import getOAuthClient from '../oauth/client'
+import getOAuthClientLegacy from '../oauth/client.legacy'
 import { createHash } from 'crypto'
 import logger from '../../../lib/logger'
 
-export default async function oauth (provider, csrfToken) {
-  const { callbackUrl } = provider
-  const client = oAuthClient(provider)
-  if (provider.version?.startsWith('2.')) {
-    // Handle OAuth v2.x
-    let url = client.getAuthorizeUrl({
-      redirect_uri: callbackUrl,
-      scope: provider.scope,
-      // A hash of the NextAuth.js CSRF token is used as the state
-      state: createHash('sha256').update(csrfToken).digest('hex'),
-      ...provider.authorizationParams
-    })
+/**
+ * Returns an OAuth `/authorization` url with params.
+ * @param {import("next").NextApiRequest} req
+ * @docs https://tools.ietf.org/html/rfc6749#section-4.1.1 (/authorize)
+ * @docs https://tools.ietf.org/html/rfc7636#section-4.3 (PKCE)
+ */
+export default async function getAuthorizationUrl (req) {
+  try {
+    const { provider } = req.options
+    const { callbackUrl } = provider
 
-    // If the authorizationUrl specified in the config has query parameters on it
-    // make sure they are included in the URL we return.
-    //
-    // This is a fix for an open issue with the oAuthClient library we are using
-    // which inadvertantly strips them.
-    //
-    // https://github.com/ciaranj/node-oauth/pull/193
-    if (provider.authorizationUrl.includes('?')) {
-      const parseUrl = new URL(provider.authorizationUrl)
-      const baseUrl = `${parseUrl.origin}${parseUrl.pathname}?`
-      url = url.replace(baseUrl, provider.authorizationUrl + '&')
+    // Handle OAuth v2.x and OIDC
+    if (provider.version?.startsWith('2.')) {
+      const { csrfToken } = provider
+      const client = getOAuthClient(provider)
+
+      const url = new URL(provider.authorizationUrl)
+      const authorizeParams = Object.fromEntries(url.searchParams.entries())
+
+      /** @type {import("openid-client").AuthorizationParameters} */
+      const params = {
+        redirect_uri: provider.callbackUrl,
+        scope: provider.scope,
+        // Preserve params from authorizationUrl
+        ...authorizeParams,
+        // Makes it possible to defined params as an object for the provider
+        ...provider.authorizationParams
+      }
+
+      switch (provider.verification) {
+        case 'pkce':
+          // TODO: handle PKCE
+          // const codeChallenge generators.codeChallenge(codeVerifier)
+          // logger.debug('OAUTH_AUTHORIZATION_URL', {
+          //   message: 'PKCE code_challenge being sent', codeChallenge
+          // })
+          // params.code_challenge = codeChallenge
+          break
+        case 'state': {
+          const state = createHash('sha256').update(csrfToken).digest('hex')
+          logger.debug('OAUTH_AUTHORIZATION_URL', {
+            message: 'State being sent', state
+          })
+          params.state = state
+          break
+        }
+        case 'none':
+        default:
+          break
+      }
+
+      logger.debug('OAUTH_AUTHORIZATION_URL', { params, provider })
+      return client.authorizationUrl(params)
     }
 
-    return url
-  }
-
-  try {
+    // Handle OAuth 1.x
+    const client = getOAuthClientLegacy(provider)
     const oAuthToken = await client.getOAuthRequestToken(callbackUrl)
-    return `${provider.authorizationUrl}?oauth_token=${oAuthToken}`
+    const url = `${provider.authorizationUrl}?oauth_token=${oAuthToken}`
+    logger.debug('OAUTH_AUTHORIZATION_URL', { url, provider })
+    return url
   } catch (error) {
     logger.error('GET_AUTHORISATION_URL_ERROR', error)
     throw error
