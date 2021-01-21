92f59b1f9f8a162432e6c7a8880c242b24358f7e
Griko Nibras <git@griko.id>
2021-01-21 14:39:28 +0700

    feat: include alt `NEXTAUTH_URL` retrieval from next runtime config

--- src/lib/env.js
+++ src/lib/env.js
@@ -1,12 +1,20 @@
+import getConfig from 'next/config'
+
 /**
- * Returns `NEXTAUTH_URL` environment variable
+ * Returns `NEXTAUTH_URL` either via environment variable or Next.js runtime config
  *
  * @returns {string}
  */
 export function getNextAuthUrl () {
-  // Return existing value from env if exists
+  // Return from environment variable if exists
   if (process.env.NEXTAUTH_URL) {
     return process.env.NEXTAUTH_URL
+  }
+
+  // Use Next.js runtime config if exists
+  const { publicRuntimeConfig } = getConfig()
+  if (publicRuntimeConfig.NEXTAUTH_URL) {
+    return publicRuntimeConfig.NEXTAUTH_URL
   }
 
   return null
