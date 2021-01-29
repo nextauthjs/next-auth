f4a954ccbbdc8b56eb64139a0c0dd873f7dd1105
Balázs Orbán <info@balazsorban.com>
2021-01-29 23:45:29 +0100

    feat: send all params to logger function (#1214)

--- src/lib/logger.js
+++ src/lib/logger.js
@@ -1,23 +1,23 @@
 const logger = {
-  error (code, ...text) {
+  error (code, ...message) {
     console.error(
       `[next-auth][error][${code.toLowerCase()}]`,
-      JSON.stringify(text),
-      `\nhttps://next-auth.js.org/errors#${code.toLowerCase()}`
+      `\nhttps://next-auth.js.org/errors#${code.toLowerCase()}`,
+      ...message
     )
   },
-  warn (code, ...text) {
+  warn (code, ...message) {
     console.warn(
       `[next-auth][warn][${code.toLowerCase()}]`,
-      JSON.stringify(text),
-      `\nhttps://next-auth.js.org/warnings#${code.toLowerCase()}`
+      `\nhttps://next-auth.js.org/warnings#${code.toLowerCase()}`,
+      ...message
     )
   },
-  debug (code, ...text) {
+  debug (code, ...message) {
     if (!process?.env?._NEXTAUTH_DEBUG) return
     console.log(
       `[next-auth][debug][${code.toLowerCase()}]`,
-      JSON.stringify(text)
+      ...message
     )
   }
 }
