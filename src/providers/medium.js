0c9d05be8eeaa524f36b782c4bf25b96fff23bbd
vova <volodimir.partytskyi@gmail.com>
2021-01-29 22:37:47 +0200

    feat(provider): Add medium

---
+++ src/providers/medium.js
@@ -0,0 +1,22 @@
+export default (options) => {
+  return {
+    id: 'medium',
+    name: 'Medium',
+    type: 'oauth',
+    version: '2.0',
+    scope: 'basicProfile',
+    params: { grant_type: 'authorization_code' },
+    accessTokenUrl: 'https://api.medium.com/v1/tokens',
+    authorizationUrl: 'https://medium.com/m/oauth/authorize?response_type=code',
+    profileUrl: 'https://api.medium.com/v1/me',
+    profile: (profile) => {
+      return {
+        id: profile.data.id,
+        name: profile.data.name,
+        email: null,
+        image: profile.data.imageUrl
+      }
+    },
+    ...options
+  }
+}
