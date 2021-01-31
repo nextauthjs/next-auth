f480bcb07739cc6beae3ae5b35e46335724bfbdb
Gerald McNicholl <gerald.mcnicholl@xero.com>
2021-02-01 09:56:03 +1300

    Adding EVEOnline provider

--- src/providers/eveonline.js
+++ src/providers/eveonline.js
@@ -1,13 +1,13 @@
 export default (options) => {
   return {
-    id: "eveonline",
-    name: "EVEOnline",
-    type: "oauth",
-    version: "2.0",
-    params: { grant_type: "authorization_code" },
-    accessTokenUrl: "https://login.eveonline.com/oauth/token",
-    authorizationUrl: "https://login.eveonline.com/oauth/authorize?response_type=code",
-    profileUrl: "https://login.eveonline.com/oauth/verify",
+    id: 'eveonline',
+    name: 'EVEOnline',
+    type: 'oauth',
+    version: '2.0',
+    params: { grant_type: 'authorization_code' },
+    accessTokenUrl: 'https://login.eveonline.com/oauth/token',
+    authorizationUrl: 'https://login.eveonline.com/oauth/authorize?response_type=code',
+    profileUrl: 'https://login.eveonline.com/oauth/verify',
     profile: (profile) => {
       return {
         id: profile.CharacterID,
