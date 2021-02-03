3966a072c897dc01583ab8554aea4fb9ed4bb5a1
Jason Kuhrt <jasonkuhrt@me.com>
2021-02-03 15:31:27 -0500

    improve(ts): colocate TS typings

---
+++ index.d.ts
@@ -0,0 +1,152 @@
+/// <reference types="node" />
+
+import { ConnectionOptions } from 'typeorm'
+import { Adapter } from './adapters'
+import { SessionProvider } from './client'
+import { JWTDecodeParams, JWTEncodeParams } from './jwt'
+import { PossibleProviders } from './providers'
+import {
+  GenericObject,
+  NextApiRequest,
+  NextApiResponse,
+  SessionBase,
+} from './_utils'
+
+interface InitOptions {
+  providers: ReadonlyArray<ReturnType<PossibleProviders>>
+  database?: string | ConnectionOptions
+  secret?: string
+  session?: Session
+  jwt?: JWTOptions
+  pages?: PageOptions
+  callbacks?: Callbacks
+  debug?: boolean
+  adapter?: Adapter
+  events?: Events
+  useSecureCookies?: boolean
+  cookies?: Cookies
+}
+
+interface AppOptions {
+  debug: boolean
+  pages: PageOptions
+  adapter: Adapter
+  baseUrl: string
+  basePath: string
+  action:
+    | 'providers'
+    | 'session'
+    | 'csrf'
+    | 'signin'
+    | 'signout'
+    | 'callback'
+    | 'verify-request'
+    | 'error'
+  provider?: string
+  cookies: Cookies
+  secret: string
+  csrfToken: string
+  providers: {
+    [provider: string]: SessionProvider
+  }
+  session: Session
+  jwt: JWTOptions
+  events: Events
+  callbacks: Callbacks
+  callbackUrl: string
+  // redirect?(redirectUrl: string): any;
+}
+
+interface PageOptions {
+  signIn?: string
+  signOut?: string
+  error?: string
+  verifyRequest?: string
+  newUser?: string | null
+}
+
+interface Cookies {
+  [cookieKey: string]: Cookie
+}
+
+interface Cookie {
+  name: string
+  options?: CookieOptions
+}
+
+interface CookieOptions {
+  httpOnly?: boolean
+  sameSite?: true | 'strict' | 'lax' | 'none'
+  path?: string
+  secure?: boolean
+  maxAge?: number
+  domain?: string
+}
+
+interface Events {
+  signIn?(message: any): Promise<void>
+  signOut?(message: any): Promise<void>
+  createUser?(message: any): Promise<void>
+  updateUser?(message: any): Promise<void>
+  linkAccount?(message: any): Promise<void>
+  session?(message: any): Promise<void>
+  error?(message: any): Promise<void>
+}
+
+interface Session {
+  jwt?: boolean
+  maxAge?: number
+  updateAge?: number
+}
+
+interface User {
+  name?: string | null
+  email?: string | null
+  image?: string | null
+}
+
+interface JWTOptions {
+  secret?: string
+  maxAge?: number
+  encryption?: boolean
+  signingKey?: string
+  encryptionKey?: string
+  encode?(options: JWTEncodeParams): Promise<string>
+  decode?(options: JWTDecodeParams): Promise<GenericObject>
+}
+
+// TODO: Improve callback typings
+interface Callbacks {
+  signIn?(
+    user: User,
+    account: GenericObject,
+    profile: GenericObject
+  ): Promise<boolean>
+  redirect?(url: string, baseUrl: string): Promise<string>
+  session?(session: SessionBase, user: User): Promise<GenericObject>
+  jwt?(
+    token: GenericObject,
+    user: User,
+    account: GenericObject,
+    profile: GenericObject,
+    isNewUser: boolean
+  ): Promise<GenericObject>
+}
+
+declare function NextAuth(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  options?: InitOptions
+): Promise<void>
+export default NextAuth
+export {
+  InitOptions,
+  AppOptions,
+  PageOptions,
+  Cookies,
+  Events,
+  Session,
+  JWTOptions,
+  User,
+  Callbacks,
+}
