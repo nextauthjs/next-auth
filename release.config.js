68695af1f3eb2ab67e6a6f4246ffe022650ce936
Balázs Orbán <info@balazsorban.com>
2021-02-09 21:19:24 +0100

    chore: update release flow

---
+++ release.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  branches: [
+    '+([0-9])?(.{+([0-9]),x}).x',
+    'main',
+    'next',
+    'next-major',
+    { name: 'beta', prerelease: true },
+    { name: 'alpha', prerelease: true }
+  ]
+}
