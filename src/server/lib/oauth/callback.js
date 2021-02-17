5ac10562dcd76c52caeb151d5900131e8efaa907
Balázs Orbán <info@balazsorban.com>
2021-02-17 19:30:39 +0100

    feat: make access_token available in profile callback

--- src/server/lib/oauth/callback.js
+++ src/server/lib/oauth/callback.js
@@ -108,7 +108,7 @@ async function getProfile ({ profileData, tokens, provider, user }) {
 
     logger.debug('PROFILE_DATA', profileData)
 
-    const profile = await provider.profile(profileData)
+    const profile = await provider.profile(profileData, tokens.access_token)
     // Return profile, raw profile and auth provider details
     return {
       profile: {
