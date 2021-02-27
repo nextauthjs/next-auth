2ff39168d6eb2576b8f7edf00c5e7f2715d68506
Praneeth <johndoeknown@protonmail.com>
2021-02-27 18:09:27 +0530

    changed multi-line to inline and added title param to send fn in src/server/pages/index.js

--- src/server/pages/index.js
+++ src/server/pages/index.js
@@ -9,24 +9,8 @@ export default function renderPage (req, res) {
   const { baseUrl, basePath, callbackUrl, csrfToken, providers, theme } = req.options
 
   res.setHeader('Content-Type', 'text/html')
-  function send (html) {
-    res.send(`<!DOCTYPE html>
-      <html lang="en">
-      <head>
-          <meta charset="UTF-8">
-          <meta http-equiv="X-UA-Compatible" content="IE=edge">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <style>
-              ${css()}
-          </style>
-          <title>Next Auth Sign in</title>
-      </head>
-      <body class="__next-auth-theme-${theme}">
-          <div class="page">
-              ${html}
-          </div>
-      </body>
-      </html>`);
+  function send ({ html, title }) {
+    res.send(`<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>${css()}</style><title>${title}</title></head><body class="__next-auth-theme-${theme}"><div class="page">${html}</div></body></html>`)
   }
 
   return {
