3aee24b5dc07092b8e2e06f46fb34078b911d79c
Balázs Orbán <info@balazsorban.com>
2021-03-05 22:47:32 +0100

    refactor: client improvements (#1428)
    * docs(client): add TS definitions to client
    
    * docs(client): add documentation links to public methods
    
    * refactor(client): simplify window sync, simplify logic
    
    * refactor(client): extract repeating logic to _fetchData
    
    * refactor(client): remove clientId
    
    * refactor(client): use session in Provider if passed

--- src/lib/logger.d.ts
+++ src/lib/logger.d.ts
@@ -3,3 +3,8 @@ export interface LoggerInstance {
   error: (code?: string, ...message: unknown[]) => void
   debug: (code?: string, ...message: unknown[]) => void
 }
+
+export declare function proxyLogger (logger: LoggerInstance, basePath: string): LoggerInstance
+
+const _logger: LoggerInstance
+export default _logger
