bc114a3d2365e5ebff44912f58af5b278b1abe08
Santeri Elo <me@santeri.xyz>
2021-03-12 12:26:28 +0200

    fix: lint errors

--- src/lib/object-utils.js
+++ src/lib/object-utils.js
@@ -1,21 +1,21 @@
-export function isObject(item) {
-  return item && typeof item === "object" && !Array.isArray(item);
+export function isObject (item) {
+  return item && typeof item === 'object' && !Array.isArray(item)
 }
 
-export function mergeDeep(target, ...sources) {
-  if (!sources.length) return target;
-  const source = sources.shift();
+export function mergeDeep (target, ...sources) {
+  if (!sources.length) return target
+  const source = sources.shift()
 
   if (isObject(target) && isObject(source)) {
     for (const key in source) {
       if (isObject(source[key])) {
-        if (!target[key]) Object.assign(target, { [key]: {} });
-        mergeDeep(target[key], source[key]);
+        if (!target[key]) Object.assign(target, { [key]: {} })
+        mergeDeep(target[key], source[key])
       } else {
-        Object.assign(target, { [key]: source[key] });
+        Object.assign(target, { [key]: source[key] })
       }
     }
   }
 
-  return mergeDeep(target, ...sources);
+  return mergeDeep(target, ...sources)
 }
--- src/server/lib/default-translations.ts
+++ src/server/lib/default-translations.ts
@@ -1,63 +1,63 @@
 export default {
   signIn: {
-    title: "Sign In",
-    submit: "Sign in with %s",
-    email: "Email",
-    dividerText: "or",
+    title: 'Sign In',
+    submit: 'Sign in with %s',
+    email: 'Email',
+    dividerText: 'or',
     errors: {
-      Signin: "Try signing with a different account.",
-      OAuthSignin: "Try signing with a different account.",
-      OAuthCallback: "Try signing with a different account.",
-      OAuthCreateAccount: "Try signing with a different account.",
-      EmailCreateAccount: "Try signing with a different account.",
-      Callback: "Try signing with a different account.",
+      Signin: 'Try signing with a different account.',
+      OAuthSignin: 'Try signing with a different account.',
+      OAuthCallback: 'Try signing with a different account.',
+      OAuthCreateAccount: 'Try signing with a different account.',
+      EmailCreateAccount: 'Try signing with a different account.',
+      Callback: 'Try signing with a different account.',
       OAuthAccountNotLinked:
-        "To confirm your identity, sign in with the same account you used originally.",
-      EmailSignin: "Check your email address.",
+        'To confirm your identity, sign in with the same account you used originally.',
+      EmailSignin: 'Check your email address.',
       CredentialsSignin:
-        "Sign in failed. Check the details you provided are correct.",
-      default: "Unable to sign in.",
-    },
+        'Sign in failed. Check the details you provided are correct.',
+      default: 'Unable to sign in.'
+    }
   },
   signOut: {
-    title: "Sign Out",
-    heading: "Are you sure you want to sign out?",
-    submit: "Sign out",
+    title: 'Sign Out',
+    heading: 'Are you sure you want to sign out?',
+    submit: 'Sign out'
   },
   verifyRequest: {
-    title: "Verify Request",
-    heading: "Check your email",
-    message: "A sign in link has been sent to your email address.",
+    title: 'Verify Request',
+    heading: 'Check your email',
+    message: 'A sign in link has been sent to your email address.'
   },
   error: {
-    title: "Error",
-    signIn: "Sign in",
+    title: 'Error',
+    signIn: 'Sign in',
     default: {
-      heading: "Error",
+      heading: 'Error'
     },
     configuration: {
-      heading: "Server error",
-      message: "There is a problem with the server configuration.",
-      serverLogHint: "Check the server logs for more information.",
+      heading: 'Server error',
+      message: 'There is a problem with the server configuration.',
+      serverLogHint: 'Check the server logs for more information.'
     },
     accessdenied: {
-      heading: "Access Denied",
-      message: "You do not have permission to sign in.",
+      heading: 'Access Denied',
+      message: 'You do not have permission to sign in.'
     },
     verification: {
-      heading: "Unable to sign in",
-      message: "The sign in link is no longer valid.",
-      expirationHint: "It may have been used already or it may have expired.",
-    },
+      heading: 'Unable to sign in',
+      message: 'The sign in link is no longer valid.',
+      expirationHint: 'It may have been used already or it may have expired.'
+    }
   },
   email: {
-    subject: "Sign in to %s",
-    text: "Sign in to %s",
+    subject: 'Sign in to %s',
+    text: 'Sign in to %s',
     html: {
-      signInAs: "Sign in as <strong>%s</strong>",
-      signIn: "Sign In",
+      signInAs: 'Sign in as <strong>%s</strong>',
+      signIn: 'Sign In',
       didNotRequestHint:
-        "If you did not request this email you can safely ignore it.",
-    },
-  },
-};
+        'If you did not request this email you can safely ignore it.'
+    }
+  }
+}
