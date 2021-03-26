839018c49141ff5f401b2d2373baae8714eb908f
Ben Fox <BenjaminWFox@gmail.com>
2021-03-25 19:20:32 -0700

    Fix lint errors

--- src/providers/azure-ad-b2c.js
+++ src/providers/azure-ad-b2c.js
@@ -1,5 +1,4 @@
 export default (options) => {
-
   const { tenantName, primaryUserFlow } = options
   const authorizeUrl = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${primaryUserFlow}/oauth2/v2.0/authorize`
   const tokenUrl = `https://${tenantName}.b2clogin.com/${tenantName}.onmicrosoft.com/${primaryUserFlow}/oauth2/v2.0/token`
@@ -20,25 +19,23 @@ export default (options) => {
     profile: (profile) => {
       let name = ''
 
-      // B2C "Display Name"
       if (profile.name) {
+        // B2C "Display Name"
         name = profile.name
-      }
-      // B2C "Given Name" & "Surname"
-      else if (profile.given_name && profile.family_name){
+      } else if (profile.given_name && profile.family_name) {
+        // B2C "Given Name" & "Surname"
         name = `${profile.given_name} ${profile.family_name}`
-      }
-      // B2C "Given Name"
-      else if (profile.given_name) {
+      } else if (profile.given_name) {
+        // B2C "Given Name"
         name = `${profile.given_name}`
       }
 
       return {
         name,
         id: profile.oid,
-        email: profile.emails[0],
+        email: profile.emails[0]
       }
     },
-   ...options
+    ...options
   }
 }
