8fcbb54e9b3dae89e41698526c4cf3f53acd4f9f
Vinícius CR <eu@viniciuscr.com.br>
2021-03-30 20:51:19 -0300

    chore: reverting unespected change

--- src/server/lib/oauth/client.js
+++ src/server/lib/oauth/client.js
@@ -167,9 +167,17 @@ async function getOAuth2AccessToken (code, provider, codeVerifier) {
           raw = querystring.parse(data)
         }
 
-        const accessToken = provider.id === 'slack'
-          ? raw.authed_user.access_token
-          : raw.access_token
+        let accessToken
+        if (provider.id === 'slack') {
+          const { ok, error } = raw
+          if (!ok) {
+            return reject(error)
+          }
+
+          accessToken = raw.authed_user.access_token
+        } else {
+          accessToken = raw.access_token
+        }
 
         resolve({
           accessToken,
