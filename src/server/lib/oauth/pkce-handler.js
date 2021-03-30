60a9ec0728ba1fc8316e356c2ebce734f10d3144
Vinícius CR <eu@viniciuscr.com.br>
2021-03-30 20:59:18 -0300

    chore: lint files

--- src/server/lib/oauth/pkce-handler.js
+++ src/server/lib/oauth/pkce-handler.js
@@ -16,7 +16,7 @@ const PKCE_MAX_AGE = 60 * 15 // 15 minutes in seconds
 export async function handleCallback (req, res) {
   const { cookies, provider, baseUrl, basePath } = req.options
   try {
-    if (![provider.protection].flat().includes('pkce')) {// Provider does not support PKCE, nothing to do.
+    if (![provider.protection].flat().includes('pkce')) { // Provider does not support PKCE, nothing to do.
       return
     }
 
