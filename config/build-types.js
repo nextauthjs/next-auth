7405e4216adddecca211590c29bbde40c04309b7
Lluis Agusti <hi@llu.lu>
2021-04-09 17:44:06 +0200

    refactor(lint): fix build-types script

--- config/build-types.js
+++ config/build-types.js
@@ -1,19 +1,19 @@
-const fs = require("fs")
-const path = require("path")
+const fs = require('fs')
+const path = require('path')
 
 const BUILD_TARGETS = [
-  "index.d.ts",
-  "client.d.ts",
-  "adapters.d.ts",
-  "providers.d.ts",
-  "jwt.d.ts",
-  "_next.d.ts",
-  "_utils.d.ts",
+  'index.d.ts',
+  'client.d.ts',
+  'adapters.d.ts',
+  'providers.d.ts',
+  'jwt.d.ts',
+  '_next.d.ts',
+  '_utils.d.ts'
 ]
 
 BUILD_TARGETS.forEach((target) => {
   fs.copyFile(
-    path.resolve("types", target),
+    path.resolve('types', target),
     path.join(process.cwd(), target),
     (err) => {
       if (err) throw err
