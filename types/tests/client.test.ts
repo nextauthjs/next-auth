085325e4a24da95f88095f575a93bda6b0097f33
Balázs Orbán <info@balazsorban.com>
2021-04-16 20:16:45 +0200

    test(ts): fix client tests

--- types/tests/client.test.ts
+++ types/tests/client.test.ts
@@ -11,7 +11,7 @@ const clientSession = {
   expires: "1234",
 }
 
-// $ExpectType [Session | null | undefined, boolean]
+// $ExpectType [Session | null, boolean]
 client.useSession()
 
 // $ExpectType Promise<Session | null>
@@ -20,10 +20,10 @@ client.getSession({ req: nextReq })
 // $ExpectType Promise<Session | null>
 client.session({ req: nextReq })
 
-// $ExpectType Promise<Record<string, AppProvider> | null>
+// $ExpectType Promise<Record<string, ClientSafeProvider> | null>
 client.getProviders()
 
-// $ExpectType Promise<Record<string, AppProvider> | null>
+// $ExpectType Promise<Record<string, ClientSafeProvider> | null>
 client.providers()
 
 // $ExpectType Promise<string | null>
@@ -32,26 +32,36 @@ client.getCsrfToken({ req: nextReq })
 // $ExpectType Promise<string | null>
 client.csrfToken({ req: nextReq })
 
-// $ExpectType Promise<void>
-client.signin("github", { data: "foo", redirect: false }, { login: "username" })
+// $ExpectType Promise<string | null>
+client.csrfToken({ ctx: { req: nextReq } })
 
-// $ExpectType Promise<SignInResponse>
-client.signin("credentials", { data: "foo", redirect: false })
+// $ExpectType Promise<undefined>
+client.signin("github", { callbackUrl: "foo" }, { login: "username" })
 
-// $ExpectType Promise<SignInResponse>
-client.signin("email", { data: "foo", redirect: false })
+// $ExpectType Promise<SignInResponse | undefined>
+client.signin("credentials", { callbackUrl: "foo", redirect: true })
 
-// $ExpectType Promise<void>
-client.signin("email", { data: "foo", redirect: true })
+// $ExpectType Promise<SignInResponse | undefined>
+client.signin("credentials", { redirect: false })
 
-// $ExpectType Promise<void>
+// $ExpectType Promise<SignInResponse | undefined>
+client.signin("email", { callbackUrl: "foo", redirect: false })
+
+// $ExpectType Promise<SignInResponse | undefined>
+client.signin("email", { callbackUrl: "foo", redirect: true })
+
+// $ExpectType Promise<undefined>
 client.signout()
 
-// $ExpectType Promise<void>
+// $ExpectType Promise<undefined>
 client.signout({ callbackUrl: "https://foo.com/callback", redirect: true })
+
+// $ExpectType Promise<SignOutResponse>
+client.signOut({ callbackUrl: "https://foo.com/callback", redirect: false })
 
 // $ExpectType ReactElement<any, any> | null
 client.Provider({
+  children: null,
   session: clientSession,
   options: {
     baseUrl: "https://foo.com",
@@ -62,18 +72,22 @@ client.Provider({
 
 // $ExpectType ReactElement<any, any> | null
 client.Provider({
+  children: null,
   session: clientSession,
 })
 
 // $ExpectType ReactElement<any, any> | null
 client.Provider({
-  session: undefined,
+  children: null,
   options: {},
 })
 
 // $ExpectType ReactElement<any, any> | null
 client.Provider({
-  session: null,
+  children: null,
+  session: {
+    expires: "",
+  },
   options: {
     baseUrl: "https://foo.com",
     basePath: "/",
