f9016e96b4a3bc883c6494468da9b4647b904170
Balázs Orbán <info@balazsorban.com>
2021-04-20 15:32:34 +0200

    chore: re-add review comment to Apple

--- src/providers/apple.js
+++ src/providers/apple.js
@@ -28,7 +28,7 @@ export default function Apple(options) {
       privateKey: null,
       keyId: null,
     },
-    protection: "none",
+    protection: "none", // REVIEW: Apple does not support state, as far as I know. Can we use "pkce" then?
     ...options,
   }
 }
