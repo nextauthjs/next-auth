f93e282c5b334b67bfeec0fdbd0243bd46b86d13
Balázs Orbán <info@balazsorban.com>
2021-04-21 13:19:56 +0200

    fix(ts): don't require default session/jwt fields

--- types/index.d.ts
+++ types/index.d.ts
@@ -364,6 +364,15 @@ export interface PagesOptions {
   newUser?: string
 }
 
+export interface DefaultSession extends Record<string, unknown> {
+  user: {
+    name: string | null
+    email: string | null
+    picture: string | null
+  }
+  expires: string
+}
+
 /**
  * Returned by `useSession`, `getSession`, returned by the `session` callback
  * and also the shape received as a prop on the `Provider` React Context
@@ -373,10 +382,7 @@ export interface PagesOptions {
  * [`Provider`](https://next-auth.js.org/getting-started/client#provider) |
  * [`session` callback](https://next-auth.js.org/configuration/callbacks#jwt-callback)
  */
-export interface Session extends Record<string, unknown> {
-  user?: User
-  expires?: string
-}
+export interface Session extends Record<string, unknown>, DefaultSession {}
 
 /** [Documentation](https://next-auth.js.org/configuration/options#session) */
 export interface SessionOptions {
