c4e069478c64a2078390e2f37da2a1938e466bef
Thanayut T <b@thanayut.in.th>
2021-04-24 15:07:39 +0700

    feat(provider): add WordPress.com provider

---
+++ src/providers/wordpress.js
@@ -0,0 +1,23 @@
+export default function WordPress(options) {
+  return {
+    id: "wordpress",
+    name: "WordPress.com",
+    type: "oauth",
+    version: "2.0",
+    scope: "auth",
+    params: { grant_type: "authorization_code" },
+    accessTokenUrl: "https://public-api.wordpress.com/oauth2/token",
+    authorizationUrl:
+      "https://public-api.wordpress.com/oauth2/authorize?response_type=code",
+    profileUrl: "https://public-api.wordpress.com/rest/v1/me",
+    profile(profile) {
+      return {
+        id: profile.ID,
+        name: profile.display_name,
+        email: profile.email,
+        image: profile.avatar_URL,
+      }
+    },
+    ...options,
+  }
+}
