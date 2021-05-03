f460ed2891f387cb07b9cd82819d757849b77b73
anubisoft <anubisoftprez@gmail.com>
2021-05-03 06:49:21 -0700

    Changed fix for bug #192 to have no querystring params in the custom signout page url.

--- src/server/index.js
+++ src/server/index.js
@@ -158,18 +158,8 @@ async function NextAuthHandler (req, res, userOptions) {
 
           return render.signin()
         case 'signout':
-          if (pages.signOut) {
-            let signoutUrl = `${pages.signOut}`
+          if (pages.signOut) return res.redirect(pages.signOut)
 
-            // Append a callback param if one was specified in the request options.
-            if (req.options.callbackUrl)
-              signoutUrl = `${signoutUrl}${pages.signOut.includes('?') ? '&' : '?'}callbackUrl=${req.options.callbackUrl}`
-
-            // Append the error code param when there's an error.
-            if (error) { signoutUrl = `${signoutUrl}${pages.signOut.includes('?') ? '&' : '?'}&error=${error}` }
-
-            return res.redirect(signoutUrl)
-          }
           return render.signout()
         case 'callback':
           if (provider) {
