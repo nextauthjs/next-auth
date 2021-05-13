7721f4d6c2c19e4a206f7522d19e9f613907d2f1
ndom91 <yo@ndo.dev>
2021-05-13 22:40:55 +0200

    feat: upgrade docusaurus + style a bit

--- www/src/theme/SearchBar/index.js
+++ www/src/theme/SearchBar/index.js
@@ -6,17 +6,19 @@
  * LICENSE file in the root directory of this source tree.
  */
 
-import React, { useRef, useCallback } from "react";
-import classnames from "classnames";
-import { useHistory } from "@docusaurus/router";
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-let loaded = false;
+import React, { useRef, useCallback, useEffect } from "react"
+import classnames from "classnames"
+import { useHistory } from "@docusaurus/router"
+import useDocusaurusContext from "@docusaurus/useDocusaurusContext"
+import "./styles.css"
+
+let loaded = false
 const Search = (props) => {
-  const initialized = useRef(false);
-  const searchBarRef = useRef(null);
-  const history = useHistory();
-  const { siteConfig = {} } = useDocusaurusContext();
-  const { baseUrl } = siteConfig;
+  const initialized = useRef(false)
+  const searchBarRef = useRef(null)
+  const history = useHistory()
+  const { siteConfig = {} } = useDocusaurusContext()
+  const { baseUrl } = siteConfig
   const initAlgolia = () => {
     if (!initialized.current) {
       new window.DocSearch({
@@ -25,25 +27,25 @@ const Search = (props) => {
         // Override algolia's default selection event, allowing us to do client-side
         // navigation and avoiding a full page refresh.
         handleSelected: (_input, _event, suggestion) => {
-          const url = baseUrl + suggestion.url;
+          const url = baseUrl + suggestion.url
           // Use an anchor tag to parse the absolute url into a relative url
           // Alternatively, we can use new URL(suggestion.url) but its not supported in IE
-          const a = document.createElement("a");
-          a.href = url;
+          const a = document.createElement("a")
+          a.href = url
           // Algolia use closest parent element id #__docusaurus when a h1 page title does not have an id
           // So, we can safely remove it. See https://github.com/facebook/docusaurus/issues/1828 for more details.
 
-          history.push(url);
+          history.push(url)
         },
-      });
-      initialized.current = true;
+      })
+      initialized.current = true
     }
-  };
+  }
 
   const getSearchData = () =>
     process.env.NODE_ENV === "production"
       ? fetch(`${baseUrl}search-doc.json`).then((content) => content.json())
-      : Promise.resolve([]);
+      : Promise.resolve([])
 
   const loadAlgolia = () => {
     if (!loaded) {
@@ -52,26 +54,38 @@ const Search = (props) => {
         import("./lib/DocSearch"),
         import("./algolia.css"),
       ]).then(([searchData, { default: DocSearch }]) => {
-        loaded = true;
-        window.searchData = searchData;
-        window.DocSearch = DocSearch;
-        initAlgolia();
-      });
+        loaded = true
+        window.searchData = searchData
+        window.DocSearch = DocSearch
+        initAlgolia()
+      })
     } else {
-      initAlgolia();
+      initAlgolia()
     }
-  };
+  }
 
   const toggleSearchIconClick = useCallback(
     (e) => {
       if (!searchBarRef.current.contains(e.target)) {
-        searchBarRef.current.focus();
+        searchBarRef.current.focus()
       }
 
-      props.handleSearchBarToggle(!props.isSearchBarExpanded);
+      props.handleSearchBarToggle &&
+        props.handleSearchBarToggle(!props.isSearchBarExpanded)
     },
     [props.isSearchBarExpanded]
-  );
+  )
+
+  useEffect(() => {
+    document.addEventListener("keypress", (e) => {
+      if (document.activeElement === searchBarRef.current) return
+      if (e.key === "/") {
+        e.preventDefault()
+        searchBarRef.current.focus()
+      }
+    })
+    return () => document.removeEventListener("keypress")
+  }, [])
 
   return (
     <div className="navbar__search" key="search-box">
@@ -101,8 +115,27 @@ const Search = (props) => {
         onBlur={toggleSearchIconClick}
         ref={searchBarRef}
       />
-    </div>
-  );
-};
 
-export default Search;
+      <svg
+        x="0px"
+        y="0px"
+        width="19px"
+        height="20px"
+        viewBox="0 0 19 20"
+        className={classnames("search-icon-keyboard", {
+          "search-icon-hidden": props.isSearchBarExpanded,
+        })}
+      >
+        <path
+          fill="none"
+          stroke="#979A9C"
+          opacity="0.4"
+          d="M3.5,0.5h12c1.7,0,3,1.3,3,3v13c0,1.7-1.3,3-3,3h-12c-1.7,0-3-1.3-3-3v-13C0.5,1.8,1.8,0.5,3.5,0.5z"
+        />
+        <path fill="#979a9c" d="M11.8,6L8,15.1H7.1L10.8,6L11.8,6z" />
+      </svg>
+    </div>
+  )
+}
+
+export default Search
--- www/src/theme/SearchBar/lib/templates.js
+++ www/src/theme/SearchBar/lib/templates.js
@@ -1,4 +1,4 @@
-const prefix = 'algolia-docsearch'
+const prefix = "algolia-docsearch"
 const suggestionPrefix = `${prefix}-suggestion`
 const footerPrefix = `${prefix}-footer`
 
@@ -89,14 +89,10 @@ const templates = {
   <form novalidate="novalidate" onsubmit="return false;" class="searchbox">
     <div role="search" class="searchbox__wrapper">
       <input id="docsearch" type="search" name="search" placeholder="Search the docs" autocomplete="off" required="required" class="searchbox__input"/>
+
       <button type="submit" title="Submit your search query." class="searchbox__submit" >
         <svg width=12 height=12 role="img" aria-label="Search">
           <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sbx-icon-search-13"></use>
-        </svg>
-      </button>
-      <button type="reset" title="Clear the search query." class="searchbox__reset hide">
-        <svg width=12 height=12 role="img" aria-label="Reset">
-          <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#sbx-icon-clear-3"></use>
         </svg>
       </button>
     </div>
@@ -104,11 +100,11 @@ const templates = {
 
 <div class="svg-icons" style="height: 0; width: 0; position: absolute; visibility: hidden">
   <svg xmlns="http://www.w3.org/2000/svg">
-    <symbol id="sbx-icon-clear-3" viewBox="0 0 40 40"><path d="M16.228 20L1.886 5.657 0 3.772 3.772 0l1.885 1.886L20 16.228 34.343 1.886 36.228 0 40 3.772l-1.886 1.885L23.772 20l14.342 14.343L40 36.228 36.228 40l-1.885-1.886L20 23.772 5.657 38.114 3.772 40 0 36.228l1.886-1.885L16.228 20z" fill-rule="evenodd"></symbol>
+    <symbol id="sbx-icon-clear-3" viewBox="0 0 40 40"></symbol>
     <symbol id="sbx-icon-search-13" viewBox="0 0 40 40"><path d="M26.806 29.012a16.312 16.312 0 0 1-10.427 3.746C7.332 32.758 0 25.425 0 16.378 0 7.334 7.333 0 16.38 0c9.045 0 16.378 7.333 16.378 16.38 0 3.96-1.406 7.593-3.746 10.426L39.547 37.34c.607.608.61 1.59-.004 2.203a1.56 1.56 0 0 1-2.202.004L26.807 29.012zm-10.427.627c7.322 0 13.26-5.938 13.26-13.26 0-7.324-5.938-13.26-13.26-13.26-7.324 0-13.26 5.936-13.26 13.26 0 7.322 5.936 13.26 13.26 13.26z" fill-rule="evenodd"></symbol>
   </svg>
 </div>
-  `
+  `,
 }
 
 export default templates
