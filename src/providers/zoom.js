0c1e2f8af80c62b5449c3de4ddc21c9f02aa8bfb
Yi-Ru Lin <deduce@gmail.com>
2021-06-03 03:49:42 +0800

    remove the default protection setting of Zoom for now

--- src/providers/zoom.js
+++ src/providers/zoom.js
@@ -6,8 +6,7 @@ export default function Zoom(options) {
     version: "2.0",
     params: { grant_type: "authorization_code" },
     accessTokenUrl: "https://zoom.us/oauth/token",
-    authorizationUrl:
-      "https://zoom.us/oauth/authorize?response_type=code",
+    authorizationUrl: "https://zoom.us/oauth/authorize?response_type=code",
     profileUrl: "https://api.zoom.us/v2/users/me",
     profile(profile) {
       return {
@@ -16,7 +15,6 @@ export default function Zoom(options) {
         email: profile.email,
       }
     },
-    protection: ["pkce", "state"],
     ...options,
   }
 }
