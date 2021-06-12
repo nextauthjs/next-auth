c07fe1b9a719df4cd62c5586da0973d3f9163475
Nico Domino <yo@ndo.dev>
2021-06-12 17:04:32 +0200

    docs: add versioned docs for unreleased v4.0.0-next.* (#2167)
    * docs: add versioned docs for unreleased canary 4.x
    
    * docs(fix): prettier broke custom image component without .mdx extension
    
    * docs(feat): automatically fetch latest stable version label
    
    * docs: cleanup script
    
    * docs: cleanup next version name
    
    * docs(chore): cleanup version leftovers
    
    * docs: fix .gitignore
    
    * docs: v4 default
    
    * docs: remove auto version generation
    
    * docs: fix missing import
    
    * docs: rm node-fetch

--- www/docusaurus.config.js
+++ www/docusaurus.config.js
@@ -42,6 +42,17 @@ module.exports = {
           position: "left",
         },
         {
+          type: "docsVersionDropdown",
+          position: "right",
+          dropdownActiveClassDisabled: true,
+          dropdownItemsAfter: [
+            {
+              to: "https://github.com/nextauthjs/next-auth/releases",
+              label: "All Releases",
+            },
+          ],
+        },
+        {
           href: "https://www.npmjs.com/package/next-auth",
           label: "npm",
           position: "right",
@@ -58,6 +69,7 @@ module.exports = {
     //   content: 'NextAuth.js now has automatic 🤖 releases 🎉! Check out the <a href="https://next-auth-git-canary.nextauthjs.vercel.app">Canary documentation 📚</a>',
     //   backgroundColor: '#2DB2F9',
     //   textColor: '#fff'
+    //   isCloseable: true,
     // },
     footer: {
       links: [
@@ -118,6 +130,13 @@ module.exports = {
       ],
       copyright: "NextAuth.js &copy; Iain Collins 2021",
     },
+    colorMode: {
+      respectPrefersColorScheme: true,
+      switchConfig: {
+        darkIcon: "🛰️",
+        lightIcon: "💡",
+      },
+    },
   },
   presets: [
     [
@@ -127,6 +146,17 @@ module.exports = {
           routeBasePath: "/",
           sidebarPath: require.resolve("./sidebars.js"),
           editUrl: "https://github.com/nextauthjs/next-auth/edit/main/www",
+          lastVersion: "current",
+          showLastUpdateAuthor: true,
+          showLastUpdateTime: true,
+          versions: {
+            current: {
+              label: "v4",
+            },
+            v3: {
+              label: "v3",
+            },
+          },
         },
         theme: {
           customCss: require.resolve("./src/css/index.css"),
--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -73,7 +73,7 @@ function Feature({ imageUrl, title, description }) {
         </div>
       )}
       <h3 className="text--center">{title}</h3>
-      <p>{description}</p>
+      <div>{description}</div>
     </div>
   )
 }
