4d3a510c44557abd0ab03977a7759f2eaa4d850f
ndom91 <yo@ndo.dev>
2021-06-26 01:53:01 +0200

    docs: cleanup css

--- www/src/pages/index.js
+++ www/src/pages/index.js
@@ -215,7 +215,7 @@ function Home() {
               </div>
             </div>
           </section>
-          <div className="home-subtitle">
+          <div className={styles.homeSubtitle}>
             <p>NextAuth.js is an open source community project.</p>
           </div>
         </main>
