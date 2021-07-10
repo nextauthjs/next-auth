5af786823ae22a04887ea415fbef8e870f57ace9
Balázs Orbán <info@balazsorban.com>
2021-07-11 00:56:41 +0200

    chore: mention OIDC client config discovery

--- src/server/lib/oauth/client.js
+++ src/server/lib/oauth/client.js
@@ -1,6 +1,10 @@
 import { Issuer } from "openid-client"
 
 /**
+ * NOTE: We can add auto discovery of the provider's endpoint
+ * that requires only one endpoint to be specified by the user.
+ * Check out `Issuer.discover`
+ *
  * Client supporting OAuth 2.x and OIDC
  * @param {import("types/internals").AppOptions} options
  */
