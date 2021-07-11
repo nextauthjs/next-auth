7dead4fbd151a608dac9511d17993c0616518423
Balázs Orbán <info@balazsorban.com>
2021-07-11 02:32:14 +0200

    chore: simplify/type signin route

--- src/server/routes/signin.js
+++ src/server/routes/signin.js
@@ -8,14 +8,16 @@ import adapterErrorHandler from "../../adapters/error-handler"
  * @param {import("types/internals").NextAuthResponse} res
  */
 export default async function signin(req, res) {
-  const { provider, baseUrl, basePath, adapter, callbacks, logger } =
-    req.options
+  const { baseUrl, basePath, adapter, callbacks, logger } = req.options
+
+  /** @type {import("types/providers").OAuthConfig | import("types/providers").EmailConfig} */
+  const provider = req.options.provider
 
   if (!provider.type) {
     return res.status(500).end(`Error: Type not specified for ${provider.name}`)
   }
 
-  if (provider.type === "oauth" && req.method === "POST") {
+  if (provider.type === "oauth") {
     try {
       const authorizationUrl = await getAuthorizationUrl(req, res)
       return res.redirect(authorizationUrl)
@@ -23,7 +25,7 @@ export default async function signin(req, res) {
       logger.error("SIGNIN_OAUTH_ERROR", error)
       return res.redirect(`${baseUrl}${basePath}/error?error=OAuthSignin`)
     }
-  } else if (provider.type === "email" && req.method === "POST") {
+  } else if (provider.type === "email") {
     if (!adapter) {
       logger.error("EMAIL_REQUIRES_ADAPTER_ERROR")
       return res.redirect(`${baseUrl}${basePath}/error?error=Configuration`)
