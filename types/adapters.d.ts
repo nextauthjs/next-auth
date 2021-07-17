b3f60ccfecb87fd5473240b2119405e9009cb76c
Balázs Orbán <info@balazsorban.com>
2021-07-18 00:31:01 +0200

    docs: update adapter method name to getUserByAccount

--- types/adapters.d.ts
+++ types/adapters.d.ts
@@ -31,7 +31,7 @@ export interface VerificationToken {
  * - `createUser`
  * - `getUser`
  * - `getUserByEmail`
- * - `getUserByProviderAccountId`
+ * - `getUserByAccount`
  * - `linkAccount`
  * - `createSession`
  * - `getSession`
