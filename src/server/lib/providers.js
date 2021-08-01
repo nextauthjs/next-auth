38142a2be5ad620eae565ae40c298f214582118f
Balázs Orbán <info@balazsorban.com>
2021-08-01 14:42:19 +0200

    feat: add defaultProfile callback

--- src/server/lib/providers.js
+++ src/server/lib/providers.js
@@ -1,23 +1,26 @@
 /**
  * Adds `signinUrl` and `callbackUrl` to each provider.
  * @param {{
- *  providers: import("..").Provider[]
+ *  providers: import("types/providers").Provider[]
  *  baseUrl: string
  *  basePath: string
  * }} options
- * @returns {import("..").NextAuthInternalOptions["provider"][]}
+ * @returns {import("types/internals").InternalOptions["providers"]}
  */
-export default function parseProviders ({ providers = [], baseUrl, basePath }) {
+export default function parseProviders({ providers = [], baseUrl, basePath }) {
   return providers.map((provider) => ({
     ...provider,
     signinUrl: `${baseUrl}${basePath}/signin/${provider.id}`,
     callbackUrl: `${baseUrl}${basePath}/callback/${provider.id}`,
-    async profile (...args) {
-      if (provider.profile) {
-        return provider.profile?.(...args)
-      }
-      // Default to profile that has a single id property
-      return { id: args[0].sub ?? args[0].id ?? 'id' }
-    }
+    profile: provider.profile ?? defaultProfile,
   }))
+}
+
+function defaultProfile(profile) {
+  return {
+    id: profile.sub ?? profile.id,
+    name: profile.name ?? null,
+    email: profile.email ?? null,
+    image: profile.image ?? null,
+  }
 }
