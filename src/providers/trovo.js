468b325bd93a0ed1d993526cd50add421bac070f
Leonardo Chimborazo <56430392+IAmNotTheLeo@users.noreply.github.com>
2021-08-17 16:56:55 +0100

    feat(providers): add Trovo

---
+++ src/providers/trovo.js
@@ -0,0 +1,22 @@
+export default function Trovo(options) {
+  return {
+    id: "trovo",
+    name: "Trovo",
+    type: "oauth",
+    version: "2.0",
+    scope: "user_details_self",
+    params: { grant_type: "authorization_code" },
+    accessTokenUrl: "https://${options.domain}/access_token",
+    authorizationUrl: "https://open.trovo.live/page/login.html",
+    getUserInfo: "https://open-api.trovo.live/openplatform/getuserinfo",
+    profile(profile) {
+      return {
+      id: profile.userID,
+      name: profile.userName,
+      email: profile.email,
+      image: profile.profilePic,
+      }
+    },
+    ...options,
+  }
+}
