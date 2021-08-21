a1b0c170cf4aa8bf0dc637d5c3b7965cf76f2a32
nathanclevenger <nateclev@gmail.com>
2021-08-21 12:44:36 -0500

    Changed default Github scope to read-only
    When using the default settings of the Github provider, with the "user" scope, it grants read/write access to profile info only.  By changing to "user:email" it will only request read-only access https://docs.github.com/en/developers/apps/building-oauth-apps/scopes-for-oauth-apps

--- src/providers/github.js
+++ src/providers/github.js
@@ -4,7 +4,7 @@ export default function GitHub(options) {
     name: "GitHub",
     type: "oauth",
     version: "2.0",
-    scope: "user",
+    scope: "user:email",
     accessTokenUrl: "https://github.com/login/oauth/access_token",
     authorizationUrl: "https://github.com/login/oauth/authorize",
     profileUrl: "https://api.github.com/user",
