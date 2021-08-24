66048ebfe7d21f917f5adea6230192589f2a1b05
Balázs Orbán <info@balazsorban.com>
2021-08-25 01:24:15 +0200

    chore: compile differently for client/server

--- config/babel.config.js
+++ config/babel.config.js
@@ -3,27 +3,33 @@
 // https://nextjs.org/docs/basic-features/supported-browsers-features
 
 module.exports = {
-  presets: [["@babel/preset-env", { targets: { node: "12" } }]],
+  presets: ["@babel/preset-typescript"],
   plugins: [
     "@babel/plugin-proposal-optional-catch-binding",
     "@babel/plugin-transform-runtime",
   ],
-  comments: false,
+  ignore: ["../src/**/__tests__/**", "../src/adapters.ts"],
   overrides: [
     {
-      test: ["../src/client/**"],
+      test: ["../src/react.tsx"],
       presets: [
         ["@babel/preset-env", { targets: { ie: "11" } }],
         ["@babel/preset-react", { runtime: "automatic" }],
       ],
     },
     {
-      test: ["../src/server/pages/**"],
+      test: ["../src/server/pages/*.tsx"],
       presets: ["preact"],
-    },
-    {
-      test: ["../src/**/*.test.js"],
-      presets: [["@babel/preset-react", { runtime: "automatic" }]],
+      plugins: [
+        [
+          "jsx-pragmatic",
+          {
+            module: "preact",
+            export: "h",
+            import: "h",
+          },
+        ],
+      ],
     },
   ],
 }
