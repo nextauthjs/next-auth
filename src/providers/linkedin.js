afa71ffcccc8470104349e33c9d59bdf0addd515
Balázs Orbán <info@balazsorban.com>
2021-09-23 01:46:10 +0200

    fix(providers): migrate LinkedIn provider to v4

--- src/providers/linkedin.js
+++ src/providers/linkedin.js
@@ -4,17 +4,30 @@ export default function LinkedIn(options) {
     id: "linkedin",
     name: "LinkedIn",
     type: "oauth",
-    authorization:
-      "https://www.linkedin.com/oauth/v2/authorization?scope=r_liteprofile",
+    authorization: {
+      url: "https://www.linkedin.com/oauth/v2/authorization",
+      params: { scope: "r_liteprofile r_emailaddress" },
+    },
     token: "https://www.linkedin.com/oauth/v2/accessToken",
-    userinfo:
-      "https://api.linkedin.com/v2/me?projection=(id,localizedFirstName,localizedLastName)",
-    profile(profile) {
+    userinfo: {
+      url: "https://api.linkedin.com/v2/me",
+      params: {
+        projection: `(id,localizedFirstName,localizedLastName,profilePicture(displayImage~digitalmediaAsset:playableStreams))`,
+      },
+    },
+    async profile(profile, tokens) {
+      const emailResponse = await fetch(
+        "https://api.linkedin.com/v2/emailAddress?q=members&projection=(elements*(handle~))",
+        { headers: { Authorization: `Bearer ${tokens.access_token}` } }
+      )
+      const emailData = await emailResponse.json()
       return {
         id: profile.id,
         name: `${profile.localizedFirstName} ${profile.localizedLastName}`,
-        email: null,
-        image: null,
+        email: emailData?.elements?.[0]?.["handle~"]?.emailAddress,
+        image:
+          profile.profilePicture?.["displayImage~"]?.elements?.[0]
+            ?.identifiers?.[0]?.identifier,
       }
     },
     options,
