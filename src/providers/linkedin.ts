2ff0d8ffb8eb74ae616a548e8c0069daab71905e
Balázs Orbán <info@balazsorban.com>
2021-09-24 00:35:03 +0200

    chore(ts): refactor LinkedIn provider to TS

--- src/providers/linkedin.ts
+++ src/providers/linkedin.ts
@@ -1,5 +1,27 @@
-/** @type {import(".").OAuthProvider} */
-export default function LinkedIn(options) {
+import { OAuthConfig, OAuthUserConfig } from "."
+
+interface Identifier {
+  identifier: string
+}
+
+interface Element {
+  identifiers?: Identifier[]
+}
+
+export interface LinkedInProfile {
+  id: string
+  localizedFirstName: string
+  localizedLastName: string
+  profilePicture: {
+    "displayImage~": {
+      elements?: Element[]
+    }
+  }
+}
+
+export default function LinkedIn<
+  P extends Record<string, any> = LinkedInProfile
+>(options: OAuthUserConfig<P>): OAuthConfig<P> {
   return {
     id: "linkedin",
     name: "LinkedIn",
