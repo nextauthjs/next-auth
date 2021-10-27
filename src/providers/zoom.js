3eeca551815d01a13beda6e0db935aee4f557bc8
dijs <richard.vanderdys@gmail.com>
2021-10-28 00:23:05 +0300

    Added support for zoom in beta

--- src/providers/zoom.js
+++ src/providers/zoom.js
@@ -1,19 +1,24 @@
 /** @type {import(".").OAuthProvider} */
 export default function Zoom(options) {
   return {
-    id: "zoom",
-    name: "Zoom",
-    type: "oauth",
-    authorization: "https://zoom.us/oauth/authorize",
-    token: "https://zoom.us/oauth/token",
-    userinfo: "https://api.zoom.us/v2/users/me",
+    id: 'zoom',
+    name: 'Zoom',
+    type: 'oauth',
+    authorization: {
+      url: 'https://zoom.us/oauth/authorize',
+      params: { scope: '' },
+    },
+    token: 'https://zoom.us/oauth/token',
+    userinfo: {
+      url: 'https://api.zoom.us/v2/users/me',
+    },
     profile(profile) {
       return {
         id: profile.id,
         name: `${profile.first_name} ${profile.last_name}`,
         email: profile.email,
-        image: null,
-      }
+        image: profile.pic_url,
+      };
     },
     options,
   }
