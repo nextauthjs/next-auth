3d233e0978a0c18ec55456e217d1a3e7d1633dcb
dijs <richard.vanderdys@gmail.com>
2021-10-28 08:39:21 +0300

    Converted to typescript

--- src/providers/zoom.ts
+++ src/providers/zoom.ts
@@ -1,13 +1,20 @@
-/** @type {import(".").OAuthProvider} */
-export default function Zoom(options) {
+import { OAuthConfig, OAuthUserConfig } from "./oauth"
+
+export interface ZoomProfile {
+  id: string
+  name: string
+  email: string
+  image: string
+}
+
+export default function Auth0<P extends Record<string, any> = ZoomProfile>(
+  options: OAuthUserConfig<P>
+): OAuthConfig<P> {
   return {
     id: 'zoom',
     name: 'Zoom',
     type: 'oauth',
-    authorization: {
-      url: 'https://zoom.us/oauth/authorize',
-      params: { scope: '' },
-    },
+    authorization: 'https://zoom.us/oauth/authorize?scope',
     token: 'https://zoom.us/oauth/token',
     userinfo: {
       url: 'https://api.zoom.us/v2/users/me',
