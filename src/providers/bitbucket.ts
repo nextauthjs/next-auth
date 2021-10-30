0fca8455ca585cdd0f1923d3ff5f4d499c7e9ef7
Arthur Falcão <arthurjvfalcao@gmail.com>
2021-10-31 00:29:57 +0100

    feat(providers): add Bitbucket

---
+++ src/providers/bitbucket.ts
@@ -0,0 +1,40 @@
+import { OAuthConfig, OAuthUserConfig } from "./oauth"
+
+interface BitbucketProfile {
+  username: string
+  account_id: string
+  display_name: string
+  links: {
+    avatar: {
+      href: string
+    }
+  }
+}
+
+export function Bitbucket<P extends Record<string, any> = BitbucketProfile>(
+  options: OAuthUserConfig<P>
+): OAuthConfig<P> {
+  return {
+    id: "bitbucket",
+    name: "Bitbucket",
+    type: "oauth",
+    authorization: {
+      url: "https://bitbucket.org/site/oauth2/authorize",
+      params: {
+        scope: ""
+      }
+    },
+    token: "https://bitbucket.org/site/oauth2/access_token",
+    userinfo: "https://api.bitbucket.org/2.0/user",
+    profile(profile) {
+      return {
+        id: profile.account_id,
+        email: null,
+        image: profile.links.avatar.href,
+        name: profile.display_name
+      }
+    },
+    checks: ["none"],
+    options,
+  }
+}
