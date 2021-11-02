8209380072656a34f055a11490c9a8160ad261a4
Estevan Jantsk <estevan.jantsk@gmail.com>
2021-11-02 16:56:59 +0200

    remove others interface

--- src/providers/pipedrive.ts
+++ src/providers/pipedrive.ts
@@ -2,41 +2,37 @@ import { OAuthConfig, OAuthUserConfig } from "./oauth"
 
 export interface PipedriveProfile {
   success: boolean;
-  data:    Data;
-}
-
-interface Data {
-  id:                     number;
-  name:                   string;
-  default_currency?:      string;
-  locale?:                string;
-  lang?:                  number;
-  email:                  string;
-  phone?:                 string;
-  activated?:             boolean;
-  last_login?:            Date;
-  created?:               Date;
-  modified?:              Date;
-  signup_flow_variation?: string;
-  has_created_company?:   boolean;
-  is_admin?:              number;
-  active_flag?:           boolean;
-  timezone_name?:         string;
-  timezone_offset?:       string;
-  role_id?:               number;
-  icon_url?:              string;
-  is_you?:                boolean;
-  company_id?:            number;
-  company_name?:          string;
-  company_domain?:        string;
-  company_country?:       string;
-  company_industry?:      string;
-  language?:              Language;
-}
-
-interface Language {
-  language_code?: string;
-  country_code?:  string;
+  data:    {
+    id:                     number;
+    name:                   string;
+    default_currency?:      string;
+    locale?:                string;
+    lang?:                  number;
+    email:                  string;
+    phone?:                 string;
+    activated?:             boolean;
+    last_login?:            Date;
+    created?:               Date;
+    modified?:              Date;
+    signup_flow_variation?: string;
+    has_created_company?:   boolean;
+    is_admin?:              number;
+    active_flag?:           boolean;
+    timezone_name?:         string;
+    timezone_offset?:       string;
+    role_id?:               number;
+    icon_url?:              string;
+    is_you?:                boolean;
+    company_id?:            number;
+    company_name?:          string;
+    company_domain?:        string;
+    company_country?:       string;
+    company_industry?:      string;
+    language?:              {
+      language_code?: string;
+      country_code?:  string;
+    };
+  };
 }
 
 export default function Pipedrive<P extends Record<string, any> = PipedriveProfile>(
