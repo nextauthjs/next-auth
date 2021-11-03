244409dfb91def706b993c4494ade6532ee387bd
dijs <richard.vanderdys@gmail.com>
2021-11-03 15:14:11 +0200

    Now reflects response from Zoom

--- src/providers/zoom.ts
+++ src/providers/zoom.ts
@@ -1,10 +1,32 @@
 import { OAuthConfig, OAuthUserConfig } from "./oauth"
 
 export interface ZoomProfile {
-  id: string
-  name: string
-  email: string
-  image: string
+  id: string,
+  first_name: string,
+  last_name: string,
+  email: string,
+  type: number,
+  role_name: string,
+  pmi: number,
+  use_pmi: boolean,
+  vanity_url: string,
+  personal_meeting_url: string,
+  timezone: string,
+  verified: number,
+  dept: string,
+  created_at: string,
+  last_login_time: string,
+  last_client_version: string,
+  pic_url: string,
+  host_key: string,
+  jid: string,
+  group_ids: string[],
+  im_group_ids: string[],
+  account_id: string,
+  language: string,
+  phone_country: string,
+  phone_number: string,
+  status: string
 }
 
 export default function Zoom<P extends Record<string, any> = ZoomProfile>(
