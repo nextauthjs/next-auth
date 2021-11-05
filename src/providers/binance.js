f99714cd3351c22eadd225b673c3b19186a40ace
badxc <35627923+CollectivelyCode@users.noreply.github.com>
2021-11-05 12:21:37 -0500

    Create binance.js

---
+++ src/providers/binance.js
@@ -0,0 +1,24 @@
+export default function Binance(options){
+    return {
+        id: "binance",
+        name: "Binance",
+        type: "oauth",
+        version: "2.0",
+        scope: "user email",
+        params: { grant_type: "authorization_code" },
+        accessTokenUrl: "https://accounts.binance.com/oauth/token",
+        authorizationUrl:
+            "https://accounts.binance.com/en/oauth/authorize?response_type=code",
+        profileUrl: "https://accounts.binance.com/oauth-api/user-info",
+        idToken: true,
+        clientId: null,
+        clientSecret: null,
+        profile(profile) {
+            return {
+                id: profile.data.id,
+                email: profile.data.email
+            }
+        },
+        ...options,
+    }
+}
