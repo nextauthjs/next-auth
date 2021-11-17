ae6afe423589747ffbd6fb44e48f315909b8dfff
Lluis Agusti <hi@llu.lu>
2021-11-17 08:43:49 +0100

    refactor(pipedrive): run prettier

--- src/providers/pipedrive.ts
+++ src/providers/pipedrive.ts
@@ -1,43 +1,43 @@
 import { OAuthConfig, OAuthUserConfig } from "./oauth"
 
 export interface PipedriveProfile {
-  success: boolean;
-  data:    {
-    id:                     number;
-    name:                   string;
-    default_currency?:      string;
-    locale?:                string;
-    lang?:                  number;
-    email:                  string;
-    phone?:                 string;
-    activated?:             boolean;
-    last_login?:            Date;
-    created?:               Date;
-    modified?:              Date;
-    signup_flow_variation?: string;
-    has_created_company?:   boolean;
-    is_admin?:              number;
-    active_flag?:           boolean;
-    timezone_name?:         string;
-    timezone_offset?:       string;
-    role_id?:               number;
-    icon_url?:              string;
-    is_you?:                boolean;
-    company_id?:            number;
-    company_name?:          string;
-    company_domain?:        string;
-    company_country?:       string;
-    company_industry?:      string;
-    language?:              {
-      language_code?: string;
-      country_code?:  string;
-    };
-  };
+  success: boolean
+  data: {
+    id: number
+    name: string
+    default_currency?: string
+    locale?: string
+    lang?: number
+    email: string
+    phone?: string
+    activated?: boolean
+    last_login?: Date
+    created?: Date
+    modified?: Date
+    signup_flow_variation?: string
+    has_created_company?: boolean
+    is_admin?: number
+    active_flag?: boolean
+    timezone_name?: string
+    timezone_offset?: string
+    role_id?: number
+    icon_url?: string
+    is_you?: boolean
+    company_id?: number
+    company_name?: string
+    company_domain?: string
+    company_country?: string
+    company_industry?: string
+    language?: {
+      language_code?: string
+      country_code?: string
+    }
+  }
 }
 
-export default function Pipedrive<P extends Record<string, any> = PipedriveProfile>(
-  options: OAuthUserConfig<P>
-): OAuthConfig<P> {
+export default function Pipedrive<
+  P extends Record<string, any> = PipedriveProfile
+>(options: OAuthUserConfig<P>): OAuthConfig<P> {
   return {
     id: "pipedrive",
     name: "Pipedrive",
@@ -54,6 +54,6 @@ export default function Pipedrive<P extends Record<string, any> = PipedriveProfi
         image: profile.icon_url,
       }
     },
-    options
+    options,
   }
 }
