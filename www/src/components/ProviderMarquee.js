26d28b6c653a017c8afc8d765d2b2c09f20d8b29
Lilia <lilia.manukyan@hotmail.com>
2021-11-25 21:29:53 +0400

    fix formatting

--- www/src/components/ProviderMarquee.js
+++ www/src/components/ProviderMarquee.js
@@ -4,21 +4,21 @@ import * as S from './ProviderMarqueeStyle'
 import times from 'lodash.times'
 
 const icons = [
-  "/img/providers/apple-black.svg",
-  "/img/providers/auth0.svg",
-  "/img/providers/aws-cognito.svg",
-  "/img/providers/battle.net.svg",
-  "/img/providers/box.svg",
-  "/img/providers/facebook-2.svg",
-  "/img/providers/github-1.svg",
-  "/img/providers/gitlab.svg",
-  "/img/providers/google-icon.svg",
-  "/img/providers/okta-3.svg",
-  "/img/providers/openid.svg",
-  "/img/providers/slack.svg",
-  "/img/providers/soundcloud.svg",
-  "/img/providers/spotify.svg",
-  "/img/providers/twitter.svg",
+  '/img/providers/apple-black.svg',
+  '/img/providers/auth0.svg',
+  '/img/providers/aws-cognito.svg',
+  '/img/providers/battle.net.svg',
+  '/img/providers/box.svg',
+  '/img/providers/facebook-2.svg',
+  '/img/providers/github-1.svg',
+  '/img/providers/gitlab.svg',
+  '/img/providers/google-icon.svg',
+  '/img/providers/okta-3.svg',
+  '/img/providers/openid.svg',
+  '/img/providers/slack.svg',
+  '/img/providers/soundcloud.svg',
+  '/img/providers/spotify.svg',
+  '/img/providers/twitter.svg',
 ]
 
 const ProviderMarquee = React.memo(({ size }) => {
