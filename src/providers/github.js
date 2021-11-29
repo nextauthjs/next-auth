d1d0db43eaa50d62c10350a6ea62ca4554404ec5
Nisala Kalupahana <avishkank@gmail.com>
2021-11-29 16:58:49 -0600

    feat(providers): ensure GitHub provider always gives an email (#3302)
    * Ensure that GitHub provider always gives an email
    
    * Update src/providers/github.js
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- src/providers/github.js
+++ src/providers/github.js
@@ -6,7 +6,31 @@ export default function GitHub(options) {
     type: "oauth",
     authorization: "https://github.com/login/oauth/authorize?scope=read:user+user:email",
     token: "https://github.com/login/oauth/access_token",
-    userinfo: "https://api.github.com/user",
+    userinfo: {
+      url: "https://api.github.com/user",
+      async request({ client, tokens }) {
+        // Get base profile
+        const profile = await client.userinfo(tokens)
+
+        // If user has email hidden, get their primary email from the GitHub API
+        if (!profile.email) {
+          const emails = await (
+            await fetch("https://api.github.com/user/emails", {
+              headers: { Authorization: `token ${tokens.access_token}` },
+            })
+          ).json()
+
+          if (emails?.length > 0) {
+            // Get primary email
+            profile.email = emails.find(email => email.primary)?.email;
+            // And if for some reason it doesn't exist, just use the first
+            if (!profile.email) profile.email = emails[0].email;
+          }
+        }
+
+        return profile
+      },
+    },
     profile(profile) {
       return {
         id: profile.id.toString(),
