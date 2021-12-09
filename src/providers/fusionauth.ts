8b9a109255812665c283be12734740a5b004479f
Alessandro Cuppari <me@alessandrojcm.dev>
2021-12-09 20:48:01 +0000

    fix(providers): refactor FusionAuth to v4 (#3376)
    * feat: updated fusionauth provider
    
    * Updated fusionauth profile interface docstring
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    * Refactored openid well know logic
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>
    
    * Removed jwks endpoint property
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

---
+++ src/providers/fusionauth.ts
@@ -0,0 +1,52 @@
+import { OAuthConfig, OAuthUserConfig } from "./oauth"
+
+/** This is the default openid signature returned from FusionAuth
+ * it can be customized using [lambda functions](https://fusionauth.io/docs/v1/tech/lambdas)
+ */
+export interface FusionAuthProfile {
+  aud: string
+  exp: number
+  iat: number
+  iss: string
+  sub: string
+  jti: string
+  authenticationType: string
+  email: string
+  email_verified: boolean
+  preferred_username: string
+  at_hash: string
+  c_hash: string
+  scope: string
+  sid: string
+}
+
+export default function FusionAuth<
+  P extends Record<string, any> = FusionAuthProfile
+>(
+  // tenantId only needed if there is more than one tenant configured on the server
+  options: OAuthUserConfig<P> & { tenantId?: string }
+): OAuthConfig<P> {
+  return {
+    id: "fusionauth",
+    name: "FusionAuth",
+    type: "oauth",
+    wellKnown: options?.tenantId
+      ? `${options.issuer}/.well-known/openid-configuration?tenantId=${options.tenantId}`
+      : `${options.issuer}/.well-known/openid-configuration`,
+    authorization: {
+      params: {
+        scope: "openid offline_access",
+        ...(options?.tenantId && { tenantId: options.tenantId }),
+      },
+    },
+    checks: ["pkce", "state"],
+    profile(profile) {
+      return {
+        id: profile.sub,
+        email: profile.email,
+        name: profile?.preferred_username,
+      }
+    },
+    options,
+  }
+}
