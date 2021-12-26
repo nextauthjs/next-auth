e6bd97347f19ba5bf0651dbae0c52102ffd70eb7
krautwigundrüben <max@krautwigundrueben.de>
2021-12-26 16:13:07 +0100

    Update strava.js
    Auth with Strava was throwing errors before, this works.

--- src/providers/strava.js
+++ src/providers/strava.js
@@ -4,17 +4,54 @@ export default function Strava(options) {
     id: "strava",
     name: "Strava",
     type: "oauth",
-    authorization: "https://www.strava.com/api/v3/oauth/authorize?scope=read",
-    token: "https://www.strava.com/api/v3/oauth/token",
+    authorization: {
+      url: "https://www.strava.com/api/v3/oauth/authorize",
+      params: {
+        scope: "read",
+        approval_prompt: "auto",
+        response_type: "code",
+        redirect_uri: "http://localhost:3000/api/auth/callback/strava"
+      }
+    },
+    token: { 
+      url: "https://www.strava.com/api/v3/oauth/token",
+      async request(context) {
+        const response = await fetch(context.provider.token.url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            ...context.params,
+            client_id: process.env.STRAVA_CLIENT_ID,
+            client_secret: process.env.STRAVA_CLIENT_SECRET,
+            grant_type: "authorization_code",
+          }),
+        });
+
+        const { refresh_token, access_token, token_type, expires_at } = await response.json();
+
+        return {
+          tokens: {
+            refresh_token,
+            access_token,
+            token_type, 
+            expires_at,
+          },
+        };
+      },
+    },
     userinfo: "https://www.strava.com/api/v3/athlete",
+
     profile(profile) {
       return {
         id: profile.id,
         name: profile.firstname,
         email: null,
-        image: profile.profile,
-      }
+        image: profile.profile
+      };
     },
-    options,
-  }
+
+    options
+  };
 }
