a0654c22f154b5c364d2d6d571058b23c4ad3911
Balázs Orbán <info@balazsorban.com>
2022-01-10 11:31:27 +0100

    chore(providers): use nested interface for consistency

--- src/providers/kakao.ts
+++ src/providers/kakao.ts
@@ -1,79 +1,74 @@
 import type { OAuthConfig, OAuthUserConfig } from "."
 
+export type DateTime = string
+export type Gender = "female" | "male"
+export type AgeRange =
+  | "1-9"
+  | "10-14"
+  | "15-19"
+  | "20-29"
+  | "30-39"
+  | "40-49"
+  | "50-59"
+  | "60-69"
+  | "70-79"
+  | "80-89"
+  | "90-"
+
 /**
  * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api#req-user-info
  * type from : https://gist.github.com/ziponia/cdce1ebd88f979b2a6f3f53416b56a77
  */
-export type DateTime = string
-export type Gender = 'female' | 'male'
-export type AgeRange =
-  | '1-9'
-  | '10-14'
-  | '15-19'
-  | '20-29'
-  | '30-39'
-  | '40-49'
-  | '50-59'
-  | '60-69'
-  | '70-79'
-  | '80-89'
-  | '90-'
-
-export class KakaoUser {
+export interface KakaoProfile {
   id: number
   has_signed_up?: boolean
   connected_at?: DateTime
   synched_at?: DateTime
-  properties?: KakaoUserProperties
-  kakao_account?: KakaoUserAccount
+  properties?: {
+    id?: string
+    status?: string
+    registered_at?: DateTime
+    msg_blocked?: boolean
+    nickname?: string
+    profile_image?: string
+    thumbnail_image?: string
+  }
+  kakao_account?: {
+    profile_needs_agreement?: boolean
+    profile_nickname_needs_agreement?: boolean
+    profile_image_needs_agreement?: boolean
+    profile?: {
+      nickname?: string
+      thumbnail_image_url?: string
+      profile_image_url?: string
+      is_default_image?: boolean
+    }
+    name_needs_agreement?: boolean
+    name?: string
+    email_needs_agreement?: boolean
+    is_email_valid?: boolean
+    is_email_verified?: boolean
+    email?: string
+    age_range_needs_agreement?: boolean
+    age_range?: AgeRange
+    birthyear_needs_agreement?: boolean
+    birthyear?: string
+    birthday_needs_agreement?: boolean
+    birthday?: string
+    birthday_type?: string
+    gender_needs_agreement?: boolean
+    gender?: Gender
+    phone_number_needs_agreement?: boolean
+    phone_number?: string
+    ci_needs_agreement?: boolean
+    ci?: string
+    ci_authenticated_at?: DateTime
+  }
 }
 
-export class KakaoUserProperties {
-  id?: string
-  status?: string
-  registered_at?: DateTime
-  msg_blocked?: boolean
-  nickname?: string
-  profile_image?: string
-  thumbnail_image?: string
-}
-export class KakaoUserAccount {
-  profile_needs_agreement?: boolean
-  profile_nickname_needs_agreement?: boolean
-  profile_image_needs_agreement?: boolean
-  profile?: KakaoUserAccountProfile
-  name_needs_agreement?: boolean
-  name?: string
-  email_needs_agreement?: boolean
-  is_email_valid?: boolean
-  is_email_verified?: boolean
-  email?: string
-  age_range_needs_agreement?: boolean
-  age_range?: AgeRange
-  birthyear_needs_agreement?: boolean
-  birthyear?: string
-  birthday_needs_agreement?: boolean
-  birthday?: string
-  birthday_type?: string
-  gender_needs_agreement?: boolean
-  gender?: Gender
-  phone_number_needs_agreement?: boolean
-  phone_number?: string
-  ci_needs_agreement?: boolean
-  ci?: string
-  ci_authenticated_at?: DateTime
-}
-
-export class KakaoUserAccountProfile {
-  nickname?: string
-  thumbnail_image_url?: string
-  profile_image_url?: string
-  is_default_image?: boolean
-}
-
-export default function Kakao<P extends Record<string, any> = KakaoUser>(
+export default function Kakao<P extends Record<string, any> = KakaoProfile>(
   options: OAuthUserConfig<P>
-  ): OAuthConfig<P> {
+): OAuthConfig<P> {
   return {
     id: "kakao",
     name: "Kakao",
@@ -82,7 +77,7 @@ export default function Kakao<P extends Record<string, any> = KakaoUser>(
     token: "https://kauth.kakao.com/oauth/token",
     userinfo: "https://kapi.kakao.com/v2/user/me",
     client: {
-      token_endpoint_auth_method: 'client_secret_post'
+      token_endpoint_auth_method: "client_secret_post",
     },
     profile(profile) {
       return {
