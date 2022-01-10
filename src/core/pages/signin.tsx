82447f8e3ebc7004cf91121725b4f5970d2276d8
Thang Vu <31528554+ThangHuuVu@users.noreply.github.com>
2022-01-10 17:57:27 +0700

    fix: display inline errors when using custom error page. (#3576)

--- src/core/pages/signin.tsx
+++ src/core/pages/signin.tsx
@@ -48,7 +48,7 @@ export default function SigninPage(props: SignInServerPageParams) {
     Callback: "Try signing in with a different account.",
     OAuthAccountNotLinked:
       "To confirm your identity, sign in with the same account you used originally.",
-    EmailSignin: "Check your email inbox.",
+    EmailSignin: "The e-mail could not be sent.",
     CredentialsSignin:
       "Sign in failed. Check the details you provided are correct.",
     SessionRequired: "Please sign in to access this page.",
