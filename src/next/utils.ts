8ac3a9cf96cbb17f3b6bb26d182f17abced19d07
Balázs Orbán <info@balazsorban.com>
2022-01-17 00:01:58 +0100

    chore: use `process.env.VERCEL` to detect Vercel

--- src/next/utils.ts
+++ src/next/utils.ts
@@ -17,7 +17,7 @@ export function setCookie(res, cookie: Cookie) {
 /** Extract the host from the environment */
 export function detectHost(forwardedHost: any) {
   // If we detect a Vercel environment, we can trust the host
-  if (process.env.VERCEL === "1") return forwardedHost
+  if (process.env.VERCEL) return forwardedHost
   // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
   return process.env.NEXTAUTH_URL
 }
