34a4f2905b322143d963c5cd27f4d1e9bd43eed8
Jon Bellah <jbellah@gmail.com>
2022-01-15 19:29:30 -0700

    Update types for GoogleProfile interface

--- src/providers/google.ts
+++ src/providers/google.ts
@@ -1,10 +1,21 @@
 import type { OAuthConfig, OAuthUserConfig } from "."
 
 export interface GoogleProfile {
-  sub: string
-  name: string
+  aud: string
+  azp: string
   email: string
+  email_verified: boolean
+  exp: number
+  family_name: string
+  given_name: string
+  hd: string
+  iat: number
+  iss: string
+  jti: string
+  name: string
+  nbf: number
   picture: string
+  sub: string
 }
 
 export default function Google<P extends Record<string, any> = GoogleProfile>(
