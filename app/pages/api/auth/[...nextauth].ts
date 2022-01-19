49360afa557d2d754c069c41281483cd5ff8ff25
Yathindra <yathindrarawya123@gmail.com>
2022-01-19 22:12:01 +0530

    Import asgardeo provider in to providers

--- app/pages/api/auth/[...nextauth].ts
+++ app/pages/api/auth/[...nextauth].ts
@@ -28,6 +28,7 @@ import AzureB2C from "next-auth/providers/azure-ad-b2c"
 import OsuProvider from "next-auth/providers/osu"
 import AppleProvider from "next-auth/providers/apple"
 import PatreonProvider from "next-auth/providers/patreon"
+import AsgardeoProvider from "next-auth/providers/asgardeo"
 
 // import { PrismaAdapter } from "@next-auth/prisma-adapter"
 // import { PrismaClient } from "@prisma/client"
@@ -189,7 +190,14 @@ export const authOptions: NextAuthOptions = {
     PatreonProvider({
       clientId: process.env.PATREON_ID,
       clientSecret: process.env.PATREON_SECRET,
-    })
+    }),
+    AsgardeoProvider({
+      domain: process.env.ASGARDEO_DOMAIN,
+      tenantId: process.env.ASGARDEO_TENANT,
+      clientId: process.env.ASGARDEO_CLIENT_ID,
+      clientSecret: process.env.ASGARDEO_CLIENT_SECRET,
+      scopes: process.env.ASGARDEO_SCOPES,
+    }),
   ],
   secret: process.env.SECRET,
   debug: true,
