fd7d9b3b5a54d33f983c14bb0add10122fc3487b
nix <damianp96@gmail.com>
2022-01-26 23:17:38 +0100

    fix(providers): transforming FACEIT token response (#3753)
    Also:
    * fix profile name

--- src/providers/faceit.js
+++ src/providers/faceit.js
@@ -10,12 +10,33 @@ export default function FACEIT(options) {
         `${options.clientId}:${options.clientSecret}`
       ).toString("base64")}`,
     },
-    token: "https://api.faceit.com/auth/v1/oauth/token",
+    token: {
+      url: "https://api.faceit.com/auth/v1/oauth/token",
+      request: async params => {
+        const { token } = params.provider;
+        const { code } = params.params;
+        const endpointUrl = typeof token === 'string' ? token : token?.url;
+        if (!endpointUrl) {
+          throw new Error('Token url not provided');
+        }
+        const url = new URL(endpointUrl);
+        const queryParams = new URLSearchParams({ code, grant_type: 'authorization_code' });
+        url.search = `?${queryParams}`;
+        const apiResponse = await fetch(url.toString(), { method: 'POST', headers: params.provider.headers })
+          .then(res => res.json());
+
+        return {
+          tokens: {
+            access_token: apiResponse.access_token,
+          }
+        }
+      }
+    },
     userinfo: "https://api.faceit.com/auth/v1/resources/userinfo",
     profile(profile) {
       return {
         id: profile.guid,
-        name: profile.name,
+        name: profile.nickname,
         email: profile.email,
         image: profile.picture,
       }
