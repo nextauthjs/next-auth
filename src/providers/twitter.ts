5e42f37f07bb202dc20942f38a0c9985f528507a
Balázs Orbán <info@balazsorban.com>
2022-02-02 02:14:09 +0100

    fix(providers): properly warn when using Twitter OAuth 2

--- src/providers/twitter.ts
+++ src/providers/twitter.ts
@@ -170,11 +170,13 @@ let warned = false
 export default function Twitter<
   P extends Record<string, any> = TwitterLegacyProfile | TwitterProfile
 >(options: OAuthUserConfig<P>): OAuthConfig<P> {
-  if (!warned && options.version === "2.0") {
-    warned = true
-    console.warn(
-      "Opted-in to Twitter OAuth 2.0. See the docs https://next-auth.js.org/providers/twitter#oauth-2"
-    )
+  if (options.version === "2.0") {
+    if (!warned) {
+      warned = true
+      console.warn(
+        "Opted-in to Twitter OAuth 2.0. See the docs https://next-auth.js.org/providers/twitter#oauth-2"
+      )
+    }
     return {
       id: "twitter",
       name: "Twitter",
