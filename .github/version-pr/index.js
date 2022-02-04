97ebab37b75ec824230196f0b4dfe6c69bab7d33
Balázs Orbán <info@balazsorban.com>
2022-02-04 19:05:40 +0100

    remove unused dependency

--- .github/version-pr/index.js
+++ .github/version-pr/index.js
@@ -1,4 +1,4 @@
-const fs = require("fs-extra")
+const fs = require("fs")
 const path = require("path")
 const core = require("@actions/core")
 
@@ -9,7 +9,7 @@ try {
   )
   const packageJSON = JSON.parse(fs.readFileSync(packageJSONPath, "utf8"))
 
-  const sha8 = process.env.GITHUB_SHA.substr(0, 8)
+  const sha8 = process.env.GITHUB_SHA.substring(0, 8)
   const prNumber = process.env.PR_NUMBER
 
   const packageVersion = `0.0.0-pr.${prNumber}.${sha8}`
