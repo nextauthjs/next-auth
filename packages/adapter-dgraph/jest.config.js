c778d3774526b91e8e827b9db2adc87e4c8b056f
ndom91 <yo@ndo.dev>
2022-02-04 16:29:14 +0100

    fix: folder rename

---
+++ packages/adapter-dgraph/jest.config.js
@@ -0,0 +1,1 @@
+module.exports = require("../adapters-test/jest.config")
--- packages/adapter-test/jest.config.js
+++ packages/adapter-test/jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   modulePathIgnorePatterns: ["dist/", "node_modules/"],
+  rootDir: "packages/adapters-test",
+  roots: ["<rootDir>/../adapters-*"]
   // TODO: Set-up with Codecov https://about.codecov.io/
   // collectCoverage: true,
   // coverageReporters: ["json", "html"],
