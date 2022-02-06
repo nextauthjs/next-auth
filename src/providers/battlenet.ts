da56f7dfae608dcc0a049c51cc53f152b2ec03ea
Juan C. Yamacho H <jcyamacho@gmail.com>
2022-02-06 01:44:53 +0100

    feat: using issuer instead of region

--- src/providers/battlenet.ts
+++ src/providers/battlenet.ts
@@ -13,25 +13,16 @@ export interface BattleNetProfile {
 }
 
 export default function BattleNet<P extends Record<string, any> = BattleNetProfile>(
-  options: OAuthUserConfig<P> & {
-    /**
-     * Battle.net employs several region-based concepts to manage and deliver game data:
-     * https://develop.battle.net/documentation/guides/regionality-and-apis
-     */
-    region: string
-  }
+  options: OAuthUserConfig<P>
 ): OAuthConfig<P> {
-  const { region } = options
-
-  const base = region.toUpperCase() === "CN"
-    ? "https://www.battlenet.com.cn/oauth"
-    : `https://${region}.battle.net/oauth`
+  
+  options.issuer = options.issuer ?? 'https://us.battle.net/oauth'
 
   return {
     id: "battlenet",
     name: "Battle.net",
     type: "oauth",
-    wellKnown: `${base}/.well-known/openid-configuration`,
+    wellKnown: `${options.issuer}/.well-known/openid-configuration`,
     profile(profile) {
       return {
         id: profile.sub,
