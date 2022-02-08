60cb03f5c5d7304a212d9f8b2f4dbd6bfe237027
Balázs Orbán <info@balazsorban.com>
2022-02-08 21:09:15 +0100

    refactor(dev): moved dev app

---
+++ apps/dev/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import("next").NextConfig} */
+module.exports = {
+  webpack(config) {
+    config.experiments = { ...config.experiments, topLevelAwait: true }
+    return config
+  },
+  typescript: { ignoreBuildErrors: true },
+}
