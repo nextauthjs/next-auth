a4c810b241903549beaf3b97730179cb19330e73
Balázs Orbán <info@balazsorban.com>
2022-02-13 01:42:22 +0100

    chore: fix utils script

--- scripts/release/utils.ts
+++ scripts/release/utils.ts
@@ -3,15 +3,25 @@ import type { PackageJson } from "type-fest"
 import fs from "fs/promises"
 import path from "path"
 
-async function read(file: string): Promise<PackageJson> {
-  const content = await fs.readFile(path.join(process.cwd(), file), "utf8")
+async function read(directory: string): Promise<PackageJson> {
+  const content = await fs.readFile(
+    path.join(process.cwd(), directory, "package.json"),
+    "utf8"
+  )
   return JSON.parse(content)
 }
 
-async function update(file: string, data: Partial<PackageJson>): Promise<void> {
-  const original = await pkgJson.read(file)
+async function update(
+  directory: string,
+  data: Partial<PackageJson>
+): Promise<void> {
+  const original = await pkgJson.read(directory)
   const content = JSON.stringify({ ...original, ...data }, null, 2)
-  await fs.writeFile(path.join(process.cwd(), file), content, "utf8")
+  await fs.writeFile(
+    path.join(process.cwd(), directory, "package.json"),
+    content,
+    "utf8"
+  )
 }
 
 export const pkgJson = { read, update }
