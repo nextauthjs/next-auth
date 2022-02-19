55050f5a9cf4976daa22f0c72bc1b3cfc48ae9c0
Karl Stoney <karl.stoney@autotrader.co.uk>
2022-02-19 16:35:57 +0000

    Fixes: https://github.com/nextauthjs/next-auth/issues/4013

---
+++ packages/next-auth/src/providers/battlenet.ts
@@ -0,0 +1,32 @@
+import type { OAuthConfig } from "."
+import OAuthProvider from './auth0'
+export interface BattleNetProfile {
+  sub: string
+  battle_tag: string
+}
+
+export default function BattleNet(
+  options: { clientId: string, clientSecret: string, region: 'US' | 'EU' | 'KR' | 'TW' | 'CN' }
+): OAuthConfig<BattleNetProfile> {
+
+  const wellKnown = options.region === 'CN' 
+    ? 'https://www.battlenet.com.cn/oauth/.well-known/openid-configuration' 
+    : `https://us.battle.net/oauth/.well-known/openid-configuration` 
+
+  return OAuthProvider({
+    id: "battlenet",
+    name: "Battle.net",
+    wellKnown,
+    idToken: true,
+    clientId: options.clientId,
+    clientSecret: options.clientSecret,
+    profile(profile) {
+      return {
+        id: profile.sub,
+        name: profile.battle_tag,
+        email: null,
+        image: null,
+      }
+    }
+  })
+}
