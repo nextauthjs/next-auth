2cb10396c7822e8958ecb68df9fe82b382699c9f
Robert Soriano <sorianorobertc@gmail.com>
2022-02-21 07:47:33 -0800

    Update example route
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- apps/playground-sveltekit/src/routes/api/auth/[...nextauth].ts
+++ apps/playground-sveltekit/src/routes/api/auth/[...nextauth].ts
@@ -1,14 +1,11 @@
 import NextAuth from '$lib';
-import type { NextAuthOptions } from 'next-auth';
 import GithubProvider from 'next-auth/providers/github';
 
-const nextAuthOptions: NextAuthOptions = {
+export const { get, post } = NextAuth({
   providers: [
     GithubProvider({
       clientId: import.meta.env.VITE_GITHUB_CLIENT_ID,
       clientSecret: import.meta.env.VITE_GITHUB_CLIENT_SECRET
     })
   ]
-};
-
-export const { get, post } = NextAuth(nextAuthOptions);
+});
