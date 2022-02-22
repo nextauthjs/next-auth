8571a0393e8593046c7279616232e01a920b6814
Balázs Orbán <info@balazsorban.com>
2022-02-23 00:19:37 +0100

    Update battlenet.ts

--- packages/next-auth/src/providers/battlenet.ts
+++ packages/next-auth/src/providers/battlenet.ts
@@ -3,7 +3,6 @@ import type { OAuthConfig, OAuthUserConfig } from "."
 export interface BattleNetProfile {
   sub: string
   battle_tag: string
-  name: string
 }
 
 export default function BattleNet<
