b4429235c051c6dcc9c6ef7514e3b70783f67485
Balázs Orbán <info@balazsorban.com>
2022-04-26 12:22:11 +0200

    fix: more strict default callback url handling

--- src/server/lib/default-callbacks.js
+++ src/server/lib/default-callbacks.js
@@ -29,7 +29,7 @@ export async function signIn() {
  * @return {Promise<string>} URL the client will be redirect to
  */
 export async function redirect(url, baseUrl) {
-  if (url.startsWith("/")) return new URL(url, baseUrl).toString()
+  if (url.startsWith("/")) return `${baseUrl}${url}`
   else if (new URL(url).origin === baseUrl) return url
   return baseUrl
 }
