c9555f6070f1bc3be6f7a943b5ccee23bc21fa12
Balázs Orbán <info@balazsorban.com>
2022-06-23 12:49:02 +0200

    merge `main`, fix workspace dependencies

--- apps/dev/next-env.d.ts
+++ apps/dev/next-env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="next" />
+/// <reference types="next/dist/styled-jsx-types/global" />
 /// <reference types="next/image-types/global" />
 
 // NOTE: This file should not be edited
