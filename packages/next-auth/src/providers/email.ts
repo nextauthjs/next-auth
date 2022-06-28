965d31239b5ff183b46efc7b1b06140b984ec629
Mikola Lysenko <mikolalysenko@gmail.com>
2022-06-28 17:05:40 -0400

    closed, see email

--- packages/next-auth/src/providers/email.ts
+++ packages/next-auth/src/providers/email.ts
@@ -85,14 +85,23 @@ export default function Email(options: EmailUserConfig): EmailConfig {
   }
 }
 
+function escapeHTML(unsafe: string) {
+  return unsafe
+    .replace(/&/g, "&amp;")
+    .replace(/"/g, "&quot;")
+    .replace(/'/g, "&#39;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+}
+
 // Email HTML body
 function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
   // Insert invisible space into domains and email address to prevent both the
   // email address and the domain from being turned into a hyperlink by email
   // clients like Outlook and Apple mail, as this is confusing because it seems
   // like they are supposed to click on their email address to sign in.
-  const escapedEmail = `${email.replace(/\./g, "&#8203;.")}`
-  const escapedHost = `${host.replace(/\./g, "&#8203;.")}`
+  const escapedEmail = `${escapeHTML(email).replace(/\./g, "&#8203;.")}`
+  const escapedHost = `${escapeHTML(host).replace(/\./g, "&#8203;.")}`
 
   // Some simple styling options
   const backgroundColor = "#f9f9f9"
@@ -121,7 +130,9 @@ function html({ url, host, email }: Record<"url" | "host" | "email", string>) {
       <td align="center" style="padding: 20px 0;">
         <table border="0" cellspacing="0" cellpadding="0">
           <tr>
-            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${url}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
+            <td align="center" style="border-radius: 5px;" bgcolor="${buttonBackgroundColor}"><a href="${encodeURI(
+    url
+  )}" target="_blank" style="font-size: 18px; font-family: Helvetica, Arial, sans-serif; color: ${buttonTextColor}; text-decoration: none; border-radius: 5px; padding: 10px 20px; border: 1px solid ${buttonBorderColor}; display: inline-block; font-weight: bold;">Sign in</a></td>
           </tr>
         </table>
       </td>
