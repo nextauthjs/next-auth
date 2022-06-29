3ea90321db54c805c8ee72c95dd3569b39dcbbcc
Jeremiah S <46010924+HXavS@users.noreply.github.com>
2022-06-28 19:46:18 -0500

    Middleware Example

--- apps/dev/middleware.ts
+++ apps/dev/middleware.ts
@@ -4,6 +4,35 @@ export const config = { matcher: ["/middleware-protected"] }
 
 // Other ways to use this middleware
 
+
+
+// import { NextResponse } from 'next/server';
+// import { withAuth } from "next-auth/middleware"
+// import { JWT } from 'next-auth/jwt';
+
+
+// //Just done here fore quick example of middleware changes. In practice would recommend extending JWT type in types/next-auth.d.ts
+// interface TokenMiddle extends Partial<JWT>{ 
+//   home: URL | null
+// }
+
+// export default withAuth(
+//   function middleware(req, ev) { //function here can run custom logic and return next response.
+//     console.log(req, ev)
+//     return undefined // NOTE: `NextMiddleware` should allow returning `void`
+//   },
+//   {
+//     pages: {
+//       signIn: "/unauthorized"
+//     },
+//     callbacks: {
+//       authorized: ({ token }) => !!token && false, //User isAuthorized should different then logged in.
+//       unAuthorized: ({ req, token }) => NextResponse.redirect(new URL((token as TokenMiddle).home ?? "/unauthorized", req.nextUrl.origin)), 
+//     }
+//   }
+// )
+
+
 // import withAuth from "next-auth/middleware"
 // import { withAuth } from "next-auth/middleware"
 
