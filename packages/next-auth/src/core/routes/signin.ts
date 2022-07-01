2966fc0145841298e7a4d3f198fe2c2cbf1c6d29
Balázs Orbán <info@balazsorban.com>
2022-07-01 23:44:13 +0200

    fix(core): move email handling

--- packages/next-auth/src/core/routes/signin.ts
+++ packages/next-auth/src/core/routes/signin.ts
@@ -41,15 +41,6 @@ export default async function signin(params: {
 
     if (!email) return { redirect: `${url}/error?error=EmailSignin` }
 
-    email = email
-      .split(",")[0]
-      .trim()
-      .replaceAll("&", "&amp;")
-      .replaceAll("<", "&lt;")
-      .replaceAll(">", "&gt;")
-      .replaceAll('"', "&quot;")
-      .replaceAll("'", "&#x27;")
-
     // Verified in `assertConfig`
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     const { getUserByEmail } = adapter!
