637dda9966da7e21858bf6405b54067b42f0aedf
Balázs Orbán <info@balazsorban.com>
2022-07-08 19:55:08 +0200

    chore: tweak release script

--- scripts/release/config.ts
+++ scripts/release/config.ts
@@ -18,11 +18,12 @@ export const config = {
   },
   rootDir: process.cwd(),
   RELEASE_COMMIT_MSG: "chore(release): bump version",
-  BREAKING_COMMIT_MSG: "BREAKING CHANGE",
+  BREAKING_COMMIT_MSG: "BREAKING CHANGE:",
   SKIP_RELEASE_MSG: "[skip release]",
   RELEASE_COMMIT_TYPES: ["feat", "fix"],
   dryRun:
     !process.env.CI ||
     !!process.env.DRY_RUN ||
     process.argv.includes("--dry-run"),
+  verbose: !!process.env.VERBOSE || process.argv.includes("--verbose"),
 }
