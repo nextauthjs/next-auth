5cfe31b3b8689df9affc83b07d9aba592aaab37c
Chancellor Clark <chancellor.clark@bigcommerce.com>
2022-07-11 18:24:48 -0500

    chore: run prettier --write

--- packages/adapter-firebase/src/converter.ts
+++ packages/adapter-firebase/src/converter.ts
@@ -1,58 +1,58 @@
-import { Timestamp } from 'firebase/firestore';
+import { Timestamp } from "firebase/firestore"
 import type {
   FirestoreDataConverter,
   QueryDocumentSnapshot,
   WithFieldValue,
-} from 'firebase/firestore';
+} from "firebase/firestore"
 
 const isTimestamp = (value: unknown): value is Timestamp =>
-  typeof value === 'object' && value !== null && value instanceof Timestamp;
+  typeof value === "object" && value !== null && value instanceof Timestamp
 
 interface GetConverterOptions {
-  excludeId?: boolean;
+  excludeId?: boolean
 }
 
 export const getConverter = <Document extends Record<string, unknown>>(
-  options?: GetConverterOptions,
+  options?: GetConverterOptions
 ): FirestoreDataConverter<Document> => ({
   // `PartialWithFieldValue` implicitly types `object` as `any`, so we want to explicitly type it
   toFirestore(object: WithFieldValue<Document>) {
-    const document: Record<string, unknown> = {};
+    const document: Record<string, unknown> = {}
 
     Object.keys(object).forEach((key) => {
       if (object[key] !== undefined) {
-        document[key] = object[key];
+        document[key] = object[key]
       }
-    });
+    })
 
-    return document;
+    return document
   },
   // We need to explicitly type `snapshot` since it uses `DocumentData` for generic type
   fromFirestore(snapshot: QueryDocumentSnapshot<Document>) {
     if (!snapshot.exists()) {
-      return snapshot;
+      return snapshot
     }
 
-    let document: Document = snapshot.data();
+    let document: Document = snapshot.data()
 
     if (!options?.excludeId) {
       document = {
         ...document,
         id: snapshot.id,
-      };
+      }
     }
 
     for (const key in document) {
-      const value = document[key];
+      const value = document[key]
 
       if (isTimestamp(value)) {
         document = {
           ...document,
           [key]: value.toDate(),
-        };
+        }
       }
     }
 
-    return document;
+    return document
   },
-});
+})
--- packages/adapter-firebase/src/index.ts
+++ packages/adapter-firebase/src/index.ts
@@ -1,5 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import type { FirebaseOptions } from 'firebase/app';
+import { initializeApp } from "firebase/app"
+import type { FirebaseOptions } from "firebase/app"
 import {
   addDoc,
   collection,
@@ -13,20 +13,25 @@ import {
   runTransaction,
   setDoc,
   where,
-  connectFirestoreEmulator
-} from 'firebase/firestore';
-import type { Account } from 'next-auth';
-import type { Adapter, AdapterSession, AdapterUser, VerificationToken } from 'next-auth/adapters';
+  connectFirestoreEmulator,
+} from "firebase/firestore"
+import type { Account } from "next-auth"
+import type {
+  Adapter,
+  AdapterSession,
+  AdapterUser,
+  VerificationToken,
+} from "next-auth/adapters"
 
-import { getConverter } from './converter';
+import { getConverter } from "./converter"
 
-type IndexableObject = Record<string, unknown>;
+type IndexableObject = Record<string, unknown>
 
 export interface FirestoreAdapterOptions {
   emulator?: {
-    host?: string;
-    port?: number;
-  };
+    host?: string
+    port?: number
+  }
 }
 
 export function FirestoreAdapter({
@@ -37,213 +42,248 @@ export function FirestoreAdapter({
   const db = getFirestore(firebaseApp)
 
   if (emulator) {
-    connectFirestoreEmulator(db, emulator?.host ?? 'localhost', emulator?.port ?? 3001);
+    connectFirestoreEmulator(
+      db,
+      emulator?.host ?? "localhost",
+      emulator?.port ?? 3001
+    )
   }
 
-  const Users = collection(db, 'users').withConverter(getConverter<AdapterUser>());
-  const Sessions = collection(db, 'sessions').withConverter(getConverter<AdapterSession & IndexableObject>());
-  const Accounts = collection(db, 'accounts').withConverter(getConverter<Account>());
-  const VerificationTokens = collection(db, 'verificationTokens').withConverter(
-    getConverter<VerificationToken & IndexableObject>({ excludeId: true }),
-  );
+  const Users = collection(db, "users").withConverter(
+    getConverter<AdapterUser>()
+  )
+  const Sessions = collection(db, "sessions").withConverter(
+    getConverter<AdapterSession & IndexableObject>()
+  )
+  const Accounts = collection(db, "accounts").withConverter(
+    getConverter<Account>()
+  )
+  const VerificationTokens = collection(db, "verificationTokens").withConverter(
+    getConverter<VerificationToken & IndexableObject>({ excludeId: true })
+  )
 
   return {
     async createUser(newUser) {
-      const userRef = await addDoc(Users, newUser);
-      const userSnapshot = await getDoc(userRef);
+      const userRef = await addDoc(Users, newUser)
+      const userSnapshot = await getDoc(userRef)
 
       if (userSnapshot.exists() && Users.converter) {
-        return Users.converter.fromFirestore(userSnapshot);
+        return Users.converter.fromFirestore(userSnapshot)
       }
 
-      throw new Error('[createUser] Failed to create user');
+      throw new Error("[createUser] Failed to create user")
     },
     async getUser(id) {
-      const userSnapshot = await getDoc(doc(Users, id));
+      const userSnapshot = await getDoc(doc(Users, id))
 
       if (userSnapshot.exists() && Users.converter) {
-        return Users.converter.fromFirestore(userSnapshot);
+        return Users.converter.fromFirestore(userSnapshot)
       }
 
-      return null;
+      return null
     },
     async getUserByEmail(email) {
-      const userQuery = query(Users, where('email', '==', email), limit(1));
-      const userSnapshots = await getDocs(userQuery);
-      const userSnpashot = userSnapshots.docs[0];
+      const userQuery = query(Users, where("email", "==", email), limit(1))
+      const userSnapshots = await getDocs(userQuery)
+      const userSnpashot = userSnapshots.docs[0]
 
       if (userSnpashot?.exists() && Users.converter) {
-        return Users.converter.fromFirestore(userSnpashot);
+        return Users.converter.fromFirestore(userSnpashot)
       }
 
-      return null;
+      return null
     },
     async getUserByAccount({ provider, providerAccountId }) {
       const accountQuery = query(
         Accounts,
-        where('provider', '==', provider),
-        where('providerAccountId', '==', providerAccountId),
-        limit(1),
-      );
-      const accountSnapshots = await getDocs(accountQuery);
-      const accountSnapshot = accountSnapshots.docs[0];
+        where("provider", "==", provider),
+        where("providerAccountId", "==", providerAccountId),
+        limit(1)
+      )
+      const accountSnapshots = await getDocs(accountQuery)
+      const accountSnapshot = accountSnapshots.docs[0]
 
       if (accountSnapshot?.exists()) {
-        const { userId } = accountSnapshot.data();
-        const userDoc = await getDoc(doc(Users, userId));
+        const { userId } = accountSnapshot.data()
+        const userDoc = await getDoc(doc(Users, userId))
 
         if (userDoc.exists() && Users.converter) {
-          return Users.converter.fromFirestore(userDoc);
+          return Users.converter.fromFirestore(userDoc)
         }
       }
 
-      return null;
+      return null
     },
 
     async updateUser(partialUser) {
-      const userRef = doc(Users, partialUser.id);
+      const userRef = doc(Users, partialUser.id)
 
-      await setDoc(userRef, partialUser, { merge: true });
+      await setDoc(userRef, partialUser, { merge: true })
 
-      const userSnapshot = await getDoc(userRef);
+      const userSnapshot = await getDoc(userRef)
 
       if (userSnapshot.exists() && Users.converter) {
-        return Users.converter.fromFirestore(userSnapshot);
+        return Users.converter.fromFirestore(userSnapshot)
       }
 
-      throw new Error('[updateUser] Failed to update user');
+      throw new Error("[updateUser] Failed to update user")
     },
 
     async deleteUser(userId) {
-      const userRef = doc(Users, userId);
-      const accountsQuery = query(Accounts, where('userId', '==', userId));
-      const sessionsQuery = query(Sessions, where('userId', '==', userId));
+      const userRef = doc(Users, userId)
+      const accountsQuery = query(Accounts, where("userId", "==", userId))
+      const sessionsQuery = query(Sessions, where("userId", "==", userId))
 
       // TODO: May be better to use events instead of transactions?
       await runTransaction(db, async (transaction) => {
-        const accounts = await getDocs(accountsQuery);
-        const sessions = await getDocs(sessionsQuery);
+        const accounts = await getDocs(accountsQuery)
+        const sessions = await getDocs(sessionsQuery)
 
-        transaction.delete(userRef);
-        accounts.forEach((account) => transaction.delete(account.ref));
-        sessions.forEach((session) => transaction.delete(session.ref));
-      });
+        transaction.delete(userRef)
+        accounts.forEach((account) => transaction.delete(account.ref))
+        sessions.forEach((session) => transaction.delete(session.ref))
+      })
     },
 
     async linkAccount(account) {
-      const accountRef = await addDoc(Accounts, account);
-      const accountSnapshot = await getDoc(accountRef);
+      const accountRef = await addDoc(Accounts, account)
+      const accountSnapshot = await getDoc(accountRef)
 
       if (accountSnapshot.exists() && Accounts.converter) {
-        return Accounts.converter.fromFirestore(accountSnapshot);
+        return Accounts.converter.fromFirestore(accountSnapshot)
       }
     },
 
     async unlinkAccount({ provider, providerAccountId }) {
       const accountQuery = query(
         Accounts,
-        where('provider', '==', provider),
-        where('providerAccountId', '==', providerAccountId),
-        limit(1),
-      );
-      const accountSnapshots = await getDocs(accountQuery);
-      const accountSnapshot = accountSnapshots.docs[0];
+        where("provider", "==", provider),
+        where("providerAccountId", "==", providerAccountId),
+        limit(1)
+      )
+      const accountSnapshots = await getDocs(accountQuery)
+      const accountSnapshot = accountSnapshots.docs[0]
 
       if (accountSnapshot?.exists()) {
-        await deleteDoc(accountSnapshot.ref);
+        await deleteDoc(accountSnapshot.ref)
       }
     },
 
     async createSession(session) {
-      const sessionRef = await addDoc(Sessions, session);
-      const sessionSnapshot = await getDoc(sessionRef);
+      const sessionRef = await addDoc(Sessions, session)
+      const sessionSnapshot = await getDoc(sessionRef)
 
       if (sessionSnapshot.exists() && Sessions.converter) {
-        return Sessions.converter.fromFirestore(sessionSnapshot);
+        return Sessions.converter.fromFirestore(sessionSnapshot)
       }
 
-      throw new Error('[createSession] Failed to create session');
+      throw new Error("[createSession] Failed to create session")
     },
 
     async getSessionAndUser(sessionToken) {
-      const sessionQuery = query(Sessions, where('sessionToken', '==', sessionToken), limit(1));
-      const sessionSnapshots = await getDocs(sessionQuery);
-      const sessionSnapshot = sessionSnapshots.docs[0];
+      const sessionQuery = query(
+        Sessions,
+        where("sessionToken", "==", sessionToken),
+        limit(1)
+      )
+      const sessionSnapshots = await getDocs(sessionQuery)
+      const sessionSnapshot = sessionSnapshots.docs[0]
 
       if (sessionSnapshot?.exists() && Sessions.converter) {
-        const session = Sessions.converter.fromFirestore(sessionSnapshot);
-        const userDoc = await getDoc(doc(Users, session.userId));
+        const session = Sessions.converter.fromFirestore(sessionSnapshot)
+        const userDoc = await getDoc(doc(Users, session.userId))
 
         if (userDoc.exists() && Users.converter) {
-          const user = Users.converter.fromFirestore(userDoc);
+          const user = Users.converter.fromFirestore(userDoc)
 
-          return { session, user };
+          return { session, user }
         }
       }
 
-      return null;
+      return null
     },
 
     async updateSession(partialSession) {
-      const sessionQuery = query(Sessions, where('sessionToken', '==', partialSession.sessionToken), limit(1));
-      const sessionSnapshots = await getDocs(sessionQuery);
-      const sessionSnapshot = sessionSnapshots.docs[0];
+      const sessionQuery = query(
+        Sessions,
+        where("sessionToken", "==", partialSession.sessionToken),
+        limit(1)
+      )
+      const sessionSnapshots = await getDocs(sessionQuery)
+      const sessionSnapshot = sessionSnapshots.docs[0]
 
       if (sessionSnapshot?.exists()) {
-        await setDoc(sessionSnapshot.ref, partialSession, { merge: true });
+        await setDoc(sessionSnapshot.ref, partialSession, { merge: true })
 
-        const sessionDoc = await getDoc(sessionSnapshot.ref);
+        const sessionDoc = await getDoc(sessionSnapshot.ref)
 
         if (sessionDoc?.exists() && Sessions.converter) {
-          const session = Sessions.converter.fromFirestore(sessionDoc);
+          const session = Sessions.converter.fromFirestore(sessionDoc)
 
-          return session;
+          return session
         }
       }
 
-      return null;
+      return null
     },
 
     async deleteSession(sessionToken) {
-      const sessionQuery = query(Sessions, where('sessionToken', '==', sessionToken), limit(1));
-      const sessionSnapshots = await getDocs(sessionQuery);
-      const sessionSnapshot = sessionSnapshots.docs[0];
+      const sessionQuery = query(
+        Sessions,
+        where("sessionToken", "==", sessionToken),
+        limit(1)
+      )
+      const sessionSnapshots = await getDocs(sessionQuery)
+      const sessionSnapshot = sessionSnapshots.docs[0]
 
       if (sessionSnapshot?.exists()) {
-        await deleteDoc(sessionSnapshot.ref);
+        await deleteDoc(sessionSnapshot.ref)
       }
     },
 
     async createVerificationToken(verificationToken) {
-      const verificationTokenRef = await addDoc(VerificationTokens, verificationToken);
-      const verificationTokenSnapshot = await getDoc(verificationTokenRef);
+      const verificationTokenRef = await addDoc(
+        VerificationTokens,
+        verificationToken
+      )
+      const verificationTokenSnapshot = await getDoc(verificationTokenRef)
 
       if (verificationTokenSnapshot.exists() && VerificationTokens.converter) {
-        const { id, ...verificationToken } = VerificationTokens.converter.fromFirestore(verificationTokenSnapshot);
+        const {
+          id,
+          ...verificationToken
+        } = VerificationTokens.converter.fromFirestore(
+          verificationTokenSnapshot
+        )
 
-        return verificationToken;
+        return verificationToken
       }
     },
 
     async useVerificationToken({ identifier, token }) {
       const verificationTokensQuery = query(
         VerificationTokens,
-        where('identifier', '==', identifier),
-        where('token', '==', token),
-        limit(1),
-      );
-      const verificationTokenSnapshots = await getDocs(verificationTokensQuery);
-      const verificationTokenSnapshot = verificationTokenSnapshots.docs[0];
+        where("identifier", "==", identifier),
+        where("token", "==", token),
+        limit(1)
+      )
+      const verificationTokenSnapshots = await getDocs(verificationTokensQuery)
+      const verificationTokenSnapshot = verificationTokenSnapshots.docs[0]
 
       if (verificationTokenSnapshot?.exists() && VerificationTokens.converter) {
-        await deleteDoc(verificationTokenSnapshot.ref);
+        await deleteDoc(verificationTokenSnapshot.ref)
 
-        const { id, ...verificationToken } = VerificationTokens.converter.fromFirestore(verificationTokenSnapshot);
+        const {
+          id,
+          ...verificationToken
+        } = VerificationTokens.converter.fromFirestore(
+          verificationTokenSnapshot
+        )
 
-        return verificationToken;
+        return verificationToken
       }
 
-      return null;
+      return null
     },
-  };
-};
+  }
+}
