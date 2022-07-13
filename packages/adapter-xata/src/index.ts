40699ce68ead6572a80862614095c03d72fd2dcd
Tejas Kumar <tejas@tejas.qa>
2022-07-13 02:28:18 +0200

    Address more comments

--- packages/adapter-xata/src/index.ts
+++ packages/adapter-xata/src/index.ts
@@ -4,88 +4,158 @@ import type { XataClient } from "./xata";
 export function XataAdapter(client: XataClient): Adapter {
   return {
     async createUser(user) {
-      const newUser = await client.db.nextauth_users.create(user);
+      const newUser = await client.db.nextauth_users.create(user)
       return { emailVerified: newUser.emailVerified || null, id: newUser.id }
     },
     async getUser(id) {
       const user = await client.db.nextauth_users.filter({ id }).getFirst()
-      return user ? { emailVerified: user.emailVerified ?? null, id: user.id, email: user?.email ?? null, image: user?.image ?? null, name: user?.name ?? null } : null;
+      return user
+        ? {
+          emailVerified: user.emailVerified ?? null,
+          id: user.id,
+          email: user?.email ?? null,
+          image: user?.image ?? null,
+          name: user?.name ?? null,
+        }
+        : null
     },
     async getUserByEmail(email) {
       const user = await client.db.nextauth_users.filter({ email }).getFirst()
-      return user ? { emailVerified: user.emailVerified ?? null, id: user.id, email: user?.email ?? null, image: user?.image ?? null, name: user?.name ?? null } : null;
+      return user
+        ? {
+          emailVerified: user.emailVerified ?? null,
+          id: user.id,
+          email: user?.email ?? null,
+          image: user?.image ?? null,
+          name: user?.name ?? null,
+        }
+        : null
     },
     async getUserByAccount({ providerAccountId, provider }) {
-      const result = await client.db.nextauth_users_accounts.select(['user.*']).filter({ 'account.providerAccountId': providerAccountId, 'account.provider': provider }).getFirst();
-      const user = result?.user;
-      return user ? { emailVerified: user.emailVerified ?? null, id: user.id, email: user?.email ?? null, image: user?.image ?? null, name: user?.name ?? null } : null;
+      const result = await client.db.nextauth_users_accounts
+        .select(["user.*"])
+        .filter({
+          "account.providerAccountId": providerAccountId,
+          "account.provider": provider,
+        })
+        .getFirst()
+      const user = result?.user
+      return user
+        ? {
+          emailVerified: user.emailVerified ?? null,
+          id: user.id,
+          email: user?.email ?? null,
+          image: user?.image ?? null,
+          name: user?.name ?? null,
+        }
+        : null
     },
     async updateUser(user) {
-      const result = await client.db.nextauth_users.update(user.id!, user);
-      return result ? { emailVerified: user.emailVerified ?? null, id: user.id!, email: user?.email ?? null, image: user?.image ?? null, name: user?.name ?? null } : { ...user, id: user.id!, emailVerified: user.emailVerified ?? null };
+      const result = await client.db.nextauth_users.update(user.id!, user)
+      return result
+        ? {
+          emailVerified: user.emailVerified ?? null,
+          id: user.id!,
+          email: user?.email ?? null,
+          image: user?.image ?? null,
+          name: user?.name ?? null,
+        }
+        : { ...user, id: user.id!, emailVerified: user.emailVerified ?? null }
     },
     async deleteUser(id) {
       return await client.db.nextauth_users.delete(id)
     },
     async linkAccount(initialAccount) {
       const { userId, ...account } = initialAccount
-      const newXataAccount = await client.db.nextauth_accounts.create({ ...account, user: { id: userId } })
-      await client.db.nextauth_users_accounts.create({ user: { id: userId }, account: { id: newXataAccount.id } })
+      const newXataAccount = await client.db.nextauth_accounts.create({
+        ...account,
+        user: { id: userId },
+      })
+      await client.db.nextauth_users_accounts.create({
+        user: { id: userId },
+        account: { id: newXataAccount.id },
+      })
     },
     async unlinkAccount({ providerAccountId, provider }) {
       /**
        * @todo refactor this when we support DELETE WHERE.
        */
-      const connectedAccount = await client.db.nextauth_users_accounts.filter({ 'account.providerAccountId': providerAccountId, 'account.provider': provider }).getFirst();
+      const connectedAccount = await client.db.nextauth_users_accounts
+        .filter({
+          "account.providerAccountId": providerAccountId,
+          "account.provider": provider,
+        })
+        .getFirst()
 
       if (!connectedAccount) {
-        return;
+        return
       }
       return await client.db.nextauth_users_accounts.delete(connectedAccount.id)
     },
     async createSession(initialSession) {
       const { userId, ...session } = initialSession
-      const newXataSession = await client.db.nextauth_sessions.create({ ...session, user: { id: userId } });
-      await client.db.nextauth_users_sessions.create({ user: { id: userId }, session: { id: newXataSession.id } })
-      return { ...newXataSession, ...session, userId };
+      const newXataSession = await client.db.nextauth_sessions.create({
+        ...session,
+        user: { id: userId },
+      })
+      await client.db.nextauth_users_sessions.create({
+        user: { id: userId },
+        session: { id: newXataSession.id },
+      })
+      return { ...newXataSession, ...session, userId }
     },
     async getSessionAndUser(sessionToken) {
-      const result = await client.db.nextauth_users_sessions.select(['user.*', 'session.*']).filter({ 'session.sessionToken': sessionToken }).getFirst()
+      const result = await client.db.nextauth_users_sessions
+        .select(["user.*", "session.*"])
+        .filter({ "session.sessionToken": sessionToken })
+        .getFirst()
       if (!result?.session || !result?.user) {
-        return null;
+        return null
       }
 
       return {
         session: {
           ...result.session,
           sessionToken: result.session.sessionToken!,
-          expires: result.session.expires!, userId: result.user.id
+          expires: result.session.expires!,
+          userId: result.user.id,
         },
         user: {
           ...result.user,
           emailVerified: result.user.emailVerified ?? null,
-        }
+        },
       }
     },
     async updateSession({ sessionToken, ...data }) {
-      const session = await client.db.nextauth_sessions.filter({ sessionToken }).getFirst();
+      const session = await client.db.nextauth_sessions
+        .filter({ sessionToken })
+        .getFirst()
       if (!session) {
-        return null;
+        return null
       }
 
       await client.db.nextauth_sessions.update({ ...session, ...data })
-      return { ...session, sessionToken, userId: data.userId!, expires: data.expires! };
+      return {
+        ...session,
+        sessionToken,
+        userId: data.userId!,
+        expires: data.expires!,
+      }
     },
 
     async deleteSession(sessionToken) {
       /**
        * @todo refactor this when we support DELETE WHERE.
        */
-      const session = await client.db.nextauth_sessions.filter({ sessionToken }).getFirst();
+      const session = await client.db.nextauth_sessions
+        .filter({ sessionToken })
+        .getFirst()
       if (!session) {
         return
       }
-      const connectedSession = await client.db.nextauth_users_sessions.filter({ 'session.sessionToken': sessionToken }).getFirst();
+      const connectedSession = await client.db.nextauth_users_sessions
+        .filter({ "session.sessionToken": sessionToken })
+        .getFirst()
       if (!connectedSession) {
         return
       }
@@ -93,19 +163,25 @@ export function XataAdapter(client: XataClient): Adapter {
       await client.db.nextauth_users_sessions.delete(connectedSession.id)
     },
     async createVerificationToken(token) {
-      await client.db.nextauth_verificationTokens.create({ expires: token.expires, identifier: token.identifier, token: token.token })
-      return token;
+      await client.db.nextauth_verificationTokens.create({
+        expires: token.expires,
+        identifier: token.identifier,
+        token: token.token,
+      })
+      return token
     },
     async useVerificationToken(token) {
       /**
        * @todo refactor this when we support DELETE WHERE.
        */
-      const xataToken = await client.db.nextauth_verificationTokens.filter({ identifier: token.identifier, token: token.token }).getFirst();
+      const xataToken = await client.db.nextauth_verificationTokens
+        .filter({ identifier: token.identifier, token: token.token })
+        .getFirst()
       if (!xataToken) {
-        return null;
+        return null
       }
       await client.db.nextauth_verificationTokens.delete(xataToken.id)
-      return { ...token, expires: new Date() };
+      return { ...token, expires: new Date() }
     },
   }
 }
--- packages/adapter-xata/src/xata.ts
+++ packages/adapter-xata/src/xata.ts
@@ -1,88 +1,90 @@
-import { buildClient, BaseClientOptions, XataRecord, ClientConstructor } from "@xata.io/client";
+import {
+  buildClient,
+  BaseClientOptions,
+  XataRecord,
+  ClientConstructor,
+} from "@xata.io/client"
 
 export interface NextauthUser {
-    email?: string | null;
-    emailVerified?: Date | null;
-    name?: string | null;
-    image?: string | null;
-    username?: string | null;
-    location?: string | null;
+  email?: string | null
+  emailVerified?: Date | null
+  name?: string | null
+  image?: string | null
+  username?: string | null
+  location?: string | null
 }
 
-export type NextauthUserRecord = NextauthUser & XataRecord;
+export type NextauthUserRecord = NextauthUser & XataRecord
 
 export interface NextauthAccount {
-    user?: NextauthUserRecord | null;
-    type?: string | null;
-    provider?: string | null;
-    providerAccountId?: string | null;
-    refresh_token?: string | null;
-    access_token?: string | null;
-    expires_at?: number | null;
-    token_type?: string | null;
-    scope?: string | null;
-    id_token?: string | null;
-    session_state?: string | null;
+  user?: NextauthUserRecord | null
+  type?: string | null
+  provider?: string | null
+  providerAccountId?: string | null
+  refresh_token?: string | null
+  access_token?: string | null
+  expires_at?: number | null
+  token_type?: string | null
+  scope?: string | null
+  id_token?: string | null
+  session_state?: string | null
 }
 
-export type NextauthAccountRecord = NextauthAccount & XataRecord;
+export type NextauthAccountRecord = NextauthAccount & XataRecord
 
 export interface NextauthVerificationToken {
-    identifier?: string | null;
-    token?: string | null;
-    expires?: Date | null;
+  identifier?: string | null
+  token?: string | null
+  expires?: Date | null
 }
 
 export type NextauthVerificationTokenRecord = NextauthVerificationToken &
-    XataRecord;
+  XataRecord
 
 export interface NextauthUsersAccount {
-    user?: NextauthUserRecord | null;
-    account?: NextauthAccountRecord | null;
+  user?: NextauthUserRecord | null
+  account?: NextauthAccountRecord | null
 }
 
-export type NextauthUsersAccountRecord = NextauthUsersAccount & XataRecord;
+export type NextauthUsersAccountRecord = NextauthUsersAccount & XataRecord
 
 export interface NextauthUsersSession {
-    user?: NextauthUserRecord | null;
-    session?: NextauthSessionRecord | null;
+  user?: NextauthUserRecord | null
+  session?: NextauthSessionRecord | null
 }
 
-export type NextauthUsersSessionRecord = NextauthUsersSession & XataRecord;
+export type NextauthUsersSessionRecord = NextauthUsersSession & XataRecord
 
 export interface NextauthSession {
-    sessionToken?: string | null;
-    expires?: Date | null;
-    user?: NextauthUserRecord | null;
+  sessionToken?: string | null
+  expires?: Date | null
+  user?: NextauthUserRecord | null
 }
 
-export type NextauthSessionRecord = NextauthSession & XataRecord;
+export type NextauthSessionRecord = NextauthSession & XataRecord
 
 export type DatabaseSchema = {
-    nextauth_users: NextauthUser;
-    nextauth_accounts: NextauthAccount;
-    nextauth_verificationTokens: NextauthVerificationToken;
-    nextauth_users_accounts: NextauthUsersAccount;
-    nextauth_users_sessions: NextauthUsersSession;
-    nextauth_sessions: NextauthSession;
-};
+  nextauth_users: NextauthUser
+  nextauth_accounts: NextauthAccount
+  nextauth_verificationTokens: NextauthVerificationToken
+  nextauth_users_accounts: NextauthUsersAccount
+  nextauth_users_sessions: NextauthUsersSession
+  nextauth_sessions: NextauthSession
+}
 
 const tables = [
-    "nextauth_users",
-    "nextauth_accounts",
-    "nextauth_verificationTokens",
-    "nextauth_users_accounts",
-    "nextauth_users_sessions",
-    "nextauth_sessions",
-];
+  "nextauth_users",
+  "nextauth_accounts",
+  "nextauth_verificationTokens",
+  "nextauth_users_accounts",
+  "nextauth_users_sessions",
+  "nextauth_sessions",
+]
 
-const DatabaseClient = buildClient() as ClientConstructor<any>;
+const DatabaseClient = buildClient() as ClientConstructor<any>
 
 export class XataClient extends DatabaseClient<DatabaseSchema> {
-    constructor(options?: BaseClientOptions) {
-        super(
-            { databaseURL: "", ...options },
-            tables
-        );
-    }
+  constructor(options?: BaseClientOptions) {
+    super({ databaseURL: "", ...options }, tables)
+  }
 }
