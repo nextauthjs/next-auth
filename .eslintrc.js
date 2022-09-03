418de3f5f0ee4502c4d10a732ffee1593be2344b
Thang Vu <thvu@hey.com>
2022-09-03 15:59:57 +0700

    fix: make eslint works

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+const path = require('path')
+
+module.exports = {
+  root: true,
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: [
+      path.resolve(__dirname, './packages/next-auth/tsconfig.json'),
+      path.resolve(__dirname, 'tsconfig.json')
+    ],
+    tsconfigRootDir: __dirname
+  },
+  extends: [
+    "standard-with-typescript",
+    "prettier"
+  ],
+  globals: {
+    localStorage: "readonly",
+    location: "readonly",
+    fetch: "readonly"
+  },
+  rules: {
+    "camelcase": "off",
+    "@typescript-eslint/naming-convention": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/restrict-template-expressions": "off"
+  },
+  plugins: ['jest'],
+  "env": {
+    "jest/globals": true
+  }
+}
