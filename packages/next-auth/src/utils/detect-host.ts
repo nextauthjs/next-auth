07ab4f8cd41781abc3fc25a3d4bf4aed7d4ae738
Balázs Orbán <info@balazsorban.com>
2022-10-05 04:03:19 +0200

    fall back host to localhost

--- packages/next-auth/src/utils/detect-host.ts
+++ packages/next-auth/src/utils/detect-host.ts
@@ -1,7 +1,9 @@
-/** Extract the host from the environment */
+const trustedHost = process.env.AUTH_TRUST_HOST ?? process.env.VERCEL
+
+/** Safely extract the host from the environment */
 export function detectHost(forwardedHost: any) {
   // If we detect a Vercel environment, we can trust the host
-  if (process.env.VERCEL) return forwardedHost
+  if (trustedHost) return forwardedHost
   // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
-  return process.env.NEXTAUTH_URL
+  return process.env.NEXTAUTH_URL ?? "http://localhost:3000"
 }
