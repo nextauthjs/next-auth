11022ed1ad210311b2d9de720a3c5a7fc93ea6fd
David Walsh <5778036+rhinodavid@users.noreply.github.com>
2022-10-10 08:24:29 -0600

    Fix adapter comment

--- apps/playground-sveltekit/svelte.config.js
+++ apps/playground-sveltekit/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from "@sveltejs/adapter-node" // or use https://github.com/sveltejs/kit/tree/master/packages/adapter-node
+import adapter from "@sveltejs/adapter-node" // or use https://github.com/sveltejs/kit/tree/master/packages/adapter-auto
 import preprocess from "svelte-preprocess"
 
 /** @type {import('@sveltejs/kit').Config} */
