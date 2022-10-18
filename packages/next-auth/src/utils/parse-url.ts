1f5826b1ae8edf5acaa85d38a8326e9187001df3
bmyun <bmyun@toss.im>
2022-10-19 01:23:38 +0900

    fix: remove docs

--- packages/next-auth/src/utils/parse-url.ts
+++ packages/next-auth/src/utils/parse-url.ts
@@ -1,13 +1,9 @@
 export interface InternalUrl {
-  /** @default "http://localhost:3000" */
   origin: string
-  /** @default "localhost:3000" */
   host: string
   /** @default "/api/auth" */
   path: string
-  /** @default "http://localhost:3000/api/auth" */
   base: string
-  /** @default "http://localhost:3000/api/auth" */
   toString: () => string
 }
 
