ea8765eb4b9382ce62b5b0dd2831027046385d6a
bmyun <bmyun@toss.im>
2022-10-19 01:26:00 +0900

    fix: occur an error when it is invalid url

--- packages/next-auth/src/utils/parse-url.ts
+++ packages/next-auth/src/utils/parse-url.ts
@@ -7,12 +7,20 @@ export interface InternalUrl {
   toString: () => string
 }
 
+function getUrl(url: string): URL {
+  try {
+    return new URL(url)
+  } catch (e) {
+    throw new Error(`${url} is not url format, please check environment variables (ex NEXTAUTH_URL, NEXTAUTH_URL, NEXTAUTH_URL_INTERNAL...)`)
+  }
+}
+
 /** Returns an `URL` like object to make requests/redirects from server-side */
 export default function parseUrl(plain?: string): InternalUrl {
   if(!plain) {
     throw new Error("url is empty, please check environment variables (ex NEXTAUTH_URL, NEXTAUTH_URL, NEXTAUTH_URL_INTERNAL...)")
   }
-  const _url = new URL(plain)
+  const _url = getUrl(plain)
   const path = (_url.pathname === "/" ? '/api/auth' : _url.pathname)
     // Remove trailing slash
     .replace(/\/$/, "")
