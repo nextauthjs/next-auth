e62f879ebda999657bec6555fe1eda4f820f7496
Balázs Orbán <info@balazsorban.com>
2022-11-04 03:38:24 +0100

    chore(adapters): run Xata tests

--- packages/adapter-xata/tests/index.test.ts
+++ packages/adapter-xata/tests/index.test.ts
@@ -5,7 +5,7 @@ import { XataClient } from "../src/xata"
 import { XataAdapter } from "../src"
 
 if (!process.env.XATA_API_KEY) {
-  test("Skipping UpstashRedisAdapter tests, since required environment variables aren't available", () => {
+  test("Skipping XataAdapter tests, since required environment variables aren't available", () => {
     expect(true).toBe(true)
   })
   process.exit(0)
@@ -13,7 +13,7 @@ if (!process.env.XATA_API_KEY) {
 
 if (process.env.CI) {
   // TODO: Fix this
-  test('Skipping UpstashRedisAdapter tests in CI because of "Request failed" errors. Should revisit', () => {
+  test('Skipping XataAdapter tests in CI because of "Request failed" errors. Should revisit', () => {
     expect(true).toBe(true)
   })
   process.exit(0)
@@ -32,17 +32,23 @@ runBasicTests({
       return data
     },
     async account({ provider, providerAccountId }) {
-      const data = await client.db.nextauth_accounts.filter({ provider, providerAccountId }).getFirst()
+      const data = await client.db.nextauth_accounts
+        .filter({ provider, providerAccountId })
+        .getFirst()
       if (!data) return null
       return data
     },
     async session(sessionToken) {
-      const data = await client.db.nextauth_sessions.filter({ sessionToken }).getFirst()
+      const data = await client.db.nextauth_sessions
+        .filter({ sessionToken })
+        .getFirst()
       if (!data) return null
       return data
     },
     async verificationToken(where) {
-      const data = await client.db.nextauth_verificationTokens.filter(where).getFirst()
+      const data = await client.db.nextauth_verificationTokens
+        .filter(where)
+        .getFirst()
       if (!data) return null
       return data
     },
