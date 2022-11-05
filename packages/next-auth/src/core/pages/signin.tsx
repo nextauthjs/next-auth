656b5930ad64f888495f6fc841abb34652b345fb
GMaiolo <guille.maiolo@gmail.com>
2022-11-05 18:32:30 -0300

    fix: extra check for error query param

--- packages/next-auth/src/core/pages/signin.tsx
+++ packages/next-auth/src/core/pages/signin.tsx
@@ -71,7 +71,9 @@ export default function SigninPage(props: SignInServerPageParams) {
     default: "Unable to sign in.",
   }
 
-  const error = errorType && (errors[errorType] ?? errors.default)
+  const error =
+    (errorType && Object.hasOwn(errors, errorType) && errors[errorType]) ||
+    errors.default
 
   return (
     <div className="signin">
