5d3253c84b81bf4d347a4ad43ea191fb24ed585d
Balázs Orbán <info@balazsorban.com>
2022-11-09 03:24:24 +0100

    fix(next): HIDE `NEXTAUTH_URL` warning locally

--- packages/next-auth/src/utils/detect-host.ts
+++ packages/next-auth/src/utils/detect-host.ts
@@ -1,8 +1,8 @@
 /** Extract the host from the environment */
 export function detectHost(forwardedHost: any) {
-  // If we detect a Vercel environment, we can trust the host
-  if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST)
-    return forwardedHost
-  // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
-  return process.env.NEXTAUTH_URL
+  if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST) return forwardedHost
+
+  return process.env.NEXTAUTH_URL ?? process.env.NODE_ENV !== "production"
+    ? "http://localhost:3000"
+    : undefined
 }
