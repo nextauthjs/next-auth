1ac3da534f414b4f45a0212b14997e904aa15791
Sebastien Castiel <sebastien@castiel.me>
2022-11-10 19:32:14 -0500

    Allow getToken to be called without req

--- apps/dev/app/server-component/page.tsx
+++ apps/dev/app/server-component/page.tsx
@@ -1,6 +1,15 @@
+import { getToken } from "next-auth/jwt"
 import { unstable_getServerSession } from "next-auth/next"
 
 export default async function Page() {
   const session = await unstable_getServerSession()
-  return <pre>{JSON.stringify(session, null, 2)}</pre>
+  const token = await getToken()
+  return (
+    <>
+      <h2>Session</h2>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <h2>Token</h2>
+      <pre>{JSON.stringify(token, null, 2)}</pre>
+    </>
+  )
 }
