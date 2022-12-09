b649ca5e7e397dafbb5f9c11eaff4c76d33aa372
Balázs Orbán <info@balazsorban.com>
2022-12-09 14:59:28 +0100

    revert middleware

--- packages/next-auth/src/utils/web.ts
+++ packages/next-auth/src/utils/web.ts
@@ -93,3 +93,16 @@ export function toResponse(res: ResponseInternal): Response {
 
   return response
 }
+
+/** Extract the host from the environment */
+export function detectHost(
+  trusted: boolean,
+  forwardedValue: string | string[] | undefined | null,
+  defaultValue: string | false
+): string | undefined {
+  if (trusted && forwardedValue) {
+    return Array.isArray(forwardedValue) ? forwardedValue[0] : forwardedValue
+  }
+
+  return defaultValue || undefined
+}
