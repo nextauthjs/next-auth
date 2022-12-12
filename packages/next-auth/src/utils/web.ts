5259d247a23d21e379ac59b13f3531b1f3d84f8f
Balázs Orbán <info@balazsorban.com>
2022-12-12 11:59:32 +0100

    fix(next): correctly bundle next-auth/middleware fixes #6025

--- packages/next-auth/src/utils/web.ts
+++ packages/next-auth/src/utils/web.ts
@@ -119,17 +119,3 @@ export function toResponse(res: ResponseInternal): Response {
 
   return response
 }
-
-// TODO: Remove
-/** Extract the host from the environment */
-export function detectHost(
-  trusted: boolean,
-  forwardedValue: string | string[] | undefined | null,
-  defaultValue: string | false
-): string | undefined {
-  if (trusted && forwardedValue) {
-    return Array.isArray(forwardedValue) ? forwardedValue[0] : forwardedValue
-  }
-
-  return defaultValue || undefined
-}
