575bcb57101f4cc11aaec33628a7dd1cd6220714
Balázs Orbán <info@balazsorban.com>
2022-12-13 23:45:32 +0100

    chore: format sveltekit playground

--- apps/playground-sveltekit/src/lib/SignInButton.svelte
+++ apps/playground-sveltekit/src/lib/SignInButton.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-	export let provider: any;
+  export let provider: any
 </script>
 
 <form action={provider.signinUrl} method="POST">
-	{#if provider.callbackUrl}
-		<input type="hidden" name="callbackUrl" value={provider.callbackUrl} />
-	{/if}
-	<button type="submit" class="button">
-		<slot>Sign in with {provider.name}</slot>
-	</button>
+  {#if provider.callbackUrl}
+    <input type="hidden" name="callbackUrl" value={provider.callbackUrl} />
+  {/if}
+  <button type="submit" class="button">
+    <slot>Sign in with {provider.name}</slot>
+  </button>
 </form>
--- apps/playground-sveltekit/src/routes/+layout.server.ts
+++ apps/playground-sveltekit/src/routes/+layout.server.ts
@@ -1,13 +1,11 @@
 import type { LayoutServerLoad } from "./$types"
 
 export const load: LayoutServerLoad = (event) => {
-  console.log('layout server load', event.locals.getSession)
+  console.log("layout server load", event.locals.getSession)
   let session
-  if (event.locals.getSession)
-   {
-  session = event.locals.getSession()
-
-   }
+  if (event.locals.getSession) {
+    session = event.locals.getSession()
+  }
   return {
     session,
   }
