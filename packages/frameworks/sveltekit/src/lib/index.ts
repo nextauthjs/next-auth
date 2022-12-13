c89e922256439b28c0fcbc69d9df4afe96678d7b
Thang Vu <hi@thvu.dev>
2022-12-13 22:25:59 +0700

    commit

--- packages/frameworks/sveltekit/src/lib/index.ts
+++ packages/frameworks/sveltekit/src/lib/index.ts
@@ -2,7 +2,7 @@ import type { Handle } from '@sveltejs/kit';
 import { AUTH_SECRET, NEXTAUTH_URL, AUTH_TRUST_HOST, VERCEL } from '$env/static/private';
 import { dev } from '$app/environment';
 
-import { AuthHandler, type AuthOptions } from 'next-auth-core';
+import { AuthHandler, type AuthOptions, type AuthAction } from 'next-auth-core';
 
 export const getServerSession = async (req: Request, options: AuthOptions): Promise<unknown> => {
 	options.secret ??= AUTH_SECRET;
@@ -29,6 +29,7 @@ interface SvelteKitAuthOptions extends AuthOptions {
 	 */
 	prefix?: string;
 }
+
 const actions = [ "providers", "session", "csrf", "signin", "signout", "callback", "verify-request", "error", "_log" ]
 
 /** The main entry point to next-auth-sveltekit */
@@ -40,17 +41,13 @@ function SvelteKitAuth({prefix = '/auth', ...options }: SvelteKitAuthOptions) {
 	return (({ event, resolve }) => {
 		const [action] = event.url.pathname.slice(prefix.length + 1).split('/');
 		const isAuth = actions.includes(action)
-		event.locals.getSession = async () => {
-			const session = getServerSession(event.request, options)
-			return session
-		};
-    if (!event.url.pathname.startsWith(prefix + '/') || !isAuth) {
+		
+		event.locals.getSession = async () => getServerSession(event.request, options)
+    
+		if (!event.url.pathname.startsWith(prefix + '/') || !isAuth) {
 			return resolve(event)
 		}
-		event.locals.getSession = async () => {
-			const session = getServerSession(event.request, options)
-			return session
-		};
+
 		return AuthHandler(event.request, options);
   }) satisfies Handle
 }
--- packages/frameworks/sveltekit/svelte.config.js
+++ packages/frameworks/sveltekit/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
