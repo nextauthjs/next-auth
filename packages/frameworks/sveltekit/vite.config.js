3c0e829f93bbe56b691b52be064220cc7bf78d08
Balázs Orbán <info@balazsorban.com>
2022-12-13 20:58:39 +0100

    format

--- packages/frameworks/sveltekit/vite.config.js
+++ packages/frameworks/sveltekit/vite.config.js
@@ -1,11 +1,11 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+import { sveltekit } from "@sveltejs/kit/vite"
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
-};
+  plugins: [sveltekit()],
+  test: {
+    include: ["src/**/*.{test,spec}.{js,ts}"],
+  },
+}
 
-export default config;
+export default config
