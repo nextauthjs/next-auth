c9bafab1af0a9917ae1a8cdaa178716be6d86c42
Balázs Orbán <info@balazsorban.com>
2022-12-14 13:35:59 +0100

    clean up playground and sveltekit docs

--- apps/playground-sveltekit/src/routes/+page.svelte
+++ apps/playground-sveltekit/src/routes/+page.svelte
@@ -5,13 +5,8 @@
 
 <h1>SvelteKit Auth Example</h1>
 <p>
-  This is an example site to demonstrate how to use <a
-    href="https://kit.svelte.dev/">SvelteKit</a
-  >
-  with <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-
-  {#if Object.keys($page.data.session || {}).length}
-    {#if $page.data.session.user.image}
+  {#if $page.data.session}
+    {#if $page.data.session.user?.image}
       <span
         style="background-image: url('{$page.data.session.user.image}')"
         class="avatar"
@@ -19,9 +14,7 @@
     {/if}
     <span class="signedInText">
       <small>Signed in as</small><br />
-      <strong
-        >{$page.data.session.user.email || $page.data.session.user.name}</strong
-      >
+      <strong>{$page.data.session.user?.name ?? "User"}</strong>
     </span>
     <button on:click={() => signOut()} class="button">Sign out</button>
   {:else}
