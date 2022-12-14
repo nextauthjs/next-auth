6edf577316da39c3c2d9e5fa4173352cbab4c2a9
Thang Vu <hi@thvu.dev>
2022-12-14 15:30:02 +0700

    fix(frameworks): run check before building for @auth/sveltekit

--- packages/frameworks-sveltekit/src/lib/index.ts
+++ packages/frameworks-sveltekit/src/lib/index.ts
@@ -2,6 +2,7 @@ import { AUTH_SECRET, AUTH_TRUST_HOST, VERCEL } from "$env/static/private"
 import { dev } from "$app/environment"
 
 import { AuthHandler, type AuthOptions, type AuthAction } from "@auth/core"
+import type { Handle } from "@sveltejs/kit"
 
 export async function getServerSession(
   req: Request,
@@ -48,7 +49,7 @@ const actions: AuthAction[] = [
 ]
 
 /** The main entry point to @auth/sveltekit */
-function SvelteKitAuth({ prefix = "/auth", ...options }: SvelteKitAuthOptions) {
+function SvelteKitAuth({ prefix = "/auth", ...options }: SvelteKitAuthOptions): Handle {
   options.secret ??= AUTH_SECRET
   options.trustHost ??= !!(AUTH_TRUST_HOST ?? VERCEL ?? dev)
 
@@ -65,7 +66,7 @@ function SvelteKitAuth({ prefix = "/auth", ...options }: SvelteKitAuthOptions) {
     }
 
     return AuthHandler(event.request, options)
-  }) satisfies Handle
+  })
 }
 
 export default SvelteKitAuth
