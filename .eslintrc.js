84291d3e8141caf0a5c0f982f0f325c0fe2d27b5
Balázs Orbán <info@balazsorban.com>
2022-12-16 15:57:55 +0100

    chore: fix formatting and linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
+// @ts-check
 const path = require("path")
 
+/** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
   root: true,
   parser: "@typescript-eslint/parser",
+  extends: ["standard-with-typescript", "prettier"],
+  rules: {
+    camelcase: "off",
+    "@typescript-eslint/naming-convention": "off",
+    "@typescript-eslint/strict-boolean-expressions": "off",
+    "@typescript-eslint/explicit-function-return-type": "off",
+    "@typescript-eslint/restrict-template-expressions": "off",
+  },
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
-      extends: ["standard-with-typescript", "prettier"],
-      rules: {
-        camelcase: "off",
-        "@typescript-eslint/naming-convention": "off",
-        "@typescript-eslint/strict-boolean-expressions": "off",
-        "@typescript-eslint/explicit-function-return-type": "off",
-        "@typescript-eslint/restrict-template-expressions": "off",
-      },
-
       parserOptions: {
         project: [
           path.resolve(__dirname, "./packages/**/tsconfig.eslint.json"),
@@ -23,19 +24,23 @@ module.exports = {
         ],
       },
     },
+    {
+      files: ["*.test.ts", "*.test.js"],
+      env: { jest: true },
+    },
+    {
+      files: ["docs"],
+      plugins: ["@docusaurus"],
+      extends: ["plugin:@docusaurus/recommended"],
+    },
   ],
-  extends: ["prettier"],
-  globals: {
-    localStorage: "readonly",
-    location: "readonly",
-    fetch: "readonly",
-  },
-  rules: {
-    camelcase: "off",
-  },
   plugins: ["jest"],
-  env: {
-    "jest/globals": true,
-  },
-  ignorePatterns: [".eslintrc.js"],
+  ignorePatterns: [
+    "**/dist/**",
+    "**/node_modules/**",
+    ".eslintrc.js",
+    "**/.turbo/**",
+    "**/coverage/**",
+    "**/build/**",
+  ],
 }
