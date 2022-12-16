a8a0beff3f758aae5eca6c3f646adcc17a8c5d9e
Robert Soriano <sorianorobertc@gmail.com>
2022-12-15 18:47:44 -0800

    chore(examples): use `@auth/core` in Nuxt playground (#6081)
    * nuxt: rewrite server handler to use @auth/core
    
    * nuxt: fix main tsconfig
    
    * nuxt: remove unused module
    
    * nuxt: update client and server ports
    
    * nuxt: remove iframe style
    
    * nuxt: update readme

--- apps/playground-nuxt/playground/pages/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <h1>Nuxt 3 + NextAuth.js Example</h1>
-    <p>
-      This is an example site to demonstrate how to use <a href="https://v3.nuxtjs.org/">Nuxt 3</a> with <a href="https://next-auth.js.org">NextAuth.js</a> for authentication.
-    </p>
-  </div>
-</template>
