582a3c339a04bf8471101faadb1a23603e6fc4ee
Balázs Orbán <info@balazsorban.com>
2022-12-21 01:54:33 +0100

    refactor(core): move types moduile to top-level

---
+++ packages/core/src/types.ts
@@ -0,0 +1,10 @@
+/**
+ *
+ * The `@auth/core/types` module contains all public types and interfaces of the core package.
+ *
+ * @module types
+ */
+
+// TODO: move types directly into this file.
+
+export * from "./lib/types.js"
