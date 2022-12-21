ea23a9344299f0643a4c88c50bda6500b44b326e
Balázs Orbán <info@balazsorban.com>
2022-12-21 01:39:35 +0100

    refactor(core): move jwt to a single file

--- packages/core/src/jwt.ts
+++ packages/core/src/jwt.ts
@@ -1,15 +1,16 @@
-import { EncryptJWT, jwtDecrypt } from "jose"
-import { hkdf } from "@panva/hkdf"
-import { SessionStore } from "../lib/cookie.js"
-import type {
-  JWT,
-  JWTDecodeParams,
-  JWTEncodeParams,
-  JWTOptions,
-} from "./types.js"
-import type { LoggerInstance } from "../index.js"
+/**
+ * ::: warning
+ * This module is is not finished yet. The documentation/API is a work in progress and will change.
+ * :::
+ *
+ * @module core/jwt
+ */
 
-export * from "./types.js"
+import { hkdf } from "@panva/hkdf"
+import { EncryptJWT, jwtDecrypt } from "jose"
+import { SessionStore } from "./lib/cookie.js"
+import { Awaitable } from "./lib/types.js"
+import type { LoggerInstance } from "./lib/utils/logger.js"
 
 const DEFAULT_MAX_AGE = 30 * 24 * 60 * 60 // 30 days
 
@@ -27,7 +28,7 @@ export async function encode(params: JWTEncodeParams) {
     .encrypt(encryptionSecret)
 }
 
-/** Decodes a NextAuth.js issued JWT. */
+/** Decodes a Auth.js issued JWT. */
 export async function decode(params: JWTDecodeParams): Promise<JWT | null> {
   const { token, secret } = params
   if (!token) return null
@@ -58,7 +59,7 @@ export interface GetTokenParams<R extends boolean = false> {
   raw?: R
   /**
    * The same `secret` used in the `NextAuth` configuration.
-   * Defaults to the `NEXTAUTH_SECRET` environment variable.
+   * Defaults to the `AUTH_SECRET` environment variable.
    */
   secret?: string
   decode?: JWTOptions["decode"]
@@ -66,7 +67,7 @@ export interface GetTokenParams<R extends boolean = false> {
 }
 
 /**
- * Takes a NextAuth.js request (`req`) and returns either the NextAuth.js issued JWT's payload,
+ * Takes a Auth.js request (`req`) and returns either the Auth.js issued JWT's payload,
  * or the raw JWT string. We look for the JWT in the either the cookies, or the `Authorization` header.
  * [Documentation](https://next-auth.js.org/tutorials/securing-pages-and-api-routes#using-gettoken)
  */
@@ -83,7 +84,7 @@ export async function getToken<R extends boolean = false>(
     raw,
     decode: _decode = decode,
     logger = console,
-    secret = process.env.NEXTAUTH_SECRET,
+    secret = process.env.AUTH_SECRET,
   } = params
 
   if (!req) throw new Error("Must pass `req` to JWT getToken()")
@@ -122,12 +123,66 @@ export async function getToken<R extends boolean = false>(
   }
 }
 
-async function getDerivedEncryptionKey(secret: string | Buffer) {
+async function getDerivedEncryptionKey(secret: string) {
   return await hkdf(
     "sha256",
     secret,
     "",
-    "NextAuth.js Generated Encryption Key",
+    "Auth.js Generated Encryption Key",
     32
   )
+}
+
+export interface DefaultJWT extends Record<string, unknown> {
+  name?: string | null
+  email?: string | null
+  picture?: string | null
+  sub?: string
+}
+
+/**
+ * Returned by the `jwt` callback and `getToken`, when using JWT sessions
+ *
+ * [`jwt` callback](https://next-auth.js.org/configuration/callbacks#jwt-callback) | [`getToken`](https://next-auth.js.org/tutorials/securing-pages-and-api-routes#using-gettoken)
+ */
+export interface JWT extends Record<string, unknown>, DefaultJWT {}
+
+export interface JWTEncodeParams {
+  /** The JWT payload. */
+  token?: JWT
+  /** The secret used to encode the Auth.js issued JWT. */
+  secret: string
+  /**
+   * The maximum age of the Auth.js issued JWT in seconds.
+   *
+   * @default 30 * 24 * 30 * 60 // 30 days
+   */
+  maxAge?: number
+}
+
+export interface JWTDecodeParams {
+  /** The Auth.js issued JWT to be decoded */
+  token?: string
+  /** The secret used to decode the Auth.js issued JWT. */
+  secret: string
+}
+
+export interface JWTOptions {
+  /**
+   * The secret used to encode/decode the Auth.js issued JWT.
+   *
+   * @deprecated  Set the `AUTH_SECRET` environment vairable or
+   * use the top-level `secret` option instead
+   */
+  secret: string
+  /**
+   * The maximum age of the Auth.js issued JWT in seconds.
+   *
+   * @default 30 * 24 * 30 * 60 // 30 days
+   */
+  maxAge: number
+  /** Override this method to control the Auth.js issued JWT encoding. */
+  encode: (params: JWTEncodeParams) => Awaitable<string>
+  /** Override this method to control the Auth.js issued JWT decoding. */
+  decode: (params: JWTDecodeParams) => Awaitable<JWT | null>
 }
