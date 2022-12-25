8ef1010bb1553f05236a0cd1dfd68cae32c5d804
Balázs Orbán <info@balazsorban.com>
2022-12-25 06:38:29 +0100

    chore: attempt to fix supabase test
    SUPABASE_SERVICE_ROLE_KEY could be an empty string

--- packages/adapter-supabase/tests/index.test.ts
+++ packages/adapter-supabase/tests/index.test.ts
@@ -10,7 +10,7 @@ import type { Account } from "next-auth"
 
 const url = process.env.SUPABASE_URL ?? "http://localhost:54321"
 const secret =
-  process.env.SUPABASE_SERVICE_ROLE_KEY ??
+  process.env.SUPABASE_SERVICE_ROLE_KEY ||
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.vI9obAHOGyVVKa3pD--kJlyxp-Z2zV9UUMAhKpNLAcU"
 
 const supabase = createClient(url, secret, { db: { schema: "next_auth" } })
