2337dd9f440ebf48085651b98ac00da080ad27ba
Finn <34718806+FinnDore@users.noreply.github.com>
2023-01-03 21:35:57 +0000

    added mattermost provider

---
+++ packages/core/src/providers/mattermost.ts
@@ -0,0 +1,53 @@
+import { OAuthConfig } from "./oauth"
+
+export interface MattermostProfile extends Record<string, any> {
+  id: string
+  username: string
+  email: string
+}
+
+export default function mattermostProvider({
+  mattermostUrl: mmUrl,
+  clientId: client_id,
+  clientSecret: client_secret,
+  callbackUrl: callbackUri,
+}: {
+  mattermostUrl: string
+  clientId: string
+  clientSecret: string
+  callbackUrl: string
+}) {
+  return {
+    id: "mattermost",
+    name: "Mattermost",
+    type: "oauth",
+    token: {
+      url: `${mmUrl}/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}`,
+    },
+    authorization: {
+      url: `${mmUrl}/oauth/authorize`,
+      params: {
+        callbackUri,
+      },
+    },
+    userinfo: {
+      async request({ tokens }) {
+        const profile = await fetch(new URL(`${mmUrl}/api/v4/users/me`), {
+          headers: {
+            Authorization: `Bearer ${tokens.access_token}`,
+          },
+        }).then(async (res) => await res.json())
+        return profile
+      },
+    },
+    profile(profile) {
+      return {
+        id: profile.id,
+        username: profile.username,
+        email: profile.email,
+      }
+    },
+    clientId: client_id,
+    clientSecret: client_secret,
+  } satisfies OAuthConfig<MattermostProfile>
+}
