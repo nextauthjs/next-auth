bd9c4b0d6a012cf3a873c7d64872380186d71d94
Balázs Orbán <info@balazsorban.com>
2023-01-05 13:11:36 +0100

    add JSDoc comments

--- packages/core/src/providers/mattermost.ts
+++ packages/core/src/providers/mattermost.ts
@@ -3,8 +3,11 @@ import type { OAuthConfig, OAuthUserConfig } from "./oauth"
 /** @see [Get a user](https://api.mattermost.com/#tag/users/operation/GetUser) */
 export interface MattermostProfile {
   id: string
+  /** The time in milliseconds a user was created */
   create_at: number
+  /** The time in milliseconds a user was last updated */
   update_at: number
+  /** The time in milliseconds a user was deleted */
   delete_at: number
   username: string
   auth_data: string
@@ -17,15 +20,22 @@ export interface MattermostProfile {
   position: string
   roles: string
   notify_props: {
+    /** Set to "true" to enable channel-wide notifications (@channel, @all, etc.), "false" to disable. Defaults to "true". */
     channel: string
     comments: string
+    /** Set to "all" to receive desktop notifications for all activity, "mention" for mentions and direct messages only, and "none" to disable. Defaults to "all". */
     desktop: string
+    /** Set to "true" to enable sound on desktop notifications, "false" to disable. Defaults to "true". */
     desktop_sound: string
     desktop_threads: string
+    /** Set to "true" to enable email notifications, "false" to disable. Defaults to "true". */
     email: string
     email_threads: string
+    /** Set to "true" to enable mentions for first name. Defaults to "true" if a first name is set, "false" otherwise. */
     first_name: string
+    /** A comma-separated list of words to count as mentions. Defaults to username and @username. */
     mention_keys: string
+    /** Set to "all" to receive push notifications for all activity, "mention" for mentions and direct messages only, and "none" to disable. Defaults to "mention". */
     push: string
     push_status: string
     push_threads: string
@@ -33,11 +43,18 @@ export interface MattermostProfile {
   last_password_update: number
   locale: string
   timezone: {
+    /** This value is set automatically when the "useAutomaticTimezone" is set to "true". */
     automaticTimezone: string
+    /** Value when setting manually the timezone, i.e. "Europe/Berlin". */
     manualTimezone: string
+    /** Set to "true" to use the browser/system timezone, "false" to set manually. Defaults to "true". */
     useAutomaticTimezone: string
   }
   disable_welcome_email: boolean
+  /** ID of accepted terms of service, if any. This field is not present if empty. */
+  terms_of_service_id: string
+  /** The time in milliseconds the user accepted the terms of service */
+  terms_of_service_create_at: number
 }
 
 export default function Mattermost<P extends MattermostProfile>(
