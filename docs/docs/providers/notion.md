7e0422a23d50184e8707dc5e3368f14e650226d1
Harrison Broadbent <harrisonbroadbent@Harrisons-MacBook-Air.local>
2023-01-06 17:23:53 +1100

    update docs and provider with code from TomYeoman

--- docs/docs/providers/notion.md
+++ docs/docs/providers/notion.md
@@ -5,6 +5,8 @@ title: Notion
 
 ## Documentation
 
+https://developers.notion.com/docs
+
 https://developers.notion.com/docs/authorization
 
 ## Configuration
@@ -21,6 +23,8 @@ The **Notion Provider** comes with a set of default options:
 
 You can override any of the options to suit your own use case.
 
+You must provide a `clientId` and `clientSecret` to use this provider, as-well as a redirect URI (due to this being required by Notion endpoint to fetch tokens, this is documented in the [notion provovider](https://github.com/nextauthjs/next-auth/blob/main/packages/next-auth/src/providers/notion.ts)).
+
 ## Example
 
 ```js
@@ -30,7 +34,68 @@ providers: [
   NotionProvider({
     clientId: process.env.NOTION_CLIENT_ID,
     clientSecret: process.env.NOTION_CLIENT_SECRET
+    redirectUri: "http://localhost:3000/api/auth/callback/notion",
   })
-]
+],
 ...
+```
+
+## Additional Notes
+
+### Next Auth Database Integration
+
+If you're using the next auth database integration (in this case, we'll be using prisma + sqlite), there are some additional steps to follow.
+
+1. Make sure your `Account` definition contains the extra fields that are returned by the access token endpoint, at time of writing this includes;
+
+```ts
+model Account {
+  id                     String  @id @default(cuid())
+  userId                 String
+  type                   String
+  provider               String
+  providerAccountId      String
+  bot_id                 String? // @db.Text
+  workspace_name         String? // @db.Text
+  workspace_icon         String? // @db.Text
+  workspace_id           String? // @db.Text
+  duplicated_template_id String? // @db.Text
+  refresh_token          String? // @db.Text
+  access_token           String? // @db.Text
+  expires_at             Int?
+  token_type             String?
+  scope                  String?
+  id_token               String? // @db.Text
+  session_state          String?
+  user                   User    @relation(fields: [userId], references: [id], onDelete: Cascade)
+
+  @@unique([provider, providerAccountId])
+}
+```
+
+
+2. If you're using SQLITE, you will find the "owner" returned in the call to fetch access token is JSON, and is not supported for example by SQLITE,
+It reccomended you either remove it (as this code does), or you could stringify and store as text if you want
+
+```ts
+const adapter = {
+  ...PrismaAdapter(prisma),
+  linkAccount: ({ owner, ...data }: any) => {
+    debugger
+    return prisma.account.create({ data })
+  },
+}
+
+export const authOptions: NextAuthOptions = {
+  adapter,
+  providers: [
+    NotionProvider({
+      clientId: env.NOTION_CLIENT_ID,
+      clientSecret: env.NOTION_CLIENT_SECRET,
+      redirectUri: "http://localhost:3000/api/auth/callback/notion",
+    }),
+  ],
+}
+
+export default NextAuth(authOptions)
 ```
--- packages/next-auth/src/providers/notion.ts
+++ packages/next-auth/src/providers/notion.ts
@@ -1,49 +1,106 @@
-import type { OAuthConfig, OAuthUserConfig } from "."
+import type { OAuthConfig, OAuthUserConfig } from ".";
 
-export interface NotionProfile extends Record<string, any> {
-	object: string
-	id: string
-	name: string
-	avatar_url: string
-	type: string
-	person: {
-		email: string
-	}
+export interface Person extends Record<string, any> {
+	email: string;
 }
 
-export default function Notion<P extends NotionProfile>(
-	options: OAuthUserConfig<P>
+// https://developers.notion.com/reference/user
+export interface User extends Record<string, any> {
+	object: "user" | "bot";
+	id: string;
+	type: string;
+	name: string;
+	avatar_url: null | string;
+	person: Person;
+	owner?: {
+		type: "workspace" | "user";
+		workspace: string;
+	};
+	workspace_name?: string | null;
+}
+
+export type Owner = {
+	type: string;
+	user: User;
+};
+
+// Notion responds with an access_token + some additional information, which we define here
+// More info -  https://developers.notion.com/docs/authorization#step-4-notion-responds-with-an-access_token-and-some-additional-information
+export interface NotionProfile extends Record<string, any> {
+	access_token: string;
+	bot_id: string;
+	duplicated_template_id: string;
+	owner?: Owner;
+	workspace_icon: string;
+	workspace_id: number;
+	workspace_name: string;
+}
+
+// Any config required that isn't part of the `OAuthUserConfig` spec should belong here
+// For example, we must pass a `redirectUri` to the Notion API when requesting tokens, therefore we add it here
+interface AdditionalConfig {
+	redirectUri: string;
+}
+
+const NOTION_HOST = "https://api.notion.com";
+const NOTION_API_VERSION = "2022-06-28";
+
+
+export default function NotionProvider<P extends NotionProfile>(
+	options: OAuthUserConfig<P> & AdditionalConfig
 ): OAuthConfig<P> {
 	return {
 		id: "notion",
 		name: "Notion",
 		type: "oauth",
-		authorization: "https://api.notion.com/v1/oauth/authorize",
-		token: "https://api.notion.com/v1/oauth/token",
+		token: {
+			params: {
+				grant_type: "authorization_code",
+				clientId: options.clientId,
+				clientSecret: options.clientSecret,
+			},
+			url: `${NOTION_HOST}/v1/oauth/token`,
+		},
 		userinfo: {
-			async request({ tokens }) {
-				const res = await fetch("https://api.notion.com/v1/users/me", {
+			url: `${NOTION_HOST}/v1/users`,
+
+			// The result of this method will be the input to the `profile` callback. 
+			// We use a custom request handler, since we need to do things such as pass the "Notion-Version" header
+			// More info: https://next-auth.js.org/configuration/providers/oauth
+			async request(context) {
+				const profile = await fetch(`${NOTION_HOST}/v1/users/me`, {
 					headers: {
-						Authorization: `Bearer ${tokens.access_token}`,
-						"Notion-Version": "2022-06-28",
+						Authorization: `Bearer ${context.tokens.access_token}`,
+						"Notion-Version": NOTION_API_VERSION,
 					},
-				})
+				});
 
 				const {
 					bot: {
 						owner: { user },
 					},
-				} = await res.json()
-				return user
+				} = await profile.json();
+
+				return user;
 			},
 		},
-		profile(profile) {
+		authorization: {
+			params: {
+				client_id: options.clientId,
+				response_type: "code",
+				owner: "user",
+				redirect_uri: options.redirectUri,
+			},
+			url: `${NOTION_HOST}/v1/oauth/authorize`,
+		},
+
+		async profile(profile, tokens) {
 			return {
 				id: profile.id,
 				name: profile.name,
 				email: profile.person.email,
 				image: profile.avatar_url,
-			}
+			};
 		},
 		style: {
 			logo: "https://raw.githubusercontent.com/nextauthjs/next-auth/main/packages/next-auth/provider-logos/notion.svg",
@@ -55,5 +112,5 @@ export default function Notion<P extends NotionProfile>(
 			textDark: "#000",
 		},
 		options,
-	}
+	};
 }
