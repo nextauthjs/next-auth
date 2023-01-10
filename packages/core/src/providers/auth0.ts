3c7c25cefade7255704ae9cd545a7ce05951347d
Balázs Orbán <info@balazsorban.com>
2023-01-10 12:51:39 +0100

    docs: improve some provider docs

--- packages/core/src/providers/auth0.ts
+++ packages/core/src/providers/auth0.ts
@@ -10,14 +10,68 @@
  * @module providers/auth0
  */
 
-import type { OAuthConfig, OAuthUserConfig } from "./index.js"
+import type { OIDCConfig, OIDCUserConfig } from "./index.js"
 
 /** @see [User Profile Structure](https://auth0.com/docs/manage-users/user-accounts/user-profiles/user-profile-structure) */
-export interface Auth0Profile extends Record<string, any> {
+export interface Auth0Profile {
+  /** The user's unique identifier. */
   sub: string
-  nickname: string
+  /** Custom fields that store info about a user that influences the user's access, such as support plan, security roles (if not using the Authorization Core feature set), or access control groups. To learn more, read Metadata Overview. */
+  app_metadata: object
+  /** Indicates whether the user has been blocked. Importing enables subscribers to ensure that users remain blocked when migrating to Auth0. */
+  blocked: boolean
+  /** Timestamp indicating when the user profile was first created. */
+  created_at: Date
+  /** (unique) The user's email address. */
   email: string
+  /** Indicates whether the user has verified their email address. */
+  email_verified: boolean
+  /** The user's family name. */
+  family_name: string
+  /** The user's given name. */
+  given_name: string
+  /** Custom fields that store info about a user that does not impact what they can or cannot access, such as work address, home address, or user preferences. To learn more, read Metadata Overview. */
+  user_metadata: object
+  /** (unique) The user's username. */
+  username: string
+  /** Contains info retrieved from the identity provider with which the user originally authenticates. Users may also link their profile to multiple identity providers; those identities will then also appear in this array. The contents of an individual identity provider object varies by provider. In some cases, it will also include an API Access Token to be used with the provider. */
+  identities: Array<{
+    /** Name of the Auth0 connection used to authenticate the user. */
+    connection: string
+    /** Indicates whether the connection is a social one. */
+    isSocial: boolean
+    /** Name of the entity that is authenticating the user, such as Facebook, Google, SAML, or your own provider. */
+    provider: string
+    /** User's unique identifier for this connection/provider. */
+    user_id: string
+    /** User info associated with the connection. When profiles are linked, it is populated with the associated user info for secondary accounts. */
+    profileData: object
+    [key: string]: any
+  }>
+  /** IP address associated with the user's last login. */
+  last_ip: string
+  /** Timestamp indicating when the user last logged in. If a user is blocked and logs in, the blocked session updates last_login. If you are using this property from inside a Rule using the user< object, its value will be associated with the login that triggered the rule; this is because rules execute after login. */
+  last_login: Date
+  /** Timestamp indicating the last time the user's password was reset/changed. At user creation, this field does not exist. This property is only available for Database connections. */
+  last_password_reset: Date
+  /** Number of times the user has logged in. If a user is blocked and logs in, the blocked session is counted in logins_count. */
+  logins_count: number
+  /** List of multi-factor providers with which the user is enrolled. */
+  multifactor: string
+  /** The user's full name. */
+  name: string
+  /** The user's nickname. */
+  nickname: string
+  /** The user's phone number. Only valid for users with SMS connections. */
+  phone_number: string
+  /** Indicates whether the user has been verified their phone number. Only valid for users with SMS connections. */
+  phone_verified: boolean
+  /** URL pointing to the user's profile picture. */
   picture: string
+  /** Timestamp indicating when the user's profile was last updated/modified. Changes to last_login are considered updates, so most of the time, updated_at will match last_login. */
+  updated_at: Date
+  /** (unique) The user's identifier. Importing allows user records to be synchronized across multiple systems without using mapping tables. */
+  user_id: string
 }
 
 /**
@@ -25,9 +79,7 @@ export interface Auth0Profile extends Record<string, any> {
  *
  * ## Example
  *
- * @example
- *
- * ```js
+ * ```ts
  * import { Auth } from "@auth/core"
  * import Auth0 from "@auth/core/providers/auth0"
  *
@@ -41,7 +93,7 @@ export interface Auth0Profile extends Record<string, any> {
  *
  * ## Resources
  *
- * @see [Link 1](https://example.com)
+ * - [Authenticate - Auth0 docs](https://auth0.com/docs/authenticate)
  *
  * ---
  *
@@ -68,9 +120,9 @@ export interface Auth0Profile extends Record<string, any> {
  * :::
  */
 
-export default function Auth0<P extends Auth0Profile>(
-  options: OAuthUserConfig<P>
-): OAuthConfig<P> {
+export default function Auth0(
+  config: OIDCUserConfig<Auth0Profile>
+): OIDCConfig<Auth0Profile> {
   return {
     id: "auth0",
     name: "Auth0",
@@ -83,6 +135,6 @@ export default function Auth0<P extends Auth0Profile>(
       bgDark: "#EB5424",
       textDark: "#fff",
     },
-    options,
+    options: config,
   }
 }
