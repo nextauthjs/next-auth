3bfa7407f75587aec378ca175cdc4377f91daee8
= <otterlord.dev@gmail.com>
2023-01-16 13:38:06 +0000

    feat: add astro package and example

---
+++ packages/frameworks-astro/src/lib/index.ts
@@ -0,0 +1,108 @@
+import { Auth } from "@auth/core"
+import type { AuthConfig, AuthAction, Session } from "@auth/core/types"
+import { type Cookie, parseString, splitCookiesString } from "set-cookie-parser"
+import { serialize } from "cookie"
+
+export interface AstroAuthConfig extends AuthConfig {
+  /**
+   * Defines the base path for the auth routes.
+   * @default '/api/auth'
+   */
+  prefix?: string
+}
+
+const actions: AuthAction[] = [
+  "providers",
+  "session",
+  "csrf",
+  "signin",
+  "signout",
+  "callback",
+  "verify-request",
+  "error",
+]
+
+// solves the same issue that exists in @auth/solid-js
+const getSetCookieCallback = (cook?: string | null): Cookie | undefined => {
+  if (!cook) return
+  const splitCookie = splitCookiesString(cook)
+  for (const cookName of [
+    "__Secure-next-auth.session-token",
+    "next-auth.session-token",
+    "next-auth.pkce.code_verifier",
+    "__Secure-next-auth.pkce.code_verifier",
+  ]) {
+    const temp = splitCookie.find((e) => e.startsWith(`${cookName}=`))
+    if (temp) {
+      return parseString(temp)
+    }
+  }
+  return parseString(splitCookie?.[0] ?? "") // just return the first cookie if no session token is found
+}
+
+async function handler(
+  prefix: string,
+  authConfig: AstroAuthConfig,
+  { request }: { request: Request }
+) {
+  const url = new URL(request.url)
+  const action = url.pathname
+    .slice(prefix.length + 1)
+    .split("/")[0] as AuthAction
+
+  if (!actions.includes(action) || !url.pathname.startsWith(prefix + "/"))
+    return
+
+  const res = await Auth(request, authConfig)
+
+  if (["callback", "signin", "signout"].includes(action)) {
+    const parsedCookie = getSetCookieCallback(
+      res.clone().headers.get("Set-Cookie")
+    )
+    if (parsedCookie) {
+      res.headers.set(
+        "Set-Cookie",
+        serialize(parsedCookie.name, parsedCookie.value, parsedCookie as any)
+      )
+    }
+  }
+  return res
+}
+
+export function AstroAuth(config: AstroAuthConfig) {
+  const { prefix = "/api/auth", ...authConfig } = config
+  // @ts-ignore import.meta.env is used by Astro
+  authConfig.secret ??= import.meta.env.AUTH_SECRET
+
+  return {
+    async get(event: any) {
+      return await handler(prefix, authConfig, event)
+    },
+    async post(event: any) {
+      return await handler(prefix, authConfig, event)
+    },
+  }
+}
+
+export async function getSession(
+  req: Request,
+  options: AuthConfig
+): Promise<Session | null> {
+  // @ts-ignore import.meta.env is used by Astro
+  options.secret ??= import.meta.env.AUTH_SECRET
+  options.trustHost ??= true
+
+  const url = new URL("/api/auth/session", req.url)
+  const response = await Auth(
+    new Request(url, { headers: req.headers }),
+    options
+  )
+
+  const { status = 200 } = response
+
+  const data = await response.json()
+
+  if (!data || !Object.keys(data).length) return null
+  if (status === 200) return data
+  throw new Error(data.message)
+}
---
+++ packages/frameworks-astro/tsup.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig((options) => ({
+  entry: ["src/**/*.ts"],
+  target: "esnext",
+  sourcemap: options.watch ? "inline" : false,
+  clean: true,
+  minify: false,
+  keepNames: false,
+  tsconfig: "./tsconfig.json",
+  format: ["esm"],
+  dts: true,
+  bundle: false,
+}));
