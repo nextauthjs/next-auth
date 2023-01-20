ff0d74cd9a98e524a1e13cb2bdd92afac882f649
Balázs Orbán <info@balazsorban.com>
2023-01-20 13:03:24 +0100

    update jest

--- packages/adapter-firebase/jest.config.js
+++ packages/adapter-firebase/jest.config.js
@@ -1,1 +1,10 @@
-module.exports = require("@next-auth/adapter-test/jest/jest-preset")
+import config from "@next-auth/adapter-test/jest/jest-preset.js"
+
+//TODO: update rest of the packages to Jest 29+
+const {testURL, ...rest} = config
+export default {
+  ...rest,
+  testEnvironmentOptions: {
+    url: testURL
+  }
+}
