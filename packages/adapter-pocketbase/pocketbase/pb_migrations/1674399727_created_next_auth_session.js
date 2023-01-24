d36077d5e6200bdd7ecadfae7d8cbd0e58a7097f
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2023-01-24 12:56:47 -0600

    fix: db migration updates

--- packages/adapter-pocketbase/pocketbase/pb_migrations/1674399727_created_next_auth_session.js
+++ packages/adapter-pocketbase/pocketbase/pb_migrations/1674399727_created_next_auth_session.js
@@ -1,63 +1,66 @@
-migrate((db) => {
-  const collection = new Collection({
-    "id": "0fbhwxgna5jb0k1",
-    "created": "2023-01-22 15:02:07.280Z",
-    "updated": "2023-01-22 15:02:07.280Z",
-    "name": "next_auth_session",
-    "type": "base",
-    "system": false,
-    "schema": [
-      {
-        "system": false,
-        "id": "8bwckxpy",
-        "name": "expires",
-        "type": "date",
-        "required": false,
-        "unique": false,
-        "options": {
-          "min": "",
-          "max": ""
-        }
-      },
-      {
-        "system": false,
-        "id": "fc4ddcvq",
-        "name": "sessionToken",
-        "type": "text",
-        "required": false,
-        "unique": false,
-        "options": {
-          "min": null,
-          "max": null,
-          "pattern": ""
-        }
-      },
-      {
-        "system": false,
-        "id": "bysaoe92",
-        "name": "userId",
-        "type": "text",
-        "required": false,
-        "unique": false,
-        "options": {
-          "min": null,
-          "max": null,
-          "pattern": ""
-        }
-      }
-    ],    
-    "listRule": "",
-    "viewRule": "",
-    "createRule": "",
-    "updateRule": "",
-    "deleteRule": "",
-    "options": {}
-  });
+migrate(
+  (db) => {
+    const collection = new Collection({
+      id: "0fbhwxgna5jb0k1",
+      created: "2023-01-22 15:02:07.280Z",
+      updated: "2023-01-22 15:02:07.280Z",
+      name: "next_auth_session",
+      type: "base",
+      system: false,
+      schema: [
+        {
+          system: false,
+          id: "bysaoe92",
+          name: "userId",
+          type: "relation",
+          required: false,
+          unique: false,
+          options: {
+            maxSelect: 1,
+            collectionId: "qhn8ngzgaw4k3x1",
+            cascadeDelete: true,
+          },
+        },
+        {
+          system: false,
+          id: "8bwckxpy",
+          name: "expires",
+          type: "date",
+          required: false,
+          unique: false,
+          options: {
+            min: "",
+            max: "",
+          },
+        },
+        {
+          system: false,
+          id: "fc4ddcvq",
+          name: "sessionToken",
+          type: "text",
+          required: false,
+          unique: false,
+          options: {
+            min: null,
+            max: null,
+            pattern: "",
+          },
+        },
+      ],
+      listRule: "",
+      viewRule: "",
+      createRule: "",
+      updateRule: "",
+      deleteRule: "",
+      options: {},
+    })
 
-  return Dao(db).saveCollection(collection);
-}, (db) => {
-  const dao = new Dao(db);
-  const collection = dao.findCollectionByNameOrId("0fbhwxgna5jb0k1");
+    return Dao(db).saveCollection(collection)
+  },
+  (db) => {
+    const dao = new Dao(db)
+    const collection = dao.findCollectionByNameOrId("0fbhwxgna5jb0k1")
 
-  return dao.deleteCollection(collection);
-})
+    return dao.deleteCollection(collection)
+  }
+)
