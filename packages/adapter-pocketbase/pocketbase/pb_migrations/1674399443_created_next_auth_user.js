fc9a7ee2eae9f29442c983199ce15d7f399f3d15
SETajK <72934958+SETajK@users.noreply.github.com>
2023-01-24 11:31:33 -0600

    feat/pocketbase adapter
    * init commit of pb adapter

---
+++ packages/adapter-pocketbase/pocketbase/pb_migrations/1674399443_created_next_auth_user.js
@@ -0,0 +1,76 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "qhn8ngzgaw4k3x1",
+    "created": "2023-01-22 14:57:23.571Z",
+    "updated": "2023-01-22 14:57:23.571Z",
+    "name": "next_auth_user",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "jzcv4fft",
+        "name": "name",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "1tzuoeth",
+        "name": "email",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "gtjm3xag",
+        "name": "emailVerified",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "kwsjxvn9",
+        "name": "image",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],
+    "listRule": "",
+    "viewRule": "",
+    "createRule": "",
+    "updateRule": "",
+    "deleteRule": "",
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("qhn8ngzgaw4k3x1");
+
+  return dao.deleteCollection(collection);
+})
---
+++ packages/adapter-pocketbase/pocketbase/pb_migrations/1674399727_created_next_auth_session.js
@@ -0,0 +1,63 @@
+migrate((db) => {
+  const collection = new Collection({
+    "id": "0fbhwxgna5jb0k1",
+    "created": "2023-01-22 15:02:07.280Z",
+    "updated": "2023-01-22 15:02:07.280Z",
+    "name": "next_auth_session",
+    "type": "base",
+    "system": false,
+    "schema": [
+      {
+        "system": false,
+        "id": "8bwckxpy",
+        "name": "expires",
+        "type": "date",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": "",
+          "max": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "fc4ddcvq",
+        "name": "sessionToken",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      },
+      {
+        "system": false,
+        "id": "bysaoe92",
+        "name": "userId",
+        "type": "text",
+        "required": false,
+        "unique": false,
+        "options": {
+          "min": null,
+          "max": null,
+          "pattern": ""
+        }
+      }
+    ],    
+    "listRule": "",
+    "viewRule": "",
+    "createRule": "",
+    "updateRule": "",
+    "deleteRule": "",
+    "options": {}
+  });
+
+  return Dao(db).saveCollection(collection);
+}, (db) => {
+  const dao = new Dao(db);
+  const collection = dao.findCollectionByNameOrId("0fbhwxgna5jb0k1");
+
+  return dao.deleteCollection(collection);
+})
