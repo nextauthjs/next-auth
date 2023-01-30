4c70a46844340a88eb86c78b8cfb1d10e2bb7629
tajkirkpatrick <50558516+tajkirkpatrick@users.noreply.github.com>
2023-01-30 15:10:51 -0600

    chore: refactor adapter try/catch blocks

--- packages/adapter-pocketbase/src/index.ts
+++ packages/adapter-pocketbase/src/index.ts
@@ -21,12 +21,10 @@ export const PocketBaseAdapter = (
 ): Adapter => {
   return {
     async createUser(user) {
-      let pb_user: any
-
       try {
         await adminLogin(client, options)
 
-        pb_user = await client
+        const pb_user = await client
           .collection("next_auth_user")
           .create<PocketBaseUser>({
             name: user.name,
@@ -34,99 +32,87 @@ export const PocketBaseAdapter = (
             email: user.email,
             emailVerified: user.emailVerified?.toISOString().replace("T", " "),
           })
+
+        if (pb_user.code)
+          throw new Error(
+            "error creating user in database - see pocketbase logs"
+          )
+
+        return format<AdapterUser>(pb_user)
       } catch (_) {
         throw new Error("error creating user - see pocketbase logs")
       }
-
-      if (pb_user.code)
-        throw new Error("error creating user in database - see pocketbase logs")
-
-      return format<AdapterUser>(pb_user)
     },
     async getUser(id) {
-      let pb_user: any
-
       try {
         await adminLogin(client, options)
 
-        pb_user = await client
+        const pb_user = await client
           .collection("next_auth_user")
           .getOne<PocketBaseUser>(id)
+
+        if (pb_user.code)
+          throw new Error(
+            "error getting user from database - see pocketbase logs"
+          )
+
+        return format<AdapterUser>(pb_user)
       } catch (_) {
         return null
       }
-
-      if (pb_user.code)
-        throw new Error(
-          "error getting user from database - see pocketbase logs"
-        )
-
-      return format<AdapterUser>(pb_user)
     },
     async getUserByEmail(email) {
-      let pb_user: any
-
       try {
         await adminLogin(client, options)
 
-        pb_user = await client
+        const pb_user = await client
           .collection("next_auth_user")
           .getFirstListItem<PocketBaseUser>(`email="${email}"`)
+
+        if (pb_user.code)
+          throw new Error(
+            "error getting user from database using email filter - see pocketbase logs"
+          )
+
+        return format<AdapterUser>(pb_user)
       } catch (_) {
         return null
       }
-
-      if (pb_user.code)
-        throw new Error(
-          "error getting user from database using email filter - see pocketbase logs"
-        )
-
-      return format<AdapterUser>(pb_user)
     },
     async getUserByAccount({ providerAccountId, provider }) {
-      let pb_account: any
-      let pb_user: any
-
       try {
         await adminLogin(client, options)
 
-        pb_account = await client
+        const pb_account = await client
           .collection("next_auth_account")
           .getFirstListItem<PocketBaseAccount>(
             `provider="${provider}" && providerAccountId="${providerAccountId}"`
           )
+
+        if (pb_account.code)
+          throw new Error(
+            "error getting user from database by account filter - see pocketbase logs"
+          )
+
+        const pb_user = await client
+          .collection("next_auth_user")
+          .getOne<PocketBaseUser>(pb_account.userId)
+
+        if (pb_user.code)
+          throw new Error(
+            "error getting user from database within account filter function - see pocketbase logs"
+          )
+
+        return format<AdapterUser>(pb_user)
       } catch (_) {
         return null
       }
-
-      if (pb_account.code)
-        throw new Error(
-          "error getting user from database by account filter - see pocketbase logs"
-        )
-
-      try {
-        pb_user = await client
-          .collection("next_auth_user")
-          .getOne<PocketBaseUser>(pb_account.userId)
-      } catch (_) {
-        throw new Error(
-          "error getting user within account filter function - see pocketbase logs"
-        )
-      }
-      if (pb_user.code)
-        throw new Error(
-          "error getting user from database within account filter function - see pocketbase logs"
-        )
-
-      return format<AdapterUser>(pb_user)
     },
     async updateUser(user) {
-      let pb_user: any
-
       try {
         await adminLogin(client, options)
 
-        pb_user = await client
+        const pb_user = await client
           .collection("next_auth_user")
           .update<PocketBaseUser>(user.id as string, {
             name: user.name,
@@ -134,22 +120,21 @@ export const PocketBaseAdapter = (
             email: user.email,
             email_verified: user.emailVerified?.toISOString().replace("T", " "),
           })
+        if (pb_user.code)
+          throw new Error(
+            "error updating user in database - see pocketbase logs"
+          )
+
+        return format<AdapterUser>(pb_user)
       } catch (_) {
         throw new Error("error updating user - see pocketbase logs")
       }
-
-      if (pb_user.code)
-        throw new Error("error updating user in database - see pocketbase logs")
-
-      return format<AdapterUser>(pb_user)
     },
     async linkAccount(account) {
-      let pb_account: any
-
       try {
         await adminLogin(client, options)
 
-        pb_account = await client
+        const pb_account = await client
           .collection("next_auth_account")
           .create<PocketBaseAccount>({
             userId: account.userId,
@@ -164,183 +149,168 @@ export const PocketBaseAdapter = (
             session_state: account.session_state,
             expires_at: Number(account.expires_at),
           })
+
+        if (pb_account.code)
+          throw new Error(
+            "error linking account in database - see pocketbase logs"
+          )
+
+        return format<AdapterAccount>(pb_account)
       } catch (_) {
         throw new Error(
           "error creating account in database - see pocketbase logs"
         )
       }
-
-      if (pb_account.code)
-        throw new Error(
-          "error linking account in database - see pocketbase logs"
-        )
-
-      return format<AdapterAccount>(pb_account)
     },
     async createSession(session) {
-      let pb_session: any
-
       try {
         await adminLogin(client, options)
 
-        pb_session = await client
+        const pb_session = await client
           .collection("next_auth_session")
           .create<PocketBaseSession>({
             expires: session.expires.toISOString().replace("T", " "),
             sessionToken: session.sessionToken,
             userId: session.userId,
           })
+
+        if (pb_session.code)
+          throw new Error(
+            "error creating session in database - see pocketbase logs"
+          )
+
+        return format<AdapterSession>(pb_session)
       } catch (error) {
         throw new Error(
           "error creating session in database - see pocketbase logs"
         )
       }
-      if (pb_session.code)
-        throw new Error(
-          "error creating session in database - see pocketbase logs"
-        )
-
-      return format<AdapterSession>(pb_session)
     },
     async getSessionAndUser(sessionToken) {
-      let pb_session: any
-
       try {
         await adminLogin(client, options)
 
-        pb_session = await client
+        const pb_session = await client
           .collection("next_auth_session")
           .getFirstListItem<PocketBaseSession>(`sessionToken="${sessionToken}"`)
+
+        if (pb_session.code)
+          throw new Error(
+            "error retrieving session from database - see pocketbase logs"
+          )
+
+        const pb_user = await client
+          .collection("next_auth_user")
+          .getOne<PocketBaseUser>(pb_session.userId)
+
+        if (pb_user.code)
+          throw new Error(
+            "error getting user from database within getSessionAndUser func - see pocketbase logs"
+          )
+
+        const session = format<AdapterSession>(pb_session)
+        const user = format<AdapterUser>(pb_user)
+        return {
+          session,
+          user,
+        }
       } catch (_) {
         return null
       }
-
-      if (pb_session.code)
-        throw new Error(
-          "error retrieving session from database - see pocketbase logs"
-        )
-
-      const pb_user = await client
-        .collection("next_auth_user")
-        .getOne<PocketBaseUser>(pb_session.userId)
-
-      if (pb_user.code)
-        throw new Error(
-          "error getting user from database within getSessionAndUser func - see pocketbase logs"
-        )
-
-      const session = format<AdapterSession>(pb_session)
-      const user = format<AdapterUser>(pb_user)
-      return {
-        session,
-        user,
-      }
     },
     async updateSession(session) {
-      let record: any
-      let pb_session: any
-
       try {
         await adminLogin(client, options)
 
-        record = await client
+        const record = await client
           .collection("next_auth_session")
           .getFirstListItem<PocketBaseSession>(
             `sessionToken="${session.sessionToken}"`
           )
-      } catch (_) {
-        return null
-      }
 
-      try {
-        pb_session = await client
+        const pb_session = await client
           .collection("next_auth_session")
           .update<PocketBaseSession>(record.id, {
             expires: session.expires?.toISOString().replace("T", " "),
             sessionToken: session.sessionToken,
             userId: session.userId,
           })
+
+        if (pb_session.code)
+          throw new Error(
+            "error updating session in database - see pocketbase logs"
+          )
+
+        return format<AdapterSession>(pb_session)
       } catch (_) {
         return null
       }
-
-      if (pb_session.code)
-        throw new Error(
-          "error updating session in database - see pocketbase logs"
-        )
-
-      return format<AdapterSession>(pb_session)
     },
     async deleteSession(sessionToken) {
-      let record: any
-
       try {
         await adminLogin(client, options)
 
-        record = await client
+        const record = await client
           .collection("next_auth_session")
           .getFirstListItem<PocketBaseSession>(`sessionToken="${sessionToken}"`)
+
+        await client.collection("next_auth_session").delete(record.id)
       } catch (_) {
         return null
       }
-
-      await client.collection("next_auth_session").delete(record.id)
     },
     async createVerificationToken(verificationToken) {
-      let pb_veriToken: any
-
       try {
         await adminLogin(client, options)
 
-        pb_veriToken = await client
+        const pb_veriToken = await client
           .collection("next_auth_verificationToken")
           .create<PocketBaseVerificationToken>({
             identifier: verificationToken.identifier,
             token: verificationToken.token,
             expires: verificationToken.expires.toISOString().replace("T", " "),
           })
+
+        if (pb_veriToken.code)
+          throw new Error(
+            "error creating verificationToken in database - see pocketbase logs"
+          )
+
+        return format<VerificationToken>(pb_veriToken)
       } catch (_) {
         return null
       }
-
-      if (pb_veriToken.code)
-        throw new Error(
-          "error creating verificationToken in database - see pocketbase logs"
-        )
-
-      return format<VerificationToken>(pb_veriToken)
     },
     async useVerificationToken({ identifier, token }) {
-      let pb_veriToken: any
       try {
         await adminLogin(client, options)
 
-        pb_veriToken = await client
+        const pb_veriToken = await client
           .collection("next_auth_verificationToken")
           .getFirstListItem<PocketBaseVerificationToken>(
             `identifier="${identifier}" && token="${token}"`
           )
+
+        if (pb_veriToken.code)
+          throw new Error(
+            "error finding verification Token within database - see pocketbase logs"
+          )
+
+        const success = await client
+          .collection("next_auth_verificationToken")
+          .delete(pb_veriToken.id)
+
+        if (success) {
+          // @ts-expect-error internal id's are not to be returned with the rest of the token
+          const { id, ...returnVal } = format<VerificationToken>(pb_veriToken)
+          return returnVal
+        } else {
+          throw new Error(
+            "unable to delete verificationToken from database - see pocketbase logs"
+          )
+        }
       } catch (_) {
         return null
-      }
-
-      if (pb_veriToken.code)
-        throw new Error(
-          "error finding verification Token within database - see pocketbase logs"
-        )
-
-      const success = await client
-        .collection("next_auth_verificationToken")
-        .delete(pb_veriToken.id)
-
-      if (success) {
-        // @ts-expect-error internal id's are not to be returned with the rest of the token
-        const { id, ...returnVal } = format<VerificationToken>(pb_veriToken)
-        return returnVal
-      } else {
-        throw new Error(
-          "unable to delete verificationToken from database - see pocketbase logs"
-        )
       }
     },
     async deleteUser(userId) {
