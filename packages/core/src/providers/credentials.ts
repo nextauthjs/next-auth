a39b8cfdf43c98c3ab64c422768b70f6d6de516a
Balázs Orbán <info@balazsorban.com>
2023-02-08 16:09:58 +0100

    fix(ts): correct `credentials` types in `authorize` callback

--- packages/core/src/providers/credentials.ts
+++ packages/core/src/providers/credentials.ts
@@ -40,8 +40,16 @@ export interface CredentialsConfig<
    * //...
    */
   authorize: (
-    /** See {@link CredentialInput} */
-    credentials: Record<keyof CredentialsInputs, string> | undefined,
+    /**
+     * The available keys are determined by {@link CredentialInput}.
+     *
+     * @note The existence/correctness of a field cannot be guaranteed at runtime,
+     * so you should always validate the input before using it.
+     *
+     * You can add basic validation depending on your use case,
+     * or you can use a popular library like [Zod](https://zod.dev) for example.
+     */
+    credentials: Partial<Record<keyof CredentialsInputs, unknown>>,
     /** The original request is forward for convenience */
     request: Request
   ) => Awaitable<User | null>
