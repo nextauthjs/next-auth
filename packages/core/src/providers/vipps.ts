ed491e6bfd06122c25de1ea4f652655d3cd127ae
itschip <chip@projecterror.dev>
2023-02-13 14:42:07 +0100

    feat(core/providers): add vipps doc

--- packages/core/src/providers/vipps.ts
+++ packages/core/src/providers/vipps.ts
@@ -1,5 +1,6 @@
 import type { OIDCConfig, OIDCUserConfig } from "./index.js"
 
+/** @see [User Profile Structure](https://vippsas.github.io/vipps-developer-docs/docs/APIs/login-api/vipps-login-api#userinfo) */
 export interface VippsProfile extends Record<string, any> {
   sid: string
   birthdate: string
@@ -12,22 +13,45 @@ export interface VippsProfile extends Record<string, any> {
   sub: string
 }
 
+interface AdditonalConfig {
+  redirectUri?: string
+}
+
+/**
+ * @see [Vipps Login API](https://vippsas.github.io/vipps-developer-docs/docs/APIs/login-api/vipps-login-api)
+ *
+ * ## Example
+ *
+ * ```ts
+ * import Vipps from "@auth/core/providers/vipps"
+ * ...
+ * providers: [
+ *  Vipps({
+ *    clientId: process.env.VIPPS_CLIENT_ID,
+ *    clientSecret: process.env.VIPPS_CLIENT_SECRET,
+ *  })
+ * ]
+ * ...
+ * ```
+ * ::: note
+ * If you're testing, make sure to override the issuer option with apitest.vipps.no
+ * :::
+ */
+
 export default function Vipps<P extends VippsProfile>(
-  options: OIDCUserConfig<P>
+  options: OIDCUserConfig<P> & AdditonalConfig
 ): OIDCConfig<P> {
+  options.issuer ??= "https://api.vipps.no/access-management-1.0/access"
   return {
     id: "vipps",
     name: "Vipps",
     type: "oidc",
-    issuer: "https://apitest.vipps.no/access-management-1.0/access/",
     client: {
       token_endpoint_auth_method: "client_secret_post",
     },
     authorization: {
       params: {
         scope: "openid name email",
-        state: "state-nextjs-vipps",
-        redirect_uri: "http://localhost:3000/api/auth/callback/vipps",
         client_id: options.clientId,
         response_type: "code",
       },
