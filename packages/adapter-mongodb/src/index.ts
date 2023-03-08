7c6aa4f1636aba70ccd6f6a636a358be9a4f9f3a
Lluis Agusti <hi@llu.lu>
2023-03-08 19:17:15 +0100

    chore: document options

--- packages/adapter-mongodb/src/index.ts
+++ packages/adapter-mongodb/src/index.ts
@@ -25,13 +25,20 @@ import type {
 } from "next-auth/adapters"
 import type { MongoClient } from "mongodb"
 
+/** This is the interface of the MongoDB adapter options. */
 export interface MongoDBAdapterOptions {
+  /**
+   * The name of the {@link https://www.mongodb.com/docs/manual/core/databases-and-collections/#collections MongoDB collections}.
+   */
   collections?: {
     Users?: string
     Accounts?: string
     Sessions?: string
     VerificationTokens?: string
   }
+  /**
+   * The name you want to give to the MongoDB database
+   */
   databaseName?: string
 }
 
