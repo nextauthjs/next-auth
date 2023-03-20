81589bf738f5bb18da066481f8176f9927951b1a
Balázs Orbán <info@balazsorban.com>
2023-03-21 00:49:39 +0100

    docs: remove/rename files/directories

--- docs/scripts/generate-providers.mjs
+++ docs/scripts/generate-providers.mjs
@@ -1,17 +1,23 @@
 import { join } from "path"
 import { readdirSync, readFileSync, writeFileSync } from "fs"
 
-// TODO: generate from core package
-const providersPath = join(process.cwd(), "/docs/reference/05-oauth-providers")
+const providersPath = join(process.cwd(), "../packages/core/src/providers")
 
 const files = readdirSync(providersPath, "utf8")
 
+const notOAuth = [
+  "index.ts",
+  "oauth-types.ts",
+  "email.ts",
+  "credentials.ts",
+  "oauth.ts",
+]
+
 const result = files.reduce((acc, file) => {
-  if (file === "index.md" || file === "_category_.json") return acc
+  if (notOAuth.includes(file)) return acc
   const provider = readFileSync(join(providersPath, file), "utf8")
-  const { id, title } = provider.match(
-    /id: (?<id>.+)\ntitle: (?<title>.+)\n/
-  ).groups
+  const { id } = provider.match(/id: "(?<id>.+)",/).groups
+  const { title } = provider.match(/name: "(?<title>.+)",/).groups
   acc[id] = title
   return acc
 }, {})
