64797d44421e9b6a849ee6290b765439c2fa0de3
Thinnakrit <thinnakrit.idesign@gmail.com>
2023-03-24 00:19:59 +0700

    Add wechat provider

---
+++ packages/next-auth/src/providers/wechat.ts
@@ -0,0 +1,55 @@
+import type { OAuthConfig, OAuthUserConfig } from "."
+
+export interface IWeChatProvider extends Record<string, any>  {
+  id: string;
+  name: string;
+  type: 'oauth';
+  version: '2.0';
+  scope: 'snsapi_login';
+  params: {
+    grant_type: 'authorization_code';
+  };
+  accessTokenUrl: string;
+  authorizationUrl: string;
+  profileUrl: string;
+  profile: (profile: any) => {
+    id: string;
+    name: string;
+    email: null;
+    image: string;
+  };
+  clientId: string;
+  clientSecret: string;
+  state: boolean;
+};
+
+export default function WeChatProvider<P extends IWeChatProvider>(
+ options: OAuthUserConfig<P>
+): OAuthConfig<P> {
+  return {
+    id: 'wechat',
+    name: 'WeChat',
+    type: 'oauth',
+    version: '2.0',
+    scope: 'snsapi_login',
+    params: { grant_type: 'authorization_code' },
+    accessTokenUrl: 'https://api.weixin.qq.com/sns/oauth2/access_token',
+    authorizationUrl:
+      `https://open.weixin.qq.com/connect/qrconnect?appid=${options.clientId}` +
+      '&redirect_uri=${options.redirectUrl}' +
+      '&response_type=code&scope=snsapi_login#wechat_redirect',
+    profileUrl: 'https://api.weixin.qq.com/sns/userinfo?openid=',
+    profile: (profile: any) => {
+      return {
+        id: profile.openid,
+        name: profile.nickname,
+        email: null,
+        image: profile.headimgurl,
+      };
+    },
+    clientId,
+    clientSecret,
+    state: true,
+    options,
+  };
+};
