c3563387454c874096251f169bb96658f6c31d7a
Balázs Orbán <info@balazsorban.com>
2023-04-11 12:03:50 +0200

    simplify

--- packages/next-auth/src/utils/detect-origin.ts
+++ packages/next-auth/src/utils/detect-origin.ts
@@ -1,10 +1,9 @@
 /** Extract the origin from the environment */
 export function detectOrigin(forwardedHost: any, protocol: any) {
   // If we detect a Vercel environment, we can trust the host
-  if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST) {
-    protocol = ["http", "https"].includes(protocol) ? protocol : "https"
-    return `${protocol}://${forwardedHost}`
-  }
+  if (process.env.VERCEL ?? process.env.AUTH_TRUST_HOST)
+    return `${protocol === "http" ? "http" : "https"}://${forwardedHost}`
+
   // If `NEXTAUTH_URL` is `undefined` we fall back to "http://localhost:3000"
   return process.env.NEXTAUTH_URL
 }
