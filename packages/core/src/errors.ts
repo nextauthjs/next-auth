ed7a42e2898836ae04d7c8d6ca9ce4c115941935
Balázs Orbán <info@balazsorban.com>
2023-04-12 12:05:33 +0200

    error on missing checks when configured

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -90,7 +90,7 @@ export class InvalidCallbackUrl extends AuthError {}
 export class InvalidEndpoints extends AuthError {}
 
 /** @todo */
-export class InvalidState extends AuthError {}
+export class InvalidCheck extends AuthError {}
 
 /** @todo */
 export class JWTSessionError extends AuthError {}
