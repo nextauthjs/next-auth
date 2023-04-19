b68f461f8b531b496919c8016e81c1d565d54bf0
Balázs Orbán <info@balazsorban.com>
2023-04-19 10:35:34 +0200

    chore: upgrade `next`

--- apps/dev/nextjs/next-env.d.ts
+++ apps/dev/nextjs/next-env.d.ts
@@ -1,5 +1,7 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+/// <reference types="next/navigation-types/compat/navigation" />
+
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
