6f96004d7577978d53a6513c5ee4382c0a449ce2
Balázs Orbán <info@balazsorban.com>
2023-04-21 12:37:21 +0200

    tweak docs

--- packages/frameworks-nextjs/src/client.tsx
+++ packages/frameworks-nextjs/src/client.tsx
@@ -186,11 +186,8 @@ export async function getProviders() {
 }
 
 /**
- * Client-side method to initiate a signin flow
- * or send the user to the signin page listing all possible providers.
- * Automatically adds the CSRF token to the request.
- *
- * [Documentation](https://next-auth.js.org/getting-started/client#signin)
+ * Initiate a signin flow or send the user to the signin page listing all possible providers.
+ * Handles CSRF protection.
  */
 export async function signIn<
   P extends RedirectableProviderType | undefined = undefined
@@ -272,10 +269,8 @@ export async function signIn<
 }
 
 /**
- * Signs the user out, by removing the session cookie.
- * Automatically adds the CSRF token to the request.
- *
- * [Documentation](https://next-auth.js.org/getting-started/client#signout)
+ * Initiate a signout, by destroying the current session.
+ * Handles CSRF protection.
  */
 export async function signOut<R extends boolean = true>(
   options?: SignOutParams<R>
@@ -317,8 +312,6 @@ export async function signOut<R extends boolean = true>(
  * Provider to wrap the app in to make session data available globally.
  * Can also be used to throttle the number of requests to the endpoint
  * `/api/auth/session`.
- *
- * [Documentation](https://next-auth.js.org/getting-started/client#sessionprovider)
  */
 export function SessionProvider(props: SessionProviderProps) {
   if (!SessionContext) {
