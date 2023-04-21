77bba4ada7cd34073fcd8a45b6d41064b95632b8
Balázs Orbán <info@balazsorban.com>
2023-04-21 11:40:46 +0200

    App Router updates

--- apps/dev/nextjs/app/api/auth/[...nextauth]/route.ts
+++ apps/dev/nextjs/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
 import { handlers } from "auth"
 export const { GET, POST } = handlers
+export const runtime = "edge"
--- apps/dev/nextjs/app/server-component/page.tsx
+++ apps/dev/nextjs/app/server-component/page.tsx
@@ -1,8 +1,39 @@
 import { auth } from "auth"
-import { headers } from "next/headers"
+import { cookies, headers } from "next/headers"
+
+function SignIn({ id, children }: any) {
+  const $cookies = cookies()
+  const csrfToken = $cookies.get("next-auth.csrf-token")?.value.split("|")[0]
+  return (
+    <form action={`/api/auth/signin/${id}`} method="post">
+      <button type="submit">{children}</button>
+      <input type="hidden" name="csrfToken" value={csrfToken} />
+    </form>
+  )
+}
+
+function SignOut({ children }: any) {
+  const $cookies = cookies()
+  const csrfToken = $cookies.get("next-auth.csrf-token")?.value.split("|")[0]
+  return (
+    <form action="/api/auth/signout" method="post">
+      <button type="submit">{children}</button>
+      <input type="hidden" name="csrfToken" value={csrfToken} />
+    </form>
+  )
+}
 
 export default async function Page() {
-  // TODO: Drop when we can move this into `@auth/nextjs` when `next/headers` is universally available for Middleware too
-  const $headers = headers() as any
-  return <pre>{JSON.stringify(await auth($headers), null, 2)}</pre>
+  const session = await auth(headers())
+  if (session) {
+    return (
+      <>
+        <pre>{JSON.stringify(session, null, 2)}</pre>
+        <SignOut>Sign out</SignOut>
+      </>
+    )
+  }
+  return <SignIn id="github">Sign in with github</SignIn>
 }
+
+export const runtime = "experimental-edge"
