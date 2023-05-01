c78ec7cdad9942a4ddde55be28f816e0a62e3874
Dean Fogarty <angrytongan@gmail.com>
2023-05-01 21:24:29 +1000

    Bring docs up-to-date as per #7041

--- packages/core/src/providers/concept2.ts
+++ packages/core/src/providers/concept2.ts
@@ -1,3 +1,14 @@
+/**
+ * <div style={{backgroundColor: "#000", display: "flex", justifyContent: "space-between", color: "#fff", padding: 16}}>
+ * <span>Built-in <b>Concept2</b> integration.</span>
+ * <a href="https://concept2.com">
+ *   <img style={{display: "block"}} src="https://authjs.dev/img/providers/concept2.svg" height="48" width="48"/>
+ * </a>
+ * </div>
+ *
+ * @module providers/concept2
+ */
+
 import type { OAuthConfig, OAuthUserConfig } from "./index.js"
 
 export interface Concept2Profile extends Record<string, any> {
@@ -15,6 +26,53 @@ export interface Concept2Profile extends Record<string, any> {
     max_heart_rate: number | null;
 };
 
+/**
+ * Add Concept2 login to your page.
+ *
+ * ### Setup
+ *
+ * #### Callback URL
+ * ```
+ * https://example.com/api/auth/callback/concept2
+ * ```
+ *
+ * #### Configuration
+ *```js
+ * import Auth from "@auth/core"
+ * import Concept2 from "@auth/core/providers/concept2"
+ *
+ * const request = new Request(origin)
+ * const response = await Auth(request, {
+ *   providers: [Concept2({ clientId: CONCEPT2_CLIENT_ID, clientSecret: CONCEPT2_CLIENT_SECRET })],
+ * })
+ * ```
+ *
+ * ### Resources
+ *
+ *  - [Concept2 OAuth documentation](https://log.concept2.com/developers/documentation/)
+ *
+ * ### Notes
+ *
+ * By default, Auth.js assumes that the Concept2 provider is
+ * based on the [OAuth 2](https://www.rfc-editor.org/rfc/rfc6749.html) specification.
+ *
+ * :::tip
+ *
+ * The Concept2 provider comes with a [default configuration](https://github.com/nextauthjs/next-auth/blob/main/packages/next-auth/src/providers/concept2.ts)).
+ * To override the defaults for your use case, check out [customizing a built-in OAuth provider](https://authjs.dev/guides/providers/custom-provider#override-default-options).
+ *
+ * :::
+ *
+ * :::info **Disclaimer**
+ *
+ * If you think you found a bug in the default configuration, you can [open an issue](https://authjs.dev/new/provider-issue).
+ *
+ * Auth.js strictly adheres to the specification and it cannot take responsibility for any deviation from
+ * the spec by the provider. You can open an issue, but if the problem is non-compliance with the spec,
+ * we might not pursue a resolution. You can ask for more help in [Discussions](https://authjs.dev/new/github-discussions).
+ *
+ * :::
+ */
 export default function Concept2<P extends Concept2Profile>(options: OAuthUserConfig<P>): OAuthConfig<P> {
     return {
         id: 'concept2',
