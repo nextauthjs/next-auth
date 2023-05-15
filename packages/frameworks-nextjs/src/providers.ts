b6329dcef9bf1a8373cf59f41edc2d0d258693ea
Balázs Orbán <info@balazsorban.com>
2023-05-15 12:15:41 +0200

    autogenerate provider imports

--- packages/frameworks-nextjs/src/providers.ts
+++
@@ -1,7 +0,0 @@
-// TODO: remove this file and replace references with `@auth/core/providers`
-
-/**
- * @deprecated use `@auth/core/providers`
- * Read more at: https://nextjs.authjs.dev/v5
- */
-export type ProviderType = any
