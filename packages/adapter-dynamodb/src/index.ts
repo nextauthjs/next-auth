a36f81059a4629837ab2f3070a7a0d3e346217e8
Mats Larsen <me@supergrecko.com>
2023-05-18 22:35:00 +0200

    docs: add terraform template for dynamodb table

--- packages/adapter-dynamodb/src/index.ts
+++ packages/adapter-dynamodb/src/index.ts
@@ -157,6 +157,49 @@ export interface DynamoDBAdapterOptions {
  *       Enabled: true
  * ```
  *
+ * If you're using Terraform to manage your infrastructure, you can use this template:
+ *
+ * ```hcl title=dynamodb.tf
+ * resource "aws_dynamodb_table" "next-auth-accounts" {
+ *   name         = "next-auth"
+ *   billing_mode = "PAY_PER_REQUEST" # Alternatively, ON_DEMAND, see https://aws.amazon.com/dynamodb/pricing/
+ *   hash_key     = "pk"
+ *   range_key    = "sk"
+ *
+ *   attribute {
+ *     name = "pk"
+ *     type = "S"
+ *   }
+ *
+ *   attribute {
+ *     name = "sk"
+ *     type = "S"
+ *   }
+ *
+ *   attribute {
+ *     name = "GSI1PK"
+ *     type = "S"
+ *   }
+ *
+ *   attribute {
+ *     name = "GSI1SK"
+ *     type = "S"
+ *   }
+ *
+ *   global_secondary_index {
+ *     hash_key        = "GSI1PK"
+ *     name            = "GSI1"
+ *     projection_type = "ALL"
+ *     range_key       = "GSI1SK"
+ *   }
+ *
+ *   ttl {
+ *     attribute_name = "expires"
+ *     enabled        = true
+ *   }
+ * }
+ * ```
+ *
  * ### Using a custom schema
  *
  * You can configure your custom table schema by passing the `options` key to the adapter constructor:
