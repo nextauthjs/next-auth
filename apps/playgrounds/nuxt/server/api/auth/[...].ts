dd60b43affb90514b4cb80ad7017deda0f47d099
wobsoriano <sorianorobertc@gmail.com>
2023-05-22 10:14:39 -0700

    fix: nuxt auth options types

--- apps/playgrounds/nuxt/server/api/auth/[...].ts
+++ apps/playgrounds/nuxt/server/api/auth/[...].ts
@@ -4,15 +4,14 @@ import type { AuthConfig } from "@auth/core"
 
 const runtimeConfig = useRuntimeConfig()
 
-export const authOptions: AuthConfig = {
+export const authOptions = {
   secret: runtimeConfig.secret,
   providers: [
-    // @ts-expect-error: TODO
     GithubProvider({
       clientId: runtimeConfig.github.clientId,
       clientSecret: runtimeConfig.github.clientSecret,
     }),
   ],
-}
+} as AuthConfig
 
 export default NuxtAuthHandler(authOptions)
