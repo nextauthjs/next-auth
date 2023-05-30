95a0cd55abc1c3fd2bc63116c5c327cbf38fce9e
Anthony Shew <anthony.shew@vercel.com>
2023-05-30 01:29:12 -0700

    PlanetScale needs varchar but types are unhappy...

--- packages/adapter-drizzle/src/utils.ts
+++ packages/adapter-drizzle/src/utils.ts
@@ -1,4 +1,4 @@
-import { AnyMySqlTable, MySqlDatabase } from "drizzle-orm/mysql-core"
+import { AnyMySqlTable, MySqlDatabase, MySqlTable } from "drizzle-orm/mysql-core"
 import { AnyPgTable, PgDatabase } from "drizzle-orm/pg-core"
 import { AnySQLiteTable, BaseSQLiteDatabase } from "drizzle-orm/sqlite-core"
 import { PlanetScaleDatabase } from "drizzle-orm/planetscale-serverless"
@@ -7,23 +7,46 @@ import { Schema as MySqlSchema } from "./mysql/schema"
 import { Schema as SQLiteSchema } from "./sqlite/schema"
 import { Schema as PlanetScaleSchema } from "./planetscale/schema"
 
-export type AdapterFlavors = "mysql" | "pg" | "sqlite" | "planetscale"
-
-export type ClientFlavors<T> = T extends AdapterFlavors
-  ? T extends "mysql"
-    ? MySqlDatabase<any, any>
-    : T extends "pg"
-    ? PgDatabase<any, PgSchema, any>
-    : T extends "sqlite"
-    ? BaseSQLiteDatabase<any, any>
-    : T extends "planetscale"
-    ? PlanetScaleDatabase<any>
-    : never
-  : never
+export type AnyMySqlDatabase = MySqlDatabase<any, any>
+export type AnyPgDatabase = PgDatabase<any, any, any>
+export type AnySQLiteDatabase = BaseSQLiteDatabase<any, any>
+export type AnyPlanetScaleDatabase = PlanetScaleDatabase<any>
 
 export interface MinimumSchema {
   mysql: MySqlSchema & Record<string, AnyMySqlTable>
   pg: PgSchema & Record<string, AnyPgTable>
   sqlite: SQLiteSchema & Record<string, AnySQLiteTable>
   planetscale: PlanetScaleSchema & Record<string, AnyMySqlTable>
+}
+
+export type SqlFlavorOptions = AnyMySqlDatabase
+  | AnyPgDatabase
+  | AnySQLiteDatabase
+  | AnyPlanetScaleDatabase
+
+export type ClientFlavors<Flavor> = Flavor extends AnyMySqlDatabase
+  ? MinimumSchema["mysql"]
+  : Flavor extends AnyPgDatabase
+  ? MinimumSchema["pg"]
+  : Flavor extends AnySQLiteDatabase
+  ? MinimumSchema["sqlite"]
+  : Flavor extends AnyPlanetScaleDatabase
+  ? MinimumSchema["planetscale"]
+  : never
+
+export function isMySqlDatabase(db: any): db is MySqlDatabase<any, any> {
+  return db instanceof MySqlDatabase<any, any>
+}
+
+export function isPgDatabase(db: any): db is PgDatabase<any, any, any> {
+  return db instanceof PgDatabase<any, any, any>
+}
+
+export function isSQLiteDatabase(db: any): db is BaseSQLiteDatabase<any, any> {
+  return db instanceof BaseSQLiteDatabase<any, any>
+}
+
+export function isPlanetScaleDatabase(db: any): db is PlanetScaleDatabase<any> {
+  // PlanetScaleDatabase is a type (not a class) so we can't use instanceof
+  return db
 }
