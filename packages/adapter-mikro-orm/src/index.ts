78964c115b941652f4059aec4c45348b794c5b4b
arjun <arjunsingh8112@gmail.com>
2023-07-07 20:35:07 +0530

    fix(adapters): add missing .js file extension (#7971)
    Add missing .js file extension

--- packages/adapter-mikro-orm/src/index.ts
+++ packages/adapter-mikro-orm/src/index.ts
@@ -24,7 +24,7 @@ import type { Adapter } from "@auth/core/adapters"
 
 import { MikroORM, wrap } from "@mikro-orm/core"
 
-import * as defaultEntities from "./lib/entities"
+import * as defaultEntities from "./lib/entities.js"
 
 export { defaultEntities }
 
