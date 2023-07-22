450558efc844e6d606f7511ff043bc73feaa02ce
Synbulat Biishev <syjalo.dev@gmail.com>
2023-07-22 18:05:23 +0300

    feat(Discord): support new username system

--- packages/next-auth/src/providers/discord.ts
+++ packages/next-auth/src/providers/discord.ts
@@ -6,6 +6,7 @@ export interface DiscordProfile extends Record<string, any> {
   banner: string
   banner_color: string
   discriminator: string
+  global_name: string
   email: string
   flags: number
   id: string
@@ -31,7 +32,10 @@ export default function Discord<P extends DiscordProfile>(
     userinfo: "https://discord.com/api/users/@me",
     profile(profile) {
       if (profile.avatar === null) {
-        const defaultAvatarNumber = parseInt(profile.discriminator) % 5
+        const defaultAvatarNumber =
+          profile.discriminator === "0"
+            ? Number(BigInt(profile.id) >> BigInt(22)) % 6
+            : parseInt(profile.discriminator) % 5
         profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
       } else {
         const format = profile.avatar.startsWith("a_") ? "gif" : "png"
@@ -39,7 +43,7 @@ export default function Discord<P extends DiscordProfile>(
       }
       return {
         id: profile.id,
-        name: profile.username,
+        name: profile.global_name ?? profile.username,
         email: profile.email,
         image: profile.image_url,
       }
