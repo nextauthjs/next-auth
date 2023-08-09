44817717620e87064ba98f5cb7adbe517c2f773f
Jordane Gengo <jordane@hive.fi>
2023-08-09 15:45:49 +0300

    fix: update type and profile to match the new way 42 api send image details

--- packages/core/src/providers/42-school.ts
+++ packages/core/src/providers/42-school.ts
@@ -21,7 +21,6 @@ export interface UserData {
   url: string
   phone: "hidden" | string | null
   displayname: string
-  image_url: string | null
   "staff?": boolean
   correction_point: number
   pool_month: string | null
@@ -144,6 +143,18 @@ export interface CampusUser {
   created_at: string
   updated_at: string | null
 }
+
+export interface Image {
+  link: string,
+  versions: {
+    micro: string,
+    small: string,
+    medium: string,
+    large: string,
+  }
+}
+
+
 export interface FortyTwoProfile extends UserData, Record<string, any> {
   groups: Array<{ id: string; name: string }>
   cursus_users: CursusUser[]
@@ -159,6 +170,7 @@ export interface FortyTwoProfile extends UserData, Record<string, any> {
   roles: Array<{ id: string; name: string }>
   campus: Campus[]
   campus_users: CampusUser[]
+  image: Image
   user: any | null
 }
 
@@ -231,7 +243,7 @@ export default function FortyTwo<P extends FortyTwoProfile>(
         id: profile.id.toString(),
         name: profile.usual_full_name,
         email: profile.email,
-        image: profile.image_url,
+        image: profile.image.link,
       }
     },
     options,
