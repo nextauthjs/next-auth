bc68794bf3dfc409b1bccd44b0a206324b43a3bf
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-08-14 20:10:21 -0400

    chore: setup initial auth/express package
    Co-Authored-By: Lachie Hill <lachiehill@gmail.com>

---
+++ packages/frameworks-express/src/index.ts
@@ -0,0 +1,17 @@
+export {}
+
+// TODO: implement Express router handler
+
+/*
+export function ExpressAuth(authConfig){
+
+return async (req, res) => {
+  const request = httpApiAdapters.request.fromExpressToFetch(req);
+  const response = await Auth(request, getAuthConfig(req));
+  await httpApiAdapters.response.fromFetchToExpress(response, res);
+  }
+
+}
+
+router.use("auth/....", ExpressAuth(authConfig))
+*/
