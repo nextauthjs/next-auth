bd3fbe9d426d0d712fc026e1d9f23173c85a49c2
hillac <lachiehill@gmail.com>
2023-08-16 10:28:20 +1000

    implement get session
    Co-authored-by: Rexford Essilfie <rexfordessilfie09@gmail.com>

--- packages/frameworks-express/src/index.ts
+++ packages/frameworks-express/src/index.ts
@@ -29,41 +29,26 @@ export function ExpressAuth(
 
 export type GetSessionResult = Promise<Session | null>
 
-// export async function getSession(
-//   req: Request,
-//   options: AuthConfig
-// ): GetSessionResult {
-//   options.secret ??= process.env.AUTH_SECRET
-//   options.trustHost ??= true
+export async function getSession(
+  req: ExpressRequest,
+  options: AuthConfig
+): GetSessionResult {
+  options.secret ??= process.env.AUTH_SECRET
+  options.trustHost ??= true
 
-//   const url = new URL("/api/auth/session", req.url)
-//   const response = await Auth(
-//     new Request(url, { headers: req.headers }),
-//     options
-//   )
+  const request = toWebRequest(req)
+  const url = new URL("/api/auth/session", request.url)
 
-//   const { status = 200 } = response
+  const response = await Auth(
+    new Request(url, { headers: request.headers }),
+    options
+  )
 
-//   const data = await response.json()
+  const { status = 200 } = response
 
-//   if (!data || !Object.keys(data).length) return null
-//   if (status === 200) return data
-//   throw new Error(data.message)
-// }
+  const data = await response.json()
 
-// TODO: implement Express router handler
-
-/*
-export function ExpressAuth(authConfig){
-
-return async (req, res) => {
-  const request = httpApiAdapters.request.fromExpressToFetch(req);
-  const response = await Auth(request, getAuthConfig(req));
-  await httpApiAdapters.response.fromFetchToExpress(response, res);
-  }
-
+  if (!data || !Object.keys(data).length) return null
+  if (status === 200) return data
+  throw new Error(data.message)
 }
-
-router.use("auth/....", ExpressAuth(authConfig))
-*/
-
