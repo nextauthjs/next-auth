6267fdd1a70822b5dc686f213e3c54edf97a5817
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-08-22 23:33:09 -0400

    docs: autogenerate @auth/express docs

--- packages/frameworks-express/src/index.ts
+++ packages/frameworks-express/src/index.ts
@@ -1,6 +1,5 @@
 /**
  *
- *
  * :::warning
  * `@auth/express` is currently experimental. The API _will_ change in the future.
  * :::
@@ -8,9 +7,7 @@
  * Express Auth is the official Express integration for Auth.js.
  * It provides a simple way to add authentication to your SvelteKit app in a few lines of code.
  *
- *
  * ## Installation
- *
  * ```bash npm2yarn2pnpm
  * npm install @auth/core @auth/express
  * ```
@@ -27,10 +24,10 @@
  * // Make sure to use these body parsers so Auth.js can receive data from the client
  * app.use(express.json())
  * app.use(express.urlencoded({ extended: true }))
- * 
+ *
  * // If app is served through a proxy, trust the proxy to allow HTTPS protocol to be detected
  * app.use('trust proxy')
-
+ *
  * app.use(
  *   "/api/auth/*",
  *   ExpressAuth({
@@ -49,8 +46,9 @@
  *
  * You will also need to load the environment variables into the Node.js environment. You can do this using a package like [`dotenv`](https://www.npmjs.com/package/dotenv).
  *
- *
+ * ### Provider Configuration
  * The callback URL used by the [providers](https://authjs.dev/reference/core/modules/providers) must be set to the following, unless you mount the `ExpressAuth` handler on a different path:
+ *
  * ```
  * [origin]/api/auth/callback/[provider]
  * ```
@@ -61,84 +59,83 @@
  *
  * ## Managing the session
  * If you are using Express with a template engine (e.g EJS, Pug), you can make the session data available to all routes via middleware as follows
+ *
  * ```ts title="app.ts"
  * import { getSession } from "@auth/express"
  *
- * export function sessionMiddleware(req: Request, res: Response, next: NextFunction) {
- *  res.locals.session = await getSession(req)
- *  next()
+ * export function authSession(req: Request, res: Response, next: NextFunction) {
+ *   res.locals.session = await getSession(req)
+ *   next()
  * }
  *
- * app.use(sessionMiddleware)
+ * app.use(authSession)
  *
  * // Now in your route
  * app.get("/", (req, res) => {
- *  const { session } = res.locals
- *  res.render("index", { session })
+ *   const { session } = res.locals
+ *   res.render("index", { user: session?.user })
  * })
  * ```
  *
- * ## Handling Authorization
+ * ## Authorization
  * You can protect routes by checking for the presence of a session and then redirect to a login page if the session is not present.
- * This can either be done per route, or for a group of routes using middleware.
+ * This can either be done per route, or for a group of routes using a middleware such as the following:
  *
- * First define your middleware as follows:
  * ```ts
- * export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
- *  const session = res.locals.session ?? (await getSession(req, authConfig))
- *  if (!session?.user){
- *    res.redirect("/login")
- *  } else {
- *    next()
- *  }
+ * export function authenticatedUser(
+ *   req: Request,
+ *   res: Response,
+ *   next: NextFunction
+ * ) {
+ *   const session = res.locals.session ?? (await getSession(req, authConfig))
+ *   if (!session?.user) {
+ *     res.redirect("/login")
+ *   } else {
+ *     next()
+ *   }
  * }
+ * ```
  *
  * ### Per Route
  * To protect a single route, simply add the middleware to the route as follows:
- *
- * ```ts title="routes/root.route.ts"
- *
- * const rootRouter = Router()
+ * ```ts title="app.ts"
+ * // ...
  *
  * // This route is protected
- * rootRouter.get("/profile", ensureAuthenticated, (req, res) => {
- *   res.render("profile", { user: res.locals.session?.user })
+ * app.get("/profile", authenticatedUser, (req, res) => {
+ *   const { session } = res.locals
+ *   res.render("profile", { user: session?.user })
  * })
  *
  * // This route is not protected
- * rootRouter.get("/", (req, res) => {
+ * app.get("/", (req, res) => {
  *   res.render("index")
  * })
  *
- * export default rootRouter
+ * app.use("/", root)
  * ```
- * Then mount the router as follows:
- * ```ts title="app.ts"
- * import rootRouter from "./routes/root.route"
- * app.use("/", rootRouter)
- * ```
- *
  * ### Per Group of Routes
  * To protect a group of routes, define a router and add the middleware to the router as follows:
- * ```ts  title="routes/protected.route.ts"
+ *
+ * ```ts title="routes/protected.route.ts"
  * import { Router } from "express"
  *
- * const protectedRouter = Router()
- * protectedRouter.use(ensureAuthenticated) // All routes defined after this will be protected
+ * const router = Router()
  *
- * protectedRouter.get("/", (req, res) => {
- *  res.render("protected")
+ * router.use(authenticatedUser) // All routes defined after this will be protected
+ *
+ * router.get("/", (req, res) => {
+ *   res.render("protected")
  * })
  *
- * export default protectedRouter
+ * export default router
  * ```
  *
  * Then we mount the router as follows:
  * ```ts title="app.ts"
- * import protectedRouter from "./routes/protected.route"
- * app.use("/protected", protectedRouter)
+ * import protected from "./routes/protected.route"
+ * app.use("/protected", protected)
  * ```
- *
  *
  * ## Notes
  *
