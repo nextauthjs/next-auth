d05b4a2075d39749c757d69a0a72665684aa10bb
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-08-22 22:10:31 -0400

    chore: update polyfills
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- packages/frameworks-express/tests/setupTests.ts
+++ packages/frameworks-express/tests/setupTests.ts
@@ -1,23 +1,2 @@
-import cryptoLib from "crypto"
-
-function setup() {
-  if (!globalThis.fetch) {
-    import("node-fetch").then((module) => {
-      // @ts-expect-error
-      globalThis.fetch = module.default
-      // @ts-expect-error
-      globalThis.Request = module.Request
-      // @ts-expect-error
-      globalThis.Response = module.Response
-      // @ts-expect-error
-      globalThis.Headers = module.Headers
-    })
-  }
-
-  if (!globalThis.crypto) {
-    // @ts-expect-error
-    globalThis.crypto = cryptoLib.webcrypto
-  }
-}
-
-setup()
+globalThis.fetch ??= require("undici").fetch
+globalThis.crypto ??= require("node:crypto").webcrypto
