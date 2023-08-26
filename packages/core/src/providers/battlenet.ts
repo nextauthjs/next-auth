294cfdb322acba77b2788466469aca536e552acb
Joey Kanaly <me@joeykanaly.com>
2023-08-26 18:36:30 -0500

    Updated documentation

--- packages/core/src/providers/battlenet.ts
+++ packages/core/src/providers/battlenet.ts
@@ -45,6 +45,8 @@ export type BattleNetIssuer =
  * issuer must be one of these values, based on the available regions:
  * ```
  * type BattleNetIssuer =
+ *   | "https://oauth.battle.net"
+ *   | "https://oauth.battlenet.com.cn"
  *   | "https://www.battlenet.com.cn/oauth"
  *   | "https://us.battle.net/oauth"
  *   | "https://eu.battle.net/oauth"
