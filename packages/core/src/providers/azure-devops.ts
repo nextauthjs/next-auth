a6c98472883c8acb0da8470553a10fc18d09124f
Rexford Essilfie <rexfordessilfie09@gmail.com>
2023-08-30 23:32:04 -0400

    chore: lint azure-devops provider

--- packages/core/src/providers/azure-devops.ts
+++ packages/core/src/providers/azure-devops.ts
@@ -116,52 +116,54 @@ export default function AzureDevOpsProvider<P extends AzureDevOpsProfile>(
      * https://docs.microsoft.com/en-us/azure/devops/integrate/get-started/authentication/oauth?view=azure-devops#scopes
      * @default vso.profile
      */
-    scope?: string;
+    scope?: string
   }
 ): OAuthConfig<P> {
-  const scope = options.scope ?? 'vso.profile';
-  const tokenEndpointUrl = 'https://app.vssps.visualstudio.com/oauth2/authorize';
-  const userInfoEndpointUrl = 'https://app.vssps.visualstudio.com/_apis/profile/profiles/me?details=true&coreAttributes=Avatar&api-version=6.0';
+  const scope = options.scope ?? "vso.profile"
+  const tokenEndpointUrl = "https://app.vssps.visualstudio.com/oauth2/authorize"
+  const userInfoEndpointUrl =
+    "https://app.vssps.visualstudio.com/_apis/profile/profiles/me?details=true&coreAttributes=Avatar&api-version=6.0"
 
   return {
-    id: 'azure-devops',
-    name: 'Azure DevOps',
-    type: 'oauth',
+    id: "azure-devops",
+    name: "Azure DevOps",
+    type: "oauth",
 
     authorization: {
-      url: 'https://app.vssps.visualstudio.com/oauth2/authorize',
-      params: { response_type: 'Assertion', scope }
+      url: "https://app.vssps.visualstudio.com/oauth2/authorize",
+      params: { response_type: "Assertion", scope },
     },
 
     token: {
       url: tokenEndpointUrl,
       async request(context) {
         const response = await fetch(tokenEndpointUrl, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-          method: 'POST',
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+          method: "POST",
           body: new URLSearchParams({
-            client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+            client_assertion_type:
+              "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
             client_assertion: context.provider.clientSecret as string,
-            grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+            grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
             assertion: context.params.code as string,
-            redirect_uri: context.provider.callbackUrl
-          })
-        });
-        return { tokens: await response.json() };
-      }
+            redirect_uri: context.provider.callbackUrl,
+          }),
+        })
+        return { tokens: await response.json() }
+      },
     },
 
     userinfo: {
       url: userInfoEndpointUrl,
       async request(context) {
-        const accessToken = context.tokens.access_token as string;
+        const accessToken = context.tokens.access_token as string
         const response = await fetch(userInfoEndpointUrl, {
           headers: {
-            Authorization: `Bearer ${accessToken}`
-          }
-        });
-        return response.json();
-      }
+            Authorization: `Bearer ${accessToken}`,
+          },
+        })
+        return response.json()
+      },
     },
 
     profile(profile) {
@@ -169,10 +171,10 @@ export default function AzureDevOpsProvider<P extends AzureDevOpsProfile>(
         id: profile.id,
         name: profile.displayName,
         email: profile.emailAddress,
-        image: `data:image/jpeg;base64,${profile.coreAttributes.Avatar.value.value}`
-      };
+        image: `data:image/jpeg;base64,${profile.coreAttributes.Avatar.value.value}`,
+      }
     },
 
-    options
-  };
+    options,
+  }
 }
