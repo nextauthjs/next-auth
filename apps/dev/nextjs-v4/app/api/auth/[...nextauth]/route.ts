b48834547a9d857b340663532d94e7ba8dd2cbc5
kahveciderin <kahveciderin@gmail.com>
2023-09-03 12:45:44 +0300

    revert testing environment

--- apps/dev/nextjs-v4/app/api/auth/[...nextauth]/route.ts
+++ apps/dev/nextjs-v4/app/api/auth/[...nextauth]/route.ts
@@ -1,57 +1,13 @@
 import NextAuth, { type NextAuthOptions } from "next-auth"
 import GitHub from "next-auth/providers/github"
-import Discord from "next-auth/providers/discord"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import CredentialsProvider from "next-auth/providers/credentials";
-
-const prisma = new PrismaClient()
 
 export const authOptions: NextAuthOptions = {
   providers: [
-    Discord({
-      clientId: process.env.DISCORD_CLIENT_ID ?? '',
-      clientSecret: process.env.DISCORD_CLIENT_SECRET ?? ''
+    GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
     }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: { label: "Username", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" }
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const user = { id: "1", name: "J Smith", email: "jsmith@example.com" }
-
-        if (user) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      }
-    })
   ],
-  callbacks: {
-    signIn: async ({ user, account, profile, email, credentials, isNewUser }) => {
-      console.log("signIn callback", { user, account, profile, email, credentials, isNewUser })
-      return true
-    }
-  },
-  events: {
-    signIn: async ({ user, account, profile, isNewUser }) => {
-      console.log("signIn event", { user, account, profile, isNewUser })
-    }
-  },
-  adapter: PrismaAdapter(prisma)
 }
 
 const handler = NextAuth(authOptions)
