8beceb8bf79d4226b3451de697f42c8a378eb605
Thang Vu <hi@thvu.dev>
2023-09-05 21:50:56 +0700

    Format

--- packages/core/src/providers/click-up.ts
+++ packages/core/src/providers/click-up.ts
@@ -9,17 +9,16 @@
  * @module providers/ClickUp
  */
 
-
 import type { OAuthConfig, OAuthUserConfig } from "./index.js"
 
 /** @see [Get the authenticated user](https://clickup.com/api/clickupreference/operation/GetAuthorizedUser/)*/
 export interface ClickUpProfile {
-    user: {
-        id: number;
-        username: string;
-        color: string;
-        profilePicture: string;
-    }
+  user: {
+    id: number
+    username: string
+    color: string
+    profilePicture: string
+  }
 }
 
 /**
@@ -71,35 +70,34 @@ export interface ClickUpProfile {
  * :::
  */
 export default function ClickUp(
-    config: OAuthUserConfig<ClickUpProfile>
-): OAuthConfig<ClickUpProfile>{
-
-    return {
-        id: 'clickup',
-        name: 'ClickUp',
-        type: 'oauth',
-        authorization: 'https://app.clickup.com/api',
-        token: 'https://api.clickup.com/api/v2/oauth/token',
-        userinfo: 'https://api.clickup.com/api/v2/user',
-        clientId: config.clientId,
-        clientSecret: config.clientSecret,
-        checks: ['state'],
-        profile: (profile: ClickUpProfile) => {
-            return {
-                id: profile.user.id.toString(),
-                name: profile.user.username,
-                profilePicture: profile.user.profilePicture,
-                color: profile.user.color
-            }
-        },
-        style: {
-            logo: "/click-up.svg",
-            logoDark: "/click-up.svg",
-            bg: "#fff",
-            bgDark: "#24292f",
-            text: "#000",
-            textDark: "#fff",
-          },
-          options: config,
-    }
+  config: OAuthUserConfig<ClickUpProfile>
+): OAuthConfig<ClickUpProfile> {
+  return {
+    id: "clickup",
+    name: "ClickUp",
+    type: "oauth",
+    authorization: "https://app.clickup.com/api",
+    token: "https://api.clickup.com/api/v2/oauth/token",
+    userinfo: "https://api.clickup.com/api/v2/user",
+    clientId: config.clientId,
+    clientSecret: config.clientSecret,
+    checks: ["state"],
+    profile: (profile: ClickUpProfile) => {
+      return {
+        id: profile.user.id.toString(),
+        name: profile.user.username,
+        profilePicture: profile.user.profilePicture,
+        color: profile.user.color,
+      }
+    },
+    style: {
+      logo: "/click-up.svg",
+      logoDark: "/click-up.svg",
+      bg: "#fff",
+      bgDark: "#24292f",
+      text: "#000",
+      textDark: "#fff",
+    },
+    options: config,
+  }
 }
