aea2d7f0f63e4cb7e373eade0af5cb743580e080
Martin Schaer <martin@schaerweb.com>
2023-09-18 00:05:56 +0100

    prepare for rest

--- packages/adapter-surrealdb/tests/rest.test.ts
+++ packages/adapter-surrealdb/tests/rest.test.ts
@@ -1,20 +1,22 @@
-// TODO: turn this file into rest.test.ts
 test("TODO: test rest", () => {
   expect(true).toBe(true)
 })
 // import { ExperimentalSurrealHTTP } from "surrealdb.js"
-// import { runBasicTests } from "@next-auth/adapter-test"
+// import { runBasicTests } from "@auth/adapter-test"
 // import fetch from "node-fetch"
 
 // import { config } from "./common"
 
 // const clientPromise = new Promise<ExperimentalSurrealHTTP<typeof fetch>>(async (resolve, reject) => {
 //   try {
-//     const db = new ExperimentalSurrealHTTP("http://localhost:8000/sql", { fetch })
-//     db.use("test", "test")
-//     await db.signin({
-//       user: "test",
-//       pass: "test",
+//     const db = new ExperimentalSurrealHTTP("http://0.0.0.0:8000", {
+//       fetch,
+//       auth: {
+//         user: "test",
+//         pass: "test",
+//       },
+//       ns: "test",
+//       db: "test",
 //     })
 //     resolve(db)
 //   } catch (e) {
