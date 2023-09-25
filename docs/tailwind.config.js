c113f55f8cfd867cedac95e7f857faec37a69722
Austin Calvelage <austin.calvelage@icloud.com>
2023-09-25 18:22:38 -0400

    Configure darkmode

--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class','[data-theme="dark"]'],
   content: ["./src/**/*.js"],
   theme: { extend: {} },
   plugins: [],
