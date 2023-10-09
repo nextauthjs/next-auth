732502cad1c977c2a2f9dc0eb8166a4e893c22e2
Martin Schaer <martin@schaerweb.com>
2023-10-09 23:26:50 +0100

    run both strategies

--- packages/adapter-surrealdb/tests/index.test.ts
+++ packages/adapter-surrealdb/tests/index.test.ts
@@ -1,18 +1,19 @@
-import Surreal from "surrealdb.js"
+import Surreal, { ExperimentalSurrealHTTP } from "surrealdb.js"
 import { runBasicTests } from "@auth/adapter-test"
+import fetch from "node-fetch"
 
 import { config } from "./common"
 
 const clientPromise = new Promise<Surreal>(async (resolve, reject) => {
-  const db = new Surreal();
+  const db = new Surreal()
   try {
-    await db.connect('http://0.0.0.0:8000/rpc', {
+    await db.connect("http://0.0.0.0:8000/rpc", {
       ns: "test",
       db: "test",
       auth: {
         user: "test",
         pass: "test",
-      }
+      },
     })
     resolve(db)
   } catch (e) {
@@ -21,3 +22,24 @@ const clientPromise = new Promise<Surreal>(async (resolve, reject) => {
 })
 
 runBasicTests(config(clientPromise))
+
+const clientPromiseRest = new Promise<ExperimentalSurrealHTTP<typeof fetch>>(
+  async (resolve, reject) => {
+    try {
+      const db = new ExperimentalSurrealHTTP("http://0.0.0.0:8000", {
+        fetch,
+        auth: {
+          user: "test",
+          pass: "test",
+        },
+        ns: "test",
+        db: "test",
+      })
+      resolve(db)
+    } catch (e) {
+      reject(e)
+    }
+  }
+)
+
+runBasicTests(config(clientPromiseRest))
