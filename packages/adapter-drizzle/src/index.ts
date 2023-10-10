73124158de5d890534fa6adf8a1c4525d5e0ee2e
Jason <50032291+jasongitmail@users.noreply.github.com>
2023-10-10 18:31:21 -0400

    docs: camelCase all property names in Drizzle schemas & make indentation consistent

--- packages/adapter-drizzle/src/index.ts
+++ packages/adapter-drizzle/src/index.ts
@@ -48,7 +48,7 @@ import type { Adapter } from "@auth/core/adapters"
  * ```
  * 
  * :::info
- * If you're using multi-project schemas, you can pass your table function as a second argument
+ * If you're using multi-project schemas, you can pass your table function as a second argument.
  * :::
  *
  * ## Setup
@@ -68,58 +68,58 @@ import type { Adapter } from "@auth/core/adapters"
  *   pgTable,
  *   text,
  *   primaryKey,
- *  integer
+ *   integer
  * } from "drizzle-orm/pg-core"
  * import type { AdapterAccount } from '@auth/core/adapters'
  *
  * export const users = pgTable("user", {
- *  id: text("id").notNull().primaryKey(),
- *  name: text("name"),
- *  email: text("email").notNull(),
- *  emailVerified: timestamp("emailVerified", { mode: "date" }),
- *  image: text("image"),
+ *   id: text("id").notNull().primaryKey(),
+ *   name: text("name"),
+ *   email: text("email").notNull(),
+ *   emailVerified: timestamp("emailVerified", { mode: "date" }),
+ *   image: text("image"),
  * })
  *
  * export const accounts = pgTable(
- * "account",
- * {
- *   userId: text("userId")
- *     .notNull()
- *     .references(() => users.id, { onDelete: "cascade" }),
- *   type: text("type").$type<AdapterAccount["type"]>().notNull(),
- *   provider: text("provider").notNull(),
- *   providerAccountId: text("providerAccountId").notNull(),
- *   refresh_token: text("refresh_token"),
- *   access_token: text("access_token"),
- *   expires_at: integer("expires_at"),
- *   token_type: text("token_type"),
- *   scope: text("scope"),
- *    id_token: text("id_token"),
- *   session_state: text("session_state"),
- * },
- * (account) => ({
- *   compoundKey: primaryKey(account.provider, account.providerAccountId),
- * })
+ *   "account",
+ *   {
+ *     userId: text("userId")
+ *       .notNull()
+ *       .references(() => users.id, { onDelete: "cascade" }),
+ *     type: text("type").$type<AdapterAccount["type"]>().notNull(),
+ *     provider: text("provider").notNull(),
+ *     providerAccountId: text("providerAccountId").notNull(),
+ *     refreshToken: text("refresh_token"),
+ *     accessToken: text("access_token"),
+ *     expiresAt: integer("expires_at"),
+ *     tokenType: text("token_type"),
+ *     scope: text("scope"),
+ *     idToken: text("id_token"),
+ *     session_state: text("session_state"),
+ *   },
+ *   (account) => ({
+ *     compoundKey: primaryKey(account.provider, account.providerAccountId),
+ *   })
  * )
  *
  * export const sessions = pgTable("session", {
- *  sessionToken: text("sessionToken").notNull().primaryKey(),
- *  userId: text("userId")
- *    .notNull()
- *    .references(() => users.id, { onDelete: "cascade" }),
- *  expires: timestamp("expires", { mode: "date" }).notNull(),
+ *   sessionToken: text("sessionToken").notNull().primaryKey(),
+ *   userId: text("userId")
+ *     .notNull()
+ *     .references(() => users.id, { onDelete: "cascade" }),
+ *   expires: timestamp("expires", { mode: "date" }).notNull(),
  * })
  *
  * export const verificationTokens = pgTable(
- *  "verificationToken",
- *  {
- *    identifier: text("identifier").notNull(),
- *    token: text("token").notNull(),
- *    expires: timestamp("expires", { mode: "date" }).notNull(),
- *  },
- *  (vt) => ({
- *    compoundKey: primaryKey(vt.identifier, vt.token),
- *  })
+ *   "verificationToken",
+ *   {
+ *     identifier: text("identifier").notNull(),
+ *     token: text("token").notNull(),
+ *     expires: timestamp("expires", { mode: "date" }).notNull(),
+ *   },
+ *   (vt) => ({
+ *     compoundKey: primaryKey(vt.identifier, vt.token),
+ *   })
  * )
  * ```
  *
@@ -127,62 +127,62 @@ import type { Adapter } from "@auth/core/adapters"
  *
  * ```ts title="schema.ts"
  * import {
- *  int,
- *  timestamp,
- *  mysqlTable,
- *  primaryKey,
- *  varchar,
+ *   int,
+ *   timestamp,
+ *   mysqlTable,
+ *   primaryKey,
+ *   varchar,
  * } from "drizzle-orm/mysql-core"
  * import type { AdapterAccount } from "@auth/core/adapters"
  *
  * export const users = mysqlTable("user", {
- *  id: varchar("id", { length: 255 }).notNull().primaryKey(),
- *  name: varchar("name", { length: 255 }),
- *  email: varchar("email", { length: 255 }).notNull(),
+ *   id: varchar("id", { length: 255 }).notNull().primaryKey(),
+ *   name: varchar("name", { length: 255 }),
+ *   email: varchar("email", { length: 255 }).notNull(),
  *   emailVerified: timestamp("emailVerified", { mode: "date", fsp: 3 }).defaultNow(),
- *  image: varchar("image", { length: 255 }),
+ *   image: varchar("image", { length: 255 }),
  * })
  *
  * export const accounts = mysqlTable(
- *  "account",
+ *   "account",
  *   {
- *    userId: varchar("userId", { length: 255 })
+ *     userId: varchar("userId", { length: 255 })
  *       .notNull()
  *       .references(() => users.id, { onDelete: "cascade" }),
- *    type: varchar("type", { length: 255 }).$type<AdapterAccount["type"]>().notNull(),
+ *     type: varchar("type", { length: 255 }).$type<AdapterAccount["type"]>().notNull(),
  *     provider: varchar("provider", { length: 255 }).notNull(),
- *    providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
- *    refresh_token: varchar("refresh_token", { length: 255 }),
- *    access_token: varchar("access_token", { length: 255 }),
- *    expires_at: int("expires_at"),
- *   token_type: varchar("token_type", { length: 255 }),
- *   scope: varchar("scope", { length: 255 }),
- *   id_token: varchar("id_token", { length: 255 }),
- *   session_state: varchar("session_state", { length: 255 }),
- * },
- * (account) => ({
- *   compoundKey: primaryKey(account.provider, account.providerAccountId),
- * })
+ *     providerAccountId: varchar("providerAccountId", { length: 255 }).notNull(),
+ *     refreshToken: varchar("refresh_token", { length: 255 }),
+ *     accessToken: varchar("access_token", { length: 255 }),
+ *     expiresAt: int("expires_at"),
+ *     tokenType: varchar("token_type", { length: 255 }),
+ *     scope: varchar("scope", { length: 255 }),
+ *     idToken: varchar("id_token", { length: 255 }),
+ *     sessionState: varchar("session_state", { length: 255 }),
+ *   },
+ *   (account) => ({
+ *     compoundKey: primaryKey(account.provider, account.providerAccountId),
+ *   })
  * )
  *
  * export const sessions = mysqlTable("session", {
- *  sessionToken: varchar("sessionToken", { length: 255 }).notNull().primaryKey(),
- *  userId: varchar("userId", { length: 255 })
- *    .notNull()
- *    .references(() => users.id, { onDelete: "cascade" }),
- *  expires: timestamp("expires", { mode: "date" }).notNull(),
+ *   sessionToken: varchar("sessionToken", { length: 255 }).notNull().primaryKey(),
+ *   userId: varchar("userId", { length: 255 })
+ *     .notNull()
+ *     .references(() => users.id, { onDelete: "cascade" }),
+ *   expires: timestamp("expires", { mode: "date" }).notNull(),
  * })
  *
  * export const verificationTokens = mysqlTable(
- * "verificationToken",
- * {
- *   identifier: varchar("identifier", { length: 255 }).notNull(),
- *   token: varchar("token", { length: 255 }).notNull(),
- *   expires: timestamp("expires", { mode: "date" }).notNull(),
- * },
- * (vt) => ({
- *   compoundKey: primaryKey(vt.identifier, vt.token),
- * })
+ *   "verificationToken",
+ *   {
+ *     identifier: varchar("identifier", { length: 255 }).notNull(),
+ *     token: varchar("token", { length: 255 }).notNull(),
+ *     expires: timestamp("expires", { mode: "date" }).notNull(),
+ *   },
+ *   (vt) => ({
+ *     compoundKey: primaryKey(vt.identifier, vt.token),
+ *   })
  * )
  * ```
  *
@@ -193,53 +193,53 @@ import type { Adapter } from "@auth/core/adapters"
  * import type { AdapterAccount } from "@auth/core/adapters"
  *
  * export const users = sqliteTable("user", {
- *  id: text("id").notNull().primaryKey(),
- *  name: text("name"),
- *  email: text("email").notNull(),
- *  emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
- *  image: text("image"),
+ *   id: text("id").notNull().primaryKey(),
+ *   name: text("name"),
+ *   email: text("email").notNull(),
+ *   emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
+ *   image: text("image"),
  * })
  *
  * export const accounts = sqliteTable(
- *  "account",
- *  {
- *    userId: text("userId")
- *      .notNull()
- *      .references(() => users.id, { onDelete: "cascade" }),
- *    type: text("type").$type<AdapterAccount["type"]>().notNull(),
- *    provider: text("provider").notNull(),
- *    providerAccountId: text("providerAccountId").notNull(),
- *    refresh_token: text("refresh_token"),
- *    access_token: text("access_token"),
- *    expires_at: integer("expires_at"),
- *    token_type: text("token_type"),
- *    scope: text("scope"),
- *    id_token: text("id_token"),
- *    session_state: text("session_state"),
- *  },
- *  (account) => ({
- *    compoundKey: primaryKey(account.provider, account.providerAccountId),
- *  })
+ *   "account",
+ *   {
+ *     userId: text("userId")
+ *       .notNull()
+ *       .references(() => users.id, { onDelete: "cascade" }),
+ *     type: text("type").$type<AdapterAccount["type"]>().notNull(),
+ *     provider: text("provider").notNull(),
+ *     providerAccountId: text("providerAccountId").notNull(),
+ *     refreshToken: text("refresh_token"),
+ *     accessToken: text("access_token"),
+ *     expiresAt: integer("expires_at"),
+ *     tokenType: text("token_type"),
+ *     scope: text("scope"),
+ *     idToken: text("id_token"),
+ *     sessionState: text("session_state"),
+ *   },
+ *   (account) => ({
+ *     compoundKey: primaryKey(account.provider, account.providerAccountId),
+ *   })
  * )
  *
  * export const sessions = sqliteTable("session", {
- * sessionToken: text("sessionToken").notNull().primaryKey(),
- * userId: text("userId")
- *   .notNull()
- *   .references(() => users.id, { onDelete: "cascade" }),
- * expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
+ *   sessionToken: text("sessionToken").notNull().primaryKey(),
+ *   userId: text("userId")
+ *     .notNull()
+ *     .references(() => users.id, { onDelete: "cascade" }),
+ *   expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
  * })
  *
  * export const verificationTokens = sqliteTable(
- * "verificationToken",
- * {
- *   identifier: text("identifier").notNull(),
- *   token: text("token").notNull(),
- *   expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
- * },
- * (vt) => ({
- *   compoundKey: primaryKey(vt.identifier, vt.token),
- * })
+ *   "verificationToken",
+ *   {
+ *     identifier: text("identifier").notNull(),
+ *     token: text("token").notNull(),
+ *     expires: integer("expires", { mode: "timestamp_ms" }).notNull(),
+ *   },
+ *   (vt) => ({
+ *     compoundKey: primaryKey(vt.identifier, vt.token),
+ *   })
  * )
  * ```
  *
