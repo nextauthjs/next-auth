e863b22d7935b77956634840819e7ada3d89abfc
Saad A. Bazaz <saadbazaz@hotmail.com>
2023-10-18 22:19:59 +0500

    Create adobe.ts
    Add the provider configuration for Adobe OAuth 2.0.

---
+++ packages/next-auth/src/providers/adobe.ts
@@ -0,0 +1,79 @@
+import type { OAuthConfig, OAuthUserConfig } from '.'
+
+export interface AdobeProfile extends Record<string, any> {
+  aud: string
+  azp: string
+  email: string
+  email_verified: boolean
+  exp: number
+  family_name: string
+  given_name: string
+  hd: string
+  iat: number
+  iss: string
+  jti: string
+  name: string
+  nbf: number
+  image: string
+  sub: string
+}
+
+export default function Adobe<P extends AdobeProfile>(
+  options: OAuthUserConfig<P>
+): OAuthConfig<P> {
+  return {
+    id: 'adobe',
+    name: 'Adobe',
+    type: 'oauth',
+    version: '2.0',
+    wellKnown:
+      'https://ims-na1.adobelogin.com/ims/.well-known/openid-configuration',
+    authorization: {
+      url: 'https://ims-na1.adobelogin.com/ims/authorize/v2',
+      params: {
+        redirect_uri:
+          process.env.NODE_ENV === 'development'
+            ? 'https://localhost:3001/api/auth/callback/adobe'
+            : process.env.VERCEL_URL
+            ? process.env.VERCEL_URL + '/api/auth/callback/adobe'
+            : '',
+        scope: 'openid address creative_sdk email profile'
+      }
+    },
+    idToken: true,
+    token: 'https://ims-na1.adobelogin.com/ims/token/v3',
+    userinfo: 'https://ims-na1.adobelogin.com/ims/userinfo/v2',
+    scope: ['AdobeID', 'openid', 'address', 'creative_sdk', 'email', 'profile'],
+    issuer: 'https://ims-na1.adobelogin.com',
+    profile(adobeStockProfile) {
+      return {
+        id: adobeStockProfile.email,
+        email: adobeStockProfile.email,
+        image: adobeStockProfile.image,
+        email_verified: adobeStockProfile.email_verified,
+        given_name: adobeStockProfile.given_name,
+        family_name: adobeStockProfile.family_name
+      }
+    },
+    style: {
+      logo: '/adobe.svg',
+      logoDark: '/adobe.svg',
+      bgDark: '#fff',
+      bg: '#fff',
+      text: '#000',
+      textDark: '#000'
+    },
+    ...options
+    // Add the following in options
+    // signinUrl:
+    //   process.env.NODE_ENV === 'development'
+    //     ? 'https://localhost:3001/api/auth/signin/adobe'
+    //     : undefined,
+    // callbackUrl:
+    //   process.env.NODE_ENV === 'development'
+    //     ? 'https://localhost:3001/api/auth/callback/adobe'
+    //     : undefined,
+    // clientId: process.env.ADOBE_CLIENT_ID,
+    // clientSecret: process.env.ADOBE_CLIENT_SECRET
+  }
+}
