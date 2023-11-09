34a856aa936a501b71b964e69bf1e44a9aee2175
Sanket Choudhari <49080676+sanketssc@users.noreply.github.com>
2023-11-09 20:48:43 +0530

    docs: increase `id_token` length (#9100)

--- packages/adapter-drizzle/src/index.ts
+++ packages/adapter-drizzle/src/index.ts
@@ -157,7 +157,7 @@ import type { Adapter } from "@auth/core/adapters"
  *    expires_at: int("expires_at"),
  *   token_type: varchar("token_type", { length: 255 }),
  *   scope: varchar("scope", { length: 255 }),
- *   id_token: varchar("id_token", { length: 255 }),
+ *   id_token: varchar("id_token", { length: 2048 }),
  *   session_state: varchar("session_state", { length: 255 }),
  * },
  * (account) => ({
