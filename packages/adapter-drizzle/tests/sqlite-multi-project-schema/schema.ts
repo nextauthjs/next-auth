14e655610780379bfc91becd2ea0e23e8dbf6143
Sriram Thiagarajan <sri.sjc@gmail.com>
2023-11-28 19:42:36 +0530

    use libsql client instead of better-sqlite3
    tests fail

--- packages/adapter-drizzle/tests/sqlite-multi-project-schema/schema.ts
+++ packages/adapter-drizzle/tests/sqlite-multi-project-schema/schema.ts
@@ -1,9 +1,11 @@
-import { drizzle } from "drizzle-orm/better-sqlite3"
-import Database from "better-sqlite3"
+import { drizzle } from "drizzle-orm/libsql"
+import { createClient } from "@libsql/client"
 import { createTables } from "../../src/lib/sqlite"
 import { sqliteTableCreator } from "drizzle-orm/sqlite-core"
 
-const sqlite = new Database("db.sqlite")
+const client = createClient({
+  url: "file:db.sqlite",
+})
 
 const sqliteTable = sqliteTableCreator((name) => `foobar_${name}`)
 
@@ -11,4 +13,4 @@ export const { users, accounts, sessions, verificationTokens } =
   createTables(sqliteTable)
 export const schema = { users, accounts, sessions, verificationTokens }
 
-export const db = drizzle(sqlite, { schema })
+export const db = drizzle(client, { schema })
--- packages/adapter-drizzle/tests/sqlite/schema.ts
+++ packages/adapter-drizzle/tests/sqlite/schema.ts
@@ -1,12 +1,14 @@
-import { drizzle } from "drizzle-orm/better-sqlite3"
-import Database from "better-sqlite3"
+import { drizzle } from "drizzle-orm/libsql"
+import { createClient } from "@libsql/client"
 import { createTables } from "../../src/lib/sqlite"
 import { sqliteTable } from "drizzle-orm/sqlite-core"
 
-const sqlite = new Database("db.sqlite")
+const client = createClient({
+  url: "file:db.sqlite",
+})
 
 export const { users, accounts, sessions, verificationTokens } =
   createTables(sqliteTable)
 export const schema = { users, accounts, sessions, verificationTokens }
 
-export const db = drizzle(sqlite, { schema })
+export const db = drizzle(client, { schema })
