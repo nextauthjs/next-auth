e716de162b7ead925e0588d58bdf9b1271370b9b
Balázs Orbán <info@balazsorban.com>
2023-12-10 02:37:08 +0000

    simplify

--- packages/adapter-kysely/src/index.ts
+++ packages/adapter-kysely/src/index.ts
@@ -17,84 +17,44 @@
 
 import { Kysely, SqliteAdapter } from "kysely"
 
-import type { Adapter } from "@auth/core/adapters"
+import type {
+  Adapter,
+  AdapterUser,
+  AdapterAccount,
+  AdapterSession,
+  VerificationToken,
+} from "@auth/core/adapters"
 
 export interface Database {
-  User: {
-    id: string
-    name: string | null
-    email: string
-    emailVerified: Date | string | null
-    image: string | null
-  }
-  Account: {
-    id: string
-    userId: string
-    type: string
-    provider: string
-    providerAccountId: string
-    refresh_token: string | null
-    access_token: string | null
-    expires_at: number | null
-    token_type: string | null
-    scope: string | null
-    id_token: string | null
-    session_state: string | null
-  }
-  Session: {
-    id: string
-    userId: string
-    sessionToken: string
-    expires: Date | string
-  }
-  VerificationToken: {
-    identifier: string
-    token: string
-    expires: Date | string
-  }
+  User: AdapterUser
+  Account: AdapterAccount
+  Session: AdapterSession
+  VerificationToken: VerificationToken
+}
+
+// https://github.com/honeinc/is-iso-date/blob/master/index.js
+const isoDateRE =
+  /(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))|(\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d([+-][0-2]\d:[0-5]\d|Z))/
+function isDate(value: any) {
+  return value && isoDateRE.test(value) && !isNaN(Date.parse(value))
 }
 
 export const format = {
-  /**
-   * Helper function to return the passed in object and its specified prop
-   * as an ISO string if SQLite is being used.
-   */
-  from<T extends Partial<Record<K, Date | null>>, K extends keyof T>(
-    data: T,
-    key: K,
-    isSqlite: boolean
-  ) {
-    const value = data[key]
-    return {
-      ...data,
-      [key]: value && isSqlite ? value.toISOString() : value,
+  from<T>(object?: Record<string, any>): T {
+    const newObject: Record<string, unknown> = {}
+    for (const key in object) {
+      const value = object[key]
+      if (isDate(value)) newObject[key] = new Date(value)
+      else newObject[key] = value
     }
+    return newObject as T
   },
-  to,
-}
-
-type ReturnData<T = never> = Record<string, Date | string | T>
-
-/**
- * Helper function to return the passed in object and its specified prop as a date.
- * Necessary because SQLite has no date type so we store dates as ISO strings.
- */
-function to<T extends Partial<ReturnData>, K extends keyof T>(
-  data: T,
-  key: K
-): Omit<T, K> & Record<K, Date>
-function to<T extends Partial<ReturnData<null>>, K extends keyof T>(
-  data: T,
-  key: K
-): Omit<T, K> & Record<K, Date | null>
-function to<T extends Partial<ReturnData<null>>, K extends keyof T>(
-  data: T,
-  key: K
-) {
-  const value = data[key]
-  return Object.assign(data, {
-    [key]: value && typeof value === "string" ? new Date(value) : value,
-  })
+  to<T>(object: Record<string, any>): T {
+    const newObject: Record<string, unknown> = {}
+    for (const [key, value] of Object.entries(object))
+      newObject[key] = value instanceof Date ? value.toISOString() : value
+    return newObject as T
+  },
 }
 
 /**
@@ -278,84 +238,75 @@ function to<T extends Partial<ReturnData<null>>, K extends keyof T>(
  */
 export function KyselyAdapter(db: Kysely<Database>): Adapter {
   const { adapter } = db.getExecutor()
-  const supportsReturning = adapter.supportsReturning
+  const { supportsReturning } = adapter
   const isSqlite = adapter instanceof SqliteAdapter
-
+  /** If the database is SQLite, turn dates into an ISO string  */
+  const to = isSqlite ? format.to : <T>(x: T) => x as T
+  /** If the database is SQLite, turn ISO strings into dates */
+  const from = isSqlite ? format.from : <T>(x: T) => x as T
   return {
     async createUser(data) {
-      const userData = Object.assign(
-        format.from(data, "emailVerified", isSqlite),
-        {
-          id: crypto.randomUUID(),
-        }
-      )
-      const query = db.insertInto("User").values(userData)
-      const result = supportsReturning
-        ? await query.returningAll().executeTakeFirstOrThrow()
-        : await query.executeTakeFirstOrThrow().then(async () => {
-            return await db
-              .selectFrom("User")
-              .selectAll()
-              .where("email", "=", `${userData.email}`)
-              .executeTakeFirstOrThrow()
-          })
-      return to(result, "emailVerified")
+      const user = { ...data, id: crypto.randomUUID() }
+      await db.insertInto("User").values(to(user)).executeTakeFirstOrThrow()
+      return user
     },
     async getUser(id) {
-      const result =
-        (await db
-          .selectFrom("User")
-          .selectAll()
-          .where("id", "=", id)
-          .executeTakeFirst()) ?? null
+      const result = await db
+        .selectFrom("User")
+        .selectAll()
+        .where("id", "=", id)
+        .executeTakeFirst()
       if (!result) return null
-      return to(result, "emailVerified")
+      return from(result)
     },
     async getUserByEmail(email) {
-      const result =
-        (await db
-          .selectFrom("User")
-          .selectAll()
-          .where("email", "=", email)
-          .executeTakeFirst()) ?? null
+      const result = await db
+        .selectFrom("User")
+        .selectAll()
+        .where("email", "=", email)
+        .executeTakeFirst()
       if (!result) return null
-      return to(result, "emailVerified")
+      return from(result)
     },
     async getUserByAccount({ providerAccountId, provider }) {
-      const result =
-        (await db
-          .selectFrom("User")
-          .innerJoin("Account", "User.id", "Account.userId")
-          .selectAll("User")
-          .where("Account.providerAccountId", "=", providerAccountId)
-          .where("Account.provider", "=", provider)
-          .executeTakeFirst()) ?? null
+      const result = await db
+        .selectFrom("User")
+        .innerJoin("Account", "User.id", "Account.userId")
+        .selectAll("User")
+        .where("Account.providerAccountId", "=", providerAccountId)
+        .where("Account.provider", "=", provider)
+        .executeTakeFirst()
       if (!result) return null
-      return to(result, "emailVerified")
+      return from(result)
     },
     async updateUser({ id, ...user }) {
-      if (!id) throw new Error("User not found")
-      const userData = format.from(user, "emailVerified", isSqlite)
+      const userData = to(user)
       const query = db.updateTable("User").set(userData).where("id", "=", id)
       const result = supportsReturning
-        ? await query.returningAll().executeTakeFirstOrThrow()
-        : await query.executeTakeFirstOrThrow().then(async () => {
-            return await db
-              .selectFrom("User")
-              .selectAll()
-              .where("id", "=", id)
-              .executeTakeFirstOrThrow()
-          })
-      return to(result, "emailVerified")
+        ? query.returningAll().executeTakeFirstOrThrow()
+        : query
+            .executeTakeFirstOrThrow()
+            .then(() =>
+              db
+                .selectFrom("User")
+                .selectAll()
+                .where("id", "=", id)
+                .executeTakeFirstOrThrow()
+            )
+      return from(await result)
     },
     async deleteUser(userId) {
-      await db.deleteFrom("User").where("User.id", "=", userId).execute()
+      await db
+        .deleteFrom("User")
+        .where("User.id", "=", userId)
+        .executeTakeFirst()
     },
     async linkAccount(account) {
       await db
         .insertInto("Account")
-        .values({ ...account, id: crypto.randomUUID() })
+        .values(to(account))
         .executeTakeFirstOrThrow()
+      return account
     },
     async unlinkAccount({ providerAccountId, provider }) {
       await db
@@ -364,48 +315,25 @@ export function KyselyAdapter(db: Kysely<Database>): Adapter {
         .where("Account.provider", "=", provider)
         .executeTakeFirstOrThrow()
     },
-    async createSession(data) {
-      const sessionData = Object.assign(
-        format.from(data, "expires", isSqlite),
-        {
-          id: crypto.randomUUID(),
-        }
-      )
-      const query = db.insertInto("Session").values(sessionData)
-      const result = supportsReturning
-        ? await query.returningAll().executeTakeFirstOrThrow()
-        : await (async () => {
-            await query.executeTakeFirstOrThrow()
-            return await db
-              .selectFrom("Session")
-              .selectAll()
-              .where("sessionToken", "=", sessionData.sessionToken)
-              .executeTakeFirstOrThrow()
-          })()
-      return to(result, "expires")
+    async createSession(session) {
+      await db.insertInto("Session").values(to(session)).execute()
+      return session
     },
-    async getSessionAndUser(sessionTokenArg) {
+    async getSessionAndUser(sessionToken) {
       const result = await db
         .selectFrom("Session")
         .innerJoin("User", "User.id", "Session.userId")
         .selectAll("User")
-        .select([
-          "Session.id as sessionId",
-          "Session.userId",
-          "Session.sessionToken",
-          "Session.expires",
-        ])
-        .where("Session.sessionToken", "=", sessionTokenArg)
+        .select(["Session.expires", "Session.userId"])
+        .where("Session.sessionToken", "=", sessionToken)
         .executeTakeFirst()
       if (!result) return null
-      const { sessionId: id, userId, sessionToken, expires, ...user } = result
-      return {
-        user: to({ ...user }, "emailVerified"),
-        session: to({ id, userId, sessionToken, expires }, "expires"),
-      }
+      const { userId, expires, ...user } = result
+      const session = { sessionToken, userId, expires }
+      return { user: from(user), session: from(session) }
     },
     async updateSession(session) {
-      const sessionData = format.from(session, "expires", isSqlite)
+      const sessionData = to(session)
       const query = db
         .updateTable("Session")
         .set(sessionData)
@@ -419,7 +347,7 @@ export function KyselyAdapter(db: Kysely<Database>): Adapter {
               .where("Session.sessionToken", "=", sessionData.sessionToken)
               .executeTakeFirstOrThrow()
           })
-      return to(result, "expires")
+      return from(result)
     },
     async deleteSession(sessionToken) {
       await db
@@ -427,33 +355,18 @@ export function KyselyAdapter(db: Kysely<Database>): Adapter {
         .where("Session.sessionToken", "=", sessionToken)
         .executeTakeFirstOrThrow()
     },
-    async createVerificationToken(verificationToken) {
-      const verificationTokenData = format.from(
-        verificationToken,
-        "expires",
-        isSqlite
-      )
-      const query = db
-        .insertInto("VerificationToken")
-        .values(verificationTokenData)
-      const result = supportsReturning
-        ? await query.returningAll().executeTakeFirstOrThrow()
-        : await query.executeTakeFirstOrThrow().then(async () => {
-            return await db
-              .selectFrom("VerificationToken")
-              .selectAll()
-              .where("token", "=", verificationTokenData.token)
-              .executeTakeFirstOrThrow()
-          })
-      return to(result, "expires")
+    async createVerificationToken(data) {
+      await db.insertInto("VerificationToken").values(to(data)).execute()
+      return data
     },
     async useVerificationToken({ identifier, token }) {
       const query = db
         .deleteFrom("VerificationToken")
         .where("VerificationToken.token", "=", token)
         .where("VerificationToken.identifier", "=", identifier)
+
       const result = supportsReturning
-        ? (await query.returningAll().executeTakeFirst()) ?? null
+        ? await query.returningAll().executeTakeFirst()
         : await db
             .selectFrom("VerificationToken")
             .selectAll()
@@ -464,7 +377,7 @@ export function KyselyAdapter(db: Kysely<Database>): Adapter {
               return res
             })
       if (!result) return null
-      return to(result, "expires")
+      return from(result)
     },
   }
 }
@@ -476,7 +389,7 @@ export function KyselyAdapter(db: Kysely<Database>): Adapter {
  * requires. When used with `kysely-codegen`, the `Codegen` type can be passed as
  * the second generic argument. The generated types will be used, and
  * `KyselyAuth` will only verify that the correct fields exist.
- **/
+ */
 export class KyselyAuth<DB extends T, T = Database> extends Kysely<DB> {}
 
 export type Codegen = {
