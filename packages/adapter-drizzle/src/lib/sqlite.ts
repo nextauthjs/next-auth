36fc34b1344f612fc897f5dd10fc38f7ddf24321
Balázs Orbán <info@balazsorban.com>
2023-12-10 03:13:28 +0000

    Update sqlite.ts

--- packages/adapter-drizzle/src/lib/sqlite.ts
+++ packages/adapter-drizzle/src/lib/sqlite.ts
@@ -82,64 +82,61 @@ export function SQLiteDrizzleAdapter(
         .get()
     },
     async getUser(data) {
-      const result = await Promise.resolve(
-        client.select().from(users).where(eq(users.id, data)).get()
-      )
+      const result = await client
+        .select()
+        .from(users)
+        .where(eq(users.id, data))
+        .get()
       return result ?? null
     },
     async getUserByEmail(data) {
-      const result = await Promise.resolve(
-        client.select().from(users).where(eq(users.email, data)).get()
-      )
+      const result = await client
+        .select()
+        .from(users)
+        .where(eq(users.email, data))
+        .get()
       return result ?? null
     },
     createSession(data) {
       return client.insert(sessions).values(data).returning().get()
     },
     async getSessionAndUser(data) {
-      const x = await Promise.resolve(
-        client
-          .select({
-            session: sessions,
-            user: users,
-          })
-          .from(sessions)
-          .where(eq(sessions.sessionToken, data))
-          .innerJoin(users, eq(users.id, sessions.userId))
-          .get()
-      )
-      return x ?? null
+      const result = await client
+        .select({ session: sessions, user: users })
+        .from(sessions)
+        .where(eq(sessions.sessionToken, data))
+        .innerJoin(users, eq(users.id, sessions.userId))
+        .get()
+      return result ?? null
     },
     async updateUser(data) {
       if (!data.id) {
         throw new Error("No user id.")
       }
 
-      const result = await Promise.resolve(
-        client
-          .update(users)
-          .set(data)
-          .where(eq(users.id, data.id))
-          .returning()
-          .get()
-      )
+      const result = await client
+        .update(users)
+        .set(data)
+        .where(eq(users.id, data.id))
+        .returning()
+        .get()
       return result ?? null
     },
     async updateSession(data) {
-      const result = await Promise.resolve(
-        client
-          .update(sessions)
-          .set(data)
-          .where(eq(sessions.sessionToken, data.sessionToken))
-          .returning()
-          .get()
-      )
+      const result = await client
+        .update(sessions)
+        .set(data)
+        .where(eq(sessions.sessionToken, data.sessionToken))
+        .returning()
+        .get()
       return result ?? null
     },
     async linkAccount(rawAccount) {
-      const updatedAccount = await Promise.resolve(
-        client.insert(accounts).values(rawAccount).returning().get()
-      )
+      const updatedAccount = await client
+        .insert(accounts)
+        .values(rawAccount)
+        .returning()
+        .get()
 
       const account: AdapterAccount = {
         ...updatedAccount,
@@ -156,74 +153,70 @@ export function SQLiteDrizzleAdapter(
       return account
     },
     async getUserByAccount(account) {
-      const x = await Promise.resolve(
-        client
-          .select()
-          .from(accounts)
-          .leftJoin(users, eq(users.id, accounts.userId))
-          .where(
-            and(
-              eq(accounts.provider, account.provider),
-              eq(accounts.providerAccountId, account.providerAccountId)
-            )
+      const result = await client
+        .select()
+        .from(accounts)
+        .leftJoin(users, eq(users.id, accounts.userId))
+        .where(
+          and(
+            eq(accounts.provider, account.provider),
+            eq(accounts.providerAccountId, account.providerAccountId)
           )
-          .get()
-      )
-      return x?.user ?? null
+        )
+        .get()
+      return result?.user ?? null
     },
     async deleteSession(sessionToken) {
-      const x = await Promise.resolve(
-        client
-          .delete(sessions)
-          .where(eq(sessions.sessionToken, sessionToken))
-          .returning()
-          .get()
-      )
-      return x ?? null
+      const result = await client
+        .delete(sessions)
+        .where(eq(sessions.sessionToken, sessionToken))
+        .returning()
+        .get()
+      return result ?? null
     },
     async createVerificationToken(token) {
-      const result = await Promise.resolve(
-        client.insert(verificationTokens).values(token).returning().get()
-      )
+      const result = await client
+        .insert(verificationTokens)
+        .values(token)
+        .returning()
+        .get()
       return result ?? null
     },
     async useVerificationToken(token) {
       try {
-        const result = await Promise.resolve(
-          client
-            .delete(verificationTokens)
-            .where(
-              and(
-                eq(verificationTokens.identifier, token.identifier),
-                eq(verificationTokens.token, token.token)
-              )
+        const result = await client
+          .delete(verificationTokens)
+          .where(
+            and(
+              eq(verificationTokens.identifier, token.identifier),
+              eq(verificationTokens.token, token.token)
             )
-            .returning()
-            .get()
-        )
+          )
+          .returning()
+          .get()
         return result ?? null
       } catch (err) {
         throw new Error("No verification token found.")
       }
     },
     async deleteUser(id) {
-      const result = await Promise.resolve(
-        client.delete(users).where(eq(users.id, id)).returning().get()
-      )
+      const result = await client
+        .delete(users)
+        .where(eq(users.id, id))
+        .returning()
+        .get()
       return result ?? null
     },
     async unlinkAccount(account) {
-      await Promise.resolve(
-        client
-          .delete(accounts)
-          .where(
-            and(
-              eq(accounts.providerAccountId, account.providerAccountId),
-              eq(accounts.provider, account.provider)
-            )
+      await client
+        .delete(accounts)
+        .where(
+          and(
+            eq(accounts.providerAccountId, account.providerAccountId),
+            eq(accounts.provider, account.provider)
           )
-          .run()
-      )
+        )
+        .run()
 
       return undefined
     },
