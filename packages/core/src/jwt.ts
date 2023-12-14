362e6ceeb0513882ca217355723d4c690a909f5a
Leo Hui <leohxj@gmail.com>
2023-12-14 12:09:42 +0800

    fix(jwt): correct default cookie name in `getToken` (#9382)

--- packages/core/src/jwt.ts
+++ packages/core/src/jwt.ts
@@ -108,8 +108,8 @@ export async function getToken(
   const {
     secureCookie,
     cookieName = secureCookie
-      ? "__Secure-auth.session-token"
-      : "auth.session-token",
+      ? "__Secure-authjs.session-token"
+      : "authjs.session-token",
     decode: _decode = decode,
     salt = cookieName,
     secret,
