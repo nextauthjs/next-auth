9a7cd32fcf848d88cd460c42f862e029e9c1996a
Balázs Orbán <info@balazsorban.com>
2023-12-24 16:19:57 +0100

    fix test config/tweak setup

--- packages/adapter-astra-db/tests/index.test.ts
+++ packages/adapter-astra-db/tests/index.test.ts
@@ -1,17 +1,13 @@
-import { runBasicTests } from "@auth/adapter-test"
-import { AstraDBAdapter, format, defaultCollections } from "../src"
+import { runBasicTests } from "utils/adapter"
+import { AstraDBAdapter, format, defaultCollections, client } from "../src"
 import type { AstraDBConfig } from "../src"
-
-if (!process.env.ASTRA_DB_ID) throw new TypeError("ASTRA_DB_ID is missing")
-if (!process.env.ASTRA_DB_APPLICATION_TOKEN)
-  throw new TypeError("ASTRA_DB_APPLICATION_TOKEN is missing")
 
 const api = {
   dbId: process.env.ASTRA_DB_ID,
+  token: process.env.ASTRA_DB_APPLICATION_TOKEN,
   region: "us-east-2",
   keyspace: "test",
-  token: process.env.ASTRA_DB_APPLICATION_TOKEN,
-} satisfies AstraDBConfig["api"]
+} satisfies AstraDBConfig
 
 const baseUrl = `https://${api.dbId}-${api.region}.apps.astra.datastax.com/api/json/v1/${api.keyspace}`
 
@@ -20,64 +16,41 @@ const users = `${baseUrl}/${defaultCollections.users}`
 const accounts = `${baseUrl}/${defaultCollections.accounts}`
 const tokens = `${baseUrl}/${defaultCollections.verificationTokens}`
 
-function init(body: any) {
-  return {
-    method: "post",
-    headers: {
-      "x-cassandra-token": api.token,
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  } satisfies RequestInit
-}
+const { request } = client(api)
 
 runBasicTests({
-  adapter: AstraDBAdapter({ api }),
+  adapter: AstraDBAdapter(api),
   db: {
     async connect() {
       await Promise.all(
         Object.keys(defaultCollections).map((name) =>
-          fetch(baseUrl, init({ createCollection: { name } }))
+          request(null, { createCollection: { name } })
         )
       )
     },
     async disconnect() {
       await Promise.all(
         Object.keys(defaultCollections).map((name) =>
-          fetch(baseUrl, init({ deleteCollection: { name } }))
+          request(null, { deleteCollection: { name } })
         )
       )
     },
     async session(sessionToken) {
       return format.from(
-        await fetch(
-          sessions,
-          init({ findOne: { filter: { sessionToken } } })
-        ).then((res) => res.json())
+        await request(sessions, { findOne: { filter: { sessionToken } } })
       )
     },
     async user(_id: string) {
       return format.from(
-        await fetch(users, init({ findOne: { filter: { _id } } })).then((res) =>
-          res.json()
-        )
+        await request(users, { findOne: { filter: { _id } } }),
+        true
       )
     },
     async account(filter) {
-      return format.from(
-        await fetch(accounts, init({ findOne: { filter } })).then((res) =>
-          res.json()
-        )
-      )
+      return format.from(await request(accounts, { findOne: { filter } }))
     },
     async verificationToken(filter) {
-      const token = format.from(
-        await fetch(tokens, init({ findOne: { filter } })).then((res) =>
-          res.json()
-        )
-      )
-      delete token?.id
-      return token
+      return format.from(await request(tokens, { findOne: { filter } }))
     },
   },
 })
