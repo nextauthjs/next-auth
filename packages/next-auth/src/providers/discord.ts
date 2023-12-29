571a1a9be17c47393ca789c96c1fb286e41b5280
Synbulat Biishev <contact@syjalo.dev>
2023-12-29 13:40:14 +0300

    revert: revert changes

--- packages/next-auth/src/providers/discord.ts
+++ packages/next-auth/src/providers/discord.ts
@@ -6,7 +6,6 @@ export interface DiscordProfile extends Record<string, any> {
   banner: string
   banner_color: string
   discriminator: string
-  global_name: string
   email: string
   flags: number
   id: string
@@ -32,10 +31,7 @@ export default function Discord<P extends DiscordProfile>(
     userinfo: "https://discord.com/api/users/@me",
     profile(profile) {
       if (profile.avatar === null) {
-        const defaultAvatarNumber =
-          profile.discriminator === "0"
-            ? Number(BigInt(profile.id) >> BigInt(22)) % 6
-            : parseInt(profile.discriminator) % 5
+        const defaultAvatarNumber = parseInt(profile.discriminator) % 5
         profile.image_url = `https://cdn.discordapp.com/embed/avatars/${defaultAvatarNumber}.png`
       } else {
         const format = profile.avatar.startsWith("a_") ? "gif" : "png"
@@ -43,7 +39,7 @@ export default function Discord<P extends DiscordProfile>(
       }
       return {
         id: profile.id,
-        name: profile.global_name ?? profile.username,
+        name: profile.username,
         email: profile.email,
         image: profile.image_url,
       }
