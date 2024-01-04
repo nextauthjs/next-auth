abbe25350cb38e72f16a6d28de895abe8d4503c5
Nico Domino <yo@ndo.dev>
2024-01-04 19:57:05 +0100

    test(express): rm undici overrides on node globalThis for jest (#9535)

--- packages/frameworks-express/tests/setupTests.ts
+++ packages/frameworks-express/tests/setupTests.ts
@@ -1,17 +1,1 @@
-import { fetch, Headers, Request, Response } from "undici"
-import { webcrypto } from "crypto"
-
-// @ts-expect-error
-globalThis.fetch ??= fetch
-
-// @ts-expect-error
-globalThis.Headers ??= Headers
-
-// @ts-expect-error
-globalThis.Request ??= Request
-
-// @ts-expect-error
-globalThis.Response ??= Response
-
-// @ts-expect-error
-globalThis.crypto ??= webcrypto
+globalThis.crypto ??= require("crypto").webcrypto
