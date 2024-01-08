5a2b0769e02b04eefaddaffd00e40296663c2bb7
Rexford Essilfie <rexfordessilfie09@gmail.com>
2024-01-08 00:41:30 -0500

    refactor: cleanup globals

--- apps/examples/express/src/globals.ts
+++ apps/examples/express/src/globals.ts
@@ -1,8 +1,5 @@
 import path from "path"
 import { fileURLToPath } from "url"
 
-export const __filename = fileURLToPath(import.meta.url)
-export const __dirname = path.dirname(__filename)
-
-global.__filename = global.__filename ?? __filename
-global.__dirname = global.__dirname ?? __dirname
+globalThis.__filename ??= fileURLToPath(import.meta.url)
+globalThis.__dirname ??= path.dirname(globalThis.__filename)
