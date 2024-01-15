17d5014a2cbcf7d75331a7892b243310923724ad
ndom91 <yo@ndo.dev>
2024-01-15 01:30:53 +0100

    feat: dynamically import providers

--- apps/dev/nextjs/auth.config.ts
+++ apps/dev/nextjs/auth.config.ts
@@ -1,10 +1,7 @@
 import type { NextAuthConfig } from "next-auth"
-import Auth0 from "next-auth/providers/auth0"
 import Credentials from "next-auth/providers/credentials"
-import Facebook from "next-auth/providers/facebook"
-import GitHub from "next-auth/providers/github"
-import Google from "next-auth/providers/google"
-import Twitter from "next-auth/providers/twitter"
+
+type Providers = any[]
 
 declare module "next-auth" {
   /**
@@ -22,27 +19,60 @@ declare module "next-auth" {
   }
 }
 
-export default {
+const providers: Providers = [
+  Credentials({
+    credentials: { password: { label: "Password", type: "password" } },
+    authorize(c) {
+      if (c.password !== "password") return null
+      return {
+        id: "test",
+        foo: "bar",
+        name: "Test User",
+        email: "test@example.com",
+      }
+    },
+  }),
+]
+
+if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
+  const GitHub = await import("next-auth/providers/github");
+  providers.push(
+    GitHub.default({ clientId: process.env.GITHUB_ID, clientSecret: process.env.GITHUB_SECRET }),
+  );
+}
+
+if (process.env.GOOGLE_ID && process.env.GOOGLE_SECRET) {
+  const Google = await import("next-auth/providers/google");
+  providers.push(
+    Google.default({ clientId: process.env.GOOGLE_ID, clientSecret: process.env.GOOGLE_SECRET }),
+  );
+}
+
+if (process.env.FACEBOOK_ID && process.env.FACEBOOK_SECRET) {
+  const Facebook = await import("next-auth/providers/facebook");
+  providers.push(
+    Facebook.default({ clientId: process.env.FACEBOOK_ID, clientSecret: process.env.FACEBOOK_SECRET }),
+  );
+}
+
+if (process.env.AUTH0_ID && process.env.AUTH0_SECRET) {
+  const Auth0 = await import("next-auth/providers/auth0");
+  providers.push(
+    Auth0.default({ clientId: process.env.AUTH0_ID, clientSecret: process.env.AUTH0_SECRET }),
+  );
+}
+
+if (process.env.TWITTER_ID && process.env.TWITTER_SECRET) {
+  const Twitter = await import("next-auth/providers/twitter");
+  providers.push(
+    Twitter.default({ clientId: process.env.TWITTER_ID, clientSecret: process.env.TWITTER_SECRET }),
+  );
+}
+
+const authConfig: NextAuthConfig = {
   debug: false,
-  providers: [
-    GitHub({ account() {} }),
-    Auth0,
-    Facebook,
-    Google,
-    Twitter,
-    Credentials({
-      credentials: { password: { label: "Password", type: "password" } },
-      authorize(c) {
-        if (c.password !== "password") return null
-        return {
-          id: "test",
-          foo: "bar",
-          name: "Test User",
-          email: "test@example.com",
-        }
-      },
-    }),
-  ],
+  trustHost: true,
+  providers,
   callbacks: {
     jwt({ token, trigger, session }) {
       if (trigger === "update") token.name = session.user.name
