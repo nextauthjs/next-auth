3c40d64f536d0bead3f605e42a99054a0c936ed6
Alessandro Cuppari <alessandro.cuppari@broadridge.com>
2024-01-17 16:25:03 +0000

    refactor: rebranded Azure AD to Microsoft Entra

--- packages/core/src/providers/microsoft-entra-id.ts
+++ packages/core/src/providers/microsoft-entra-id.ts
@@ -1,16 +1,16 @@
 /**
  * <div style={{backgroundColor: "#0072c6", display: "flex", justifyContent: "space-between", color: "#fff", padding: 16}}>
- * <span>Built-in <b>Azure AD</b> integration.</span>
- * <a href="https://learn.microsoft.com/en-us/azure/active-directory">
- *   <img style={{display: "block"}} src="https://authjs.dev/img/providers/azure.svg" height="48" width="48"/>
+ * <span>Built-in <b>Microsoft Entra ID</b> integration.</span>
+ * <a href="https://learn.microsoft.com/en-us/entra/identity">
+ *   <img style={{display: "block"}} src="https://authjs.dev/img/providers/microsoft-entra.svg" height="48" width="48"/>
  * </a>
  * </div>
  *
- * @module providers/azure-ad
+ * @module providers/microsoft-entra-id
  */
 import type { OAuthConfig, OAuthUserConfig } from "./index.js"
 
-export interface AzureADProfile extends Record<string, any> {
+export interface MicrosoftEntraIDProfile extends Record<string, any> {
   sub: string
   nickname: string
   email: string
@@ -24,35 +24,35 @@ export interface AzureADProfile extends Record<string, any> {
  *
  * #### Callback URL
  * ```
- * https://example.com/api/auth/callback/azure-ad
+ * https://example.com/api/auth/callback/microsoft-entra-id
  * ```
  *
  * #### Configuration
  *```js
  * import Auth from "@auth/core"
- * import AzureAd from "@auth/core/providers/azure-ad"
+ * import MicrosoftEntra from "@auth/core/providers/microsoft-entra-id"
  *
  * const request = new Request(origin)
  * const response = await Auth(request, {
- *   providers: [AzureAd({ clientId: AZURE_AD_CLIENT_ID, clientSecret: AZURE_AD_CLIENT_SECRET })],
+ *   providers: [MicrosoftEntra({ clientId: MICROSOFT_ENTRA_CLIENT_ID, clientSecret: MICROSOFT_ENTRA_CLIENT_SECRET })],
  * })
  * ```
  *
  * ### Resources
  *
- *  - [AzureAd OAuth documentation](https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-oauth2-auth-code-flow/)
- *  - [AzureAd OAuth apps](https://docs.microsoft.com/en-us/azure/active-directory/develop/quickstart-register-app/)
+ *  - [Microsoft Entra OAuth documentation](https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-auth-code-flow)
+ *  - [Microsoft Entra OAuth apps](https://learn.microsoft.com/en-us/entra/identity-platform/quickstart-register-app)
  *
  * @example
  *
  * ### To allow specific Active Directory users access:
  *
- * - In https://portal.azure.com/ search for "Azure Active Directory", and select your organization.
+ * - In https://portal.azure.com/ search for "Microsoft Entra ID", and select your organization.
  * - Next, go to "App Registration" in the left menu, and create a new one.
  * - Pay close attention to "Who can use this application or access this API?"
  *   - This allows you to scope access to specific types of user accounts
- *   - Only your tenant, all azure tenants, or all azure tenants and public Microsoft accounts (Skype, Xbox, Outlook.com, etc.)
- * - When asked for a redirection URL, use `https://yourapplication.com/api/auth/callback/azure-ad` or for development `http://localhost:3000/api/auth/callback/azure-ad`.
+ *   - Only your tenant, all Microsoft tenants, or all Microsoft tenants and public Microsoft accounts (Skype, Xbox, Outlook.com, etc.)
+ * - When asked for a redirection URL, use `https://yourapplication.com/api/auth/callback/microsoft-entra-id` or for development `http://localhost:3000/api/auth/callback/microsoft-entra-id`.
  * - After your App Registration is created, under "Client Credential" create your Client secret.
  * - Now copy your:
  *   - Application (client) ID
@@ -62,28 +62,28 @@ export interface AzureADProfile extends Record<string, any> {
  * In `.env.local` create the following entries:
  *
  * ```
- * AZURE_AD_CLIENT_ID=<copy Application (client) ID here>
- * AZURE_AD_CLIENT_SECRET=<copy generated client secret value here>
- * AZURE_AD_TENANT_ID=<copy the tenant id here>
+ * MICROSOFT_ENTRA_CLIENT_ID=<copy Application (client) ID here>
+ * MICROSOFT_ENTRA_CLIENT_SECRET=<copy generated client secret value here>
+ * MICROSOFT_ENTRA_TENANT_ID=<copy the tenant id here>
  * ```
  *
- * That will default the tenant to use the `common` authorization endpoint. [For more details see here](https://docs.microsoft.com/en-us/azure/active-directory/develop/active-directory-v2-protocols#endpoints).
+ * That will default the tenant to use the `common` authorization endpoint. [For more details see here](https://learn.microsoft.com/en-us/entra/identity-platform/v2-protocols#endpoints).
  *
  * :::note
- * Azure AD returns the profile picture in an ArrayBuffer, instead of just a URL to the image, so our provider converts it to a base64 encoded image string and returns that instead. See: https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples. The default image size is 48x48 to avoid [running out of space](https://next-auth.js.org/faq#:~:text=What%20are%20the%20disadvantages%20of%20JSON%20Web%20Tokens%3F) in case the session is saved as a JWT.
+ * Microsoft Entra returns the profile picture in an ArrayBuffer, instead of just a URL to the image, so our provider converts it to a base64 encoded image string and returns that instead. See: https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples. The default image size is 48x48 to avoid [running out of space](https://next-auth.js.org/faq#:~:text=What%20are%20the%20disadvantages%20of%20JSON%20Web%20Tokens%3F) in case the session is saved as a JWT.
  * :::
  *
- * In `pages/api/auth/[...nextauth].js` find or add the `AzureAD` entries:
+ * In `pages/api/auth/[...nextauth].js` find or add the `MicrosoftEntraID` entries:
  *
  * ```js
- * import AzureADProvider from "next-auth/providers/azure-ad";
+ * import AzureADProvider from "next-auth/providers/microsoft-entra-id";
  *
  * ...
  * providers: [
- *   AzureADProvider({
- *     clientId: process.env.AZURE_AD_CLIENT_ID,
- *     clientSecret: process.env.AZURE_AD_CLIENT_SECRET,
- *     tenantId: process.env.AZURE_AD_TENANT_ID,
+ *   MicrosoftEntraIDProvider({
+ *     clientId: process.env.MICROSOFT_ENTRA_CLIENT_ID,
+ *     clientSecret: process.env.MICROSOFT_ENTRA_CLIENT_SECRET,
+ *     tenantId: process.env.MICROSOFT_ENTRA_TENANT_ID,
  *   }),
  * ]
  * ...
@@ -92,12 +92,12 @@ export interface AzureADProfile extends Record<string, any> {
  *
  * ### Notes
  *
- * By default, Auth.js assumes that the AzureAd provider is
+ * By default, Auth.js assumes that the MicrosoftEntra provider is
  * based on the [OAuth 2](https://www.rfc-editor.org/rfc/rfc6749.html) specification.
  *
  * :::tip
  *
- * The AzureAd provider comes with a [default configuration](https://github.com/nextauthjs/next-auth/blob/main/packages/core/src/providers/azure-ad.ts).
+ * The MicrosoftEntra provider comes with a [default configuration](https://github.com/nextauthjs/next-auth/blob/main/packages/core/src/providers/microsoft-entra-id.ts).
  * To override the defaults for your use case, check out [customizing a built-in OAuth provider](https://authjs.dev/guides/providers/custom-provider#override-default-options).
  *
  * :::
@@ -112,10 +112,10 @@ export interface AzureADProfile extends Record<string, any> {
  *
  * :::
  */
-export default function AzureAD<P extends AzureADProfile>(
+export default function MicrosoftEntraID<P extends MicrosoftEntraIDProfile>(
   options: OAuthUserConfig<P> & {
     /**
-     * https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples
+     * https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples
      *
      * @default 48
      */
@@ -127,8 +127,8 @@ export default function AzureAD<P extends AzureADProfile>(
   const { tenantId = "common", profilePhotoSize = 48, ...rest } = options
   rest.issuer ??= `https://login.microsoftonline.com/${tenantId}/v2.0`
   return {
-    id: "azure-ad",
-    name: "Azure Active Directory",
+    id: "microsoft-entra-id",
+    name: "Microsoft Entra ID",
     type: "oidc",
     wellKnown: `${rest.issuer}/.well-known/openid-configuration?appid=${options.clientId}`,
     authorization: {
@@ -137,7 +137,7 @@ export default function AzureAD<P extends AzureADProfile>(
       },
     },
     async profile(profile, tokens) {
-      // https://docs.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0#examples
+      // https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples
       const response = await fetch(
         `https://graph.microsoft.com/v1.0/me/photos/${profilePhotoSize}x${profilePhotoSize}/$value`,
         { headers: { Authorization: `Bearer ${tokens.access_token}` } }
@@ -161,7 +161,7 @@ export default function AzureAD<P extends AzureADProfile>(
         image: image ?? null,
       }
     },
-    style: { logo: "/azure.svg", text: "#fff", bg: "#0072c6" },
+    style: { logo: "/microsoft-entra.svg", text: "#fff", bg: "#0072c6" },
     options: rest,
   }
 }
