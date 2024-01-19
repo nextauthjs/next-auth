bfe83b0b618c4f96a12d7958e7b90df4e8886807
Balázs Orbán <info@balazsorban.com>
2024-01-19 13:05:58 +0100

    fix: parse action and provider id

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -91,7 +91,7 @@ export async function Auth(
 ): Promise<Response | ResponseInternal> {
   setLogger(config.logger, config.debug)
 
-  const internalRequest = await toInternalRequest(request)
+  const internalRequest = await toInternalRequest(request, config)
 
   if (internalRequest instanceof Error) {
     logger.error(internalRequest)
@@ -412,11 +412,16 @@ export interface AuthConfig {
   redirectProxyUrl?: string
 
   /**
-   * 
    * Use this option to enable experimental features.
    * When enabled, it will print a warning message to the console.
    * @note Experimental features are not guaranteed to be stable and may change or be removed without notice. Please use with caution.
    * @default {}
    */
   experimental?: Record<string, boolean>
+  /**
+   * The base path of the Auth.js API endpoints.
+   *
+   * @default "/auth"
+   */
+  basePath?: string
 }
