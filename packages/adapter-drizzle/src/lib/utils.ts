d2532c312cc4c78c28c8916f719c4aaa33d2202b
Balázs Orbán <info@balazsorban.com>
2024-01-23 11:59:26 +0100

    refactor: strip undefined values via util function (#9723)

--- packages/adapter-drizzle/src/lib/utils.ts
+++ packages/adapter-drizzle/src/lib/utils.ts
@@ -39,3 +39,13 @@ export type TableFn<Flavor> = Flavor extends AnyMySqlDatabase
     : Flavor extends AnySQLiteDatabase
       ? SQLiteTableFn
       : AnySQLiteTable
+
+type NonNullableProps<T> = {
+  [P in keyof T]: null extends T[P] ? never : P
+}[keyof T]
+
+export function stripUndefined<T>(obj: T): Pick<T, NonNullableProps<T>> {
+  const result = {} as T
+  for (const key in obj) if (obj[key] !== undefined) result[key] = obj[key]
+  return result
+}
