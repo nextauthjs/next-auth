80460a72582a2e003095d81b7273a993dac0bcf6
Balázs Orbán <info@balazsorban.com>
2024-01-26 00:21:39 +0100

    chore: add test coverage (#9805)

--- packages/core/vitest.config.js
+++ packages/core/vitest.config.js
@@ -6,6 +6,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [],
   test: {
+    coverage: { include: ['src/**/*.ts'] },
     setupFiles: ['./test/test-setup.ts'],
   },
 })
