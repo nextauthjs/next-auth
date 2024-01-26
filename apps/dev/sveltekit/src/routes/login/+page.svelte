f0d618707604dd1f83cf90c66d97a475b21b8ab0
ndom91 <yo@ndo.dev>
2024-01-26 23:55:42 +0100

    feat: add custom signin page to sveltekit dev app

---
+++ apps/dev/sveltekit/src/routes/login/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import { SignIn } from "@auth/sveltekit/components"
+</script>
+
+<SignIn provider={"github"} signInPage="login" className="w-full">
+  <div>
+    <span class="flex justify-center w-full"> Signin with Github </span>
+  </div>
+</SignIn>
