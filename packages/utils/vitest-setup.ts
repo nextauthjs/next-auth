660d908c85b02cfdd8ca49acfd55d3e6be7b5ce9
Balázs Orbán <info@balazsorban.com>
2024-01-26 21:52:56 +0100

    drop fake timers

--- packages/utils/vitest-setup.ts
+++ packages/utils/vitest-setup.ts
@@ -1,11 +1,1 @@
-import { afterEach, beforeAll, beforeEach, vi } from "vitest"
-
 globalThis.crypto ??= require("node:crypto").webcrypto
-
-beforeEach(() => {
-  vi.useFakeTimers()
-})
-
-afterEach(() => {
-  vi.useRealTimers()
-})
