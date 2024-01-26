d905fc396a35d1d3f09e2d4cdb0665440acb765c
Balázs Orbán <info@balazsorban.com>
2024-01-26 19:32:35 +0100

    fixes

--- packages/adapter-d1/test/index.test.ts
+++ packages/adapter-d1/test/index.test.ts
@@ -18,14 +18,6 @@ import { D1Database, D1DatabaseAPI } from "@miniflare/d1"
 import { runBasicTests } from "utils/adapter"
 import Database from "better-sqlite3"
 
-if (process.env.CI) {
-  // TODO: Fix this
-  test('Skipping D1Adapter tests in CI because of "Error: Must use import to load ES Module: next-auth/node_modules/.pnpm/undici@5.20.0/node_modules/undici/lib/llhttp/llhttp.wasm" errors. Should revisit', () => {
-    expect(true).toBe(true)
-  })
-  process.exit(0)
-}
-
 const sqliteDB = new Database(":memory:")
 let db = new D1Database(new D1DatabaseAPI(sqliteDB as any))
 let adapter = D1Adapter(db)
