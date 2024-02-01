f1f35525d0fa489dbc098b4a13355e2ca14654b6
Mateusz Dionizy <62315827+deronek@users.noreply.github.com>
2024-02-01 01:06:15 +0100

    fix: allow Facebook authorization config to be merged (#9866)

--- packages/core/src/providers/facebook.ts
+++ packages/core/src/providers/facebook.ts
@@ -84,7 +84,12 @@ export default function Facebook<P extends FacebookProfile>(
     id: "facebook",
     name: "Facebook",
     type: "oauth",
-    authorization: "https://www.facebook.com/v15.0/dialog/oauth?scope=email",
+    authorization: {
+      url: "https://www.facebook.com/v15.0/dialog/oauth",
+      params: {
+        scope: 'email',
+      },
+    },
     token: "https://graph.facebook.com/oauth/access_token",
     userinfo: {
       // https://developers.facebook.com/docs/graph-api/reference/user/#fields
