3118f1d65b70137dbea7978cbefe0b0f513550b9
ndom91 <yo@ndo.dev>
2024-02-05 15:59:27 +0100

    fix: more testing with mocking component that returns jsx

--- packages/core/test/pages.test.ts
+++ packages/core/test/pages.test.ts
@@ -2,61 +2,103 @@ import { vi, expect, it, describe, beforeEach } from "vitest"
 import renderPage from "../src/lib/pages/index"
 import { pagesOptions } from "./fixtures/pages"
 import { init } from "../src/lib/init"
-import SignoutPageOriginal from "../src/lib/pages/signout"
 
 const mocks = vi.hoisted(() => ({
-  // SignoutPage: vi.fn((props) => `<div>${props.url}</div>`),
-  SignoutPage: vi.fn(),
-  // SignoutPage: vi.doMock('./increment.js', () => ({ increment: () => ++mockedIncrement }))
+  SignoutPage: vi.fn().mockImplementation((props: any) => {
+    console.log("SignoutPage.props", props)
+    return `<form action="${props.url.toString()}" method="POST">`
+  }),
+  SigninPage: vi.fn(),
 }))
 
-vi.mock("preact-render-to-string", () => ({
-  default: vi.fn(),
-  renderToString: vi.fn(),
+vi.mock("preact-render-to-string", () => {
+  return {
+    renderToString: (e: any) => {
+      return e
+    },
+  }
+})
+
+vi.mock("../src/lib/pages/signin", () => ({
+  default: mocks.SigninPage,
 }))
 
 vi.mock("../src/lib/pages/signout", () => ({
   default: mocks.SignoutPage,
 }))
 
-describe("built-in pages", () => {
-  // beforeEach(() => {
-  //   vi.resetAllMocks()
-  // })
-
-  it("should attempt to render signout page", async () => {
-    // When visiting `/auth/signout`, for example
-    const { options } = await init({
-      authOptions: pagesOptions,
-      action: "signout",
-      providerId: "github",
-      url: new URL("http://localhost:3000/auth/signout"),
-      cookies: {},
-      isPost: true,
-      csrfDisabled: true,
+describe("pages", () => {
+  describe("sign-out", () => {
+    beforeEach(() => {
+      vi.resetAllMocks()
     })
-    console.log("init.options", options)
 
-    const render = renderPage({ ...options, query: {}, cookies: [] })
-    const signOutPage = render.signout()
-    console.log("signoutPage.page", signOutPage)
+    it("should attempt to render signout page", async () => {
+      // When visiting `/auth/signout`, for example
+      const { options } = await init({
+        authOptions: pagesOptions,
+        action: "signout",
+        providerId: "github",
+        url: new URL("http://localhost:3000/auth/signout"),
+        cookies: {},
+        isPost: true,
+        csrfDisabled: true,
+      })
 
-    expect(mocks.SignoutPage).toHaveBeenCalled()
+      const render = renderPage({ ...options, query: {}, cookies: [] })
+      const signOutPage = render.signout()
+
+      console.log(signOutPage)
+
+      expect(mocks.SignoutPage).toHaveBeenCalled()
+      expect(signOutPage.body).toContain(`<title>Sign Out</title>`)
+      expect(signOutPage.body).toContain(
+        `action="http://localhost:3000/auth/signout"`
+      )
+    })
+
+    it("should return correct URL for signout page form action", async () => {
+      // When visiting `/auth/signout`, for example
+      const { options } = await init({
+        authOptions: pagesOptions,
+        action: "signout",
+        providerId: "github",
+        url: new URL("http://localhost:3000/auth/signout"),
+        cookies: {},
+        isPost: true,
+        csrfDisabled: true,
+      })
+
+      expect(options.url).toBeInstanceOf(URL)
+      expect(options.url.toString()).toBe("http://localhost:3000/auth/signout")
+    })
   })
-
-  it("should return correct URL for signout page form action", async () => {
-    // When visiting `/auth/signout`, for example
-    const { options } = await init({
-      authOptions: pagesOptions,
-      action: "signout",
-      providerId: "github",
-      url: new URL("http://localhost:3000/auth/signout"),
-      cookies: {},
-      isPost: true,
-      csrfDisabled: true,
+  describe("sign-in", () => {
+    beforeEach(() => {
+      vi.resetAllMocks()
     })
 
-    expect(options.url).toBeInstanceOf(URL)
-    expect(options.url.toString()).toBe("http://localhost:3000/auth/signout")
+    it("should attempt to render signin page", async () => {
+      // When visiting `/auth/signin`, for example
+      const { options } = await init({
+        authOptions: pagesOptions,
+        action: "signin",
+        providerId: "github",
+        url: new URL("http://localhost:3000/auth/signin"),
+        cookies: {},
+        isPost: true,
+        csrfDisabled: true,
+      })
+
+      const render = renderPage({ ...options, query: {}, cookies: [] })
+      const signInPage = render.signin()
+      // console.log(signInPage)
+
+      expect(mocks.SigninPage).toHaveBeenCalled()
+      expect(signInPage.body).toContain(`<title>Sign In</title>`)
+      // expect(signInPage.body).toContain(
+      //   `action="http://localhost:3000/auth/signin"`
+      // )
+    })
   })
 })
