c82fa13fba2064f45adf243baf007fa55f3e3857
ndom91 <yo@ndo.dev>
2024-02-06 16:07:42 +0100

    fix: add sveltekit signin btn to auto-generated oauth provider code

--- docs/components/OAuthInstructions/components/SignInCode.tsx
+++ docs/components/OAuthInstructions/components/SignInCode.tsx
@@ -1,12 +1,12 @@
-import { Code } from "@/components/Code";
-import { Pre, Code as NXCode } from "nextra/components";
-import { TSIcon } from "./TSIcon";
+import { Code } from "@/components/Code"
+import { Pre, Code as NXCode } from "nextra/components"
+import { TSIcon } from "./TSIcon"
 
 interface Props {
-  providerName: string;
-  providerId: string;
+  providerName: string
+  providerId: string
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  highlight: any; // TODO...
+  highlight: any // TODO...
 }
 
 export function SignInCode({ providerId, providerName, highlight }: Props) {
@@ -49,6 +49,92 @@ export function SignIn() {
           </NXCode>
         </Pre>
       </Code.Next>
+      <Code.Svelte>
+        <p>
+          First, we need a SvelteKit route to handle the `POST` requests to the
+          `/signin` route
+        </p>
+        <Pre
+          data-theme="default"
+          data-copy=""
+          data-language="tsx"
+          icon={TSIcon}
+          data-filename="src/routes/signin/+page.server.ts"
+        >
+          <NXCode>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: highlight
+                  ? highlight(
+                      // prettier-ignore
+                      `import { signIn } from "../../auth"
+import type { Actions } from "./$types"
+export const actions: Actions = { default: signIn }
+`
+                    )
+                  : null,
+              }}
+            />
+          </NXCode>
+        </Pre>
+        <p className="mt-4">
+          SvelteKit requires a client-side `+page.svelte` file to go along with
+          it, so we can add this empty file to make it happy.
+        </p>
+        <Pre
+          data-theme="default"
+          data-copy=""
+          data-language="tsx"
+          icon={TSIcon}
+          data-filename="src/routes/signin/+page.svelte"
+        >
+          <NXCode>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: highlight
+                  ? highlight(
+                      // prettier-ignore
+                      `
+<!-- empty file -->
+`
+                    )
+                  : null,
+              }}
+            />
+          </NXCode>
+        </Pre>
+        <p className="mt-4">
+          Finally, we can use the exported `SignIn` Svelte component to add a
+          button to our UI that will attempt a server-side login.
+        </p>
+        <Pre
+          data-theme="default"
+          data-copy=""
+          data-language="tsx"
+          icon={TSIcon}
+          data-filename="src/routes/+page.svelte"
+        >
+          <NXCode>
+            <span
+              dangerouslySetInnerHTML={{
+                __html: highlight
+                  ? highlight(
+                      // prettier-ignore
+                      `<script lang="ts">
+  import {SignIn} from "@auth/sveltekit/components"
+</script>
+
+<div>
+  <img src="/img/logo.svg" alt="Company Logo" />
+  <SignIn provider="${providerId}" signInPage="signin" />
+</div> `
+                    )
+                  : null,
+              }}
+            />
+          </NXCode>
+        </Pre>
+      </Code.Svelte>
     </Code>
-  );
+  )
 }
