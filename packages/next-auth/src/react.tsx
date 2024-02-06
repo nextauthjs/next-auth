1511db3c2a8e255469e4c969ee3cfa5e36515c76
Nico Domino <yo@ndo.dev>
2024-02-06 10:22:09 +0100

    Discard changes to packages/next-auth/src/react.tsx

--- packages/next-auth/src/react.tsx
+++ packages/next-auth/src/react.tsx
@@ -10,7 +10,6 @@
 "use client"
 
 import * as React from "react"
-import { startAuthentication, startRegistration } from "@simplewebauthn/browser"
 import {
   apiBaseUrl,
   ClientSessionError,
@@ -25,7 +24,6 @@ import type {
   RedirectableProviderType,
 } from "@auth/core/providers"
 import type { LoggerInstance, Session } from "@auth/core/types"
-import type { WebAuthnOptionsResponseBody } from "@auth/core/lib/utils/webauthn-utils"
 import type {
   AuthClientConfig,
   ClientSafeProvider,
@@ -210,35 +208,6 @@ export async function getProviders() {
 }
 
 /**
- * Fetch webauthn options from server and prompt user for authentication or registration.
- * Returns either the completed WebAuthn response or an error request.
- * 
- * @param providerID provider ID
- * @param options SignInOptions
- * @returns WebAuthn response or error
- */
-async function webAuthnOptions(providerID: string, options?: SignInOptions) {
-  const baseUrl = apiBaseUrl(__NEXTAUTH)
-
-  // @ts-expect-error
-  const params = new URLSearchParams(options)
-
-  const optionsResp = await fetch(`${baseUrl}/webauthn-options/${providerID}?${params}`)
-  if (!optionsResp.ok) {
-    return { error: optionsResp }
-  }
-  const optionsData: WebAuthnOptionsResponseBody = await optionsResp.json()
-
-  if (optionsData.action === "authenticate") {
-    const webAuthnResponse = await startAuthentication(optionsData.options)
-    return { data: webAuthnResponse, action: "authenticate"}
-  } else {
-    const webAuthnResponse = await startRegistration(optionsData.options)
-    return { data: webAuthnResponse, action: "register" }
-  }
-}
-
-/**
  * Initiate a signin flow or send the user to the signin page listing all possible providers.
  * Handles CSRF protection.
  */
@@ -274,24 +243,11 @@ export async function signIn<
 
   const isCredentials = providers[provider].type === "credentials"
   const isEmail = providers[provider].type === "email"
-  const isWebAuthn = providers[provider].type === "webauthn"
-  const isSupportingReturn = isCredentials || isEmail || isWebAuthn
+  const isSupportingReturn = isCredentials || isEmail
 
   const signInUrl = `${baseUrl}/${
-    isCredentials || isWebAuthn ? "callback" : "signin"
+    isCredentials ? "callback" : "signin"
   }/${provider}`
-
-  // Execute WebAuthn client flow if needed
-  const webAuthnBody: Record<string, unknown> = {}
-  if (isWebAuthn) {
-    const { data, error, action } = await webAuthnOptions(provider, options)
-    if (error) {
-      logger.error(new Error(await error.text()))
-      return
-    }
-    webAuthnBody.data = JSON.stringify(data)
-    webAuthnBody.action = action
-  }
 
   const csrfToken = await getCsrfToken()
   const res = await fetch(
