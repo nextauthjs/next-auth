e05113d3be2c139ef833533bf46a6f906cc48cc9
ndom91 <yo@ndo.dev>
2024-02-06 01:27:36 +0100

    fix: minor types cleanup

--- docs/next-env.d.ts
+++ docs/next-env.d.ts
@@ -3,16 +3,3 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
-
-// import mdx files
-declare module '*.mdx' {
-  let MDXComponent: (props) => JSX.Element;
-  export default MDXComponent;
-}
-
-declare module '*.svg' {
-  let SVGComponent: (props) => JSX.Element;
-  export default SVGComponent;
-}
-
-type TODO = any
