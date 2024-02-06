88cb71cd93ce0bd29ad3d28610b49b5e7cf3f0d1
ndom91 <yo@ndo.dev>
2024-02-06 20:34:53 +0100

    fix: typescript issue, profile.id should always be set

--- packages/core/src/lib/actions/callback/oauth/callback.ts
+++ packages/core/src/lib/actions/callback/oauth/callback.ts
@@ -207,7 +207,7 @@ export async function getUserAndAccount(
         ...tokens,
         provider: provider.id,
         type: provider.type,
-        providerAccountId: userFromProfile.id?.toString(),
+        providerAccountId: userFromProfile.id?.toString()!,
       },
     }
   } catch (e) {
