2a4e637d0024e69466de8d98acd24b2860bd8b17
ndom91 <yo@ndo.dev>
2024-02-11 23:13:50 +0100

    feat: update sveltekit dev app with new styling and updated serverside + client methods

--- apps/dev/sveltekit/src/auth.ts
+++ apps/dev/sveltekit/src/auth.ts
@@ -1,10 +1,10 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/sveltekit/providers/github"
 import Credentials from "@auth/sveltekit/providers/credentials"
+import Nodemailer from "@auth/sveltekit/providers/nodemailer"
 import Facebook from "@auth/sveltekit/providers/facebook"
 import Auth0 from "@auth/sveltekit/providers/auth0"
 import Discord from "@auth/sveltekit/providers/discord"
-import Email from "@auth/sveltekit/providers/email"
 import Google from "@auth/sveltekit/providers/google"
 import Twitter from "@auth/sveltekit/providers/twitter"
 import LinkedIn from "@auth/sveltekit/providers/linkedin"
@@ -21,7 +21,6 @@ import SendGrid from "@auth/sveltekit/providers/sendgrid"
 import { createStorage } from "unstorage"
 import { UnstorageAdapter } from "@auth/unstorage-adapter"
 
-
 const storage = createStorage()
 export const { handle, signIn, signOut } = SvelteKitAuth({
   adapter: UnstorageAdapter(storage),
@@ -30,11 +29,11 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
   },
   providers: [
     SendGrid,
-    Email({ server: "smtps://0.0.0.0:465?tls.rejectUnauthorized=false" }),
+    Nodemailer({ server: "smtps://0.0.0.0:465?tls.rejectUnauthorized=false" }),
     Credentials({
       credentials: { password: { label: "Password", type: "password" } },
       async authorize(credentials) {
-        if (credentials.password !== "pw") return null
+        if (credentials.password !== "password") return null
         return {
           name: "Fill Murray",
           email: "bill@fillmurray.com",
@@ -44,22 +43,22 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
         }
       },
     }),
-    Google,
-    Facebook,
     GitHub,
-    Discord,
-    Twitter,
-    Slack,
-    LinkedIn,
-    Okta,
-    Apple,
-    Auth0,
-    Spotify,
-    Instagram,
-    Cognito,
-    Twitch,
-    Reddit,
-    AzureAD,
+    // Google,
+    // Facebook,
+    // Discord,
+    // Twitter,
+    // Slack,
+    // LinkedIn,
+    // Okta,
+    // Apple,
+    // Auth0,
+    // Spotify,
+    // Instagram,
+    // Cognito,
+    // Twitch,
+    // Reddit,
+    // AzureAD,
   ],
   theme: {
     logo: "https://authjs.dev/img/logo/logo-sm.webp",
--- apps/dev/sveltekit/src/routes/+page.svelte
+++ apps/dev/sveltekit/src/routes/+page.svelte
@@ -1,7 +1,76 @@
+<script lang="ts">
+  import { page } from "$app/stores"
+  import { SignIn, SignOut } from "@auth/sveltekit/components"
+  // import { signIn, signOut } from "../auth"
+</script>
+
 <h1>SvelteKit Auth Example</h1>
-<p>
-  This is an example site to demonstrate how to use <a
-    href="https://kit.svelte.dev/">SvelteKit</a
-  >
-  with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
-</p>
+<div class="container">
+  <p>
+    This is an example site to demonstrate how to use
+    <a href="https://kit.svelte.dev/">SvelteKit</a>
+    with <a href="https://sveltekit.authjs.dev">SvelteKit Auth</a> for authentication.
+  </p>
+
+  <div class="card">
+    <div class="card-header">
+      <h3>Server Actions</h3>
+    </div>
+    <div class="card-body">
+      These actions are all using the SvelteKit exported components to attempt
+      their action via server actions.
+      <div class="actions">
+        <SignIn provider="github">
+          <span slot="submitButton">Sign In with GitHub</span>
+        </SignIn>
+        <SignIn provider="sendgrid">
+          <div slot="email" class="input-wrapper">
+            <label class="section-header" for="sendgrid-input"> Email </label>
+            <input
+              id="sendgrid-input"
+              type="email"
+              name="email"
+              placeholder="email@example.com"
+              required
+            />
+          </div>
+          <span slot="submitButton">Sign In with SendGrid</span>
+        </SignIn>
+        <SignIn
+          provider="credentials"
+          authorizationParams={{
+            foo: "bar",
+          }}
+        >
+          <span slot="submitButton">Sign In with Credentials</span>
+          <div slot="credentials" class="input-wrapper">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+        </SignIn>
+      </div>
+    </div>
+  </div>
+  <div class="card">
+    <div class="card-header">
+      <h3>Client Actions</h3>
+    </div>
+    <div class="card-body">
+      These actions are all using the methods exported from `auth.ts`
+      <div class="actions">
+        <div>
+          <button on:click={() => signIn("github")}>Sign In with GitHub</button>
+        </div>
+        <div>
+          <div class="input-wrapper">
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required />
+          </div>
+          <button on:click={() => signIn("credentials")}
+            >Sign In with Credentials</button
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
