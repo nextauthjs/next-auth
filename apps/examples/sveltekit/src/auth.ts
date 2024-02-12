678f332d920cb31eff37e1bb4b0c99839d92c879
ndom91 <yo@ndo.dev>
2024-02-12 18:09:15 +0100

    fix: simplify src/auth.ts

--- apps/examples/sveltekit/src/auth.ts
+++ apps/examples/sveltekit/src/auth.ts
@@ -3,19 +3,7 @@ import GitHub from "@auth/sveltekit/providers/github"
 import Google from "@auth/sveltekit/providers/google"
 import Facebook from "@auth/sveltekit/providers/facebook"
 import Auth0 from "@auth/sveltekit/providers/auth0"
-// import Passkey from "@auth/sveltekit/providers/passkey"
-import { env } from "$env/dynamic/private"
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  debug: env.NODE_ENV !== "production" ? true : false,
-  providers: [
-    GitHub,
-    Google,
-    Facebook,
-    Auth0,
-    // Passkey
-  ],
-  // experimental: {
-  //   enableWebAuthn: true,
-  // },
+  providers: [GitHub, Google, Facebook, Auth0],
 })
