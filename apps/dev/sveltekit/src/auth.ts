bf989ca67193f3f002a576a75f68e665d45d8080
ndom91 <yo@ndo.dev>
2024-02-12 17:31:29 +0100

    fix: sveltekit dev app disable unnecessary providers

--- apps/dev/sveltekit/src/auth.ts
+++ apps/dev/sveltekit/src/auth.ts
@@ -1,26 +1,23 @@
 import { SvelteKitAuth } from "@auth/sveltekit"
 import GitHub from "@auth/sveltekit/providers/github"
 import Credentials from "@auth/sveltekit/providers/credentials"
-import Facebook from "@auth/sveltekit/providers/facebook"
-import Auth0 from "@auth/sveltekit/providers/auth0"
-import Discord from "@auth/sveltekit/providers/discord"
-import Nodemailer from "@auth/sveltekit/providers/nodemailer"
 import Google from "@auth/sveltekit/providers/google"
-import Twitter from "@auth/sveltekit/providers/twitter"
-import LinkedIn from "@auth/sveltekit/providers/linkedin"
-import Instagram from "@auth/sveltekit/providers/instagram"
-import Okta from "@auth/sveltekit/providers/okta"
-import Apple from "@auth/sveltekit/providers/apple"
-import Slack from "@auth/sveltekit/providers/slack"
-import Twitch from "@auth/sveltekit/providers/twitch"
-import Cognito from "@auth/sveltekit/providers/cognito"
-import AzureAD from "@auth/sveltekit/providers/azure-ad"
-import Reddit from "@auth/sveltekit/providers/reddit"
-import Spotify from "@auth/sveltekit/providers/spotify"
-import SendGrid from "@auth/sveltekit/providers/sendgrid"
+// import Facebook from "@auth/sveltekit/providers/facebook"
+// import Auth0 from "@auth/sveltekit/providers/auth0"
+// import Discord from "@auth/sveltekit/providers/discord"
+// import Twitter from "@auth/sveltekit/providers/twitter"
+// import LinkedIn from "@auth/sveltekit/providers/linkedin"
+// import Instagram from "@auth/sveltekit/providers/instagram"
+// import Okta from "@auth/sveltekit/providers/okta"
+// import Apple from "@auth/sveltekit/providers/apple"
+// import Slack from "@auth/sveltekit/providers/slack"
+// import Twitch from "@auth/sveltekit/providers/twitch"
+// import Cognito from "@auth/sveltekit/providers/cognito"
+// import AzureAD from "@auth/sveltekit/providers/azure-ad"
+// import Reddit from "@auth/sveltekit/providers/reddit"
+// import Spotify from "@auth/sveltekit/providers/spotify"
 import { createStorage } from "unstorage"
 import { UnstorageAdapter } from "@auth/unstorage-adapter"
-
 
 const storage = createStorage()
 export const { handle, signIn, signOut } = SvelteKitAuth({
@@ -29,8 +26,6 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
     strategy: "jwt",
   },
   providers: [
-    SendGrid,
-    Nodemailer({ server: "smtps://0.0.0.0:465?tls.rejectUnauthorized=false" }),
     Credentials({
       credentials: { password: { label: "Password", type: "password" } },
       async authorize(credentials) {
@@ -45,21 +40,21 @@ export const { handle, signIn, signOut } = SvelteKitAuth({
       },
     }),
     Google,
-    Facebook,
     GitHub,
-    Discord,
-    Twitter,
-    Slack,
-    LinkedIn,
-    Okta,
-    Apple,
-    Auth0,
-    Spotify,
-    Instagram,
-    Cognito,
-    Twitch,
-    Reddit,
-    AzureAD,
+    // Facebook,
+    // Discord,
+    // Twitter,
+    // Slack,
+    // LinkedIn,
+    // Okta,
+    // Apple,
+    // Auth0,
+    // Spotify,
+    // Instagram,
+    // Cognito,
+    // Twitch,
+    // Reddit,
+    // AzureAD,
   ],
   theme: {
     logo: "https://authjs.dev/img/logo/logo-sm.webp",
