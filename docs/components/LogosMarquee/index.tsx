8652fb23f785396fdb89f45eb21eea0e0cae300c
ndom91 <yo@ndo.dev>
2024-02-15 14:06:04 +0100

    fix: cleanup logosmarquee

--- docs/components/LogosMarquee/index.tsx
+++ docs/components/LogosMarquee/index.tsx
@@ -1,40 +1,80 @@
-import Marquee from "react-fast-marquee"
-import Img from "next/image"
-import manifest from "@/data/manifest.json"
+import { memo, useEffect, useState } from "react";
+import Img from "next/image";
+import Marquee, { Motion, randomIntFromInterval } from "react-marquee-slider";
+import manifest from "@/data/manifest.json";
 
-const logoSize = 96 // px
+const clamp = (min: number, num: number, max: number) =>
+  Math.min(Math.max(num, min), max);
+const logoSize = 96; // px
 
-export function LogosMarquee() {
+function changeScale() {
+  if (typeof window !== "undefined") {
+    const width = window.innerWidth;
+    return clamp(30, Number((40 + width * 0.01).toFixed(2)), 80);
+  }
+}
+
+function changeLogoCount() {
+  if (typeof window !== "undefined") {
+    const width = window.innerWidth;
+    return clamp(10, Number((10 + width * 0.005).toFixed(0)), 30);
+  }
+}
+
+export const ProviderMarquee = memo(() => {
+  const [scale, setScale] = useState(changeScale);
+  const [logoCount, setLogoCount] = useState(changeLogoCount);
+
+  useEffect(() => {
+    // Window resize handling
+    function handleEvent() {
+      setScale(changeScale);
+      setLogoCount(changeLogoCount);
+    }
+
+    window.addEventListener("resize", handleEvent);
+    return () => window.removeEventListener("resize", handleEvent);
+  }, []);
+
   return (
-    <div className="py-2 w-full">
+    <div className="absolute top-0 left-0 w-full h-full pointer-events-none">
       <Marquee
-        direction="right"
-        gradient={true}
-        gradientWidth={200}
-        gradientColor="#171717"
-        autoFill
+        onFinish={() => null}
+        onInit={() => null}
+        direction="ltr"
+        key="1"
+        velocity={5}
+        scatterRandomly
+        resetAfterTries={200}
       >
-        {Object.entries({
-          ...manifest.providersEmail,
-          ...manifest.providersOAuth,
-        })
+        {Object.entries(manifest.providersOAuth)
           .sort(() => Math.random() - 0.5)
+          .filter((_, i) => i < logoCount)
           .map(([key, name]) => (
-            <div
-              key={key}
-              style={{ width: logoSize, height: logoSize }}
-              className="flex p-3 mr-4 bg-white rounded-3xl shadow-lg select-none dark:bg-gray-400 shadow-neutral-950"
+            <Motion
+              key={`company-${key}`}
+              backgroundColors={{
+                earth: "transparent",
+                solarSystem: "transparent",
+                buffer: "transparent",
+              }}
+              initDeg={randomIntFromInterval(0, 360)}
+              direction={Math.random() > 0.5 ? "clockwise" : "counterclockwise"}
+              velocity={10}
+              radius={scale}
             >
               <Img
-                className="drop-shadow-lg"
                 src={`/img/providers/${key}.svg`}
+                className="opacity-40 grayscale dark:invert"
                 width={logoSize}
                 height={logoSize}
                 alt={`${name} logo`}
               />
-            </div>
+            </Motion>
           ))}
       </Marquee>
     </div>
-  )
-}
+  );
+});
+
+ProviderMarquee.displayName = "ProviderMarquee";
