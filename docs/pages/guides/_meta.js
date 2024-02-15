e447aa38a444e6aea4d75da496b5c46bc0c7a79a
ndom91 <yo@ndo.dev>
2024-02-15 13:55:38 +0100

    fix: move new "creating a framework integration" guide over

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -2,6 +2,7 @@ export default {
   "configuring-oauth-providers": "Configuring OAuth",
   "oauth-github-setup": "Basic OAuth with Github",
   "creating-a-database-adapter": "Creating a Database Adapter",
+  "creating-a-framework-integration": "Creating a Framework Integration",
   "role-based-access-control": "Role-Based Access Control",
   testing: "Testing",
   debugging: "Debugging",
