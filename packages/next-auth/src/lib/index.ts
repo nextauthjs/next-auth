7040c4f581322f6c77deb2a7a95ed80a5a50afdd
AJ Stars <ayushjamwal10@gmail.com>
2024-02-17 07:49:19 +0530

    fix: removed unexpected console.logs #10034 (#10051)

--- packages/next-auth/src/lib/index.ts
+++ packages/next-auth/src/lib/index.ts
@@ -192,7 +192,6 @@ export function initAuth(
         ...args: Parameters<NextAuthMiddleware | AppRouteHandlerFn>
       ) => {
         return handleAuth(args, config, userMiddlewareOrRoute).then((res) => {
-          console.log(Object.fromEntries(res.headers))
           return res
         })
       }
@@ -272,9 +271,6 @@ async function handleAuth(
   // Preserve cookies from the session response
   for (const cookie of sessionResponse.headers.getSetCookie())
     finalResponse.headers.append("set-cookie", cookie)
-
-  console.log(Object.fromEntries(finalResponse.headers))
-  console.log(Object.fromEntries(response.headers))
 
   return finalResponse
 }
