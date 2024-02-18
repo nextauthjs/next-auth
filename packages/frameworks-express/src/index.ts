72b3d1c1001c503b78732ccefa342e75c10c6b35
Johannes Waigel <johannes@waigel.me>
2024-02-18 01:50:26 +0000

    docs: fix missing async keyword in express authorization middleware example

--- packages/frameworks-express/src/index.ts
+++ packages/frameworks-express/src/index.ts
@@ -66,7 +66,7 @@
  * This can either be done per route, or for a group of routes using a middleware such as the following:
  *
  * ```ts
- * export function authenticatedUser(
+ * export async function authenticatedUser(
  *   req: Request,
  *   res: Response,
  *   next: NextFunction
