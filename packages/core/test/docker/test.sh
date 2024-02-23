5319c81b2495fecf945b9f672453e8cba55a6c09
ndom91 <yo@ndo.dev>
2024-02-23 21:04:31 +0100

    fix: load playwright env file

--- packages/core/test/docker/test.sh
+++ packages/core/test/docker/test.sh
@@ -4,6 +4,7 @@ cd ../../apps/examples/nextjs/ || exit
 
 docker-compose -f docker-compose.yml \
   --progress=plain \
+  --env-file ../../../packages/core/.env \
   up \
   --detach \
   --build
@@ -11,11 +12,13 @@ docker-compose -f docker-compose.yml \
 echo "waiting 10 seconds for container to start..."
 sleep 10
 
-export DEBUG=pw:webserver
+export TEST_DOCKER=1
 
 # Always stop container, but exit with 1 when tests are failing
-if pnpm dlx playwright test -c ../../../packages/utils/playwright.config.ts; then
-  docker-compose down
+if playwright test -c ../../../packages/utils/playwright.config.ts; then
+  # docker-compose down
+  echo "Tests passed"
 else
-  docker-compose down && exit 1
+  # docker-compose down && exit 1
+  exit 1
 fi
