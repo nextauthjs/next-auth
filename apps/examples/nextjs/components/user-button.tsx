587c71080a5ecfdd008bf35e2527376f58953928
Nico Domino <yo@ndo.dev>
2024-02-28 16:42:10 +0100

    Discard changes to apps/examples/nextjs/components/user-button.tsx

--- apps/examples/nextjs/components/user-button.tsx
+++ apps/examples/nextjs/components/user-button.tsx
@@ -1,6 +1,6 @@
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 import { Button } from "./ui/button"
-import { auth, signOut } from "auth"
+import { auth } from "auth"
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -14,48 +14,35 @@ export default async function UserButton() {
   const session = await auth()
   if (!session?.user) return <SignIn />
   return (
-    <div className="flex gap-6 items-center">
-      <form
-        action={async () => {
-          "use server"
-          await signOut()
-        }}
-        className="w-full"
-      >
-        <Button type="submit" variant="ghost" className="py-2 px-4 w-full">
-          Sign out
-        </Button>
-      </form>
-      <DropdownMenu>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="relative w-8 h-8 rounded-full">
-            <Avatar className="w-8 h-8">
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" className="relative w-8 h-8 rounded-full">
+          <Avatar className="w-8 h-8">
+            {session.user.image && (
               <AvatarImage
-                src={
-                  session.user.image ??
-                  "https://source.boringavatars.com/marble/120"
-                }
+                src={session.user.image}
                 alt={session.user.name ?? ""}
               />
-            </Avatar>
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent className="w-56" align="end" forceMount>
-          <DropdownMenuLabel className="font-normal">
-            <div className="flex flex-col space-y-1">
-              <p className="text-sm font-medium leading-none">
-                {session.user.name}
-              </p>
-              <p className="text-xs leading-none text-muted-foreground">
-                {session.user.email}
-              </p>
-            </div>
-          </DropdownMenuLabel>
-          <DropdownMenuItem>
-            <SignOut />
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-    </div>
+            )}
+            <AvatarFallback>{session.user.email}</AvatarFallback>
+          </Avatar>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56" align="end" forceMount>
+        <DropdownMenuLabel className="font-normal">
+          <div className="flex flex-col space-y-1">
+            <p className="text-sm font-medium leading-none">
+              {session.user.name}
+            </p>
+            <p className="text-xs leading-none text-muted-foreground">
+              {session.user.email}
+            </p>
+          </div>
+        </DropdownMenuLabel>
+        <DropdownMenuItem>
+          <SignOut />
+        </DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
