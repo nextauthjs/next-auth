850f85781fbfd589d47751dcd49e2436d71fba6c
Alec M <a.mingione@newgennow.com>
2024-03-07 11:18:17 -0700

    :recycle: Reactor(provider) NetSuite `profile` with defaults
    Use the default `runtime` schema from NetSuite. Then users can add more later on.

--- packages/core/src/providers/netsuite.ts
+++ packages/core/src/providers/netsuite.ts
@@ -14,6 +14,8 @@
 *  Read more about Oauth 2 setup here: https://docs.oracle.com/en/cloud/saas/netsuite/ns-online-help/section_157771281570.html
 */
 
+import type { OAuthConfig, OAuthUserConfig } from "./index.js"
+
 export interface OAuthNetSuiteOptions {
   clientId: string,
   clientSecret: string,
@@ -35,7 +37,16 @@ export interface OAuthNetSuiteOptions {
   accountId: string, // EX: TSTDRV1234567 or 81555 for prod
   scope: string, // EX: restlets rest_webservices or restlets or rest_webservices suiteanalytics_connect restlets
   userInfoUrl: string, // Either a restlet or suitelet returning runtime info or record info -> RESTlet reccommended
-  profileCallback: (profile, token) => {}, // Access data returned from the user info endpoint and return it into the session obj
+}
+
+export interface NetSuiteProfile {
+  // Main N/runtime.getCurrentUser() object return
+  id: string
+  name: string
+  email: string
+  location: string
+  role: string
+  contact?: string
 }
 
 /**
@@ -202,9 +213,9 @@ export interface OAuthNetSuiteOptions {
  *
  * :::
  */
-export default function NetSuite(
-  config: OAuthNetSuiteOptions
-): string | object {
+export default function NetSuite<P extends NetSuiteProfile>(
+  config: OAuthUserConfig<Record<string, any>>
+): OAuthConfig<Record<string, any>> {
   return {
     id: "netsuite",
     name: "NetSuite",
@@ -229,7 +240,17 @@ export default function NetSuite(
       }
     },
     userinfo: `${config.userInfoUrl}`,
-    profile: (profile: Object, token) => config.profileCallback(profile, token),
+    profile(profile) {
+      // This is the default runtime.getCurrentUser() object returned from the RESTlet or SUITELet
+      return {
+        id: profile.id,
+        name: profile.name,
+        email: profile.email,
+        location: profile.location,
+        role: profile.role,
+        contact: profile?.contact
+      }
+    },
     clientId: `${config.clientId}`,
     clientSecret: `${config.clientSecret}`,
     style: { logo: "/netsuite.png", bg: "#3a4f5f", text: "#fff" },
