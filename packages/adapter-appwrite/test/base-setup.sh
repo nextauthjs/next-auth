600d84d464bf9606e6066041b2a25272136bade9
sourabpramanik <shubpramanik241@gmail.com>
2024-03-10 08:56:34 +0530

    chore: cleanup

--- packages/adapter-appwrite/test/base-setup.sh
+++ packages/adapter-appwrite/test/base-setup.sh
@@ -2,7 +2,7 @@ if [ -f .env ]; then
     # Load Environment Variables
     export $(cat .env | grep -v '#' | sed 's/\r$//' | awk '/=/ {print $1}')
 fi
-exit_status=$?
+
 execute_curl() {
     local http_status=$(curl --write-out %{http_code} --silent --output /dev/null "$@")
     echo "$http_status"
