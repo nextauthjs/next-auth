54856ff777c60f90488f2ed810bc0fd2ca1b4427
hillac <lachiehill@gmail.com>
2024-03-22 17:58:15 +1000

    change to latest config style

--- packages/frameworks-fastify/src/index.ts
+++ packages/frameworks-fastify/src/index.ts
@@ -142,7 +142,7 @@
  * @module @auth/fastify
  */
 
-import { Auth } from "@auth/core"
+import { Auth, setEnvDefaults, createActionURL } from "@auth/core"
 import type { AuthConfig, Session } from "@auth/core/types"
 import type { FastifyRequest, FastifyPluginAsync } from 'fastify'
 import { toWebRequest, toFastifyReply } from "./lib/index.js"
@@ -155,56 +155,51 @@ export type {
   User,
 } from "@auth/core/types"
 
-function FastifyAuthPlugin(authConfig: Omit<AuthConfig, "raw">): FastifyPluginAsync {
+export function FastifyAuth(config: Omit<AuthConfig, "raw">): FastifyPluginAsync {
   return async (fastify, opts) => {
     fastify.route({
       method: ['GET', 'POST'],
       url: '/*',
       handler: async (request, reply) => {
-        const response = await Auth(toWebRequest(request), authConfig);
-        return toFastifyReply(response, reply);
+        config.basePath = getBasePath(request)
+        setEnvDefaults(process.env, config)
+        const response = await Auth(toWebRequest(request), config)
+        return toFastifyReply(response, reply)
       }
-    });
-  };
-}
-
-export function FastifyAuth(
-  config: AuthConfig
-): ReturnType<typeof FastifyAuthPlugin> {
-  const { ...authOptions } = config
-  authOptions.secret ??= process.env.AUTH_SECRET
-  authOptions.redirectProxyUrl ??= process.env.AUTH_REDIRECT_PROXY_URL
-  authOptions.trustHost ??= !!(
-    process.env.AUTH_TRUST_HOST ??
-    process.env.VERCEL ??
-    process.env.NODE_ENV !== "production"
-  )
-
-  return FastifyAuthPlugin(authOptions)
+    })
+  }
 }
 
 export type GetSessionResult = Promise<Session | null>
 
 export async function getSession(
   req: FastifyRequest,
-  options: Omit<AuthConfig, "raw">
+  config: Omit<AuthConfig, "raw">
 ): GetSessionResult {
-  options.secret ??= process.env.AUTH_SECRET
-  options.trustHost ??= true
-
-  const request = toWebRequest(req)
-  const url = new URL("/api/auth/session", request.url)
+  setEnvDefaults(process.env, config)
+  const url = createActionURL(
+    "session",
+    req.protocol,
+    // @ts-expect-error
+    new Headers(req.headers),
+    process.env,
+    config.basePath
+  )
 
   const response = await Auth(
-    new Request(url, { headers: request.headers }),
-    options
+    new Request(url, { headers: { cookie: req.headers.cookie ?? "" } }),
+    config
   )
 
   const { status = 200 } = response
-  
+
   const data = await response.json()
 
   if (!data || !Object.keys(data).length) return null
   if (status === 200) return data
   throw new Error(data.message)
+}
+
+function getBasePath(req: FastifyRequest) {
+  return req.routeOptions.config.url.split('/*')[0]
 }
