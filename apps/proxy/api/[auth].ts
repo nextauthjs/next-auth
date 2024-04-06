bbe9a71e32e1382cffe4324af9fa37c1a83e41bf
Balázs Orbán <info@balazsorban.com>
2024-04-06 17:28:32 +0200

    add providers

--- apps/proxy/api/[auth].ts
+++ apps/proxy/api/[auth].ts
@@ -1,27 +1,38 @@
-import { Auth, AuthConfig } from "@auth/core"
+import { Auth, setEnvDefaults, type AuthConfig } from "@auth/core"
+import Apple from "@auth/core/providers/apple"
+import Auth0 from "@auth/core/providers/auth0"
+import AzureDevops from "@auth/core/providers/azure-devops"
+import Cognito from "@auth/core/providers/cognito"
+import Facebook from "@auth/core/providers/facebook"
 import GitHub from "@auth/core/providers/github"
+import Google from "@auth/core/providers/google"
+import LinkedIn from "@auth/core/providers/linkedin"
+import Passage from "@auth/core/providers/passage"
+import Pinterest from "@auth/core/providers/pinterest"
+import Twitch from "@auth/core/providers/twitch"
+import Twitter from "@auth/core/providers/twitter"
+
+const authConfig: AuthConfig = {
+  providers: [
+    Apple,
+    Auth0,
+    AzureDevops,
+    Cognito,
+    Facebook,
+    GitHub,
+    Google,
+    LinkedIn,
+    Passage,
+    Pinterest,
+    Twitch,
+    Twitter,
+  ],
+  basePath: "/api",
+}
+setEnvDefaults(process.env, authConfig)
 
 export default function handler(req: Request) {
-  return Auth(req, getDefaults({ providers: [GitHub] }))
+  return Auth(req, authConfig)
 }
 
 export const config = { runtime: "edge" }
-
-export function getDefaults(config: AuthConfig) {
-  config.secret ??= process.env.AUTH_SECRET
-  config.trustHost ??= !!process.env.VERCEL
-  config.redirectProxyUrl ??= process.env.AUTH_REDIRECT_PROXY_URL
-  config.providers = config.providers.map((p) => {
-    const finalProvider = typeof p === "function" ? p() : p
-    if (finalProvider.type === "oauth" || finalProvider.type === "oidc") {
-      const ID = finalProvider.id.toUpperCase()
-      finalProvider.clientId ??= process.env[`AUTH_${ID}_ID`]
-      finalProvider.clientSecret ??= process.env[`AUTH_${ID}_SECRET`]
-      if (finalProvider.type === "oidc") {
-        finalProvider.issuer ??= process.env[`AUTH_${ID}_ISSUER`]
-      }
-    }
-    return finalProvider
-  })
-  return config
-}
