da85876424d7331fc20010da11fdd8556f24dee3
Nico Domino <yo@ndo.dev>
2024-04-06 17:31:35 +0200

    chore: add more providers to example app (#10493)
    * fix: rm azure devops from example app (deprecated)
    
    * fix: add more providers to example app
    
    * fix: update example app auth.ts
    
    * add providers
    
    * fix: add new providers to proxy config
    
    ---------
    
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- apps/examples/nextjs/auth.ts
+++ apps/examples/nextjs/auth.ts
@@ -1,69 +1,33 @@
 import NextAuth from "next-auth"
 
-// import Atlassian from "next-auth/providers/atlassian"
-// import Authentik from "next-auth/providers/authentik"
-// import AzureAD from "next-auth/providers/azure-ad"
-// import AzureB2C from "next-auth/providers/azure-ad-b2c"
-// import Battlenet from "next-auth/providers/battlenet"
-// import Box from "next-auth/providers/box"
-// import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
-// import Bungie from "next-auth/providers/bungie"
-// import Coinbase from "next-auth/providers/coinbase"
-// import Discord from "next-auth/providers/discord"
-// import Dropbox from "next-auth/providers/dropbox"
-// import DuendeIDS6 from "next-auth/providers/duende-identity-server6"
-// import Eveonline from "next-auth/providers/eveonline"
-// import Faceit from "next-auth/providers/faceit"
-// import FortyTwoSchool from "next-auth/providers/42-school"
-// import Foursquare from "next-auth/providers/foursquare"
-// import Freshbooks from "next-auth/providers/freshbooks"
-// import Fusionauth from "next-auth/providers/fusionauth"
-// import Gitlab from "next-auth/providers/gitlab"
-// import Hubspot from "next-auth/providers/hubspot"
-// import Instagram from "next-auth/providers/instagram"
-// import Kakao from "next-auth/providers/kakao"
-// import Keycloak from "next-auth/providers/keycloak"
-// import Line from "next-auth/providers/line"
-// import Mailchimp from "next-auth/providers/mailchimp"
-// import Mailru from "next-auth/providers/mailru"
-// import Medium from "next-auth/providers/medium"
-// import Naver from "next-auth/providers/naver"
-// import Netlify from "next-auth/providers/netlify"
-// import Okta from "next-auth/providers/okta"
-// import Onelogin from "next-auth/providers/onelogin"
-// import Osso from "next-auth/providers/osso"
-// import Osu from "next-auth/providers/osu"
-// import Patreon from "next-auth/providers/patreon"
-// import Pipedrive from "next-auth/providers/pipedrive"
-// import Reddit from "next-auth/providers/reddit"
-// import Salesforce from "next-auth/providers/salesforce"
-// import Slack from "next-auth/providers/slack"
-// import Spotify from "next-auth/providers/spotify"
-// import Strava from "next-auth/providers/strava"
-// import Todoist from "next-auth/providers/todoist"
-// import Trakt from "next-auth/providers/trakt"
-// import UnitedEffects from "next-auth/providers/united-effects"
-// import Vk from "next-auth/providers/vk"
-// import Wikimedia from "next-auth/providers/wikimedia"
-// import Wordpress from "next-auth/providers/wordpress"
-// import WorkOS from "next-auth/providers/workos"
-// import Yandex from "next-auth/providers/yandex"
-// import Zitadel from "next-auth/providers/zitadel"
-// import Zoho from "next-auth/providers/zoho"
-// import Zoom from "next-auth/providers/zoom"
-
 import Apple from "next-auth/providers/apple"
+import Atlassian from "next-auth/providers/atlassian"
 import Auth0 from "next-auth/providers/auth0"
-import AzureDevops from "next-auth/providers/azure-devops"
+import AzureAD from "next-auth/providers/azure-ad"
+import AzureB2C from "next-auth/providers/azure-ad-b2c"
+import BoxyHQSAML from "next-auth/providers/boxyhq-saml"
 import Cognito from "next-auth/providers/cognito"
+import Coinbase from "next-auth/providers/coinbase"
+import Discord from "next-auth/providers/discord"
+import Dropbox from "next-auth/providers/dropbox"
 import Facebook from "next-auth/providers/facebook"
 import GitHub from "next-auth/providers/github"
+import Gitlab from "next-auth/providers/gitlab"
 import Google from "next-auth/providers/google"
+import Hubspot from "next-auth/providers/hubspot"
+import Keycloak from "next-auth/providers/keycloak"
 import LinkedIn from "next-auth/providers/linkedin"
+import Netlify from "next-auth/providers/netlify"
+import Okta from "next-auth/providers/okta"
 import Passage from "next-auth/providers/passage"
 import Pinterest from "next-auth/providers/pinterest"
+import Reddit from "next-auth/providers/reddit"
+import Slack from "next-auth/providers/slack"
+import Spotify from "next-auth/providers/spotify"
 import Twitch from "next-auth/providers/twitch"
 import Twitter from "next-auth/providers/twitter"
+import WorkOS from "next-auth/providers/workos"
+import Zoom from "next-auth/providers/zoom"
 
 import type { NextAuthConfig } from "next-auth"
 
@@ -73,21 +37,33 @@ export const config = {
   },
   providers: [
     Apple,
+    Atlassian,
     Auth0,
-    AzureDevops({
-      // TODO: Remove when env inference is corrected (prev. AUTH_AZURE-DEVOPS_ID)
-      clientId: process.env.AUTH_AZURE_DEVOPS_ID,
-      clientSecret: process.env.AUTH_AZURE_DEVOPS_SECRET,
-    }),
+    AzureAD,
+    AzureB2C,
+    BoxyHQSAML,
     Cognito,
+    Coinbase,
+    Discord,
+    Dropbox,
     Facebook,
     GitHub,
+    Gitlab,
     Google,
+    Hubspot,
+    Keycloak,
     LinkedIn,
+    Netlify,
+    Okta,
     Passage,
     Pinterest,
+    Reddit,
+    Slack,
+    Spotify,
     Twitch,
     Twitter,
+    WorkOS,
+    Zoom,
   ],
   basePath: "/auth",
   callbacks: {
--- apps/proxy/api/[auth].ts
+++ apps/proxy/api/[auth].ts
@@ -1,27 +1,70 @@
-import { Auth, AuthConfig } from "@auth/core"
+import { Auth, setEnvDefaults, type AuthConfig } from "@auth/core"
+import Apple from "@auth/core/providers/apple"
+import Atlassian from "@auth/core/providers/atlassian"
+import Auth0 from "@auth/core/providers/auth0"
+import AzureAD from "@auth/core/providers/azure-ad"
+import AzureB2C from "@auth/core/providers/azure-ad-b2c"
+import BoxyHQSAML from "@auth/core/providers/boxyhq-saml"
+import Cognito from "@auth/core/providers/cognito"
+import Coinbase from "@auth/core/providers/coinbase"
+import Discord from "@auth/core/providers/discord"
+import Dropbox from "@auth/core/providers/dropbox"
+import Facebook from "@auth/core/providers/facebook"
 import GitHub from "@auth/core/providers/github"
+import Gitlab from "@auth/core/providers/gitlab"
+import Google from "@auth/core/providers/google"
+import Hubspot from "@auth/core/providers/hubspot"
+import Keycloak from "@auth/core/providers/keycloak"
+import LinkedIn from "@auth/core/providers/linkedin"
+import Netlify from "@auth/core/providers/netlify"
+import Okta from "@auth/core/providers/okta"
+import Passage from "@auth/core/providers/passage"
+import Pinterest from "@auth/core/providers/pinterest"
+import Reddit from "@auth/core/providers/reddit"
+import Slack from "@auth/core/providers/slack"
+import Spotify from "@auth/core/providers/spotify"
+import Twitch from "@auth/core/providers/twitch"
+import Twitter from "@auth/core/providers/twitter"
+import WorkOS from "@auth/core/providers/workos"
+import Zoom from "@auth/core/providers/zoom"
+
+const authConfig: AuthConfig = {
+  providers: [
+    Apple,
+    Atlassian,
+    Auth0,
+    AzureAD,
+    AzureB2C,
+    BoxyHQSAML,
+    Cognito,
+    Coinbase,
+    Discord,
+    Dropbox,
+    Facebook,
+    GitHub,
+    Gitlab,
+    Google,
+    Hubspot,
+    Keycloak,
+    LinkedIn,
+    Netlify,
+    Okta,
+    Passage,
+    Pinterest,
+    Reddit,
+    Slack,
+    Spotify,
+    Twitch,
+    Twitter,
+    WorkOS,
+    Zoom,
+  ],
+  basePath: "/api",
+}
+setEnvDefaults(process.env, authConfig)
 
 export default function handler(req: Request) {
-  return Auth(req, getDefaults({ providers: [GitHub] }))
+  return Auth(req, authConfig)
 }
 
 export const config = { runtime: "edge" }
-
-export function getDefaults(config: AuthConfig) {
-  config.secret ??= process.env.AUTH_SECRET
-  config.trustHost ??= !!process.env.VERCEL
-  config.redirectProxyUrl ??= process.env.AUTH_REDIRECT_PROXY_URL
-  config.providers = config.providers.map((p) => {
-    const finalProvider = typeof p === "function" ? p() : p
-    if (finalProvider.type === "oauth" || finalProvider.type === "oidc") {
-      const ID = finalProvider.id.toUpperCase()
-      finalProvider.clientId ??= process.env[`AUTH_${ID}_ID`]
-      finalProvider.clientSecret ??= process.env[`AUTH_${ID}_SECRET`]
-      if (finalProvider.type === "oidc") {
-        finalProvider.issuer ??= process.env[`AUTH_${ID}_ISSUER`]
-      }
-    }
-    return finalProvider
-  })
-  return config
-}
