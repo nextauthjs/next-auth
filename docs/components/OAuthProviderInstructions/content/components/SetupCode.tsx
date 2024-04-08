65826cc22bda8c1703a3e9471c84fff37da8f84e
ndom91 <yo@ndo.dev>
2024-04-08 14:27:22 +0200

    fix: cleanup more copy and add express examples

--- docs/components/OAuthProviderInstructions/content/components/SetupCode.tsx
+++ docs/components/OAuthProviderInstructions/content/components/SetupCode.tsx
@@ -104,7 +104,7 @@ import express from "express"
 const app = express()
  
 // If app is served through a proxy, trust the proxy to allow HTTPS protocol to be detected
-app.use('trust proxy')
+app.set('trust proxy', true)
 app.use("/auth/*", ExpressAuth({ providers: [ ${providerName} ] }))
 `),
           }}
