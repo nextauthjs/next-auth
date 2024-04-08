b04c241d2d4a8b6a5affd3a584225aa27e750aaf
ndom91 <yo@ndo.dev>
2024-04-08 18:27:33 +0200

    fix: update docs index page

--- docs/components/FrameworkExamples/index.tsx
+++ docs/components/FrameworkExamples/index.tsx
@@ -1,18 +1,20 @@
+import Link from "next/link"
+import Image from "next/image"
+import { RichTabs } from "@/components/RichTabs"
 import React, { useEffect, useState } from "react"
 import { addClassToHast, codeToHtml } from "shiki"
 import { Framework, frameworkDetails } from "utils/frameworks"
-import { RichTabs } from "@/components/RichTabs"
-import Image from "next/image"
 import SvelteKit from "../../public/img/etc/sveltekit.svg"
 import Express from "../../public/img/etc/express.svg"
 import NextJs from "../../public/img/etc/nextjs.svg"
+import { CaretRight } from "@phosphor-icons/react/dist/csr/CaretRight"
 
 async function renderNextJs(framework: Framework) {
   const tailwindDarkMode = document.documentElement.classList.contains("dark")
 
   return codeToHtml(frameworkDetails[framework].code, {
     lang: "ts",
-    theme: tailwindDarkMode ? "rose-pine" : "rose-pine-moon",
+    theme: tailwindDarkMode ? "catppuccin-mocha" : "catppuccin-latte",
     transformers: [
       {
         pre(node) {
@@ -42,83 +44,89 @@ export function FrameworkExamples() {
   }
 
   return (
-    <div className="flex justify-center w-full">
-      <RichTabs
-        onTabChange={handleTabChange}
-        orientation="vertical"
-        defaultValue="nextjs"
-        className="flex flex-col justify-center !pb-4 md:flex-row md:max-w-full w-[75vw]"
-      >
-        <RichTabs.List className="flex flex-row gap-2 justify-start p-2 mb-4 bg-white rounded-xl shadow-md md:flex-col md:mr-4 md:mb-0 dark:bg-neutral-950">
+    <RichTabs
+      onTabChange={handleTabChange}
+      orientation="horizontal"
+      defaultValue="nextjs"
+      className="w-full flex flex-col !p-0 !m-0 max-w-2xl lg:self-start"
+    >
+      <RichTabs.List className="justify-between p-2 bg-white rounded-t-lg shadow-md backdrop-blur dark:bg-neutral-950/40">
+        <div className="flex gap-2">
           <RichTabs.Trigger
             value="nextjs"
             orientation="vertical"
-            className="!border-0 aria-selected:!bg-violet-600/40 !h-32 !w-32 flex-1 p-4 rounded-md focus:outline-none transition-all duration-300"
+            className="!border-0 aria-selected:!bg-violet-600 aria-selected:dark:!bg-violet-900 dark:bg-neutral-800 bg-gray-200 !h-auto !w-auto !flex-row !gap-2 !justify-start p-2 px-3 rounded-md outline-none transition-all duration-300"
           >
-            <div className="flex flex-col justify-center items-center h-full">
-              <Image
-                width="64"
-                src={NextJs}
-                alt="Next.js Logo"
-                className="dark:invert"
-              />
-            </div>
+            <Image
+              width="24"
+              src={NextJs}
+              alt="Next.js Logo"
+              className="dark:invert"
+            />
+            Next.js
           </RichTabs.Trigger>
           <RichTabs.Trigger
             value="sveltekit"
             orientation="vertical"
-            className="!border-0 aria-selected:!bg-violet-600/40 !h-32 !w-32 flex-1 p-4 rounded-md focus:outline-none transition-all duration-300"
+            className="!border-0 aria-selected:!bg-violet-600 aria-selected:dark:!bg-violet-900 dark:bg-neutral-800 bg-gray-200 !h-auto !w-auto !flex-row !gap-2 !justify-start p-2 px-3 rounded-md outline-none transition-all duration-300"
           >
-            <div className="flex flex-col justify-center items-center h-full">
-              <Image width="64" src={SvelteKit} alt="Next.js Logo" />
-            </div>
+            <Image width="24" src={SvelteKit} alt="SvelteKit Logo" />
+            SvelteKit
           </RichTabs.Trigger>
           <RichTabs.Trigger
             value="express"
             orientation="vertical"
-            className="!border-0 aria-selected:!bg-violet-600/40 !h-32 !w-32 flex-1 p-4 rounded-md focus:outline-none transition-all duration-300"
+            className="!border-0 aria-selected:!bg-violet-600 aria-selected:dark:!bg-violet-900 dark:bg-neutral-800 bg-gray-200 !h-auto !w-auto !flex-row !gap-2 !justify-start p-2 px-3 rounded-md outline-none transition-all duration-300"
           >
-            <div className="flex flex-col justify-center items-center h-full dark:invert">
-              <Image width="64" src={Express} alt="Next.js Logo" />
-            </div>
+            <Image width="24" src={Express} alt="Express Logo" className="dark:invert" />
+            Express
           </RichTabs.Trigger>
-        </RichTabs.List>
-        <div className="md:w-[clamp(20rem,_50vw,_45rem)] w-full p-2 dark:bg-neutral-950 bg-white rounded-xl shadow-md">
-          <RichTabs.Content
-            orientation="vertical"
-            value="nextjs"
-            className="h-full"
-            tabIndex={-1}
-          >
-            <div
-              className="w-full h-full"
-              dangerouslySetInnerHTML={{ __html: example }}
-            />
-          </RichTabs.Content>
-          <RichTabs.Content
-            value="sveltekit"
-            className="h-full"
-            tabIndex={-1}
-            orientation="vertical"
-          >
-            <div
-              className="w-full h-full"
-              dangerouslySetInnerHTML={{ __html: example }}
-            />
-          </RichTabs.Content>
-          <RichTabs.Content
-            value="express"
-            className="h-full"
-            tabIndex={-1}
-            orientation="vertical"
-          >
-            <div
-              className="w-full h-full"
-              dangerouslySetInnerHTML={{ __html: example }}
-            />
-          </RichTabs.Content>
+          <Link href="/integrations" className="!border-0 aria-selected:!bg-violet-600 aria-selected:dark:!bg-violet-900 dark:bg-neutral-900 bg-gray-200 !h-auto !w-auto !flex-row !gap-2 !justify-start p-2 px-3 rounded-md outline-none transition-all duration-300 flex gap-2 items-center">
+            More
+            <CaretRight />
+          </Link>
         </div>
-      </RichTabs>
-    </div>
+        <div className="inline-flex gap-2 items-center self-end pr-4 h-12">
+          <div className="bg-green-300 rounded-full size-4"></div>
+          <div className="bg-amber-300 rounded-full size-4"></div>
+          <div className="bg-red-300 rounded-full size-4"></div>
+        </div>
+      </RichTabs.List>
+      <div className="w-full p-2 pt-0 dark:bg-neutral-950/30 backdrop-blur bg-white rounded-b-lg shadow-md">
+        <RichTabs.Content
+          orientation="vertical"
+          value="nextjs"
+          className="h-full"
+          tabIndex={-1}
+        >
+          <div
+            className="w-full h-full"
+            dangerouslySetInnerHTML={{ __html: example }}
+          />
+        </RichTabs.Content>
+        <RichTabs.Content
+          value="sveltekit"
+          className="h-full"
+          tabIndex={-1}
+          orientation="vertical"
+        >
+          <div
+            className="w-full h-full"
+            dangerouslySetInnerHTML={{ __html: example }}
+          />
+        </RichTabs.Content>
+        <RichTabs.Content
+          value="express"
+          className="h-full"
+          tabIndex={-1}
+          orientation="vertical"
+        >
+          <div
+            className="w-full h-full"
+            dangerouslySetInnerHTML={{ __html: example }}
+          />
+        </RichTabs.Content>
+      </div>
+    </RichTabs>
   )
 }
