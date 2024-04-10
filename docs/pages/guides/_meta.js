4c6f8b0ac769e8d20458ac680ec01b65ade47aab
Nico Domino <yo@ndo.dev>
2024-04-10 14:00:38 +0200

    chore(docs): add resend guide (#10527)
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- docs/pages/guides/_meta.js
+++ docs/pages/guides/_meta.js
@@ -6,7 +6,7 @@ export default {
   "extending-the-session": "Extending the Session",
   "restricting-user-access": "Restricting users accessing to the app",
   "role-based-access-control": "Role-Based Access Control",
-  "configuring-github": "Configuring Github for OAuth",
+  "configuring-github": "Configuring GitHub for OAuth",
   "configuring-resend": "Configuring Resend for magic links",
   "configuring-oauth-providers": "Configuring OAuth providers",
   "creating-a-database-adapter": "Creating a Database Adapter",
