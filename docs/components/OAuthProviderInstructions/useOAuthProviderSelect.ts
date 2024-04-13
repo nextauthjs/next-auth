fb898acfd5fd96a21568516a335843c5482d5920
ndom91 <yo@ndo.dev>
2024-04-13 18:16:27 +0200

    chore(docs): cleanup dependencies and selection color

--- docs/components/OAuthProviderInstructions/useOAuthProviderSelect.ts
+++ docs/components/OAuthProviderInstructions/useOAuthProviderSelect.ts
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react"
-import { useQueryState, parseAsString } from "nuqs"
+import { useState } from "react"
 import manifest from "@/data/manifest.json"
 
 const providerList = Object.entries(manifest.providersOAuth).map(
@@ -10,17 +9,7 @@ const providerList = Object.entries(manifest.providersOAuth).map(
 
 export function useOAuthProviderSelect() {
   const [term, setTerm] = useState("")
-  const [selected, setSelected] = useQueryState(
-    "provider",
-    parseAsString.withDefault("")
-  )
-
-  useEffect(() => {
-    // if landing to the page with a provider pre-selected
-    if (!term && selected) {
-      setTerm(manifest.providersOAuth[selected])
-    }
-  }, [selected])
+  const [selected, setSelected] = useState("")
 
   function handleSearchItem(term: string) {
     setTerm(term)
