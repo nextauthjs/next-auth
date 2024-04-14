addda8993c8a575633089718374d1eb7d86d469f
ndom91 <yo@ndo.dev>
2024-04-14 11:47:38 +0200

    fix(docs): use tailwind instead of css modules

--- docs/components/OAuthProviderInstructions/content/components/StepTitle.tsx
+++ docs/components/OAuthProviderInstructions/content/components/StepTitle.tsx
@@ -1,5 +1,4 @@
 import { ReactNode } from "react"
-import styles from "../overrides.module.css"
 
 interface Props {
   children: ReactNode
@@ -8,7 +7,7 @@ interface Props {
 export function StepTitle({ children }: Props) {
   return (
     <h3
-      className={`font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-8 text-2xl ${styles["step-title-align"]}`}
+      className={`font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-8 text-2xl before:flex before:items-center before:justify-center before:!-mt-[1px]`}
     >
       {children}
     </h3>
