98a62d4e9b212fdcfeffe17bbb8d2e244c53c894
Lluis Agusti <hi@llu.lu>
2024-04-14 10:58:59 +0200

    fix(oauth-instructions): number alignment

--- docs/components/OAuthProviderInstructions/content/components/StepTitle.tsx
+++ docs/components/OAuthProviderInstructions/content/components/StepTitle.tsx
@@ -1,13 +1,16 @@
-import { ReactNode } from "react";
+import { ReactNode } from "react"
+import styles from "../overrides.module.css"
 
 interface Props {
-  children: ReactNode;
+  children: ReactNode
 }
 
 export function StepTitle({ children }: Props) {
   return (
-    <h3 className="font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-8 text-2xl">
+    <h3
+      className={`font-semibold tracking-tight text-slate-900 dark:text-slate-100 mt-8 text-2xl ${styles["step-title-align"]}`}
+    >
       {children}
     </h3>
-  );
+  )
 }
