20cfebe5d2d6eb0b3db8d78d5585d83997736a51
ndom91 <yo@ndo.dev>
2024-04-15 14:23:43 +0200

    chore(docs): create replacement phosphor svg icon files

---
+++ docs/components/Icons/index.tsx
@@ -0,0 +1,13 @@
+// Downloaded and copied from: https://phosphoricons.com/
+
+export { Check } from "./Check"
+export { Link } from "./Link"
+export { Plus } from "./Plus"
+export { ArrowSquareOut } from "./ArrowSquareOut"
+export { Sparkle } from "./Sparkle"
+export { Flask } from "./Flask"
+export { SealWarning } from "./SealWarning"
+export { ChatCircleText } from "./ChatCircleText"
+export { GitBranch } from "./GitBranch"
+export { GithubLogo } from "./GithubLogo"
+export { CaretRight } from "./CaretRight"
