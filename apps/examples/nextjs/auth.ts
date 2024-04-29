d0b8860dfd8797593b8b767c4b2f62dec2ce935c
Falco Winkler <8613031+falcowinkler@users.noreply.github.com>
2024-04-29 13:57:11 +0200

    extract RefreshAccessToken

--- apps/examples/nextjs/auth.ts
+++ apps/examples/nextjs/auth.ts
@@ -28,6 +28,7 @@ import WorkOS from "next-auth/providers/workos"
 import Zoom from "next-auth/providers/zoom"
 
 import type { NextAuthConfig } from "next-auth"
+import { refreshAccessToken } from "./lib/refreshAccessToken"
 
 export const config = {
   theme: { logo: "https://authjs.dev/img/logo-sm.png" },
@@ -96,43 +97,5 @@ export const config = {
     },
   },
 } satisfies NextAuthConfig
-
-async function refreshAccessToken(token: any) {
-  try {
-    const url = `${process.env.AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/token`
-    const response = await fetch(url, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: new URLSearchParams({
-        client_id: process.env.AUTH_KEYCLOAK_ID!,
-        client_secret: process.env.AUTH_KEYCLOAK_SECRET!,
-        grant_type: "refresh_token",
-        refresh_token: token.refreshToken,
-      }),
-    })
-
-    const refreshedTokens = await response.json()
-
-    if (!response.ok) {
-      throw refreshedTokens
-    }
-
-    return {
-      ...token,
-      accessToken: refreshedTokens.access_token,
-      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
-      refreshToken: refreshedTokens.refresh_token,
-    }
-  } catch (error) {
-    console.error("Error refreshing access token:", error)
-
-    return {
-      ...token,
-      error: "RefreshAccessTokenError",
-    }
-  }
-}
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
---
+++ apps/examples/nextjs/lib/refreshAccessToken.ts
@@ -0,0 +1,37 @@
+export async function refreshAccessToken(token: any) {
+  try {
+    const url = `${process.env.AUTH_KEYCLOAK_ISSUER}/protocol/openid-connect/token`
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: process.env.AUTH_KEYCLOAK_ID!,
+        client_secret: process.env.AUTH_KEYCLOAK_SECRET!,
+        grant_type: "refresh_token",
+        refresh_token: token.refreshToken,
+      }),
+    })
+
+    const refreshedTokens = await response.json()
+
+    if (!response.ok) {
+      throw refreshedTokens
+    }
+
+    return {
+      ...token,
+      accessToken: refreshedTokens.access_token,
+      accessTokenExpires: Date.now() + refreshedTokens.expires_in * 1000,
+      refreshToken: refreshedTokens.refresh_token,
+    }
+  } catch (error) {
+    console.error("Error refreshing access token:", error)
+
+    return {
+      ...token,
+      error: "RefreshAccessTokenError",
+    }
+  }
+}
