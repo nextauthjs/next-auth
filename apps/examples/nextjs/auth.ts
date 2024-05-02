e4eb63a2a30e9e2e116382997819a6f49d76dc2d
Winkler <8613031+falcowinkler@users.noreply.github.com>
2024-05-02 22:07:08 +0200

    docs: add example authenticating against 3rd-party APIs (#10761)
    * external api example
    
    * external api example only when keycloak is used
    
    * Refactor code, prettify 'Make api request' button and add description
    
    * remove superflous comments
    
    * extract RefreshAccessToken
    
    * extract example backend url
    
    * Point to CNAME for example backend
    
    * fix incorrect expiry time variable in token
    
    * chore(docs): cleanup netsuite doc page
    
    * chore(docs): revert to nextra@3.0.0-alpha.22
    
    * Add Draft documentation on integrating third party backends.
    
    * Revert "chore(docs): revert to nextra@3.0.0-alpha.22"
    
    This reverts commit 8d3bb0be64ad2b860221eeec053c8d9068118e53.
    
    * Revert "chore(docs): cleanup netsuite doc page"
    
    This reverts commit 92cd262e7cb653d31ee162668adb3221b102783d.
    
    * Remove refresh token
    
    * Move route to proxy level
    
    * Delete apps/examples/nextjs/package-lock.json
    
    * add link to docu
    
    * implement review comments
    
    * remove superflous log
    
    ---------
    
    Co-authored-by: Nico Domino <yo@ndo.dev>
    Co-authored-by: Balázs Orbán <info@balazsorban.com>

--- apps/examples/nextjs/auth.ts
+++ apps/examples/nextjs/auth.ts
@@ -1,4 +1,5 @@
 import NextAuth from "next-auth"
+import "next-auth/jwt"
 
 import Apple from "next-auth/providers/apple"
 import Auth0 from "next-auth/providers/auth0"
@@ -76,11 +77,30 @@ export const config = {
       if (pathname === "/middleware-example") return !!auth
       return true
     },
-    jwt({ token, trigger, session }) {
+    jwt({ token, trigger, session, account }) {
       if (trigger === "update") token.name = session.user.name
+      if (account?.provider === "keycloak") {
+        return { ...token, accessToken: account.access_token }
+      }
       return token
+    },
+    async session({ session, token }) {
+      session.accessToken = token.accessToken
+      return session
     },
   },
 } satisfies NextAuthConfig
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config)
+
+declare module "next-auth" {
+  interface Session {
+    accessToken?: string
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    accessToken?: string
+  }
+}
