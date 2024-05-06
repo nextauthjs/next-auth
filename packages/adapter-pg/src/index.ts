d55b15622a0dee8cd0b4551526cbf8904c07b678
Kawahara Shotaro <121674121+k-taro56@users.noreply.github.com>
2024-05-06 20:59:14 +0900

    chore(adapter-pg): fix broken link (#10803)
    Co-authored-by: Nico Domino <yo@ndo.dev>

--- packages/adapter-pg/src/index.ts
+++ packages/adapter-pg/src/index.ts
@@ -34,7 +34,7 @@ export function mapExpiresAt(account: any): any {
 /**
  * ## Setup
  *
- * The SQL schema for the tables used by this adapter is as follows. Learn more about the models at our doc page on [Database Models](https://authjs.dev/getting-started/adapters#models).
+ * The SQL schema for the tables used by this adapter is as follows. Learn more about the models at our doc page on [Database Models](https://authjs.dev/getting-started/database#models).
  *
  * ```sql
  * CREATE TABLE verification_token (
