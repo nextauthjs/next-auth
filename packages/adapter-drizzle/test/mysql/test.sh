cb9681734efbf8597f944ad17316355d5c42af25
ndom91 <yo@ndo.dev>
2024-05-08 19:30:24 +0200

    fix(drizzle): allow mysql test db more time to start up

--- packages/adapter-drizzle/test/mysql/test.sh
+++ packages/adapter-drizzle/test/mysql/test.sh
@@ -14,7 +14,7 @@ docker run -d --rm \
   mysql:8 \
   --default-authentication-plugin=mysql_native_password
 
-echo "Waiting 10s for db to start..." && sleep 10
+echo "Waiting 15s for db to start..." && sleep 15
 
 # Push schema and seed
 drizzle-kit generate:mysql --config=./test/mysql/drizzle.config.ts
