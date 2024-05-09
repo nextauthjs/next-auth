05a5cc7adb2844598e22a62c70596a21883e39f0
Nico Domino <yo@ndo.dev>
2024-05-09 16:48:32 +0200

    chore(adapters): cleanup `sqlite3` + `better-sqlite3` for `libsql` (#10813)

--- packages/adapter-drizzle/test/sqlite/schema.ts
+++ packages/adapter-drizzle/test/sqlite/schema.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/better-sqlite3"
-import Database from "better-sqlite3"
+import Database from "libsql"
 import { defineTables } from "../../src/lib/sqlite.ts"
 
 export const {
