e40707ebbfffd13c0c1d57ff742362e0fdad8f48
ndom91 <yo@ndo.dev>
2024-05-13 17:55:52 +0200

    fix: disable supabase auth debug output

--- packages/adapter-supabase/test/index.test.ts
+++ packages/adapter-supabase/test/index.test.ts
@@ -14,7 +14,6 @@ const secret =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6InNlcnZpY2Vfcm9sZSJ9.vI9obAHOGyVVKa3pD--kJlyxp-Z2zV9UUMAhKpNLAcU"
 
 const supabase = createClient(url, secret, {
-  auth: { debug: true },
   db: { schema: "next_auth" },
 })
 
