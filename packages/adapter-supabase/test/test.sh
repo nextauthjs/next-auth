8813ad2aa68e734e6085cdf6a3a980def234487f
ndom91 <yo@ndo.dev>
2024-05-13 17:57:17 +0200

    fix: cleanup test.sh output a bit

--- packages/adapter-supabase/test/test.sh
+++ packages/adapter-supabase/test/test.sh
@@ -3,7 +3,7 @@
 # Start database and apply migrations
 pnpm exec supabase start
 
-echo "Waiting 15s for db to start..." && sleep 15
+printf "\nWaiting 10s for db to start..." && sleep 10
 
 # Always stop Supabase, but exit with 1 when tests are failing
 if vitest run -c ../utils/vitest.config.ts; then
