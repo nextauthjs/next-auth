fda4258c49e64577efd1fe1a4897a9b67ec1bc63
David Sanchez <david38sanchez@gmail.com>
2024-06-06 15:27:40 -0400

    Remove not used code

--- packages/core/src/providers/amazon.ts
+++ packages/core/src/providers/amazon.ts
@@ -76,8 +76,6 @@ export default function Amazon<P extends AmazonProfile>(
   options: OAuthUserConfig<P> & { scope?: string }
 ): OAuthConfig<P> {
   const scope = options.scope ?? "profile profile:user_id"
-  const tokenEndpointUrl = "https://api.amazon.com/auth/o2/token"
-  const userInfoEndpointUrl = "https://api.amazon.com/user/profile"
 
   return {
     id: "amazon",
@@ -87,41 +85,8 @@ export default function Amazon<P extends AmazonProfile>(
       url: "https://www.amazon.com/ap/oa",
       params: { scope },
     },
-    token: {
-      url: tokenEndpointUrl,
-      async request({ provider, params }) {
-        const url = new URL(tokenEndpointUrl);
-        url.searchParams.set("grant_type", "authorization_code");
-        url.searchParams.set("code", params.code as string);
-        url.searchParams.set("client_id", provider.clientId as string);
-        url.searchParams.set("client_secret", provider.clientSecret as string);
-        url.searchParams.set("redirect_uri", provider.callbackUrl);
-
-        const response = await fetch(url, { method: "POST" });
-        const data = await response.json();
-
-        return {
-          tokens: {
-            access_token: data.access_token,
-            refresh_token: data.refresh_token,
-            token_type: data.token_type,
-            expires_at: Math.ceil(Date.now() / 1000 + +Number(data.expires_in)),
-          },
-        };
-      },
-    },
-    userinfo: {
-      url: userInfoEndpointUrl,
-      async request(context) {
-        const accessToken = context.tokens.access_token as string
-        const response = await fetch(userInfoEndpointUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        return response.json()
-      },
-    },
+    token: "https://api.amazon.com/auth/o2/token",
+    userinfo: "https://api.amazon.com/user/profile",
     profile(profile: P) {
       if (profile.Profile)
         return {
