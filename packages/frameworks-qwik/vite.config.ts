f6e66343aeccacf6f7099a1c0088bb01a8d5c768
Balázs Orbán <info@balazsorban.com>
2024-06-21 22:05:06 +0200

    fix(qwik): correct exports, and files (#11227)
    * fix(qwik): correct exports, and files
    
    * change to .js

--- packages/frameworks-qwik/vite.config.ts
+++ packages/frameworks-qwik/vite.config.ts
@@ -13,7 +13,7 @@ export default defineConfig(() => {
       lib: {
         entry: ["./src/index.ts"],
         formats: ["es"],
-        fileName: (_, entryName) => `${entryName}.qwik.mjs`,
+        fileName: (_, entryName) => `${entryName}.qwik.js`,
       },
       rollupOptions: {
         external: [
