4ff671263c9dbc94aeb170ab629d9b91176eaff9
Balázs Orbán <info@balazsorban.com>
2024-08-09 15:43:19 +0100

    Update tiktok.ts

--- packages/core/src/providers/tiktok.ts
+++ packages/core/src/providers/tiktok.ts
@@ -196,7 +196,7 @@ export interface TiktokProfile extends Record<string, any> {
  * :::
  */
 export default function TikTok<P extends TiktokProfile>(
-  options: OAuthUserConfig<P>
+  options: OAuthUserConfig<P>,
 ): OAuthConfig<P> {
   return {
     id: "tiktok",
@@ -212,9 +212,9 @@ export default function TikTok<P extends TiktokProfile>(
     },
 
     token: {
-      url: 'https://open.tiktokapis.com/v2/oauth/token/',
-        async request({ params, provider }) {
-          const res = await fetch(provider.token?.url as URL, {
+      url: "https://open.tiktokapis.com/v2/oauth/token/",
+      async request({ params, provider }) {
+        const res = await fetch(provider.token?.url!, {
           method: "POST",
           headers: {
             "Cache-Control": "no-cache",
