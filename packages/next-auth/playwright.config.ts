4eb85582dae4dce4b0a0d9296b0199783ac09f12
Thang Vu <hi@thvu.dev>
2024-09-08 14:07:08 +0700

    change dev command location

--- packages/next-auth/playwright.config.ts
+++ packages/next-auth/playwright.config.ts
@@ -44,8 +44,8 @@ export default defineConfig({
     },
   ],
   webServer: {
-    cwd: "../../apps/dev/nextjs",
-    command: "pnpm dev",
+    cwd: "../../",
+    command: `AUTH_SECRET=${process.env.AUTH_SECRET} pnpm dev:e2e:next`,
     url: baseURL,
     stdout: "pipe",
     reuseExistingServer: !process.env.CI,
