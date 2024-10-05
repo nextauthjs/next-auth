97447b5e1652abbe59dc6a8fdb71d5cfdedb1626
Balázs Orbán <info@balazsorban.com>
2024-10-06 01:30:47 +0200

    modify Entra ID

--- packages/core/src/providers/microsoft-entra-id.ts
+++ packages/core/src/providers/microsoft-entra-id.ts
@@ -8,7 +8,8 @@
  *
  * @module providers/microsoft-entra-id
  */
-import type { OAuthConfig, OAuthUserConfig } from "./index.js"
+import { processResponse } from "../lib/utils/custom-fetch.js"
+import type { OIDCConfig, OIDCUserConfig } from "./index.js"
 
 export interface MicrosoftEntraIDProfile extends Record<string, any> {
   sub: string
@@ -16,18 +17,6 @@ export interface MicrosoftEntraIDProfile extends Record<string, any> {
   email: string
   picture: string
 }
-
-export type MicrosoftEntraIDOptions<P extends MicrosoftEntraIDProfile> =
-  OAuthUserConfig<P> & {
-    /**
-     * https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples
-     *
-     * @default 48
-     */
-    profilePhotoSize?: 48 | 64 | 96 | 120 | 240 | 360 | 432 | 504 | 648
-    /** @default "common" */
-    tenantId?: string
-  }
 
 /**
  *
@@ -102,9 +91,8 @@ export type MicrosoftEntraIDOptions<P extends MicrosoftEntraIDProfile> =
  *
  * providers: [
  *   MicrosoftEntraID({
- *     clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
- *     clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
- *     tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
+ *     clientId: env.AUTH_MICROSOFT_ENTRA_ID_ID,
+ *     clientSecret: env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
  *   }),
  * ]
  * ```
@@ -131,21 +119,26 @@ export type MicrosoftEntraIDOptions<P extends MicrosoftEntraIDProfile> =
  *
  * :::
  */
-export default function MicrosoftEntraID<P extends MicrosoftEntraIDProfile>(
-  options: MicrosoftEntraIDOptions<P>
-): OAuthConfig<P> {
-  const { tenantId = "common", profilePhotoSize = 48, ...rest } = options
-  rest.issuer ??= `https://login.microsoftonline.com/${tenantId}/v2.0`
+export default function MicrosoftEntraID(
+  config: OIDCUserConfig<MicrosoftEntraIDProfile> & {
+    /**
+     * https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples
+     *
+     * @default 48
+     */
+    profilePhotoSize?: 48 | 64 | 96 | 120 | 240 | 360 | 432 | 504 | 648
+  }
+): OIDCConfig<MicrosoftEntraIDProfile> {
+  const hasIssuer = !!config.issuer
+  const discovery = "https://login.microsoftonline.com/common/v2.0"
+  config.issuer ??= discovery
+  const { profilePhotoSize = 48 } = config
   return {
     id: "microsoft-entra-id",
     name: "Microsoft Entra ID",
     type: "oidc",
-    wellKnown: `${rest.issuer}/.well-known/openid-configuration?appid=${options.clientId}`,
-    authorization: {
-      params: {
-        scope: "openid profile email User.Read",
-      },
-    },
+    idToken: false,
+    authorization: { params: { scope: "openid profile email User.Read" } },
     async profile(profile, tokens) {
       // https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples
       const response = await fetch(
@@ -172,6 +165,25 @@ export default function MicrosoftEntraID<P extends MicrosoftEntraIDProfile>(
       }
     },
     style: { text: "#fff", bg: "#0072c6" },
-    options: rest,
+
+    async [processResponse](response) {
+      // If issuer is set, don't process response
+      if (hasIssuer) return response
+      const contentType = response.headers.get("content-type")
+      if (!contentType?.includes("application/json")) return response
+
+      const json = await response.clone().json()
+      if ("issuer" in json && json.issuer !== discovery) {
+        // Reprocess discovery response
+        return Response.json({ ...json, issuer: discovery })
+      } else if ("id_token" in json) {
+        // Reprocess token response
+        const { id_token: _, ...tokenSet } = json
+        return Response.json(tokenSet)
+      }
+
+      return response
+    },
+    options: config,
   }
 }
