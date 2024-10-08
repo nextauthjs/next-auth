c812b9cece24b8c8f5d9e58bdcb6941140e9d866
Code-DJ <8212554+Code-DJ@users.noreply.github.com>
2024-10-08 15:03:53 -0500

    await headers and cookies

--- packages/next-auth/src/lib/index.ts
+++ packages/next-auth/src/lib/index.ts
@@ -127,7 +127,7 @@ export function initAuth(
     return async (...args: WithAuthArgs) => {
       if (!args.length) {
         // React Server Components
-        const _headers = headers()
+        const _headers = await headers()
         const _config = await config(undefined) // Review: Should we pass headers() here instead?
         onLazyLoad?.(_config)
 
