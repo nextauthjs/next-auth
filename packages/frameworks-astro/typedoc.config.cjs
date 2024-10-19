f88e2fcced0cf666206512045cc8cc63b7fc1745
Reuben Tier <otterlord.dev@gmail.com>
2024-10-19 19:21:02 +0100

    Remove middleware.ts

--- packages/frameworks-astro/typedoc.config.cjs
+++ packages/frameworks-astro/typedoc.config.cjs
@@ -4,7 +4,7 @@
  * @type {import('typedoc').TypeDocOptions & import('typedoc-plugin-markdown').MarkdownTheme}
  */
 module.exports = {
-  entryPoints: ["src/index.ts", "src/client.ts", "src/config.ts", "src/middleware.ts", "src/server.ts"],
+  entryPoints: ["src/index.ts", "src/client.ts", "src/config.ts", "src/server.ts"],
   entryPointStrategy: "expand",
   tsconfig: "./tsconfig.json",
   entryModule: "@auth/astro",
