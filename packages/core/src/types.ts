8a9c270ef0b4c50fbbdb2477aaaf24b5cae6d74a
Yahya Shannat <73648944+YASHDEV42@users.noreply.github.com>
2024-10-21 15:46:51 +0300

    Update User interface to contain role

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -258,6 +258,7 @@ export interface User {
   name?: string | null
   email?: string | null
   image?: string | null
+  role?: string | null
 }
 
 // Below are types that are only supposed be used by next-auth internally
