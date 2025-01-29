c378a10c0aafcb7b2b21cbb6358cce18fba9b50e
Thang Vu <hi@thvu.dev>
2025-01-29 16:06:29 +0700

    docs: dark mode for DocSearch

--- docs/components/DocSearch/wrapper.tsx
+++ docs/components/DocSearch/wrapper.tsx
@@ -1,8 +1,19 @@
 import { DocSearch } from "@docsearch/react"
+import { useTheme } from "nextra-theme-docs"
+import { useEffect } from "react"
 
 import "@docsearch/css"
 
 function App() {
+  const { resolvedTheme } = useTheme()
+
+  useEffect(() => {
+    if (resolvedTheme) {
+      // hack to get DocSearch to use dark mode colors if applicable
+      document.documentElement.setAttribute("data-theme", resolvedTheme)
+    }
+  }, [resolvedTheme])
+
   return (
     <DocSearch
       appId={process.env.NEXT_PUBLIC_ALGOLIA_APP_ID!}
