8712e1206767600e94315ffbf489c9cd53934d4a
Ben Hovinga <23349127+benhovinga@users.noreply.github.com>
2025-02-05 14:53:45 -0330

    Fix MicrosoftEntraID TSDoc Clarified the usage of issuer parameter

--- packages/core/src/providers/microsoft-entra-id.ts
+++ packages/core/src/providers/microsoft-entra-id.ts
@@ -335,27 +335,55 @@ export interface MicrosoftEntraIDProfile {
 }
 
 /**
+ * ### Setup
  *
- * Add Microsoft Entra ID login to your page.
+ * #### Callback URL
  *
- * ## Setup
- *
- * ### Callback URL
  * ```
- * https://example.com/auth/callback/microsoft-entra-id
+ * https://example.com/api/auth/callback/microsoft-entra-id
  * ```
  *
- * ### Configuration
+ * #### Environment Variables
  *
- * @example
+ * ```env
+ * AUTH_MICROSOFT_ENTRA_ID_ID="<Application (client) ID>"
+ * AUTH_MICROSOFT_ENTRA_ID_SECRET="<Client secret value>"
+ * AUTH_MICROSOFT_ENTRA_ID_ISSUER="https://login.microsoftonline.com/<Directory (tenant) ID>/v2.0/"
+ * ```
  *
- * ```ts
+ * #### Configuration
+ *
+ * When the `issuer` parameter is omitted it will default to
+ * `"https://login.microsoftonline.com/common/v2.0/"`.
+ * This allows any Microsoft account (Personal, School or Work) to log in.
+ *
+ * ```typescript
  * import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id"
  * ...
  * providers: [
  *   MicrosoftEntraID({
- *     clientId: env.AUTH_MICROSOFT_ENTRA_ID_ID,
- *     clientSecret: env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+ *     clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+ *     clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+ *   }),
+ * ]
+ * ...
+ * ```
+ *
+ * To only allow your organization's users to log in you will need to configure
+ * the `issuer` parameter with your Directory (tenant) ID.
+ *
+ * ```env
+ * AUTH_MICROSOFT_ENTRA_ID_ISSUER="https://login.microsoftonline.com/<Directory (tenant) ID>/v2.0/"
+ * ```
+ *
+ * ```typescript
+ * import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id"
+ * ...
+ * providers: [
+ *   MicrosoftEntraID({
+ *     clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+ *     clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+ *     issuer: process.env.AUTH_MICROSOFT_ENTRA_ID_ISSUER,
  *   }),
  * ]
  * ...
@@ -366,46 +394,39 @@ export interface MicrosoftEntraIDProfile {
  *  - [Microsoft Entra OAuth documentation](https://learn.microsoft.com/en-us/entra/identity-platform/v2-oauth2-auth-code-flow)
  *  - [Microsoft Entra OAuth apps](https://learn.microsoft.com/en-us/entra/identity-platform/quickstart-register-app)
  *
- * ### To allow specific Active Directory users access:
- *
- * By default, the Entra ID provider lets the users to log in with any Microsoft account (either Personal, School or Work).
- *
- * To only allow your organization's users to log in, you'll need to set the `issuer`, in addition to the client id and secret.
- *
- * @example
- * ```ts
- * import MicrosoftEntraID from "@auth/core/providers/microsoft-entra-id"
- *
- * providers: [
- *   MicrosoftEntraID({
- *     clientId: env.AUTH_MICROSOFT_ENTRA_ID_ID,
- *     clientSecret: env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
- *     issuer: env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
- *   }),
- * ]
- * ```
- *
  * ### Notes
  *
- * Microsoft Entra ID returns the profile picture in an ArrayBuffer, instead of just a URL to the image, so our provider converts it to a base64 encoded image string and returns that instead. See: https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples. The default image size is 48x48 to avoid [running out of space](https://next-auth.js.org/faq#:~:text=What%20are%20the%20disadvantages%20of%20JSON%20Web%20Tokens%3F) in case the session is saved as a JWT.
+ * Microsoft Entra ID returns the profile picture in an ArrayBuffer, instead of
+ * just a URL to the image, so our provider converts it to a base64 encoded
+ * image string and returns that instead. See:
+ * https://learn.microsoft.com/en-us/graph/api/profilephoto-get?view=graph-rest-1.0&tabs=http#examples.
+ * The default image size is 48x48 to avoid
+ * [running out of space](https://next-auth.js.org/faq#json-web-tokens)
+ * in case the session is saved as a JWT.
  *
- * By default, Auth.js assumes that the Microsoft Entra ID provider is
- * based on the [Open ID Connect](https://openid.net/specs/openid-connect-core-1_0.html) specification.
+ * By default, Auth.js assumes that the Microsoft Entra ID provider is based on
+ * the [Open ID Connect](https://openid.net/specs/openid-connect-core-1_0.html)
+ * specification.
  *
  * :::tip
  *
- * The Microsoft Entra ID provider comes with a [default configuration](https://github.com/nextauthjs/next-auth/blob/main/packages/core/src/providers/microsoft-entra-id.ts).
- * To override the defaults for your use case, check out [customizing a built-in OAuth provider](https://authjs.dev/guides/configuring-oauth-providers).
+ * The Microsoft Entra ID provider comes with a
+ * [default configuration](https://github.com/nextauthjs/next-auth/blob/main/packages/core/src/providers/microsoft-entra-id.ts).
+ * To override the defaults for your use case, check out
+ * [customizing a built-in OAuth provider](https://authjs.dev/guides/configuring-oauth-providers).
  *
  * :::
  *
  * :::info **Disclaimer**
  *
- * If you think you found a bug in the default configuration, you can [open an issue](https://authjs.dev/new/provider-issue).
+ * If you think you found a bug in the default configuration, you can
+ * [open an issue](https://authjs.dev/new/provider-issue).
  *
- * Auth.js strictly adheres to the specification and it cannot take responsibility for any deviation from
- * the spec by the provider. You can open an issue, but if the problem is non-compliance with the spec,
- * we might not pursue a resolution. You can ask for more help in [Discussions](https://authjs.dev/new/github-discussions).
+ * Auth.js strictly adheres to the specification and it cannot take
+ * responsibility for any deviation from the spec by the provider. You can open
+ * an issue, but if the problem is non-compliance with the spec, we might not
+ * pursue a resolution. You can ask for more help in
+ * [Discussions](https://authjs.dev/new/github-discussions).
  *
  * :::
  */
@@ -447,14 +468,11 @@ export default function MicrosoftEntraID(
       }
 
       return {
-        id: profile.sub,
-        name: profile.name,
-        email: profile.email,
+        ...profile,
         image: image ?? null,
       }
     },
     style: { text: "#fff", bg: "#0072c6" },
-    /** Entra ID returns the wrong issuer @see https://github.com/MicrosoftDocs/azure-docs/issues/113944 */
     async [customFetch](...args) {
       const url = new URL(args[0] instanceof Request ? args[0].url : args[0])
       if (url.pathname.endsWith(".well-known/openid-configuration")) {
