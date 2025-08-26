ef8a3393cae86083a482aed9e53b5baa8b969f0a
Aman Meherally <amanmeherally@gmail.com>
2025-08-26 19:05:47 -0400

    fix: migrated to RichTabs

--- docs/components/Hero/CodeTabs.tsx
+++ docs/components/Hero/CodeTabs.tsx
@@ -2,10 +2,50 @@ import React, { useState, useEffect } from "react"
 import { type Highlighter, createHighlighter } from "shiki"
 import Image from "next/image"
 
+import { RichTabs } from "@/components/RichTabs"
+import { CaretRight } from "../Icons"
+
+// Icons
 import expressIcon from "../../public/img/etc/express.svg"
 import nextIcon from "../../public/img/etc/nextjs.svg"
 import qwikIcon from "../../public/img/etc/qwik.svg"
 import sveltekitIcon from "../../public/img/etc/sveltekit.svg"
+
+const codeSnippets = {
+  nextjs: `// auth.ts
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+export const { auth, handlers } = NextAuth({ providers: [GitHub] })
+
+// middleware.ts
+export { auth as middleware } from "@auth"
+
+// app/api/auth/[...nextauth]/route.ts
+import { handlers } from "@auth"
+export const { GET, POST } = handlers`,
+  sveltekit: `// src/auth.ts
+import { SvelteKitAuth } from "@auth/sveltekit"
+import GitHub from '@auth/sveltekit/providers/github'
+
+export const { handle } = SvelteKitAuth({
+  providers: [GitHub],
+})
+
+// src/hooks.server.ts
+export { handle } from "./auth"`,
+  express: `// server.ts
+import express from "express"
+import { ExpressAuth } from "@auth/express"
+import GitHub from "@auth/express/providers/github"
+
+const app = express()
+
+app.use("/auth/*", ExpressAuth({ providers: [GitHub] }))`,
+  qwik: `// src/routes/plugin@auth.ts
+import { QwikAuth } from "@auth/qwik"
+import GitHub from "@auth/qwik/providers/github"
+export const { onRequest, useSession } = QwikAuth$(() => ({ providers: [GitHub] }))`,
+}
 
 const ExpressIcon = () => (
   <Image
@@ -16,6 +56,7 @@ const ExpressIcon = () => (
     className="dark:invert"
   />
 )
+
 const NextIcon = () => (
   <Image
     src={nextIcon.src}
@@ -25,17 +66,25 @@ const NextIcon = () => (
     className="filter dark:invert"
   />
 )
+
 const QwikIcon = () => (
   <Image src={qwikIcon.src} alt="Qwik" width={24} height={24} />
 )
+
 const SvelteKitIcon = () => (
   <Image src={sveltekitIcon.src} alt="SvelteKit" width={24} height={24} />
 )
-const CaretRight = () => <span>›</span>
 
 export const CodeTabs = () => {
   const [activeTab, setActiveTab] = useState("nextjs")
   const [highlighter, setHighlighter] = useState<Highlighter[] | null>([])
+
+  const tabs = [
+    { value: "express", Icon: ExpressIcon, name: "Express" },
+    { value: "nextjs", Icon: NextIcon, name: "Next.js" },
+    { value: "qwik", Icon: QwikIcon, name: "Qwik" },
+    { value: "sveltekit", Icon: SvelteKitIcon, name: "SvelteKit" },
+  ]
 
   useEffect(() => {
     ;(async () => {
@@ -77,89 +126,62 @@ export const CodeTabs = () => {
     })
   }
 
-  const codeSnippets = {
-    nextjs: `// auth.ts
-import NextAuth from "next-auth"
-import GitHub from "next-auth/providers/github"
-export const { auth, handlers } = NextAuth({ providers: [GitHub] })
-
-// middleware.ts
-export { auth as middleware } from "@auth"
-
-// app/api/auth/[...nextauth]/route.ts
-import { handlers } from "@auth"
-export const { GET, POST } = handlers`,
-    sveltekit: `// src/auth.ts
-import { SvelteKitAuth } from "@auth/sveltekit"
-import GitHub from '@auth/sveltekit/providers/github'
-
-export const { handle } = SvelteKitAuth({
-  providers: [GitHub],
-})
-
-// src/hooks.server.ts
-export { handle } from "./auth"`,
-    express: `// server.ts
-import express from "express"
-import { ExpressAuth } from "@auth/express"
-import GitHub from "@auth/express/providers/github"
-
-const app = express()
-
-app.use("/auth/*", ExpressAuth({ providers: [GitHub] }))`,
-    qwik: `// src/routes/plugin@auth.ts
-import { QwikAuth } from "@auth/qwik"
-import GitHub from "@auth/qwik/providers/github"
-export const { onRequest, useSession } = QwikAuth$(() => ({ providers: [GitHub] }))`,
-  }
-
-  const tabs = [
-    { value: "express", Icon: ExpressIcon, name: "Express" },
-    { value: "nextjs", Icon: NextIcon, name: "Next.js" },
-    { value: "qwik", Icon: QwikIcon, name: "Qwik" },
-    { value: "sveltekit", Icon: SvelteKitIcon, name: "SvelteKit" },
-  ]
-
   return (
     <div className="w-full max-w-3xl rounded-lg border border-neutral-200 bg-[#FFF4] p-2 px-4 backdrop-blur-sm dark:border-neutral-800 dark:bg-[#FFFFFF03]">
-      <div className="flex items-center justify-between border-b border-neutral-200 p-2 pb-4 dark:border-neutral-800">
-        <div className="flex items-center gap-2">
-          {tabs.map((tab) => (
-            <button
-              key={tab.value}
-              onClick={() => setActiveTab(tab.value)}
-              className={`flex h-10 w-10 items-center justify-center rounded-lg bg-[#FFF4] outline-none dark:bg-[#FFFFFF09] ${
-                activeTab === tab.value &&
-                "border border-neutral-200 dark:border-neutral-800"
-              }`}
-            >
-              <tab.Icon />
-            </button>
-          ))}
+      <RichTabs
+        defaultValue={activeTab}
+        onTabChange={setActiveTab}
+        tabKey="framework"
+        className="mt-0 pt-0"
+      >
+        <div className="flex items-center justify-between border-b border-neutral-200 pb-4 pt-2 dark:border-neutral-800">
+          <RichTabs.List className="gap-3">
+            {tabs.map((tab) => (
+              <RichTabs.Trigger
+                key={tab.value}
+                value={tab.value}
+                className="flex max-h-10 max-w-10 items-center justify-center rounded-lg bg-[#FFF4] outline-none dark:bg-[#FFFFFF09]"
+              >
+                <tab.Icon />
+              </RichTabs.Trigger>
+            ))}
+          </RichTabs.List>
           <a
             href="/getting-started/integrations"
-            className="flex h-10 items-center gap-2 rounded-lg p-2 px-3 text-sm font-medium text-neutral-600 transition-colors duration-300 hover:bg-black/5 dark:text-neutral-400 hover:dark:bg-white/5"
+            className="flex h-10 items-center gap-2 rounded-lg bg-[#FFF4] p-2 px-4 text-sm font-medium text-neutral-600 backdrop-blur-sm transition-colors duration-300 hover:bg-black/5 dark:bg-[#FFFFFF03] dark:text-neutral-400 hover:dark:bg-white/5"
           >
             <span>More</span>
-            <CaretRight />
+            <CaretRight className="h-3" />
           </a>
         </div>
-      </div>
 
-      <div className="py-4">
-        <div
-          className="dark:hidden [&>*]:!bg-transparent [&_*]:whitespace-pre-wrap"
-          dangerouslySetInnerHTML={{
-            __html: highlightLight(codeSnippets[activeTab], "ts"),
-          }}
-        />
-        <div
-          className="hidden dark:block [&>*]:!bg-transparent [&_*]:whitespace-pre-wrap"
-          dangerouslySetInnerHTML={{
-            __html: highlightDark(codeSnippets[activeTab], "ts"),
-          }}
-        />
-      </div>
+        {tabs.map((tab) => (
+          <RichTabs.Content
+            key={tab.value}
+            value={tab.value}
+            className="border-none py-4 shadow-none"
+          >
+            <div
+              className="dark:hidden [&>*]:!bg-transparent [&_*]:whitespace-pre-wrap"
+              dangerouslySetInnerHTML={{
+                __html: highlightLight(
+                  codeSnippets[tab.value as keyof typeof codeSnippets],
+                  "ts"
+                ),
+              }}
+            />
+            <div
+              className="hidden dark:block [&>*]:!bg-transparent [&_*]:whitespace-pre-wrap"
+              dangerouslySetInnerHTML={{
+                __html: highlightDark(
+                  codeSnippets[tab.value as keyof typeof codeSnippets],
+                  "ts"
+                ),
+              }}
+            />
+          </RichTabs.Content>
+        ))}
+      </RichTabs>
     </div>
   )
 }
