dafde5999f995323c58b489a79b794afb32b174f
Aman Meherally <75181008+TheAmanM@users.noreply.github.com>
2025-08-27 06:04:51 -0400

    Updated index.tsx to remove asterisks

--- docs/components/Providers/index.tsx
+++ docs/components/Providers/index.tsx
@@ -5,7 +5,7 @@ export default function Providers() {
     <section className="relative flex flex-col items-center justify-center overflow-hidden py-12">
       <div className="z-10 flex flex-col items-center justify-center gap-4 px-8 text-center">
         <h2 className="text-3xl font-bold tracking-tight text-neutral-800 dark:text-neutral-200">
-          Sign in with <span className="italic">* anything *</span>
+          Sign in with <span className="italic">anything</span>
         </h2>
         <div className="max-w-2xl text-lg text-neutral-600 dark:text-neutral-400">
           With over 100 popular providers, you can let your users sign in with
