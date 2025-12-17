551edc50f68d9abfbbb9b12beb13f6affc67035d
Kolja P <ptashym@gmail.com>
2025-12-17 13:02:54 +0100

    feat: Introduced Gumroad provider

--- apps/dev/nextjs/auth.ts
+++ apps/dev/nextjs/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
 import Keycloak from "next-auth/providers/keycloak"
 import GitHub from "next-auth/providers/github"
+import Gumroad from "next-auth/providers/gumroad"
 
 // import { PrismaClient } from "@prisma/client"
 // import { PrismaAdapter } from "@auth/prisma-adapter"
@@ -66,8 +67,9 @@ export const { handlers, auth, signIn, signOut, unstable_update } = NextAuth({
         }
       },
     }),
-    GitHub,
-    Keycloak,
+    // GitHub,
+    // Keycloak,
+    Gumroad,
   ],
 
   callbacks: {
